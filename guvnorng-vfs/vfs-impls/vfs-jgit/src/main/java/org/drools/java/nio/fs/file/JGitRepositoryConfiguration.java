7797ebeb29bac9aac91300cdc98ed40412c59138
Jervis Liu <jervisliu@gmail.com>
2012-06-15 00:20:32 +0800

    add missing file

---
+++ guvnorng-vfs/vfs-impls/vfs-jgit/src/main/java/org/drools/java/nio/fs/file/JGitRepositoryConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2012 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.java.nio.fs.file;
+
+public class JGitRepositoryConfiguration {
+    private String repositoryName;
+    private String fromGitURL;
+    private String userName;
+    private String password;
+    public String getRepositoryName() {
+        return repositoryName;
+    }
+    public void setRepositoryName(String repositoryName) {
+        this.repositoryName = repositoryName;
+    }
+    public String getFromGitURL() {
+        return fromGitURL;
+    }
+    public void setFromGitURL(String fromGitURL) {
+        this.fromGitURL = fromGitURL;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
