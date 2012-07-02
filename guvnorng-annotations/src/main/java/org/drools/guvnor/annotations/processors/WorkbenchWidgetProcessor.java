32ff790cf2a5edca3fca8cf3b2cfb5f70f9dabe7
Michael Anstis <michael.anstis@gmail.com>
2012-07-02 21:28:27 +0100

    Basic codegen.

--- guvnorng-annotations/src/main/java/org/drools/guvnor/annotations/processors/WorkbenchWidgetProcessor.java
+++ guvnorng-annotations/src/main/java/org/drools/guvnor/annotations/processors/WorkbenchWidgetProcessor.java
@@ -15,6 +15,9 @@
  */
 package org.drools.guvnor.annotations.processors;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -23,8 +26,11 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
 
 import org.drools.guvnor.client.annotations.WorkbenchWidget;
 
@@ -42,9 +48,50 @@ public class WorkbenchWidgetProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set< ? extends TypeElement> annotations,
                            RoundEnvironment roundEnv) {
-        for ( Element elem : roundEnv.getElementsAnnotatedWith( WorkbenchWidget.class ) ) {
-            WorkbenchWidget wbw = elem.getAnnotation( WorkbenchWidget.class );
-            String message = "WorkbenchWidget found in " + elem.getSimpleName()
+        for ( Element e : roundEnv.getElementsAnnotatedWith( WorkbenchWidget.class ) ) {
+            System.out.println( "-----> e.kind = " + e.getKind() );
+            if ( e.getKind() == ElementKind.CLASS ) {
+                TypeElement classElement = (TypeElement) e;
+                PackageElement packageElement = (PackageElement) classElement.getEnclosingElement();
+
+                try {
+                    final String packageName = packageElement.getQualifiedName().toString();
+                    final String className = classElement.getSimpleName() + "Smurf";
+                    System.out.println( "-----> Generating source code for [" + className + "]" );
+
+                    JavaFileObject jfo = processingEnv.getFiler().createSourceFile( packageName + "." + className );
+                    Writer w = jfo.openWriter();
+                    BufferedWriter bw = new BufferedWriter( w );
+                    bw.append( "package " ).append( packageElement.getQualifiedName() ).append( ";" );
+                    bw.newLine();
+                    bw.newLine();
+                    bw.append( "import org.drools.guvnor.client.editors.test.generated.TestActivity;" );
+                    bw.newLine();
+                    bw.append( "import javax.enterprise.context.Dependent;" );
+                    bw.newLine();
+                    bw.append( "import org.drools.guvnor.client.mvp.NameToken;" );
+                    bw.newLine();
+                    bw.newLine();
+                    bw.append( "@Dependent" );
+                    bw.newLine();
+                    bw.append( "@NameToken(\"Smurf\")" );
+                    bw.newLine();
+                    bw.append( "public class " ).append( className ).append( " extends TestActivity {" );
+                    bw.newLine();
+                    bw.append( "}" );
+                    bw.newLine();
+                    bw.close();
+                    w.close();
+
+                    System.out.println( "-----> Source code generated..." );
+
+                } catch ( IOException ioe ) {
+                    System.out.println( ioe.getMessage() );
+                }
+            }
+
+            WorkbenchWidget wbw = e.getAnnotation( WorkbenchWidget.class );
+            String message = "WorkbenchWidget found in " + e.getSimpleName()
                              + " with NameToken " + wbw.nameToken();
             processingEnv.getMessager().printMessage( Diagnostic.Kind.NOTE,
                                                       message );
