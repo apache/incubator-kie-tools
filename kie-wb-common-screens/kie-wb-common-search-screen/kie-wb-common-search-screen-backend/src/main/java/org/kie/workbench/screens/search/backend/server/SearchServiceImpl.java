211dc1f54cef203d482e46e9d596446c9bdd1acc
Michael Anstis <michael.anstis@gmail.com>
2013-05-14 12:44:10 +0100

    Move Search Screen

---
+++ kie-wb-common-screens/kie-wb-common-search-screen/kie-wb-common-search-screen-backend/src/main/java/org/kie/workbench/screens/search/backend/server/SearchServiceImpl.java
@@ -0,0 +1,165 @@
+package org.kie.workbench.screens.search.backend.server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.jboss.errai.bus.server.annotations.Service;
+import org.kie.commons.io.IOSearchService;
+import org.kie.commons.io.IOService;
+import org.kie.commons.io.attribute.DublinCoreView;
+import org.kie.commons.java.nio.base.version.VersionAttributeView;
+import org.kie.commons.java.nio.file.Path;
+import org.kie.workbench.screens.search.model.QueryMetadataPageRequest;
+import org.kie.workbench.screens.search.model.SearchPageRow;
+import org.kie.workbench.screens.search.model.SearchTermPageRequest;
+import org.kie.workbench.screens.search.service.SearchService;
+import org.kie.guvnor.services.backend.metadata.attribute.OtherMetaView;
+import org.kie.kieora.search.DateRange;
+import org.uberfire.backend.FileExplorerRootService;
+import org.uberfire.backend.Root;
+import org.uberfire.backend.server.util.Paths;
+import org.uberfire.client.tables.PageResponse;
+import org.uberfire.shared.workbench.type.ResourceTypeDefinition;
+
+@Service
+
+@ApplicationScoped
+public class SearchServiceImpl implements SearchService {
+
+    @Inject
+    @Named("ioSearchStrategy")
+    private IOSearchService ioSearchService;
+
+    @Inject
+    @Named("ioStrategy")
+    private IOService ioService;
+
+    @Inject
+    private FileExplorerRootService rootService;
+
+    @Inject
+    private Paths paths;
+
+    private PageResponse<SearchPageRow> emptyResponse = null;
+
+    @Inject
+    @Any
+    private Instance<ResourceTypeDefinition> typeRegister;
+
+    private Map<String, ResourceTypeDefinition> types = new HashMap<String, ResourceTypeDefinition>();
+
+    @PostConstruct
+    private void init() {
+        for ( ResourceTypeDefinition actviveType : typeRegister ) {
+            types.put( actviveType.getShortName().toLowerCase(), actviveType );
+        }
+        emptyResponse = new PageResponse<SearchPageRow>();
+        emptyResponse.setPageRowList( Collections.<SearchPageRow>emptyList() );
+        emptyResponse.setStartRowIndex( 0 );
+        emptyResponse.setTotalRowSize( 0 );
+        emptyResponse.setLastPage( true );
+        emptyResponse.setTotalRowSizeExact( true );
+    }
+
+    @Override
+    public PageResponse<SearchPageRow> fullTextSearch( final SearchTermPageRequest pageRequest ) {
+        final int hits = ioSearchService.fullTextSearchHits( pageRequest.getTerm(), roots() );
+        if ( hits > 0 ) {
+            final List<Path> pathResult = ioSearchService.fullTextSearch( pageRequest.getTerm(), pageRequest.getPageSize(), pageRequest.getStartRowIndex(), roots() );
+            return buildResponse( pathResult, hits, pageRequest.getPageSize(), pageRequest.getStartRowIndex() );
+        }
+        return emptyResponse;
+    }
+
+    @Override
+    public PageResponse<SearchPageRow> queryMetadata( final QueryMetadataPageRequest pageRequest ) {
+
+        final Map<String, Object> attrs = new HashMap<String, Object>( pageRequest.getMetadata() );
+
+        if ( pageRequest.getCreatedAfter() != null || pageRequest.getCreatedBefore() != null ) {
+            attrs.put( "creationTime", toDateRange( pageRequest.getCreatedBefore(), pageRequest.getCreatedAfter() ) );
+        }
+        if ( pageRequest.getLastModifiedAfter() != null || pageRequest.getLastModifiedBefore() != null ) {
+            attrs.put( "lastModifiedTime", toDateRange( pageRequest.getLastModifiedBefore(), pageRequest.getLastModifiedAfter() ) );
+        }
+
+        final int hits = ioSearchService.searchByAttrsHits( attrs, roots() );
+        if ( hits > 0 ) {
+            final List<Path> pathResult = ioSearchService.searchByAttrs( attrs, pageRequest.getPageSize(), pageRequest.getStartRowIndex(), roots() );
+            return buildResponse( pathResult, hits, pageRequest.getPageSize(), pageRequest.getStartRowIndex() );
+        }
+        return emptyResponse;
+    }
+
+    private PageResponse<SearchPageRow> buildResponse( final List<Path> pathResult,
+                                                       final int hits,
+                                                       final int pageSize,
+                                                       final int startRow ) {
+        final List<SearchPageRow> result = new ArrayList<SearchPageRow>( pathResult.size() );
+        for ( final Path path : pathResult ) {
+            final SearchPageRow row = new SearchPageRow( paths.convert( path, false ) );
+
+            final DublinCoreView dcoreView = ioService.getFileAttributeView( path, DublinCoreView.class );
+            final OtherMetaView otherMetaView = ioService.getFileAttributeView( path, OtherMetaView.class );
+            final VersionAttributeView versionAttributeView = ioService.getFileAttributeView( path, VersionAttributeView.class );
+
+            row.setCreator( versionAttributeView.readAttributes().history().records().size() > 0 ? versionAttributeView.readAttributes().history().records().get( 0 ).author() : "" );
+            row.setLastContributor( versionAttributeView.readAttributes().history().records().size() > 0 ? versionAttributeView.readAttributes().history().records().get( versionAttributeView.readAttributes().history().records().size() - 1 ).author() : "" );
+            row.setLastModified( new Date( versionAttributeView.readAttributes().lastModifiedTime().toMillis() ) );
+            row.setCreatedDate( new Date( versionAttributeView.readAttributes().creationTime().toMillis() ) );
+            row.setDescription( dcoreView.readAttributes().descriptions().size() > 0 ? dcoreView.readAttributes().descriptions().get( 0 ) : "" );
+            result.add( row );
+        }
+
+        final PageResponse<SearchPageRow> response = new PageResponse<SearchPageRow>();
+        response.setTotalRowSize( hits );
+        response.setPageRowList( result );
+        response.setTotalRowSizeExact( true );
+        response.setStartRowIndex( startRow );
+        response.setLastPage( ( pageSize * startRow + 2 ) >= hits );
+
+        return response;
+    }
+
+    private Path[] roots() {
+        final Path[] roots = new Path[ rootService.listRoots().size() ];
+        int i = 0;
+        for ( final Root root : rootService.listRoots() ) {
+            roots[ i ] = paths.convert( root.getPath() );
+            i++;
+        }
+        return roots;
+    }
+
+    private DateRange toDateRange( final Date before,
+                                   final Date after ) {
+        return new DateRange() {
+            @Override
+            public Date before() {
+                if ( before == null ) {
+                    return new Date();
+                }
+                return before;
+            }
+
+            @Override
+            public Date after() {
+                if ( after == null ) {
+                    return new Date( 0 );
+                }
+                return after;
+            }
+        };
+    }
+
+}
---
+++ kie-wb-common-screens/kie-wb-common-search-screen/kie-wb-common-search-screen-client/src/main/java/org/kie/workbench/screens/search/client/widgets/SearchResultTable.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2010 JBoss Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.kie.workbench.screens.search.client.widgets;
+
+import java.util.Collections;
+import java.util.Date;
+
+import com.google.gwt.cell.client.DateCell;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.view.client.AsyncDataProvider;
+import com.google.gwt.view.client.HasData;
+import org.jboss.errai.bus.client.api.RemoteCallback;
+import org.jboss.errai.ioc.client.container.IOC;
+import org.kie.workbench.screens.search.client.resources.i18n.Constants;
+import org.kie.workbench.screens.search.client.resources.images.ImageResources;
+import org.kie.workbench.screens.search.model.QueryMetadataPageRequest;
+import org.kie.workbench.screens.search.model.SearchPageRow;
+import org.kie.workbench.screens.search.model.SearchTermPageRequest;
+import org.kie.workbench.screens.search.service.SearchService;
+import org.uberfire.client.workbench.type.ClientResourceType;
+import org.uberfire.client.workbench.type.ClientTypeRegistry;
+import org.uberfire.client.workbench.widgets.tables.AbstractPathPagedTable;
+import org.uberfire.client.common.CheckboxCellImpl;
+import org.uberfire.client.tables.ColumnPicker;
+import org.uberfire.client.tables.ComparableImageResource;
+import org.uberfire.client.tables.ComparableImageResourceCell;
+import org.uberfire.client.tables.PageResponse;
+import org.uberfire.client.tables.SortableHeader;
+import org.uberfire.client.tables.SortableHeaderGroup;
+import org.uberfire.client.tables.TitledTextCell;
+import org.uberfire.client.tables.TitledTextColumn;
+
+import static org.jboss.errai.bus.client.api.base.MessageBuilder.*;
+
+/**
+ * Widget with a table of Assets.
+ */
+public class SearchResultTable extends AbstractPathPagedTable<SearchPageRow> {
+
+    private static final int PAGE_SIZE = 10;
+
+    private ClientTypeRegistry clientTypeRegistry = null;
+
+    public SearchResultTable() {
+        super( PAGE_SIZE );
+
+        setDataProvider( new AsyncDataProvider<SearchPageRow>() {
+            protected void onRangeChanged( HasData<SearchPageRow> display ) {
+                updateRowCount( 0, true );
+                updateRowData( 0, Collections.<SearchPageRow>emptyList() );
+            }
+        } );
+    }
+
+    public SearchResultTable( final QueryMetadataPageRequest queryRequest ) {
+        super( PAGE_SIZE );
+
+        if ( queryRequest.getPageSize() == null ) {
+            queryRequest.setPageSize( PAGE_SIZE );
+        }
+
+        setDataProvider( new AsyncDataProvider<SearchPageRow>() {
+            protected void onRangeChanged( HasData<SearchPageRow> display ) {
+                queryRequest.setStartRowIndex( pager.getPageStart() );
+                queryRequest.setPageSize( pageSize );
+
+                createCall( new RemoteCallback<PageResponse<SearchPageRow>>() {
+                    public void callback( final PageResponse<SearchPageRow> response ) {
+
+                        updateRowCount( response.getTotalRowSize(),
+                                        response.isTotalRowSizeExact() );
+                        updateRowData( response.getStartRowIndex(),
+                                       response.getPageRowList() );
+                    }
+                }, SearchService.class ).queryMetadata( queryRequest );
+            }
+        } );
+    }
+
+    public SearchResultTable( final SearchTermPageRequest searchRequest ) {
+        super( PAGE_SIZE );
+
+        if ( searchRequest.getPageSize() == null ) {
+            searchRequest.setPageSize( PAGE_SIZE );
+        }
+
+        setDataProvider( new AsyncDataProvider<SearchPageRow>() {
+            protected void onRangeChanged( HasData<SearchPageRow> display ) {
+                searchRequest.setStartRowIndex( pager.getPageStart() );
+                searchRequest.setPageSize( pageSize );
+
+                createCall( new RemoteCallback<PageResponse<SearchPageRow>>() {
+                    public void callback( final PageResponse<SearchPageRow> response ) {
+                        updateRowCount( response.getTotalRowSize(),
+                                        response.isTotalRowSizeExact() );
+                        updateRowData( response.getStartRowIndex(),
+                                       response.getPageRowList() );
+                    }
+                }, SearchService.class ).fullTextSearch( searchRequest );
+            }
+        } );
+    }
+
+    @Override
+    protected void addAncillaryColumns( final ColumnPicker<SearchPageRow> columnPicker,
+                                        final SortableHeaderGroup<SearchPageRow> sortableHeaderGroup ) {
+
+        final Column<SearchPageRow, ComparableImageResource> formatColumn = new Column<SearchPageRow, ComparableImageResource>( new ComparableImageResourceCell() ) {
+
+            public ComparableImageResource getValue( SearchPageRow row ) {
+                final ClientResourceType associatedType = getClientTypeRegistry().resolve( row.getPath() );
+
+                final Image icon;
+                if ( associatedType.getIcon() == null || !( associatedType.getIcon() instanceof Image ) ) {
+                    icon = new Image( ImageResources.INSTANCE.file() );
+                } else {
+                    icon = (Image) associatedType.getIcon();
+                }
+
+                return new ComparableImageResource( associatedType.getShortName(), icon );
+            }
+        };
+
+        columnPicker.addColumn( formatColumn,
+                                new SortableHeader<SearchPageRow, ComparableImageResource>(
+                                        sortableHeaderGroup,
+                                        Constants.INSTANCE.Format(),
+                                        formatColumn ),
+                                true );
+
+        final TitledTextColumn<SearchPageRow> titleColumn = new TitledTextColumn<SearchPageRow>() {
+            public TitledTextCell.TitledText getValue( SearchPageRow row ) {
+                return new TitledTextCell.TitledText( row.getPath().getFileName(),
+                                                      row.getAbbreviatedDescription() );
+            }
+        };
+        columnPicker.addColumn( titleColumn,
+                                new SortableHeader<SearchPageRow, TitledTextCell.TitledText>(
+                                        sortableHeaderGroup,
+                                        Constants.INSTANCE.Name(),
+                                        titleColumn ),
+                                true );
+
+        final Column<SearchPageRow, Date> createdDateColumn = new Column<SearchPageRow, Date>(
+                new DateCell( DateTimeFormat.getFormat( DateTimeFormat.PredefinedFormat.DATE_TIME_MEDIUM ) ) ) {
+            public Date getValue( SearchPageRow row ) {
+                return row.getCreatedDate();
+            }
+        };
+        columnPicker.addColumn( createdDateColumn,
+                                new SortableHeader<SearchPageRow, Date>(
+                                        sortableHeaderGroup,
+                                        Constants.INSTANCE.CreatedDate(),
+                                        createdDateColumn ),
+                                false );
+
+        final Column<SearchPageRow, Date> lastModifiedColumn = new Column<SearchPageRow, Date>(
+                new DateCell( DateTimeFormat.getFormat( DateTimeFormat.PredefinedFormat.DATE_TIME_MEDIUM ) ) ) {
+            public Date getValue( final SearchPageRow row ) {
+                return row.getLastModified();
+            }
+        };
+        columnPicker.addColumn( lastModifiedColumn,
+                                new SortableHeader<SearchPageRow, Date>(
+                                        sortableHeaderGroup,
+                                        Constants.INSTANCE.LastModified(),
+                                        lastModifiedColumn ),
+                                true );
+
+        final Column<SearchPageRow, Boolean> isDisabledColumn = new Column<SearchPageRow, Boolean>( new CheckboxCellImpl( true ) ) {
+            public Boolean getValue( final SearchPageRow row ) {
+                return row.isDisabled();
+            }
+        };
+        columnPicker.addColumn( isDisabledColumn,
+                                new SortableHeader<SearchPageRow, Boolean>(
+                                        sortableHeaderGroup,
+                                        Constants.INSTANCE.Disabled(),
+                                        isDisabledColumn ),
+                                false );
+
+    }
+
+    private ClientTypeRegistry getClientTypeRegistry() {
+        if ( clientTypeRegistry == null ) {
+            clientTypeRegistry = IOC.getBeanManager().lookupBean( ClientTypeRegistry.class ).getInstance();
+        }
+        return clientTypeRegistry;
+    }
+}
