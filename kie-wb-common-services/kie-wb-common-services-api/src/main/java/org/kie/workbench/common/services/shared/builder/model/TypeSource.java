fc53ba19b3f1143c843e42ddb0b4d139e57790e5
Michael Anstis <michael.anstis@gmail.com>
2013-05-24 14:54:34 +0100

    Make TypeSource @Portable

--- kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/builder/model/TypeSource.java
+++ kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/builder/model/TypeSource.java
@@ -1,8 +1,11 @@
 package org.kie.workbench.common.services.shared.builder.model;
 
+import org.jboss.errai.common.client.api.annotations.Portable;
+
 /**
  * An enum to define where a Type was defined
  */
+@Portable
 public enum TypeSource {
     JAVA_PROJECT, //Within the project
     JAVA_DEPENDENCY, //Within a dependency
