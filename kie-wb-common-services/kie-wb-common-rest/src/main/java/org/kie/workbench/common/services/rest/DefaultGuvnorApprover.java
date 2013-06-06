8371620188d368f42234b2f8cc8f69e4dd4690f7
jervisliu <jervisliu@gmail.com>
2013-06-06 21:29:02 +0800

    guvnor request approval processor. will move the default approval processor kjar to a separate module later

--- kie-wb-common-services/kie-wb-common-rest/src/main/java/org/kie/workbench/common/services/rest/DefaultGuvnorApprover.java
+++ kie-wb-common-services/kie-wb-common-rest/src/main/java/org/kie/workbench/common/services/rest/DefaultGuvnorApprover.java
@@ -10,6 +10,7 @@ import org.kie.api.runtime.rule.FactHandle;
 import org.kie.workbench.common.services.shared.rest.JobRequest;
 
 import java.io.File;
+import java.net.URL;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -19,21 +20,35 @@ public class DefaultGuvnorApprover {
         KieServices ks = KieServices.Factory.get();
         KieRepository kr = ks.getRepository();
 
-        KieModule kModule = kr.addKieModule(ks.getResources().newFileSystemResource(getKJar("kjar")));
+        KieModule kModule = kr.addKieModule(ks.getResources().newFileSystemResource(getKJar("/DefaultGuvnorApprover-0.9.jar")));
 
         KieContainer kContainer = ks.newKieContainer(kModule.getReleaseId());
 
         KieSession kSession = kContainer.newKieSession();
-        //kSession.setGlobal("out", out);
 
         FactHandle jobRequestFactHandle  = kSession.insert(jobRequest);
         kSession.fireAllRules();
         
         //TODO:
+        System.out.println("approval request result: " + true);
         return true;
     }
-
-    public static File getKJar(String kjarName) {
+    
+    public File getKJar(String kjarName) {
+        if (!kjarName.endsWith(".jar")) {
+            throw new RuntimeException("The kjar name is invalid");
+        }
+        
+        URL u = this.getClass().getResource(kjarName);
+        File kjarFile = new File(u.getFile());
+        if (!kjarFile.exists()) {
+            throw new RuntimeException("The kjar [" + kjarName + "] does not exist");
+        }
+        
+        return kjarFile;
+    }
+    
+/*    public static File getKJar(String kjarName) {
         if (!kjarName.endsWith(".jar")) {
             throw new RuntimeException("The kjar name is invalid");
         }
@@ -44,5 +59,12 @@ public class DefaultGuvnorApprover {
         }
         
         return kjarFile;
+    }*/
+    
+    public static void main(String[] args) {
+    	DefaultGuvnorApprover a = new DefaultGuvnorApprover();
+    	JobRequest request = new JobRequest();
+    	boolean result = a.requestApproval(request);
+    	System.out.println("request result: " + result);
     }
 }
