24e9c89f951f0ff4a227f9d05f495a9247d9bb34
Toni Rikkola <toni@rikkola.net>
2013-07-29 10:45:42 +0300

    extends for guided rule editor

--- drools-wb-screens/drools-wb-test-scenario-editor/drools-wb-test-scenario-editor-client/src/main/java/org/drools/workbench/screens/testscenario/client/RuleSelectionEvent.java
+++ drools-wb-screens/drools-wb-test-scenario-editor/drools-wb-test-scenario-editor-client/src/main/java/org/drools/workbench/screens/testscenario/client/RuleSelectionEvent.java
@@ -16,13 +16,6 @@
 
 package org.drools.workbench.screens.testscenario.client;
 
-/**
- * Created by IntelliJ IDEA.
- * User: nheron
- * Date: 7 nov. 2009
- * Time: 19:35:30
- * To change this template use File | Settings | File Templates.
- */
 public interface RuleSelectionEvent {
     public void ruleSelected(String name);
 }
