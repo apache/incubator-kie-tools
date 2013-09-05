ed6b8da0906aae0a732d658a451b75bd46409f93
Toni Rikkola <toni@rikkola.net>
2013-09-05 15:47:37 +0300

    996880 - Project screen: Source not working

--- kie-wb-common-screens/kie-wb-common-project-editor/kie-wb-common-project-editor-client/src/main/java/org/kie/workbench/common/screens/projecteditor/client/forms/KModuleEditorScreenPresenter.java
+++ kie-wb-common-screens/kie-wb-common-project-editor/kie-wb-common-project-editor-client/src/main/java/org/kie/workbench/common/screens/projecteditor/client/forms/KModuleEditorScreenPresenter.java
@@ -1,83 +1,37 @@
 package org.kie.workbench.common.screens.projecteditor.client.forms;
 
-import javax.inject.Inject;
-
-import com.google.gwt.user.client.ui.Widget;
-import org.guvnor.common.services.project.model.KModuleModel;
-import org.guvnor.common.services.project.service.KModuleService;
-import org.jboss.errai.common.client.api.RemoteCallback;
-import org.jboss.errai.common.client.api.Caller;
+import com.google.gwt.user.client.ui.IsWidget;
+import org.kie.workbench.common.screens.defaulteditor.client.editor.GuvnorTextEditorPresenter;
 import org.kie.workbench.common.screens.projecteditor.client.resources.i18n.ProjectEditorConstants;
 import org.kie.workbench.common.screens.projecteditor.client.type.KModuleResourceType;
-import org.kie.workbench.common.widgets.client.callbacks.HasBusyIndicatorDefaultErrorCallback;
-import org.kie.workbench.common.widgets.client.menu.FileMenuBuilder;
-import org.kie.workbench.common.widgets.client.menu.FileMenuBuilderImpl;
 import org.uberfire.backend.vfs.Path;
-import org.uberfire.lifecycle.OnStartup;
 import org.uberfire.client.annotations.WorkbenchEditor;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPartTitle;
 import org.uberfire.client.annotations.WorkbenchPartView;
+import org.uberfire.lifecycle.OnOpen;
+import org.uberfire.lifecycle.OnStartup;
 import org.uberfire.mvp.PlaceRequest;
 import org.uberfire.workbench.model.menu.Menus;
 
-@WorkbenchEditor(identifier = "kmoduleScreen", supportedTypes = { KModuleResourceType.class })
-public class KModuleEditorScreenPresenter {
-
-    private       boolean            isReadOnly;
-    private       Path               path;
-    private KModuleEditorScreenView view;
-    private final KModuleEditorPanel kModuleEditorPanel;
-    private       Menus              menus;
-    private final FileMenuBuilderImpl menuBuilder;
-    private Caller<KModuleService> projectEditorService;
-
-    @Inject
-    public KModuleEditorScreenPresenter( KModuleEditorScreenView view,
-                                         KModuleEditorPanel kModuleEditorPanel,
-                                         FileMenuBuilderImpl menuBuilder,
-                                         Caller<KModuleService> projectEditorService) {
-        this.view = view;
-        this.kModuleEditorPanel = kModuleEditorPanel;
-        this.menuBuilder = menuBuilder;
-        this.projectEditorService = projectEditorService;
-    }
+@WorkbenchEditor(identifier = "kmoduleScreen", supportedTypes = {KModuleResourceType.class})
+public class KModuleEditorScreenPresenter
+        extends GuvnorTextEditorPresenter {
 
     @OnStartup
-    public void init( final Path path,
-                      final PlaceRequest request ) {
-        this.path = path;
-        this.isReadOnly = request.getParameter( "readOnly", null ) == null ? false : true;
-
-        this.path = path;
-
-        //Busy popup is handled by ProjectEditorScreen
-        projectEditorService.call(getModelSuccessCallback(),
-                new HasBusyIndicatorDefaultErrorCallback(view)).load(path);
-
-
-        fillMenuBar();
-    }
-
-    private RemoteCallback<KModuleModel> getModelSuccessCallback() {
-        return new RemoteCallback<KModuleModel>() {
-
-            @Override
-            public void callback(final KModuleModel model) {
-                kModuleEditorPanel.setData(model, false);
-            }
-        };
-    }
-
-    private void fillMenuBar() {
-        if ( isReadOnly ) {
-            menus = menuBuilder.addRestoreVersion( path ).build();
-        }
+    public void onStartup(final Path path,
+            final PlaceRequest place) {
+        super.onStartup(path, place);
     }
 
     @WorkbenchMenu
-    public Menus buildMenuBar() {
-        return menus;
+    public Menus getMenus() {
+        return super.getMenus();
+    }
+
+    @OnOpen
+    public void onOpen() {
+        super.onOpen();
     }
 
     @WorkbenchPartTitle
@@ -86,8 +40,8 @@ public class KModuleEditorScreenPresenter {
     }
 
     @WorkbenchPartView
-    public Widget asWidget() {
-        return kModuleEditorPanel.asWidget();
+    public IsWidget asWidget() {
+        return super.getWidget();
     }
 
 }
