24b5f2ac32ff495d20ffbbc4c1dc0d230bb8ccf9
Toni Rikkola <toni@rikkola.net>
2013-09-05 10:15:21 +0300

    BZ-996880 - Project screen: Source not working

--- uberfire-widgets/uberfire-widgets-core/uberfire-widgets-core-client/src/main/java/org/uberfire/client/editors/texteditor/TextEditorPresenter.java
+++ uberfire-widgets/uberfire-widgets-core/uberfire-widgets-core-client/src/main/java/org/uberfire/client/editors/texteditor/TextEditorPresenter.java
@@ -16,39 +16,29 @@
 
 package org.uberfire.client.editors.texteditor;
 
-import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import com.google.gwt.user.client.ui.IsWidget;
-import org.jboss.errai.common.client.api.RemoteCallback;
 import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.api.RemoteCallback;
 import org.uberfire.backend.vfs.Path;
 import org.uberfire.backend.vfs.VFSService;
 import org.uberfire.lifecycle.IsDirty;
-import org.uberfire.lifecycle.OnClose;
-import org.uberfire.lifecycle.OnOpen;
-import org.uberfire.lifecycle.OnSave;
 import org.uberfire.lifecycle.OnStartup;
-import org.uberfire.client.annotations.WorkbenchEditor;
-import org.uberfire.client.annotations.WorkbenchPartTitle;
-import org.uberfire.client.annotations.WorkbenchPartView;
-import org.uberfire.client.workbench.type.DotResourceType;
 
-@Dependent
-@WorkbenchEditor(identifier = "TextEditor", supportedTypes = { TextResourceType.class, DotResourceType.class })
-public class TextEditorPresenter {
+public abstract class TextEditorPresenter {
 
     public interface View
             extends
             IsWidget {
 
-        void setContent( String content );
+        void setContent(String content);
 
         String getContent();
 
         void setFocus();
 
-        void setDirty( boolean dirty );
+        void setDirty(boolean dirty);
 
         boolean isDirty();
     }
@@ -59,7 +49,7 @@ public class TextEditorPresenter {
     @Inject
     private Caller<VFSService> vfsServices;
 
-    private Path path;
+    protected Path path;
 
     @OnStartup
     public void onStartup( final Path path ) {
@@ -73,17 +63,7 @@ public class TextEditorPresenter {
                     view.setContent( response );
                 }
             }
-        } ).readAllString( path );
-    }
-
-    @OnSave
-    public void onSave() {
-        vfsServices.call( new RemoteCallback<Path>() {
-            @Override
-            public void callback( Path response ) {
-                view.setDirty( false );
-            }
-        } ).write( path, view.getContent() );
+        } ).readAllString(path);
     }
 
     @IsDirty
@@ -91,22 +71,10 @@ public class TextEditorPresenter {
         return view.isDirty();
     }
 
-    @OnClose
-    public void onClose() {
-        this.path = null;
-    }
-
-    @OnOpen
     public void onOpen() {
         view.setFocus();
     }
 
-    @WorkbenchPartTitle
-    public String getTitle() {
-        return "Text Editor [" + path.getFileName() + "]";
-    }
-
-    @WorkbenchPartView
     public IsWidget getWidget() {
         return view;
     }
---
+++ uberfire-widgets/uberfire-widgets-core/uberfire-widgets-core-client/src/main/java/org/uberfire/client/editors/texteditor/TextEditorWorkbenchEditor.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2013 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.uberfire.client.editors.texteditor;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import com.google.gwt.user.client.ui.IsWidget;
+import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.api.RemoteCallback;
+import org.uberfire.backend.vfs.Path;
+import org.uberfire.backend.vfs.VFSService;
+import org.uberfire.client.annotations.WorkbenchEditor;
+import org.uberfire.client.annotations.WorkbenchPartTitle;
+import org.uberfire.client.annotations.WorkbenchPartView;
+import org.uberfire.client.workbench.type.DotResourceType;
+import org.uberfire.lifecycle.IsDirty;
+import org.uberfire.lifecycle.OnClose;
+import org.uberfire.lifecycle.OnOpen;
+import org.uberfire.lifecycle.OnSave;
+import org.uberfire.lifecycle.OnStartup;
+
+@Dependent
+@WorkbenchEditor(identifier = "TextEditor", supportedTypes = {TextResourceType.class, DotResourceType.class})
+public class TextEditorWorkbenchEditor
+        extends TextEditorPresenter {
+
+    @Inject
+    private Caller<VFSService> vfsServices;
+
+    @OnStartup
+    public void onStartup(final Path path) {
+        vfsServices.call(new RemoteCallback<String>() {
+            @Override
+            public void callback(String response) {
+                if (response == null) {
+                    view.setContent("-- empty --");
+                } else {
+                    view.setContent(response);
+                }
+            }
+        }).readAllString(path);
+    }
+
+    @OnSave
+    public void onSave() {
+        vfsServices.call(new RemoteCallback<Path>() {
+            @Override
+            public void callback(Path response) {
+                view.setDirty(false);
+            }
+        }).write(path, view.getContent());
+    }
+
+    @IsDirty
+    public boolean isDirty() {
+        return super.isDirty();
+    }
+
+    @OnClose
+    public void onClose() {
+        this.path = null;
+    }
+
+    @OnOpen
+    public void onOpen() {
+        super.onOpen();
+    }
+
+    @WorkbenchPartTitle
+    public String getTitle() {
+        return "Text Editor [" + path.getFileName() + "]";
+    }
+
+    @WorkbenchPartView
+    public IsWidget getWidget() {
+        return super.getWidget();
+    }
+
+}
