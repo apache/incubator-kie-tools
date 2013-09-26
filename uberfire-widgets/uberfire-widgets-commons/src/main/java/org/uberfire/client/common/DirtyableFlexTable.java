5293e0d97feafcf0a26bba9270ff38e5c33467a4
Michael Anstis <michael.anstis@gmail.com>
2013-09-26 16:42:59 +0100

    BZ1012418 - Cannot close Guided Rule Editor after removing fact pattern

--- uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/client/common/DirtyableFlexTable.java
+++ uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/client/common/DirtyableFlexTable.java
@@ -16,40 +16,25 @@
 
 package org.uberfire.client.common;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
 import com.google.gwt.user.client.ui.Widget;
-import org.kie.commons.data.Pair;
 
 public class DirtyableFlexTable
         extends FlexTable
         implements DirtyableContainer {
 
-    private int length;
-    private List<Pair<Integer, Integer>> list = new ArrayList<Pair<Integer, Integer>>();
-
     public boolean hasDirty() {
-        for ( final Pair<Integer, Integer> coord : list ) {
-            final Widget element = getWidget( coord.getK1(), coord.getK2() );
-            if ( ( element instanceof DirtyableWidget && ( (DirtyableWidget) element ).isDirty() ) ||
-                    ( element instanceof DirtyableContainer && ( (DirtyableContainer) element ).hasDirty() ) ) {
+        Iterator<Widget> itr = iterator();
+        while ( itr.hasNext() ) {
+            Widget w = itr.next();
+            if ( ( w instanceof DirtyableWidget && ( (DirtyableWidget) w ).isDirty() ) || ( w instanceof DirtyableContainer && ( (DirtyableContainer) w ).hasDirty() ) ) {
                 return true;
             }
         }
         return false;
-    }
-
-    public void setWidget( int row,
-                           int column,
-                           Widget arg2 ) {
-        super.setWidget( row, column, arg2 );
-
-        if ( arg2 instanceof IDirtyable ) {
-            list.add( length++, Pair.newPair( row, column ) );
-        }
     }
 
     public void setHorizontalAlignmentForFlexCellFormatter( int row,
