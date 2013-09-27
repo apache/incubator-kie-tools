e8fc84e0b7d4ec4f3bc36bff0fda41cb8dffb1f6
Toni Rikkola <toni@rikkola.net>
2013-09-27 12:49:28 +0300

    BZ-996572 - Kmodule editor: limited capability

--- kie-wb-common-widgets/kie-wb-common-ui/src/main/java/org/kie/workbench/common/widgets/client/menu/ProjectScreenPlaceRequest.java
+++ kie-wb-common-widgets/kie-wb-common-ui/src/main/java/org/kie/workbench/common/widgets/client/menu/ProjectScreenPlaceRequest.java
@@ -1,7 +1,9 @@
 package org.kie.workbench.common.widgets.client.menu;
 
+import org.jboss.errai.common.client.api.annotations.Portable;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
 
+@Portable
 public class ProjectScreenPlaceRequest
         extends DefaultPlaceRequest {
 
