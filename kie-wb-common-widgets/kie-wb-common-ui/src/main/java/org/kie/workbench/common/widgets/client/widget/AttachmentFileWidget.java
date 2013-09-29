d3d23b76fe61d519ccb602035601d296f52ad928
jervisliu <jervisliu@gmail.com>
2013-09-29 18:47:44 +0800

    bz-999874:XLS Decision Table creation fails with useless message if no file is selected

--- kie-wb-common-widgets/kie-wb-common-ui/src/main/java/org/kie/workbench/common/widgets/client/widget/AttachmentFileWidget.java
+++ kie-wb-common-widgets/kie-wb-common-ui/src/main/java/org/kie/workbench/common/widgets/client/widget/AttachmentFileWidget.java
@@ -1,18 +1,19 @@
+
 /*
- * Copyright 2010 JBoss Inc
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2010 JBoss Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.kie.workbench.common.widgets.client.widget;
 
@@ -21,6 +22,8 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FileUpload;
 import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
+import com.google.gwt.user.client.ui.FormPanel.SubmitHandler;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import org.guvnor.common.services.shared.file.upload.FileManagerFields;
@@ -30,110 +33,156 @@ import org.uberfire.backend.vfs.Path;
 import org.uberfire.client.common.popups.errors.ErrorPopup;
 
 /**
- * This wraps a file uploader utility
- */
+* This wraps a file uploader utility
+*/
 public class AttachmentFileWidget extends Composite {
 
-    private final FormPanel form = new FormPanel();
-    private final HorizontalPanel fields = new HorizontalPanel();
+   private final FormPanel form = new FormPanel();
+   private final HorizontalPanel fields = new HorizontalPanel();
 
-    private final TextBox fieldFilePath = getHiddenField( FileManagerFields.FORM_FIELD_PATH,
-                                                          "" );
-    private final TextBox fieldFileName = getHiddenField( FileManagerFields.FORM_FIELD_NAME,
-                                                          "" );
-    private final TextBox fieldFileFullPath = getHiddenField( FileManagerFields.FORM_FIELD_FULL_PATH,
+   private final TextBox fieldFilePath = getHiddenField( FileManagerFields.FORM_FIELD_PATH,
+                                                         "" );
+   private final TextBox fieldFileName = getHiddenField( FileManagerFields.FORM_FIELD_NAME,
+                                                         "" );
+   private final TextBox fieldFileFullPath = getHiddenField( FileManagerFields.FORM_FIELD_FULL_PATH,
+                                                             "" );
+   private final TextBox fieldFileOperation = getHiddenField( FileManagerFields.FORM_FIELD_OPERATION,
                                                               "" );
-    private final TextBox fieldFileOperation = getHiddenField( FileManagerFields.FORM_FIELD_OPERATION,
-                                                               "" );
 
-    private Command successCallback;
-    private Command errorCallback;
+   private Command successCallback;
+   private Command errorCallback;
+   private String[] validFileExtensions  = null;
 
-    public AttachmentFileWidget() {
-        form.setEncoding( FormPanel.ENCODING_MULTIPART );
-        form.setMethod( FormPanel.METHOD_POST );
-
-        form.addSubmitCompleteHandler( new FormPanel.SubmitCompleteHandler() {
-
+   public AttachmentFileWidget() {
+       final FileUpload up = new FileUpload();
+       up.setName( FileManagerFields.UPLOAD_FIELD_NAME_ATTACH );
+              form.setEncoding( FormPanel.ENCODING_MULTIPART );
+       form.setMethod( FormPanel.METHOD_POST );
+       form.addSubmitHandler(new SubmitHandler() {
             @Override
-            public void onSubmitComplete( final FormPanel.SubmitCompleteEvent event ) {
-                reset();
-                if ( "OK".equalsIgnoreCase( event.getResults() ) ) {
-                    executeCallback( successCallback );
-                    Window.alert( CommonConstants.INSTANCE.UploadSuccess() );
-                } else {
-                    executeCallback( errorCallback );
-                    ErrorPopup.showMessage( event.getResults() );
+            public void onSubmit(SubmitEvent event) {
+                String fileName = up.getFilename();
+                if (fileName == null || "".equals(fileName)) {
+                    Window.alert("Please selete a file to upload");
+                    event.cancel();
+                }
+                if (validFileExtensions != null
+                        && validFileExtensions.length != 0) {
+                    boolean isValid = false;
+                    for (String extension : validFileExtensions) {
+                        if (fileName.endsWith(extension)) {
+                            isValid = true;
+                            break;
+                        }
+                    }
+                    if (!isValid) {
+                        Window.alert("The file type is not supported");
+                        event.cancel();
+                    }
                 }
             }
+        });
+       form.addSubmitCompleteHandler( new FormPanel.SubmitCompleteHandler() {
 
-        } );
+           @Override
+           public void onSubmitComplete( final FormPanel.SubmitCompleteEvent event ) {
+               reset();
+               if ( "OK".equalsIgnoreCase( event.getResults() ) ) {
+                   executeCallback( successCallback );
+                   Window.alert( CommonConstants.INSTANCE.UploadSuccess() );
+               } else {
+                   executeCallback( errorCallback );
+                   ErrorPopup.showMessage( event.getResults() );
+               }
+           }
 
-        final FileUpload up = new FileUpload();
-        up.setName( FileManagerFields.UPLOAD_FIELD_NAME_ATTACH );
-        fields.add( up );
+       } );
 
-        fields.add( fieldFilePath );
-        fields.add( fieldFileName );
-        fields.add( fieldFileFullPath );
-        fields.add( fieldFileOperation );
+       fields.add( up );
 
-        form.add( fields );
+       fields.add( fieldFilePath );
+       fields.add( fieldFileName );
+       fields.add( fieldFileFullPath );
+       fields.add( fieldFileOperation );
 
-        initWidget( form );
+       form.add( fields );
+
+       initWidget( form );
+   }
+
+   private void executeCallback( final Command callback ) {
+       if ( callback == null ) {
+           return;
+       }
+       callback.execute();
+   }
+
+   public void reset() {
+       form.reset();
+   }
+
+   public void submit( final Path context,
+                       final String fileName,
+                       final String targetUrl,
+                       final Command successCallback,
+                       final Command errorCallback ) {
+       this.successCallback = successCallback;
+       this.errorCallback = errorCallback;
+
+       fieldFileName.setText( fileName );
+       fieldFilePath.setText( context.toURI() );
+       fieldFileOperation.setText( FileOperation.CREATE.toString() );
+       fieldFileFullPath.setText( "" );
+
+       form.setAction( targetUrl );
+       form.submit();
+   }
+   
+   public void submit( final Path context,
+           final String fileName,
+           final String targetUrl,
+           final Command successCallback,
+           final Command errorCallback,
+           final String[] validFileExtensions) {
+       this.validFileExtensions = validFileExtensions;
+
+       submit(context, fileName, targetUrl, successCallback, errorCallback);
     }
-
-    private void executeCallback( final Command callback ) {
-        if ( callback == null ) {
-            return;
-        }
-        callback.execute();
-    }
-
-    public void reset() {
-        form.reset();
-    }
-
-    public void submit( final Path context,
-                        final String fileName,
-                        final String targetUrl,
-                        final Command successCallback,
-                        final Command errorCallback ) {
-        this.successCallback = successCallback;
-        this.errorCallback = errorCallback;
-
-        fieldFileName.setText( fileName );
-        fieldFilePath.setText( context.toURI() );
-        fieldFileOperation.setText( FileOperation.CREATE.toString() );
-        fieldFileFullPath.setText( "" );
-
-        form.setAction( targetUrl );
-        form.submit();
-    }
-
+   
     public void submit( final Path path,
-                        final String targetUrl,
-                        final Command successCallback,
-                        final Command errorCallback ) {
-        this.successCallback = successCallback;
-        this.errorCallback = errorCallback;
+                       final String targetUrl,
+                       final Command successCallback,
+                       final Command errorCallback ) {
+       this.successCallback = successCallback;
+       this.errorCallback = errorCallback;
 
-        fieldFileOperation.setText( FileOperation.UPDATE.toString() );
-        fieldFileFullPath.setText( path.toURI() );
-        fieldFileName.setText( "" );
-        fieldFilePath.setText( "" );
+       fieldFileOperation.setText( FileOperation.UPDATE.toString() );
+       fieldFileFullPath.setText( path.toURI() );
+       fieldFileName.setText( "" );
+       fieldFilePath.setText( "" );
 
-        form.setAction( targetUrl );
-        form.submit();
+       form.setAction( targetUrl );
+       form.submit();
     }
 
-    private TextBox getHiddenField( final String name,
-                                    final String value ) {
-        final TextBox t = new TextBox();
-        t.setName( name );
-        t.setText( value );
-        t.setVisible( false );
-        return t;
+    public void submit(final Path path, 
+            final String targetUrl,
+            final Command successCallback, 
+            final Command errorCallback,
+            final String[] validFileExtensions) {
+        this.validFileExtensions = validFileExtensions;
+        
+        submit(path, targetUrl, successCallback, errorCallback);
     }
+
+   private TextBox getHiddenField( final String name,
+                                   final String value ) {
+       final TextBox t = new TextBox();
+       t.setName( name );
+       t.setText( value );
+       t.setVisible( false );
+       return t;
+   }
 
 }
+
