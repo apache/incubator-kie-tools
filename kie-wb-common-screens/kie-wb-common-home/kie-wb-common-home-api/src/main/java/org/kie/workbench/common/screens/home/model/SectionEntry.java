157cd2917e13d0e6d0ab5c6e0f0bb882539429e1
David Gutierrez <dgutierr@redhat.com>
2013-10-17 23:17:51 +0200

    BZ-1005981 - 5 roles available for BPM Suite

--- kie-wb-common-screens/kie-wb-common-home/kie-wb-common-home-api/src/main/java/org/kie/workbench/common/screens/home/model/SectionEntry.java
+++ kie-wb-common-screens/kie-wb-common-home/kie-wb-common-home-api/src/main/java/org/kie/workbench/common/screens/home/model/SectionEntry.java
@@ -1,15 +1,22 @@
 package org.kie.workbench.common.screens.home.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.uberfire.commons.validation.PortablePreconditions;
 import org.uberfire.mvp.Command;
+import org.uberfire.security.authz.RuntimeResource;
+
+import static java.util.Collections.emptyList;
 
 /**
  * A Section Entry within a Section on the Home Page
  */
-public class SectionEntry {
+public class SectionEntry implements RuntimeResource {
 
     private final String caption;
     private final Command onClickCommand;
+    private Collection<String> roles = new ArrayList<String>();
 
     public SectionEntry( final String caption,
                          final Command onClickCommand ) {
@@ -25,5 +32,23 @@ public class SectionEntry {
 
     public Command getOnClickCommand() {
         return onClickCommand;
+    }
+
+    public void setRoles(Collection<String> roles) {
+        this.roles = PortablePreconditions.checkNotNull("roles", roles);
+    }
+
+    @Override
+    public String getSignatureId() {
+        return getClass().getName() + "#" + caption;
+    }
+
+    @Override
+    public Collection<String> getRoles() {
+        return roles;
+    }
+    @Override
+    public Collection<String> getTraits() {
+        return emptyList();
     }
 }
