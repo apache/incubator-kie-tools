09f7b238d9603be26ed39d5570972e9f5e360d17
Jonathan Fuerth <jfuerth@gmail.com>
2013-10-28 11:42:31 -0400

    Made runtime plugin scanning fail gracefully when getRealPath() returns null

--- uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/plugin/RuntimePluginsServiceServerImpl.java
+++ uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/plugin/RuntimePluginsServiceServerImpl.java
@@ -3,12 +3,18 @@ package org.uberfire.backend.server.plugin;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.ServletContext;
 
 import org.jboss.errai.bus.server.annotations.Service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.uberfire.backend.plugin.RuntimePluginsService;
 import org.uberfire.java.nio.file.DirectoryStream;
 import org.uberfire.java.nio.file.Files;
@@ -18,6 +24,8 @@ import org.uberfire.java.nio.file.Paths;
 @Service
 @ApplicationScoped
 public class RuntimePluginsServiceServerImpl implements RuntimePluginsService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(RuntimePluginsServiceServerImpl.class);
 
     @Inject
     @Named("uf")
@@ -35,11 +43,17 @@ public class RuntimePluginsServiceServerImpl implements RuntimePluginsService {
 
     @Override
     public String getTemplateContent( String url ) {
+      String realPath = getRealPath( "plugins" );
+      if (realPath == null) {
+        LOGGER.info("Not fetching template content for " + url + " because getRealPath() is"
+                + " returning null. (This app is probably deployed in an unexploded .war)");
+        return "";
+      }
         final Path template;
         if ( url.startsWith( "/" ) ) {
-            template = Paths.get( URI.create( "file://" + getRealPath( "plugins" ) + url ) );
+            template = Paths.get( URI.create( "file://" + realPath + url ) );
         } else {
-            template = Paths.get( URI.create( "file://" + getRealPath( "plugins" ) + "/" + url ) );
+            template = Paths.get( URI.create( "file://" + realPath + "/" + url ) );
         }
 
         if ( Files.isRegularFile( template ) ) {
@@ -50,9 +64,15 @@ public class RuntimePluginsServiceServerImpl implements RuntimePluginsService {
 
     private Collection<String> directoryContent( final String directory,
                                                  final String glob ) {
+      String realPath = getRealPath( directory );
+      if (realPath == null) {
+        LOGGER.info("Not listing directory content for " + directory + "/" + glob +
+                " because getRealPath() is returning null. (This app is probably deployed in an unexploded .war)");
+        return Collections.emptyList();
+      }
         final Collection<String> result = new ArrayList<String>();
 
-        final Path pluginsRootPath = Paths.get( URI.create( "file://" + getRealPath( directory ) ) );
+        final Path pluginsRootPath = Paths.get( URI.create( "file://" + realPath ) );
 
         if ( Files.isDirectory( pluginsRootPath ) ) {
             final DirectoryStream<Path> stream = Files.newDirectoryStream( pluginsRootPath, glob );
@@ -66,7 +86,13 @@ public class RuntimePluginsServiceServerImpl implements RuntimePluginsService {
     }
 
     private String getRealPath( final String path ) {
-        return servletContext.getRealPath( path ).replaceAll( "\\\\", "/" ).replaceAll( " ", "%20" );
+        String realPath = servletContext.getRealPath( path );
+        if (realPath == null) {
+          return null;
+        }
+        else {
+          return realPath.replaceAll( "\\\\", "/" ).replaceAll( " ", "%20" );
+        }
     }
 
 }
