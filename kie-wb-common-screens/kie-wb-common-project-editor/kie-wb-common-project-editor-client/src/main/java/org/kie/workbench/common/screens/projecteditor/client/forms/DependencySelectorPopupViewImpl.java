98037d520b19eca8b307086dd8eb0050ab77c3f4
Michael Anstis <michael.anstis@gmail.com>
2013-11-12 19:38:47 +0000

    BZ1029419 - Project dependencies - Add from repository dialog copies asset repository list too much

--- kie-wb-common-screens/kie-wb-common-project-editor/kie-wb-common-project-editor-client/src/main/java/org/kie/workbench/common/screens/projecteditor/client/forms/DependencySelectorPopupViewImpl.java
+++ kie-wb-common-screens/kie-wb-common-project-editor/kie-wb-common-project-editor-client/src/main/java/org/kie/workbench/common/screens/projecteditor/client/forms/DependencySelectorPopupViewImpl.java
@@ -1,17 +1,17 @@
 package org.kie.workbench.common.screens.projecteditor.client.forms;
 
+import javax.inject.Inject;
+
 import com.google.gwt.cell.client.ButtonCell;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.TextHeader;
 import com.google.gwt.user.client.ui.Widget;
-import org.jboss.errai.common.client.api.Caller;
-import org.guvnor.m2repo.client.editor.PagedJarTable;
+import org.guvnor.m2repo.client.widgets.AbstractPagedJarTable;
 import org.guvnor.m2repo.model.JarListPageRow;
 import org.guvnor.m2repo.service.M2RepoService;
+import org.jboss.errai.common.client.api.Caller;
 import org.uberfire.client.common.Popup;
-
-import javax.inject.Inject;
 
 public class DependencySelectorPopupViewImpl
         extends Popup
@@ -21,29 +21,29 @@ public class DependencySelectorPopupViewImpl
     private Presenter presenter;
 
     @Inject
-    public DependencySelectorPopupViewImpl( Caller<M2RepoService> m2RepoService ) {
+    public DependencySelectorPopupViewImpl( final Caller<M2RepoService> m2RepoService ) {
         this.m2RepoService = m2RepoService;
     }
 
     @Override
     public Widget getContent() {
-        PagedJarTable pagedJarTable = new PagedJarTable( m2RepoService );
-        pagedJarTable.hideColumnPicker();
+        final AbstractPagedJarTable pagedJarTable = new DependencyPagedJarTable( m2RepoService );
 
-        Column<JarListPageRow, String> selectColumn = new Column<JarListPageRow, String>( new ButtonCell() ) {
+        final Column<JarListPageRow, String> selectColumn = new Column<JarListPageRow, String>( new ButtonCell() ) {
             public String getValue( JarListPageRow row ) {
                 return "Select";
             }
         };
         selectColumn.setFieldUpdater( new FieldUpdater<JarListPageRow, String>() {
-            public void update( int index,
-                                JarListPageRow row,
-                                String value ) {
+            public void update( final int index,
+                                final JarListPageRow row,
+                                final String value ) {
                 presenter.onPathSelection( row.getPath() );
             }
         } );
 
-        pagedJarTable.addColumn( selectColumn, new TextHeader( "" ) );
+        pagedJarTable.addColumn( selectColumn,
+                                 new TextHeader( "" ) );
 
         return pagedJarTable;
     }
@@ -52,4 +52,5 @@ public class DependencySelectorPopupViewImpl
     public void setPresenter( Presenter presenter ) {
         this.presenter = presenter;
     }
+
 }
