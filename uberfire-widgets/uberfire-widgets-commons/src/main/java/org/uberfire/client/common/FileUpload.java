7a84817b2ae96b63848d3d0b063e866f218f2bd0
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2013-11-17 09:02:32 -0300

    new file upload widget

---
+++ uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/client/common/FileUpload.java
@@ -0,0 +1,70 @@
+package org.uberfire.client.common;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.AnchorElement;
+import com.google.gwt.dom.client.InputElement;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.EventListener;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import org.uberfire.mvp.Command;
+
+public class FileUpload
+        extends Composite {
+
+    interface FileUploadBinder extends UiBinder<Widget, FileUpload> {
+
+    }
+
+    @UiField
+    AnchorElement uploadButton;
+
+    @UiField
+    AnchorElement chooseButton;
+
+    @UiField
+    InputElement file;
+
+    @UiField
+    InputElement fileText;
+
+    private static FileUploadBinder uiBinder = GWT.create( FileUploadBinder.class );
+
+    public FileUpload( final Command command ) {
+        initWidget( uiBinder.createAndBindUi( this ) );
+        fileText.setReadOnly( true );
+
+        DOM.sinkEvents( (Element) file.cast(), Event.ONCHANGE );
+        DOM.setEventListener( (Element) file.cast(), new EventListener() {
+            public void onBrowserEvent( final Event event ) {
+                fileText.setValue( file.getValue() );
+            }
+        } );
+
+        DOM.sinkEvents( (Element) chooseButton.cast(), Event.ONCLICK );
+        DOM.setEventListener( (Element) chooseButton.cast(), new EventListener() {
+            public void onBrowserEvent( final Event event ) {
+                file.click();
+            }
+        } );
+
+        DOM.sinkEvents( (Element) uploadButton.cast(), Event.ONCLICK );
+        DOM.setEventListener( (Element) uploadButton.cast(), new EventListener() {
+            public void onBrowserEvent( final Event event ) {
+                command.execute();
+            }
+        } );
+    }
+
+    public void setName( final String name ) {
+        file.setName( name );
+    }
+
+    public String getFilename() {
+        return file.getValue();
+    }
+}
