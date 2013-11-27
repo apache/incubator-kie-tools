44d920c442613079738969500803b669eea96a06
Michael Anstis <michael.anstis@gmail.com>
2013-11-27 11:50:59 +0000

    BZ1034664 - DefaultGuvnorApprover fills server.log with exceptions

--- kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/rest/JobRequest.java
+++ kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/rest/JobRequest.java
@@ -1,35 +1,42 @@
+/*
+ * Copyright 2013 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kie.workbench.common.services.shared.rest;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
 
 @Portable
 public class JobRequest {
-    public enum Status {
-        GONE,
-        ACCEPTED,
-        BAD_REQUEST,
-        RESOURCE_NOT_EXIST,
-        DUPLICATE_RESOURCE,
-        SERVER_ERROR,
-        SUCCESS,
-        FAIL,
-        DENIED
-    }
-    private String jobId;
-    private Status status;
 
-    
+    private String jobId;
+    private JobStatus status;
+
     public String getJobId() {
         return jobId;
     }
-    public void setJobId(String jobId) {
+
+    public void setJobId( String jobId ) {
         this.jobId = jobId;
     }
-	public Status getStatus() {
-		return status;
-	}
-	public void setStatus(Status status) {
-		this.status = status;
-	}
+
+    public JobStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus( JobStatus status ) {
+        this.status = status;
+    }
 
 }
--- kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/rest/JobResult.java
+++ kie-wb-common-services/kie-wb-common-services-api/src/main/java/org/kie/workbench/common/services/shared/rest/JobResult.java
@@ -1,48 +1,55 @@
 package org.kie.workbench.common.services.shared.rest;
 
-import java.util.Date;
 import java.util.List;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
-import org.kie.workbench.common.services.shared.rest.JobRequest.Status;
 
 @Portable
 public class JobResult {
 
-    private Status status;
+    private JobStatus status;
     private String jobId;
     private String result;
     private long lastModified;
     private List<String> detailedResult;
-    
-    public Status getStatus() {
+
+    public JobStatus getStatus() {
         return status;
     }
-    public void setStatus(Status status) {
+
+    public void setStatus( JobStatus status ) {
         this.status = status;
     }
+
     public String getJobId() {
         return jobId;
     }
-    public void setJobId(String jobId) {
+
+    public void setJobId( String jobId ) {
         this.jobId = jobId;
     }
-	public String getResult() {
-		return result;
-	}
-	public void setResult(String result) {
-		this.result = result;
-	}
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult( String result ) {
+        this.result = result;
+    }
+
     public long getLastModified() {
         return lastModified;
     }
-    public void setLastModified(long lastModified) {
+
+    public void setLastModified( long lastModified ) {
         this.lastModified = lastModified;
     }
-	public List<String> getDetailedResult() {
-		return detailedResult;
-	}
-	public void setDetailedResult(List<String> detailedResult) {
-		this.detailedResult = detailedResult;
-	}
+
+    public List<String> getDetailedResult() {
+        return detailedResult;
+    }
+
+    public void setDetailedResult( List<String> detailedResult ) {
+        this.detailedResult = detailedResult;
+    }
 }
