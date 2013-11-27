bcc7a4ab852bc8f7f06175c780acf3925f0c2f70
Michael Anstis <michael.anstis@gmail.com>
2013-11-27 20:20:08 +0000

    Improve SimplePopup to check interaction of Focus Events and PopupView

--- uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/popup/SimplePopUp.java
+++ uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/screens/popup/SimplePopUp.java
@@ -1,11 +1,15 @@
 package org.uberfire.client.screens.popup;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
 import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.Label;
+import com.github.gwtbootstrap.client.ui.TextBox;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import org.uberfire.lifecycle.OnStartup;
 import org.uberfire.client.annotations.WorkbenchPartTitle;
@@ -25,9 +29,32 @@ public class SimplePopUp {
 
     private PlaceRequest place;
 
+    private VerticalPanel view = new VerticalPanel();
+    private Label l;
+    private TextBox t;
+    private Button b;
+
+    @PostConstruct
+    public void setup() {
+        l = new Label( "Click to close" );
+        t = new TextBox();
+        b = new Button( "Close" );
+        b.addClickHandler( new ClickHandler() {
+
+            @Override
+            public void onClick( final ClickEvent event ) {
+                beforeClosePlaceEvent.fire( new BeforeClosePlaceEvent( place ) );
+            }
+        } );
+        view.add( l );
+        view.add( t );
+        view.add( b );
+    }
+
     @OnStartup
     public void onStartup( final PlaceRequest place ) {
         this.place = place;
+        b.setFocus( true );
     }
 
     @WorkbenchPartTitle
@@ -37,15 +64,7 @@ public class SimplePopUp {
 
     @WorkbenchPartView
     public Widget getView() {
-        Button b = new Button( "Close" );
-        b.addClickHandler( new ClickHandler() {
-
-            @Override
-            public void onClick( final ClickEvent event ) {
-                beforeClosePlaceEvent.fire( new BeforeClosePlaceEvent( place ) );
-            }
-        } );
-        return b;
+        return view;
     }
 
 }
