70817ecfef25cd4e7531ae930a71d3f9d8a76f4c
Michael Anstis <michael.anstis@gmail.com>
2013-12-04 10:28:53 +0000

    BZ993052: Guided Rule with DSL should translate DSL into DRL when viewing source

--- drools-wb-screens/drools-wb-guided-rule-editor/drools-wb-guided-rule-editor-backend/src/main/java/org/drools/workbench/screens/guided/rule/backend/server/GuidedRuleDSLRSourceService.java
+++ drools-wb-screens/drools-wb-guided-rule-editor/drools-wb-guided-rule-editor-backend/src/main/java/org/drools/workbench/screens/guided/rule/backend/server/GuidedRuleDSLRSourceService.java
@@ -16,21 +16,52 @@
 
 package org.drools.workbench.screens.guided.rule.backend.server;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import org.drools.compiler.lang.Expander;
+import org.drools.compiler.lang.dsl.DSLMappingFile;
+import org.drools.compiler.lang.dsl.DSLTokenizedMappingFile;
+import org.drools.compiler.lang.dsl.DefaultExpander;
 import org.drools.workbench.models.commons.backend.rule.RuleModelDRLPersistenceImpl;
 import org.drools.workbench.models.datamodel.rule.RuleModel;
 import org.drools.workbench.screens.guided.rule.type.GuidedRuleDSLRResourceTypeDefinition;
-import org.uberfire.java.nio.file.Path;
+import org.guvnor.common.services.backend.file.FileDiscoveryService;
+import org.guvnor.common.services.project.service.ProjectService;
+import org.kie.workbench.common.services.backend.file.DslFileFilter;
 import org.kie.workbench.common.services.backend.source.BaseSourceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.uberfire.backend.server.util.Paths;
+import org.uberfire.io.IOService;
+import org.uberfire.java.nio.file.Path;
 
 @ApplicationScoped
 public class GuidedRuleDSLRSourceService
         extends BaseSourceService<RuleModel> {
 
+    private static final Logger logger = LoggerFactory.getLogger( GuidedRuleDSLRSourceService.class );
+
+    private static final DslFileFilter FILTER_DSLS = new DslFileFilter();
+
     @Inject
     private GuidedRuleDSLRResourceTypeDefinition resourceType;
+
+    @Inject
+    @Named("ioStrategy")
+    private IOService ioService;
+
+    @Inject
+    private FileDiscoveryService fileDiscoveryService;
+
+    @Inject
+    private ProjectService projectService;
 
     @Override
     public String getPattern() {
@@ -40,7 +71,45 @@ public class GuidedRuleDSLRSourceService
     @Override
     public String getSource( final Path path,
                              final RuleModel model ) {
-        return new StringBuilder().append( RuleModelDRLPersistenceImpl.getInstance().marshal( model ) ).toString();
+        final String dslr = RuleModelDRLPersistenceImpl.getInstance().marshal( model );
+        final Expander expander = getDSLExpander( path );
+        final String drl = expander.expand( dslr );
+        return drl;
+    }
+
+    /**
+     * Returns an expander for DSLs (only if there is a DSL configured for this package).
+     */
+    public Expander getDSLExpander( final Path path ) {
+        final Expander expander = new DefaultExpander();
+        final List<DSLMappingFile> dsls = getDSLMappingFiles( path );
+        for ( DSLMappingFile dsl : dsls ) {
+            expander.addDSLMapping( dsl.getMapping() );
+        }
+        return expander;
+    }
+
+    private List<DSLMappingFile> getDSLMappingFiles( final Path path ) {
+        final List<DSLMappingFile> dsls = new ArrayList<DSLMappingFile>();
+        final org.uberfire.backend.vfs.Path vfsPath = Paths.convert( path );
+        final org.uberfire.backend.vfs.Path packagePath = projectService.resolvePackage( vfsPath ).getPackageMainResourcesPath();
+        final org.uberfire.java.nio.file.Path nioPackagePath = Paths.convert( packagePath );
+        final Collection<Path> dslPaths = fileDiscoveryService.discoverFiles( nioPackagePath,
+                                                                              FILTER_DSLS );
+        for ( final org.uberfire.java.nio.file.Path dslPath : dslPaths ) {
+            final String dslDefinition = ioService.readAllString( dslPath );
+            final DSLTokenizedMappingFile dslFile = new DSLTokenizedMappingFile();
+            try {
+                if ( dslFile.parseAndLoad( new StringReader( dslDefinition ) ) ) {
+                    dsls.add( dslFile );
+                } else {
+                    logger.error( "Unable to parse DSL definition: " + dslDefinition );
+                }
+            } catch ( IOException ioe ) {
+                logger.error( ioe.getMessage() );
+            }
+        }
+        return dsls;
     }
 
 }
