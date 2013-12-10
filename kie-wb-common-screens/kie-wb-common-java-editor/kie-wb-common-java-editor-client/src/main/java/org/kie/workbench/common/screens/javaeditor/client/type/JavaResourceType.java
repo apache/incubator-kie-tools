194e4879493f540ceecf98ed201ac95c58e21138
Michael Anstis <michael.anstis@gmail.com>
2013-12-10 18:59:46 +0000

    BZ1040121 - Update icons used in Project Explorer

--- kie-wb-common-screens/kie-wb-common-java-editor/kie-wb-common-java-editor-client/src/main/java/org/kie/workbench/common/screens/javaeditor/client/type/JavaResourceType.java
+++ kie-wb-common-screens/kie-wb-common-java-editor/kie-wb-common-java-editor-client/src/main/java/org/kie/workbench/common/screens/javaeditor/client/type/JavaResourceType.java
@@ -1,19 +1,22 @@
 package org.kie.workbench.common.screens.javaeditor.client.type;
 
-import org.kie.workbench.common.screens.javaeditor.type.JavaResourceTypeDefinition;
-
-import com.google.gwt.user.client.ui.IsWidget;
-import org.uberfire.client.workbench.type.ClientResourceType;
-
 import javax.enterprise.context.ApplicationScoped;
+
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.IsWidget;
+import org.kie.workbench.common.screens.javaeditor.client.resources.JavaEditorResources;
+import org.kie.workbench.common.screens.javaeditor.type.JavaResourceTypeDefinition;
+import org.uberfire.client.workbench.type.ClientResourceType;
 
 @ApplicationScoped
 public class JavaResourceType
         extends JavaResourceTypeDefinition
         implements ClientResourceType {
 
+    private static final Image IMAGE = new Image( JavaEditorResources.INSTANCE.images().typeJava() );
+
     @Override
     public IsWidget getIcon() {
-        return null;
+        return IMAGE;
     }
 }
