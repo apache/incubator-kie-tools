bdd0f9e27f0f9481bee6a1a4e95adbbc052b50ab
nmirasch <neus.miras@gmail.com>
2014-01-24 17:56:10 +0100

    BZ:1000403 - Functions not recognized

---
+++ kie-wb-common-services/kie-wb-common-services-backend/src/main/java/org/kie/workbench/common/services/backend/file/DrlFileFilter.java
@@ -0,0 +1,17 @@
+package org.kie.workbench.common.services.backend.file;
+
+import org.uberfire.java.nio.file.DirectoryStream;
+import org.uberfire.java.nio.file.Path;
+
+/**
+ * Filter to match DSL source files
+ */
+public class DrlFileFilter implements DirectoryStream.Filter<Path> {
+
+    @Override
+    public boolean accept( final Path path ) {
+        final String fileName = path.getFileName().toString();
+        return fileName.toLowerCase().endsWith( ".drl" );
+    }
+
+}
