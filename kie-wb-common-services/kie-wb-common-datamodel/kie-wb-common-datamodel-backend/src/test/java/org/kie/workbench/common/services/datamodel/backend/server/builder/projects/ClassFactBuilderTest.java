235384b0730d911bca301f4bfae8eefcba95782c
Toni Rikkola <toni@rikkola.net>
2014-01-24 16:23:35 +0200

    BZ-1044973 - Guided rule editor does not let the user to set objects as a parameters for method calls that have objects super type as a parameter -Fix for broken test

--- kie-wb-common-services/kie-wb-common-datamodel/kie-wb-common-datamodel-backend/src/test/java/org/kie/workbench/common/services/datamodel/backend/server/builder/projects/ClassFactBuilderTest.java
+++ kie-wb-common-services/kie-wb-common-datamodel/kie-wb-common-datamodel-backend/src/test/java/org/kie/workbench/common/services/datamodel/backend/server/builder/projects/ClassFactBuilderTest.java
@@ -1,16 +1,11 @@
 package org.kie.workbench.common.services.datamodel.backend.server.builder.projects;
 
-import java.util.Set;
-
 import org.drools.workbench.models.commons.backend.oracle.ProjectDataModelOracleImpl;
-import org.drools.workbench.models.datamodel.oracle.Annotation;
 import org.drools.workbench.models.datamodel.oracle.TypeSource;
 import org.junit.Test;
 import org.kie.workbench.common.services.datamodel.backend.server.testclasses.superclasses.PapaSmurf;
-import org.kie.workbench.common.services.datamodel.backend.server.testclasses.superclasses.Smurf;
 
 import static org.junit.Assert.*;
-import static org.kie.workbench.common.services.datamodel.backend.server.ProjectDataModelOracleTestUtils.assertContains;
 
 public class ClassFactBuilderTest {
 
@@ -19,12 +14,12 @@ public class ClassFactBuilderTest {
         final ProjectDataModelOracleBuilder builder = ProjectDataModelOracleBuilder.newProjectOracleBuilder();
         final ProjectDataModelOracleImpl oracle = new ProjectDataModelOracleImpl();
 
-        final ClassFactBuilder cb = new ClassFactBuilder( builder,
+        final ClassFactBuilder cb = new ClassFactBuilder(builder,
                 PapaSmurf.class,
                 false,
-                TypeSource.JAVA_PROJECT );
-        cb.build( oracle );
+                TypeSource.JAVA_PROJECT);
+        cb.build(oracle);
 
-        assertEquals(2, oracle.getProjectSuperTypes().size());
+        assertEquals(2, oracle.getProjectSuperTypes().get(PapaSmurf.class.getName()).size());
     }
 }
