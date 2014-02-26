a2a5434322f9aa8b02120210790051cb91772244
Jonathan Fuerth <jfuerth@gmail.com>
2014-02-26 12:14:12 -0500

    Added docs based on what I could figure out about @Startup annotation

--- uberfire-commons/src/main/java/org/uberfire/commons/services/cdi/Startup.java
+++ uberfire-commons/src/main/java/org/uberfire/commons/services/cdi/Startup.java
@@ -1,15 +1,25 @@
 package org.uberfire.commons.services.cdi;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Designates the target type as a server-side bean that should be created immediately when the application is deployed
+ * within the server. Normally, CDI beans are instantiated lazily when first needed, but {@code @Startup} beans
+ * have their PostConstruct methods called early in the server-side CDI application lifecycle.
+ */
 @Retention(RUNTIME)
+@Documented
 @Target({ TYPE })
 public @interface Startup {
 
+    /**
+     * Specifies which cohort this {@Startup} bean is initialized in.
+     */
     StartupType value() default StartupType.EAGER;
 
     int priority() default 0;
--- uberfire-commons/src/main/java/org/uberfire/commons/services/cdi/StartupType.java
+++ uberfire-commons/src/main/java/org/uberfire/commons/services/cdi/StartupType.java
@@ -1,11 +1,30 @@
 package org.uberfire.commons.services.cdi;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.spi.AfterDeploymentValidation;
+
 /**
- * Types of Startup
+ * Argument type for the {@link Startup} annotation.
  */
 public enum StartupType {
 
+    /**
+     * The target bean's {@link PostConstruct} method will be invoked immediately after the all BOOTSTRAP beans
+     * have had their PostConstruct methods called.
+     * <p>
+     * Among EAGER startup beans, the order they are called is not specified, but it is guaranteed that all BOOTSTRAP
+     * startup beans are processed before any EAGER startup beans are processed.
+     */
     EAGER,
+
+    /**
+     * The target bean's {@link PostConstruct} method will be invoked immediately after the CDI container fires the
+     * {@code AfterDeploymentValidation} event.
+     * See {@link AfterDeploymentValidation} in the CDI documentation for details.
+     * <p>
+     * Among BOOSTRAP startup beans, the order they are called is not specified, but it is guaranteed that all BOOTSTRAP
+     * startup beans are processed before any EAGER startup beans are processed.
+     */
     BOOTSTRAP
 
 }
