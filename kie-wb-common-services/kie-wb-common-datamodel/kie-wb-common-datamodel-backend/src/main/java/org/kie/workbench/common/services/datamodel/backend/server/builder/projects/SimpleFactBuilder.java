2cf7ece20760ff61cf19380a8cd620348072e704
Toni Rikkola <toni@rikkola.net>
2014-03-26 11:36:30 +0200

    BZ-Guided rule editor fails to open rules that use fields from fields whose type is not imported

--- kie-wb-common-services/kie-wb-common-datamodel/kie-wb-common-datamodel-backend/src/main/java/org/kie/workbench/common/services/datamodel/backend/server/builder/projects/SimpleFactBuilder.java
+++ kie-wb-common-services/kie-wb-common-datamodel/kie-wb-common-datamodel-backend/src/main/java/org/kie/workbench/common/services/datamodel/backend/server/builder/projects/SimpleFactBuilder.java
@@ -1,5 +1,8 @@
 package org.kie.workbench.common.services.datamodel.backend.server.builder.projects;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.drools.workbench.models.datamodel.oracle.ModelField;
 import org.drools.workbench.models.datamodel.oracle.TypeSource;
 
@@ -24,4 +27,8 @@ public class SimpleFactBuilder extends BaseFactBuilder {
         return this;
     }
 
+    @Override
+    public Map<String, FactBuilder> getInternalBuilders() {
+        return Collections.emptyMap();
+    }
 }
