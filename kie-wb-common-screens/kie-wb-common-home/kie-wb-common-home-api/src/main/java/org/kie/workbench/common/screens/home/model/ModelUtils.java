ee3cb8bae55309fe5ff37d730098226554cb3eda
Michael Anstis <manstis@gmail.com>
2014-04-14 12:56:09 +0100

    BZ1086499 - User can access unauthorized repositories via [Administration] even after configuring roles

--- kie-wb-common-screens/kie-wb-common-home/kie-wb-common-home-api/src/main/java/org/kie/workbench/common/screens/home/model/ModelUtils.java
+++ kie-wb-common-screens/kie-wb-common-home/kie-wb-common-home-api/src/main/java/org/kie/workbench/common/screens/home/model/ModelUtils.java
@@ -1,5 +1,7 @@
 package org.kie.workbench.common.screens.home.model;
 
+import java.util.Collection;
+
 import org.uberfire.mvp.Command;
 
 /**
@@ -20,6 +22,15 @@ public class ModelUtils {
                                                  final Command command ) {
         final SectionEntry entry = new SectionEntry( caption,
                                                      command );
+        return entry;
+    }
+
+    public static SectionEntry makeSectionEntry( final String caption,
+                                                 final Command command,
+                                                 final Collection<String> roles ) {
+        final SectionEntry entry = new SectionEntry( caption,
+                                                     command );
+        entry.setRoles( roles );
         return entry;
     }
 
