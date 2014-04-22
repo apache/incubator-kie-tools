8dd594869137dcedd11d31214cacbc10a9bdb054
Jonathan Fuerth <jfuerth@gmail.com>
2014-04-22 14:15:58 -0400

    UberFire 0.5.0 milestone 1 (depends on Errai 3.0-SNAPSHOT)

---
+++ uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/backend/server/impl/Guava14Workaround.java
@@ -0,0 +1,25 @@
+package org.uberfire.backend.server.impl;
+
+import java.util.Set;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.Service;
+
+/**
+ * Produces an empty {@code Set<Service>} in order to prevent deploy-time failure.
+ * The issue is <a href="https://code.google.com/p/guava-libraries/issues/detail?id=1527">described in the Guava bug
+ * tracker</a>. This workaround is described in <a
+ * href="https://code.google.com/p/guava-libraries/issues/detail?id=1433#c20">a comment on separate issue</a>.
+ * 
+ */
+@ApplicationScoped
+public class Guava14Workaround {
+
+    @Produces Set<Service> dummyServices() {
+        return ImmutableSet.of();
+    }
+
+}
