e36b166dd3b0d85e819dfc38150b72fd66b33684
Jiří Locker <jiri.locker@gmail.com>
2014-06-24 12:10:23 +0200

    Fixed the test to actually reproduce the issue

--- uberfire-security/uberfire-security-server/src/test/java/org/uberfire/security/server/URLResourceManagerTest.java
+++ uberfire-security/uberfire-security-server/src/test/java/org/uberfire/security/server/URLResourceManagerTest.java
@@ -1,6 +1,15 @@
 package org.uberfire.security.server;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
 
@@ -9,28 +18,44 @@ import static org.junit.Assert.*;
 public class URLResourceManagerTest {
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException, ExecutionException, TimeoutException {
         final URLResourceManager manager = new URLResourceManager( null );
         assertFalse( manager.requiresAuthentication( new URLResource( "/a.ico" ) ) );
         assertTrue( manager.requiresAuthentication( new URLResource( "/a.html" ) ) );
 
-        for ( int i = 0; i < 2000; i++ ) {
-            final int index = i;
-            new Thread( new Runnable() {
-                @Override
-                public void run() {
-                    assertFalse( manager.requiresAuthentication( new URLResource( "/a.ico" ) ) );
-                    assertTrue( manager.requiresAuthentication( new URLResource( "/a.html" ) ) );
+        final int batchSize = 100; // number of concurrent threads in each batch
+        final Random rnd = new Random( 0 );
+        final ExecutorService es = Executors.newCachedThreadPool();
+        for ( int i = 0; i < 100; i++ ) {
+            // log overall progress
+            if ( i % 10 == 0 ) {
+                System.out.println( "Starting batch #" + i );
+            }
+            final List<Callable<Boolean>> requests = new ArrayList<Callable<Boolean>>( batchSize );
+            final List<Future<Boolean>> results = new ArrayList<Future<Boolean>>( batchSize );
 
-                    assertFalse( manager.requiresAuthentication( new URLResource( "/url" + ( index - new Random( 100 ).nextInt() ) + "/a.ico" ) ) );
-                    assertTrue( manager.requiresAuthentication( new URLResource( "/url" + ( index - new Random( 100 ).nextInt() ) + "/a.html" ) ) );
+            for ( int j = 0; j < batchSize; j++ ) {
+                final int index = i * batchSize + j;
+                requests.add( new Callable<Boolean>() {
+                    @Override
+                    public Boolean call() throws Exception {
+                        return manager.requiresAuthentication( new URLResource( "/url" +
+                                ( index + rnd.nextInt( 100 ) ) + "/a.ico" ) );
+                    }
+                } );
+            }
 
-                    assertFalse( manager.requiresAuthentication( new URLResource( "/url" + index + "/a.ico" ) ) );
-                    assertTrue( manager.requiresAuthentication( new URLResource( "/url" + index + "/a.html" ) ) );
-                }
-            } ).run();
+            for ( int j = 0; j < batchSize; j++ ) {
+                // submit all created callables
+                results.add( es.submit( requests.get( j ) ) );
+            }
+
+            for ( int j = 0; j < batchSize; j++ ) {
+                // if any of the callables doesn't finish within the following timeout, it is probably stuck
+                // and TimeoutException will be thrown
+                assertFalse( results.get( j ).get( 1000, TimeUnit.MILLISECONDS ) );
+            }
         }
-
     }
 
 }
