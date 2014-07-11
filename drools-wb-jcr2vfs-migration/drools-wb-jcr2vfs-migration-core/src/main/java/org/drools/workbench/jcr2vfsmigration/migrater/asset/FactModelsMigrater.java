75e30d60c61972af1a149c308174f29775f3b3af
Michael Anstis <manstis@gmail.com>
2014-07-11 15:28:06 +0100

    Fix for failing tests

--- drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-migration-core/src/main/java/org/drools/workbench/jcr2vfsmigration/migrater/asset/FactModelsMigrater.java
+++ drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-migration-core/src/main/java/org/drools/workbench/jcr2vfsmigration/migrater/asset/FactModelsMigrater.java
@@ -15,13 +15,15 @@ import org.drools.guvnor.client.asseteditor.drools.factmodel.FieldMetaModel;
 import org.drools.guvnor.client.common.AssetFormats;
 import org.drools.guvnor.client.rpc.Asset;
 import org.drools.guvnor.client.rpc.Module;
-import org.drools.guvnor.server.RepositoryAssetService;
 import org.drools.repository.AssetItem;
 import org.drools.workbench.jcr2vfsmigration.migrater.PackageImportHelper;
-import org.drools.workbench.jcr2vfsmigration.migrater.util.MigrationPathManager;
-import org.guvnor.common.services.project.model.Project;
 import org.guvnor.common.services.project.service.ProjectService;
-import org.kie.workbench.common.screens.datamodeller.model.*;
+import org.kie.workbench.common.screens.datamodeller.model.AnnotationDefinitionTO;
+import org.kie.workbench.common.screens.datamodeller.model.AnnotationMemberDefinitionTO;
+import org.kie.workbench.common.screens.datamodeller.model.DataModelTO;
+import org.kie.workbench.common.screens.datamodeller.model.DataObjectTO;
+import org.kie.workbench.common.screens.datamodeller.model.ObjectPropertyTO;
+import org.kie.workbench.common.screens.datamodeller.model.PropertyTypeTO;
 import org.kie.workbench.common.screens.datamodeller.service.DataModelerService;
 import org.kie.workbench.common.services.datamodeller.core.AnnotationDefinition;
 import org.kie.workbench.common.services.datamodeller.core.AnnotationMemberDefinition;
@@ -37,163 +39,163 @@ import org.uberfire.java.nio.file.StandardCopyOption;
 @ApplicationScoped
 public class FactModelsMigrater extends BaseAssetMigrater {
 
-    protected static final Logger logger = LoggerFactory.getLogger(FactModelsMigrater.class);
+    protected static final Logger logger = LoggerFactory.getLogger( FactModelsMigrater.class );
 
     @Inject
     @Named("ioStrategy")
     private IOService ioService;
-    
+
     @Inject
     PackageImportHelper packageImportHelper;
 
-    private ProjectService<KieProject> projectService;
-    
     @Inject
-    private DataModelerService modelerService;    
+    private ProjectService<KieProject> projectService;
 
-    private Map <String, String> orderedBaseTypes = new TreeMap<String, String>();
+    @Inject
+    private DataModelerService modelerService;
+
+    private Map<String, String> orderedBaseTypes = new TreeMap<String, String>();
     private Map<String, AnnotationDefinitionTO> annotationDefinitions;
 
-    @Inject
-    public FactModelsMigrater(ProjectService projectService) {
-        this.projectService = projectService;
-    }
+    public Path migrate( Module jcrModule,
+                         AssetItem jcrAssetItem,
+                         Path previousVersionPath ) {
+        if ( !AssetFormats.DRL_MODEL.equals( jcrAssetItem.getFormat() ) ) {
+            throw new IllegalArgumentException( "The jcrAsset (" + jcrAssetItem.getName()
+                                                        + ") has the wrong format (" + jcrAssetItem.getFormat() + ")." );
+        }
 
-    public Path migrate(Module jcrModule, AssetItem jcrAssetItem, Path previousVersionPath) {
-        if (!AssetFormats.DRL_MODEL.equals(jcrAssetItem.getFormat())) {
-            throw new IllegalArgumentException("The jcrAsset (" + jcrAssetItem.getName()
-                    + ") has the wrong format (" + jcrAssetItem.getFormat() + ").");
-        }
-        
-        Path path = migrationPathManager.generatePathForAsset(jcrModule, jcrAssetItem);   
+        Path path = migrationPathManager.generatePathForAsset( jcrModule, jcrAssetItem );
         //The asset was renamed in this version. We move this asset first.
-        if(previousVersionPath != null && !previousVersionPath.equals(path)) {
-             ioService.move(Paths.convert( previousVersionPath ), Paths.convert( path ), StandardCopyOption.REPLACE_EXISTING);
+        if ( previousVersionPath != null && !previousVersionPath.equals( path ) ) {
+            ioService.move( Paths.convert( previousVersionPath ), Paths.convert( path ), StandardCopyOption.REPLACE_EXISTING );
         }
-        
-        KieProject project = projectService.resolveProject(path);
-        
+
+        KieProject project = projectService.resolveProject( path );
+
         initBasePropertyTypes();
-        initAnnotationDefinitions();        
-        
-        if(project == null) {
-        	Path projectRootPath = migrationPathManager.generatePathForModule(jcrModule.getName());
-        	//Quick hack to pass mock values for pomPath etc, to make Project constructor happy. We only use projectRootPath anyway
-        	project = new KieProject( projectRootPath,
-        			projectRootPath,
-        			projectRootPath,
-        			projectRootPath,
-                    "" );
+        initAnnotationDefinitions();
+
+        if ( project == null ) {
+            Path projectRootPath = migrationPathManager.generatePathForModule( jcrModule.getName() );
+            //Quick hack to pass mock values for pomPath etc, to make Project constructor happy. We only use projectRootPath anyway
+            project = new KieProject( projectRootPath,
+                                      projectRootPath,
+                                      projectRootPath,
+                                      projectRootPath,
+                                      "" );
         }
-   
+
         try {
-            Asset jcrAsset = jcrRepositoryAssetService.loadRuleAsset(jcrAssetItem.getUUID());
-            
-            FactModels factModels = ((FactModels) jcrAsset.getContent());
-            DataModelTO dataModelTO  = new DataModelTO();
-            
-            String packageName = getPackageName(jcrModule);
-            packageName = migrationPathManager.normalizePackageName(packageName);
+            Asset jcrAsset = jcrRepositoryAssetService.loadRuleAsset( jcrAssetItem.getUUID() );
+
+            FactModels factModels = ( (FactModels) jcrAsset.getContent() );
+            DataModelTO dataModelTO = new DataModelTO();
+
+            String packageName = getPackageName( jcrModule );
+            packageName = migrationPathManager.normalizePackageName( packageName );
             AnnotationDefinitionTO positionAnnotationDef = getPositionAnnotationDefinition();
 
             for ( FactMetaModel factMetaModel : factModels.models ) {
-                DataObjectTO dataObjectTO = createDataObject(packageName, factMetaModel.getName(), factMetaModel.getSuperType());
-            	List<AnnotationMetaModel> annotationMetaModel = factMetaModel.getAnnotations();                
-            	addAnnotations(dataObjectTO, annotationMetaModel);
+                DataObjectTO dataObjectTO = createDataObject( packageName, factMetaModel.getName(), factMetaModel.getSuperType() );
+                List<AnnotationMetaModel> annotationMetaModel = factMetaModel.getAnnotations();
+                addAnnotations( dataObjectTO, annotationMetaModel );
                 List<FieldMetaModel> fields = factMetaModel.getFields();
 
                 int position = 0;
-                for(FieldMetaModel fieldMetaModel : fields) {
-                	String filedName = fieldMetaModel.name;
-                	String fildType = fieldMetaModel.type;
-                	//Guvnor 5.5 (and earlier) does not have MultipleType
-                	boolean isMultiple = false;
-                	boolean isBaseType = isBaseType(fildType);
-                    ObjectPropertyTO property = new ObjectPropertyTO(filedName,
-                    		fildType,
-                            isMultiple,
-                            isBaseType);
-                    property.addAnnotation(positionAnnotationDef, AnnotationDefinitionTO.VALUE_PARAM, position+"" );
+                for ( FieldMetaModel fieldMetaModel : fields ) {
+                    String filedName = fieldMetaModel.name;
+                    String fildType = fieldMetaModel.type;
+                    //Guvnor 5.5 (and earlier) does not have MultipleType
+                    boolean isMultiple = false;
+                    boolean isBaseType = isBaseType( fildType );
+                    ObjectPropertyTO property = new ObjectPropertyTO( filedName,
+                                                                      fildType,
+                                                                      isMultiple,
+                                                                      isBaseType );
+                    property.addAnnotation( positionAnnotationDef, AnnotationDefinitionTO.VALUE_PARAM, position + "" );
                     position++;
-                	//field has no annotation in Guvnor 5.5 (and earlier)
-                    dataObjectTO.getProperties().add(property);
+                    //field has no annotation in Guvnor 5.5 (and earlier)
+                    dataObjectTO.getProperties().add( property );
                 }
-                
-                dataModelTO.getDataObjects().add(dataObjectTO);
-            }
-            
-            modelerService.saveModel(dataModelTO, project);
 
-        } catch (SerializationException e) {
+                dataModelTO.getDataObjects().add( dataObjectTO );
+            }
+
+            modelerService.saveModel( dataModelTO, project );
+
+        } catch ( SerializationException e ) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
-        return path;
-     }
 
+        return path;
+    }
 
     private AnnotationDefinitionTO getPositionAnnotationDefinition() {
         AnnotationDefinition positionAnnotationDef = PositionAnnotationDefinition.getInstance();
-        AnnotationDefinitionTO positionAnnotationDefTO = new AnnotationDefinitionTO(positionAnnotationDef.getName(), positionAnnotationDef.getClassName(), positionAnnotationDef.getShortDescription(), positionAnnotationDef.getDescription(), positionAnnotationDef.isObjectAnnotation(), positionAnnotationDef.isPropertyAnnotation());
+        AnnotationDefinitionTO positionAnnotationDefTO = new AnnotationDefinitionTO( positionAnnotationDef.getName(), positionAnnotationDef.getClassName(), positionAnnotationDef.getShortDescription(), positionAnnotationDef.getDescription(), positionAnnotationDef.isObjectAnnotation(), positionAnnotationDef.isPropertyAnnotation() );
         AnnotationMemberDefinitionTO memberDefinitionTO;
-        for (AnnotationMemberDefinition memberDefinition : positionAnnotationDef.getAnnotationMembers()) {
-            memberDefinitionTO = new AnnotationMemberDefinitionTO(memberDefinition.getName(), memberDefinition.getClassName(), memberDefinition.isPrimitiveType(), memberDefinition.isEnum(), memberDefinition.defaultValue(), memberDefinition.getShortDescription(), memberDefinition.getDescription());
-            positionAnnotationDefTO.addMember(memberDefinitionTO);
+        for ( AnnotationMemberDefinition memberDefinition : positionAnnotationDef.getAnnotationMembers() ) {
+            memberDefinitionTO = new AnnotationMemberDefinitionTO( memberDefinition.getName(), memberDefinition.getClassName(), memberDefinition.isPrimitiveType(), memberDefinition.isEnum(), memberDefinition.defaultValue(), memberDefinition.getShortDescription(), memberDefinition.getDescription() );
+            positionAnnotationDefTO.addMember( memberDefinitionTO );
         }
         return positionAnnotationDefTO;
     }
 
     //The JCR Module name also contains the project name. This code attempts to create a package name
     //from the full JCR Module name (assuming they're formatted "projectName.subModule1.subModule2" etc
-    private String getPackageName(Module jcrModule) {
+    private String getPackageName( Module jcrModule ) {
         String packageName = jcrModule.getName();
         return packageName;
     }
-    
+
     private void initBasePropertyTypes() {
-    	List<PropertyTypeTO> baseTypes = modelerService.getBasePropertyTypes();
-        if (baseTypes != null) {
-            for (PropertyTypeTO type : baseTypes) {
-                orderedBaseTypes.put(type.getName(), type.getClassName());
+        List<PropertyTypeTO> baseTypes = modelerService.getBasePropertyTypes();
+        if ( baseTypes != null ) {
+            for ( PropertyTypeTO type : baseTypes ) {
+                orderedBaseTypes.put( type.getName(), type.getClassName() );
             }
         }
     }
-    
-    public Boolean isBaseType(String type) {
-        return orderedBaseTypes.containsValue(type);
+
+    public Boolean isBaseType( String type ) {
+        return orderedBaseTypes.containsValue( type );
     }
-    
+
     private void initAnnotationDefinitions() {
-    	annotationDefinitions = modelerService.getAnnotationDefinitions();
+        annotationDefinitions = modelerService.getAnnotationDefinitions();
     }
-    
-    private DataObjectTO createDataObject(String packageName, String name, String superClass) {
-        DataObjectTO dataObject = new DataObjectTO(name, packageName, superClass);
+
+    private DataObjectTO createDataObject( String packageName,
+                                           String name,
+                                           String superClass ) {
+        DataObjectTO dataObject = new DataObjectTO( name, packageName, superClass );
         return dataObject;
     }
-    
-    private void addAnnotations(DataObjectTO dataObject, List<AnnotationMetaModel> annotationMetaModelList) {
-    	for(AnnotationMetaModel annotationMetaModel : annotationMetaModelList) {
-    		String name = annotationMetaModel.name;
-    		Map<String, String> values = annotationMetaModel.values;
- 
-    		String key = AnnotationDefinitionTO.VALUE_PARAM;
-    		String value = "";
-    		
-    		if(values.size() > 0) {
-    			key = values.keySet().iterator().next();
-    			value = values.values().iterator().next();
-    		}
-    		
-    		if("Role".equals(name)) {
-                dataObject.addAnnotation(annotationDefinitions.get(AnnotationDefinitionTO.ROLE_ANNOTATION), key, value);		
-    		} else if("Position".equals(name)) {
-                dataObject.addAnnotation(annotationDefinitions.get(AnnotationDefinitionTO.POSITION_ANNOTATION), key, value);
-	   		} else if("Equals".equals(name)) {
-                dataObject.addAnnotation(annotationDefinitions.get(AnnotationDefinitionTO.KEY_ANNOTATION), key, value);
-    		}
-    	}    		
+
+    private void addAnnotations( DataObjectTO dataObject,
+                                 List<AnnotationMetaModel> annotationMetaModelList ) {
+        for ( AnnotationMetaModel annotationMetaModel : annotationMetaModelList ) {
+            String name = annotationMetaModel.name;
+            Map<String, String> values = annotationMetaModel.values;
+
+            String key = AnnotationDefinitionTO.VALUE_PARAM;
+            String value = "";
+
+            if ( values.size() > 0 ) {
+                key = values.keySet().iterator().next();
+                value = values.values().iterator().next();
+            }
+
+            if ( "Role".equals( name ) ) {
+                dataObject.addAnnotation( annotationDefinitions.get( AnnotationDefinitionTO.ROLE_ANNOTATION ), key, value );
+            } else if ( "Position".equals( name ) ) {
+                dataObject.addAnnotation( annotationDefinitions.get( AnnotationDefinitionTO.POSITION_ANNOTATION ), key, value );
+            } else if ( "Equals".equals( name ) ) {
+                dataObject.addAnnotation( annotationDefinitions.get( AnnotationDefinitionTO.KEY_ANNOTATION ), key, value );
+            }
+        }
     }
 
 }
