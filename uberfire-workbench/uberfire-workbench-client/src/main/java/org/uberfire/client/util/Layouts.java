597d9a39293ac3469393fea307a1fa912b8ef02e
Jonathan Fuerth <jfuerth@gmail.com>
2014-08-20 15:38:45 -0400

    Added a new debug method that dumps info for widgets below a certain point
    This complements the one that was there already, which dumps info for widgets
    above a certain point.

--- uberfire-workbench/uberfire-workbench-client/src/main/java/org/uberfire/client/util/Layouts.java
+++ uberfire-workbench/uberfire-workbench-client/src/main/java/org/uberfire/client/util/Layouts.java
@@ -4,6 +4,8 @@ import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.ProvidesResize;
 import com.google.gwt.user.client.ui.RequiresResize;
 import com.google.gwt.user.client.ui.Widget;
@@ -15,7 +17,7 @@ public class Layouts {
      * Sets the CSS on the given widget so it automatically fills the available space. This uses the same technique as
      * GWT's LayoutPanel system: absoulte positioning with top, left, right, and bottom set to 0px. Note that, like
      * LayoutPanels, this only works if the host page is in standards mode (has a {@code <!doctype html>} header).
-     * 
+     *
      * @param w
      *            the widget that should always fill its available space, rather than being sized to fit its contents.
      */
@@ -29,19 +31,97 @@ public class Layouts {
         s.setHeight( 100.0, Unit.PCT );
     }
 
+    /**
+     * Returns a multi-line string detailing layout information about the given widget and each of its ancestors in the
+     * widget tree.
+     *
+     * @param w the widget to start at. Null is permitted, and results in this method returning an empty string.
+     * @return information about w and its ancestors, one widget per line.
+     */
     public static String getContainmentHierarchy( Widget w ) {
         StringBuilder sb = new StringBuilder();
         int depth = 0;
-        while (w != null) {
-            sb.append("  " + depth + " - " + w.getOffsetWidth() + "x" + w.getOffsetHeight() + " - " +
-                    w.getClass().getName() + "@" + System.identityHashCode( w ) +
-                    (w instanceof RequiresResize ? " RequiresResize" : "") +
-                    (w instanceof ProvidesResize ? " ProvidesResize" : "") +
-                    " position: " + w.getElement().getStyle().getPosition() + "\n");
-            w.ensureDebugId("containment-parent-" + depth);
+        while ( w != null ) {
+            sb.append( "  " + depth + " - " + widgetInfo( w ) );
+            w.ensureDebugId( "containment-parent-" + depth );
             w = w.getParent();
             depth++;
         }
         return sb.toString();
     }
+
+    private static String widgetInfo( Widget w ) {
+        String widgetInfo;
+        try {
+            widgetInfo = w.getOffsetWidth() + "x" + w.getOffsetHeight() + " - " +
+                    w.getClass().getName() + "@" + System.identityHashCode( w ) +
+                    (w instanceof RequiresResize ? " RequiresResize" : "") +
+                    (w instanceof ProvidesResize ? " ProvidesResize" : "") +
+                    " position: " + w.getElement().getStyle().getPosition() + "\n";
+        } catch ( Throwable t ) {
+            widgetInfo = "?x? - " +
+                    w.getClass().getName() + "@" + System.identityHashCode( w ) +
+                    ": " + t.toString() + "\n";
+        }
+        return widgetInfo;
+    }
+
+    /**
+     * Returns a multi-line string detailing layout information about the given widget and each of its descendants in
+     * the widget tree.
+     *
+     * @param w
+     *            the widget to start at. Null is permitted.
+     * @return information about w and its descendants, one widget per line. Each line is indented with leading spaces
+     *         to illustrate the containment hierarchy.
+     */
+    public static String getContainedHierarchy( final Widget startAt ) {
+        IndentedLineAccumulator result = new IndentedLineAccumulator();
+        getContainedHierarchyRecursively( startAt, 0, result );
+        return result.toString();
+    }
+
+    private static void getContainedHierarchyRecursively( final Widget startAt,
+                                                          int depth,
+                                                          IndentedLineAccumulator result ) {
+        if ( startAt == null ) {
+            result.append( depth,
+                           "(null)" );
+            return;
+        }
+        result.append( depth,
+                       widgetInfo( startAt ) );
+        if ( startAt instanceof HasWidgets ) {
+            for ( Widget child : ((HasWidgets) startAt) ) {
+                getContainedHierarchyRecursively( child,
+                                                  depth + 1,
+                                                  result );
+            }
+        } else if ( startAt instanceof Composite ) {
+            getContainedHierarchyRecursively( extractWidget( ((Composite) startAt) ),
+                                              depth + 1,
+                                              result );
+        }
+    }
+
+    private static class IndentedLineAccumulator {
+        final StringBuilder sb = new StringBuilder();
+
+        private void append( int depth, String s ) {
+            for ( int i = 0; i < depth; i++ ) {
+                sb.append(" ");
+            }
+            sb.append( s );
+        }
+
+        @Override
+        public String toString() {
+            return sb.toString();
+        }
+    }
+
+    private static native Widget extractWidget( Composite composite ) /*-{
+        return composite.@com.google.gwt.user.client.ui.Composite::widget;
+    }-*/;
+
 }
