ecf3128ab562c4afd30516049903eb91b6350a71
Jonathan Fuerth <jfuerth@gmail.com>
2014-08-21 18:11:09 -0400

    Filled in some missing javadoc

--- uberfire-workbench/uberfire-workbench-client/src/main/java/org/uberfire/client/mvp/PerspectiveActivity.java
+++ uberfire-workbench/uberfire-workbench-client/src/main/java/org/uberfire/client/mvp/PerspectiveActivity.java
@@ -23,18 +23,36 @@ import org.uberfire.workbench.model.toolbar.ToolBar;
 public interface PerspectiveActivity extends ContextSensitiveActivity {
 
     /**
-     * Returns the layout (panels and their parts) that should be used if no persisted state is available.
-     * 
+     * Returns a new copy of the layout (panels and their parts) that should be used if no persisted state is available.
+     * Each time this method is called, it must produce a new PerspectiveDefinition. This rule applies whether or not
+     * the perspective is transient.
+     *
      * @return the perspective layout to use when a previously saved one is not available.
      * @see #isTransient()
      * @see WorkbenchServicesProxy#loadPerspective(String, org.uberfire.mvp.ParameterizedCommand)
      */
     PerspectiveDefinition getDefaultPerspectiveLayout();
 
+    /**
+     * Returns the unique identifier for this perspective. Should match the CDI bean name (the argument to the
+     * activity's {@code @Named} annotation).
+     */
     String getIdentifier();
 
+    /**
+     * Returns true if this perspective should be displayed automatically when the application starts. Each application
+     * needs exactly one default perspective.
+     *
+     * @return true if this is the default perspective; false if it is not.
+     */
     boolean isDefault();
 
+    /**
+     * Returns a new copy of the menus that should be used with this perspective. Each time this method is called, it
+     * must produce a new set of menus.
+     *
+     * @return the menus to use while this perspective is active.
+     */
     Menus getMenus();
 
     ToolBar getToolBar();
@@ -42,7 +60,7 @@ public interface PerspectiveActivity extends ContextSensitiveActivity {
     /**
      * Tells whether this perspective's state (layout and size of panels, parts contained in each panel) should be saved
      * per user or not.
-     * 
+     *
      * @return false if this perspective's state should be saved and retrieved from the server; true if this perspective
      *         should always start up in its default layout.
      * @see WorkbenchServicesProxy#loadPerspective(String, org.uberfire.mvp.ParameterizedCommand)
