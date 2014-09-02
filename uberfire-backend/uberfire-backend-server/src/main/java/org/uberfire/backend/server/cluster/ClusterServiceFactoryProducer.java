efc198273075749c8d7044eb353845358913ebb4
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2014-09-02 17:52:04 -0300

    isolating CDI from cluster sercive factory setup in order to make it re-usable for non CDI applications like drools exec server

--- uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/cluster/ClusterServiceFactoryProducer.java
+++ uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/cluster/ClusterServiceFactoryProducer.java
@@ -9,45 +9,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.uberfire.commons.cluster.ClusterService;
 import org.uberfire.commons.cluster.ClusterServiceFactory;
-import org.uberfire.commons.message.MessageHandlerResolver;
 import org.uberfire.commons.services.cdi.ApplicationStarted;
-import org.uberfire.io.impl.cluster.helix.ClusterServiceHelix;
 
 @ApplicationScoped
 public class ClusterServiceFactoryProducer {
 
-    private static final Logger logger = LoggerFactory.getLogger(ClusterServiceFactoryProducer.class);
+    private static final Logger logger = LoggerFactory.getLogger( ClusterServiceFactoryProducer.class );
 
     private final ClusterServiceFactory factory;
     private ClusterService clusterService = null;
 
     ClusterServiceFactoryProducer() {
-        final String clusterName = System.getProperty( "org.uberfire.cluster.id", null );
-        final String zkAddress = System.getProperty( "org.uberfire.cluster.zk", null );
-        final String localId = System.getProperty( "org.uberfire.cluster.local.id", null );
-        final String resourceName = System.getProperty( "org.uberfire.cluster.vfs.lock", null );
-        final boolean autostart = Boolean.parseBoolean(System.getProperty("org.uberfire.cluster.autostart", "true"));
-
-        if ( clusterName == null || zkAddress == null || localId == null || resourceName == null ) {
-            this.factory = null;
-        } else {
-            this.factory = new ClusterServiceFactory() {
-                @Override
-                public ClusterService build( final MessageHandlerResolver resolver ) {
-                    if ( clusterService == null ) {
-                        clusterService = new ClusterServiceHelix( clusterName, zkAddress, localId, resourceName, resolver );
-                    } else {
-                        clusterService.addMessageHandlerResolver( resolver );
-                    }
-                    return clusterService;
-                }
-
-                @Override
-                public boolean isAutoStart() {
-                    return autostart;
-                }
-            };
-        }
+        this.factory = ClusterServiceFactorySetup.buildFactory();
     }
 
     @Produces
@@ -56,12 +29,11 @@ public class ClusterServiceFactoryProducer {
         return factory;
     }
 
-
-    public void startOnEvent(@Observes ApplicationStarted event) {
-        logger.debug("Received event for application started {}", clusterService);
-        if (clusterService != null) {
-            logger.debug("About to create cluster service...");
-            clusterService.start();
+    public void startOnEvent( @Observes ApplicationStarted event ) {
+        logger.debug( "Received event for application started {}", clusterService );
+        if ( factory != null && factory instanceof ClusterServiceFactorySimpleImpl ) {
+            logger.debug( "About to create cluster service..." );
+            ( (ClusterServiceFactorySimpleImpl) factory ).startClusterService();
         }
     }
 }
---
+++ uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/cluster/ClusterServiceFactorySimpleImpl.java
@@ -0,0 +1,55 @@
+package org.uberfire.backend.server.cluster;
+
+import org.uberfire.commons.cluster.ClusterService;
+import org.uberfire.commons.cluster.ClusterServiceFactory;
+import org.uberfire.commons.message.MessageHandlerResolver;
+import org.uberfire.io.impl.cluster.helix.ClusterServiceHelix;
+
+/**
+ * TODO: update me
+ */
+public class ClusterServiceFactorySimpleImpl implements ClusterServiceFactory {
+
+    private ClusterService clusterService;
+    private final String clusterName;
+    private final String zkAddress;
+    private final String localId;
+    private final String resourceName;
+    private final boolean autostart;
+
+    public ClusterServiceFactorySimpleImpl( final String clusterName,
+                                            final String zkAddress,
+                                            final String localId,
+                                            final String resourceName,
+                                            final boolean autostart ) {
+        this.clusterName = clusterName;
+        this.zkAddress = zkAddress;
+        this.localId = localId;
+        this.resourceName = resourceName;
+        this.autostart = autostart;
+    }
+
+    @Override
+    public ClusterService build( final MessageHandlerResolver resolver ) {
+        if ( clusterService == null ) {
+            clusterService = new ClusterServiceHelix( clusterName, zkAddress, localId, resourceName, resolver );
+            if ( autostart ) {
+                clusterService.start();
+            }
+        } else {
+            clusterService.addMessageHandlerResolver( resolver );
+        }
+        return clusterService;
+    }
+
+    @Override
+    public boolean isAutoStart() {
+        return autostart;
+    }
+
+    public void startClusterService() {
+        if ( clusterService != null ) {
+            clusterService.start();
+        }
+    }
+}
