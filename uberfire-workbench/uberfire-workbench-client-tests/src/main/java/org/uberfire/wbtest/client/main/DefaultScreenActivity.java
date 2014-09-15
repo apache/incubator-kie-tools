882d3b89b43d48f05758a6c1133bf944e862a846
Jonathan Fuerth <jfuerth@gmail.com>
2014-09-15 16:43:40 -0400

    Added clickable navigation to the default perspective. Makes interactive testing sessions easier.

--- uberfire-workbench/uberfire-workbench-client-tests/src/main/java/org/uberfire/wbtest/client/main/DefaultScreenActivity.java
+++ uberfire-workbench/uberfire-workbench-client-tests/src/main/java/org/uberfire/wbtest/client/main/DefaultScreenActivity.java
@@ -4,12 +4,20 @@ import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.jboss.errai.ioc.client.container.IOCBeanDef;
+import org.jboss.errai.ioc.client.container.SyncBeanManager;
+import org.uberfire.client.mvp.PerspectiveActivity;
 import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.mvp.PlaceRequest;
+import org.uberfire.mvp.impl.DefaultPlaceRequest;
 import org.uberfire.wbtest.client.api.AbstractTestScreenActivity;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 @Dependent
 @Named( "org.uberfire.wbtest.client.main.DefaultScreenActivity" )
@@ -17,7 +25,10 @@ public class DefaultScreenActivity extends AbstractTestScreenActivity {
 
     public static final String DEBUG_ID = "DefaultScreenActivity";
 
-    private final Label widget = new Label( "Welcome to the default perspective!" );
+    private final VerticalPanel perspectives = new VerticalPanel();
+
+    @Inject private SyncBeanManager bm;
+    @Inject private PlaceManager placeManager;
 
     @Inject
     public DefaultScreenActivity( PlaceManager pm ) {
@@ -27,12 +38,27 @@ public class DefaultScreenActivity extends AbstractTestScreenActivity {
     @Override
     public void onStartup( PlaceRequest place ) {
         super.onStartup( place );
-        widget.ensureDebugId( DEBUG_ID );
+        perspectives.ensureDebugId( DEBUG_ID );
+        perspectives.add( new Label( "Welcome to the default perspective!" ) );
+        perspectives.add( new Label( "Some other perspectives you might enjoy:" ) );
+
+        for ( final IOCBeanDef<PerspectiveActivity> perspectiveBean : bm.lookupBeans( PerspectiveActivity.class ) ) {
+            Button b = new Button( perspectiveBean.getBeanClass().getName() );
+            b.addClickHandler( new ClickHandler() {
+                @Override
+                public void onClick( ClickEvent event ) {
+                    placeManager.goTo( new DefaultPlaceRequest( perspectiveBean.getBeanClass().getName() ) );
+                }
+            } );
+            perspectives.add( b );
+        }
+
+        perspectives.add( new Label( "Have a nice day" ) );
     }
 
     @Override
     public IsWidget getWidget() {
-        return widget;
+        return perspectives;
     }
 
 }
