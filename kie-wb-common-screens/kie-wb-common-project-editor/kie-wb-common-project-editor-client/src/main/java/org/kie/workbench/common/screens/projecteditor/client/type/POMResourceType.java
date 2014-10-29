8e6516c34917acebe6034b7f322537bd268d9220
Toni Rikkola <toni@rikkola.net>
2014-10-29 14:49:51 +0200

    Maven builder for guvnor-wb. Moving few classes into guvnor

--- kie-wb-common-screens/kie-wb-common-project-editor/kie-wb-common-project-editor-client/src/main/java/org/kie/workbench/common/screens/projecteditor/client/type/POMResourceType.java
+++ kie-wb-common-screens/kie-wb-common-project-editor/kie-wb-common-project-editor-client/src/main/java/org/kie/workbench/common/screens/projecteditor/client/type/POMResourceType.java
@@ -3,7 +3,7 @@ package org.kie.workbench.common.screens.projecteditor.client.type;
 import javax.enterprise.context.ApplicationScoped;
 
 import com.google.gwt.user.client.ui.IsWidget;
-import org.kie.workbench.common.screens.projecteditor.type.POMResourceTypeDefinition;
+import org.guvnor.common.services.project.editor.type.POMResourceTypeDefinition;
 import org.uberfire.client.workbench.type.ClientResourceType;
 
 @ApplicationScoped
