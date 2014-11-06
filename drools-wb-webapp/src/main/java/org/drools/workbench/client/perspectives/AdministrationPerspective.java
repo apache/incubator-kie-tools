51d165873a095d3c1a0e92fdc5d7948e38a2f885
Walter Medvedeo <wmedvedeo@gmail.com>
2014-11-06 15:23:25 +0100

    Create Repository Wizard added to drools-wb showcase

--- drools-wb-webapp/src/main/java/org/drools/workbench/client/perspectives/AdministrationPerspective.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/client/perspectives/AdministrationPerspective.java
@@ -26,12 +26,14 @@ import com.google.gwt.user.client.ui.PopupPanel;
 import org.drools.workbench.client.resources.i18n.AppConstants;
 import org.guvnor.structure.client.editors.repository.clone.CloneRepositoryForm;
 import org.guvnor.structure.client.editors.repository.create.CreateRepositoryForm;
+import org.guvnor.asset.management.client.editors.repository.wizard.CreateRepositoryWizard;
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.kie.workbench.common.services.security.AppRoles;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
 import org.uberfire.client.annotations.Perspective;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPerspective;
+import org.uberfire.client.callbacks.Callback;
 import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.client.workbench.panels.impl.MultiListWorkbenchPanelPresenter;
 import org.uberfire.client.workbench.panels.impl.SimpleWorkbenchPanelPresenter;
@@ -132,15 +134,14 @@ public class AdministrationPerspective {
                 new Command() {
                     @Override
                     public void execute() {
-                        final CreateRepositoryForm newRepositoryWizard = iocManager.lookupBean( CreateRepositoryForm.class ).getInstance();
+                        final CreateRepositoryWizard newRepositoryWizard = iocManager.lookupBean( CreateRepositoryWizard.class ).getInstance();
                         //When pop-up is closed destroy bean to avoid memory leak
-                        newRepositoryWizard.addCloseHandler( new CloseHandler<CreateRepositoryForm>() {
-                            @Override
-                            public void onClose( CloseEvent<CreateRepositoryForm> event ) {
+                        newRepositoryWizard.onCloseCallback( new Callback<Void>() {
+                            @Override public void callback( Void result ) {
                                 iocManager.destroyBean( newRepositoryWizard );
                             }
                         } );
-                        newRepositoryWizard.show();
+                        newRepositoryWizard.start();
                     }
                 } ).endMenu().build().getItems().get( 0 ) );
         return menuItems;
