7929911328c106d9f1efc19d7ea2feff78d23350
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2014-11-28 21:38:21 -0200

    BZ-1132081: now uf-ext provides a secure ioservice impl. apps can choose it if it makes sense.

---
+++ uberfire-extensions/uberfire-security/uberfire-servlet-security/src/test/java/org/uberfire/ext/security/server/io/IOServiceSecuritySetupTest.java
@@ -0,0 +1,64 @@
+package org.uberfire.ext.security.server.io;
+
+import org.jboss.errai.security.shared.api.identity.User;
+import org.jboss.errai.security.shared.exception.UnauthorizedException;
+import org.junit.Test;
+import org.uberfire.backend.server.security.FileSystemResourceAdaptor;
+import org.uberfire.java.nio.file.FileSystem;
+import org.uberfire.security.Resource;
+import org.uberfire.security.authz.AuthorizationManager;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class IOServiceSecuritySetupTest {
+
+    @Test
+    public void nonSecureExecuted() {
+        final FileSystem fs = mock( FileSystem.class );
+        final IOSecurityService service = new IOSecurityService( new MockIOService(), new MockAuthenticationService(), new AuthorizationManager() {
+            @Override
+            public boolean supports( Resource resource ) {
+                return resource instanceof FileSystemResourceAdaptor;
+            }
+
+            @Override
+            public boolean authorize( Resource resource,
+                                      User user ) throws UnauthorizedException {
+                return true;
+            }
+        } );
+
+        try {
+            service.startBatch( fs );
+        } catch ( InterruptedException e ) {
+            fail( "error" );
+        }
+    }
+
+    @Test
+    public void secureExecuted() {
+        final FileSystem fs = mock( FileSystem.class );
+        final IOSecurityService service = new IOSecurityService( new MockIOService(), new MockAuthenticationService(), new AuthorizationManager() {
+            @Override
+            public boolean supports( Resource resource ) {
+                return resource instanceof FileSystemResourceAdaptor;
+            }
+
+            @Override
+            public boolean authorize( Resource resource,
+                                      User user ) throws UnauthorizedException {
+                return false;
+            }
+        } );
+
+        try {
+            service.startBatch( fs );
+            fail( "error" );
+        } catch ( SecurityException e ) {
+        } catch ( InterruptedException e ) {
+            fail( "error" );
+        }
+    }
+
+}
