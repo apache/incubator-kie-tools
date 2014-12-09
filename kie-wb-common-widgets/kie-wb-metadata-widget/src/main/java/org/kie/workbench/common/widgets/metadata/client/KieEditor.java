cd5872844dc2497c30aa0c25021b2748e22c35f7
Toni Rikkola <toni@rikkola.net>
2014-12-09 15:55:10 +0200

    Bug 1170128 - Test Scenario: Version history in Overview is empty

--- kie-wb-common-widgets/kie-wb-metadata-widget/src/main/java/org/kie/workbench/common/widgets/metadata/client/KieEditor.java
+++ kie-wb-common-widgets/kie-wb-metadata-widget/src/main/java/org/kie/workbench/common/widgets/metadata/client/KieEditor.java
@@ -143,7 +143,6 @@ public abstract class KieEditor extends BaseEditor {
 
     protected void resetEditorPages( final Overview overview ) {
 
-        versionRecordManager.setVersions(overview.getMetadata().getVersion());
         this.overviewWidget.setContent(overview, versionRecordManager.getPathToLatest());
         this.metadata = overview.getMetadata();
 
