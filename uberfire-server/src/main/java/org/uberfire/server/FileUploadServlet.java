1e263173b51da82be1a0be4ceb64492af4d226bc
Michael Anstis <michael.anstis@gmail.com>
2014-12-09 16:42:25 +0000

    BZ1091204: Add extension to the uploaded item according to the extension of uploaded file if it was not specified

--- uberfire-server/src/main/java/org/uberfire/server/FileUploadServlet.java
+++ uberfire-server/src/main/java/org/uberfire/server/FileUploadServlet.java
@@ -15,6 +15,13 @@ import org.uberfire.io.IOService;
 public class FileUploadServlet
         extends BaseUploadServlet {
 
+    private static final String PARAM_PATH = "path";
+    private static final String PARAM_FOLDER = "folder";
+    private static final String PARAM_FILENAME = "fileName";
+
+    private static final String RESPONSE_OK = "OK";
+    private static final String RESPONSE_FAIL = "FAIL";
+
     @Inject
     @Named("ioStrategy")
     private IOService ioService;
@@ -24,24 +31,52 @@ public class FileUploadServlet
                            HttpServletResponse response ) throws ServletException, IOException {
 
         try {
-            if ( request.getParameter( "path" ) != null ) {
-                writeFile( ioService, ioService.get( new URI( request.getParameter( "path" ) ) ), getFileItem( request ) );
-
-                writeResponse( response, "OK" );
-            } else if ( request.getParameter( "folder" ) != null ) {
+            if ( request.getParameter( PARAM_PATH ) != null ) {
                 writeFile( ioService,
-                           ioService.get( new URI( request.getParameter( "folder" ) + "/" + request.getParameter( "fileName" ) ) ),
+                           ioService.get( new URI( request.getParameter( PARAM_PATH ) ) ),
                            getFileItem( request ) );
 
-                writeResponse( response, "OK" );
+                writeResponse( response,
+                               RESPONSE_OK );
+
+            } else if ( request.getParameter( PARAM_FOLDER ) != null ) {
+
+                //See https://bugzilla.redhat.com/show_bug.cgi?id=1091204
+                //If the User-provided file name has an extension use that; otherwise use the same extension as the original (OS FileSystem) extension
+                String targetFileName;
+                final String originalFileName = getFileItem( request ).getName();
+                final String providedFileName = request.getParameter( PARAM_FILENAME );
+                if ( providedFileName.contains( "." ) ) {
+                    targetFileName = providedFileName;
+                } else {
+                    targetFileName = providedFileName + getExtension( originalFileName );
+                }
+
+                writeFile( ioService,
+                           ioService.get( new URI( request.getParameter( PARAM_FOLDER ) + "/" + targetFileName ) ),
+                           getFileItem( request ) );
+
+                writeResponse( response,
+                               RESPONSE_OK );
             }
 
         } catch ( FileUploadException e ) {
             logError( e );
-            writeResponse( response, "FAIL" );
+            writeResponse( response,
+                           RESPONSE_FAIL );
+
         } catch ( URISyntaxException e ) {
             logError( e );
-            writeResponse( response, "FAIL" );
+            writeResponse( response,
+                           RESPONSE_FAIL );
         }
     }
+
+    private String getExtension( final String originalFileName ) {
+        if ( originalFileName.contains( "." ) ) {
+            return "." + originalFileName.substring( originalFileName.lastIndexOf( "." ) + 1 );
+        }
+        return "";
+    }
+
 }
