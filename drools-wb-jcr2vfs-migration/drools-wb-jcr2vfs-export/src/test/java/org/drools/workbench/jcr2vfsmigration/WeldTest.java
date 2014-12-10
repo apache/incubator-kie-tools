788f55b41b04771c16286a609a92a2aa2c434c01
Jan Schatteman <jschatte@redhat.com>
2014-12-10 18:01:06 +0100

    Add a couple of basic weld container tests.

---
+++ drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-export/src/test/java/org/drools/workbench/jcr2vfsmigration/WeldTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.drools.workbench.jcr2vfsmigration;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WeldTest {
+
+    @Test
+    public void testWeldContainer() {
+        try {
+            Weld weld = new Weld();
+            WeldContainer weldContainer = weld.initialize();
+            JcrExporter jcrExporter = weldContainer.instance().select( JcrExporter.class ).get();
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            fail( "Error in weld container initialization; check dependencies!" );
+        }
+    }
+
+}
