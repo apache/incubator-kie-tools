246b68d148b783f95ee1fa45c3b3cacaf9e00faf
Jan Schatteman <jschatte@redhat.com>
2014-12-11 17:52:49 +0100

    Provide our own SessionInfoFactory and exclude the default one provided by Uberfire.

---
+++ drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-import/src/main/java/org/drools/workbench/jcr2vfsmigration/cdi/SessionInfoFactory.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.workbench.jcr2vfsmigration.cdi;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Singleton;
+
+import org.uberfire.rpc.SessionInfo;
+import org.uberfire.rpc.impl.SessionInfoImpl;
+
+@ApplicationScoped
+public class SessionInfoFactory {
+
+    private SessionInfo sessionInfo;
+
+    @PostConstruct
+    public void onStartup() {
+        this.sessionInfo = new SessionInfoImpl();
+    }
+
+    @Produces
+    public SessionInfo getSessionInfo() {
+        return this.sessionInfo;
+    }
+
+}
