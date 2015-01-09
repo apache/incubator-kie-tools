f44bc1e95d48de3a1e31c39beae6fc0055dfd030
Michael Anstis <michael.anstis@gmail.com>
2015-01-09 13:03:53 +0000

    BZ1180224: Upload file creates empty file in repository

--- uberfire-server/src/main/java/org/uberfire/server/FileUploadServlet.java
+++ uberfire-server/src/main/java/org/uberfire/server/FileUploadServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.uberfire.io.IOService;
 import org.uberfire.java.nio.file.Path;
@@ -41,10 +42,11 @@ public class FileUploadServlet
                 }
 
                 final Path path = ioService.get( uri );
+                final FileItem fileItem = getFileItem( request );
 
                 writeFile( ioService,
                            path,
-                           getFileItem( request ) );
+                           fileItem );
 
                 writeResponse( response,
                                RESPONSE_OK );
@@ -54,7 +56,8 @@ public class FileUploadServlet
                 //See https://bugzilla.redhat.com/show_bug.cgi?id=1091204
                 //If the User-provided file name has an extension use that; otherwise use the same extension as the original (OS FileSystem) extension
                 String targetFileName;
-                final String originalFileName = getFileItem( request ).getName();
+                final FileItem fileItem = getFileItem( request );
+                final String originalFileName = fileItem.getName();
                 final String providedFileName = request.getParameter( PARAM_FILENAME );
                 if ( providedFileName.contains( "." ) ) {
                     targetFileName = providedFileName;
@@ -64,7 +67,8 @@ public class FileUploadServlet
 
                 final URI uri = new URI( request.getParameter( PARAM_FOLDER ) + "/" + targetFileName );
 
-                if ( !validateAccess( uri, response ) ) {
+                if ( !validateAccess( uri,
+                                      response ) ) {
                     return;
                 }
 
@@ -72,7 +76,7 @@ public class FileUploadServlet
 
                 writeFile( ioService,
                            path,
-                           getFileItem( request ) );
+                           fileItem );
 
                 writeResponse( response,
                                RESPONSE_OK );
