5e49d851b11cfd634971be37c98ae6b6390c9fd2
Michael Anstis <michael.anstis@gmail.com>
2015-02-04 14:26:37 +0000

    BZ1189128: Indexing: Operation appears sporadic

--- drools-wb-webapp/src/main/java/org/drools/workbench/backend/server/LuceneConfigProducer.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/backend/server/LuceneConfigProducer.java
@@ -23,9 +23,19 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.kie.workbench.common.screens.datamodeller.model.index.terms.FieldTypeIndexTerm;
+import org.kie.workbench.common.screens.datamodeller.model.index.terms.JavaTypeIndexTerm;
+import org.kie.workbench.common.screens.datamodeller.model.index.terms.JavaTypeInterfaceIndexTerm;
+import org.kie.workbench.common.screens.datamodeller.model.index.terms.JavaTypeNameIndexTerm;
+import org.kie.workbench.common.screens.datamodeller.model.index.terms.JavaTypeParentIndexTerm;
+import org.kie.workbench.common.services.refactoring.backend.server.indexing.FullyQualifiedClassNameAnalyzer;
 import org.kie.workbench.common.services.refactoring.backend.server.indexing.RuleAttributeNameAnalyzer;
+import org.kie.workbench.common.services.refactoring.model.index.terms.PackageNameIndexTerm;
 import org.kie.workbench.common.services.refactoring.model.index.terms.ProjectRootPathIndexTerm;
+import org.kie.workbench.common.services.refactoring.model.index.terms.RuleAttributeIndexTerm;
+import org.kie.workbench.common.services.refactoring.model.index.terms.RuleAttributeValueIndexTerm;
 import org.kie.workbench.common.services.refactoring.model.index.terms.RuleIndexTerm;
+import org.kie.workbench.common.services.refactoring.model.index.terms.TypeIndexTerm;
 import org.uberfire.ext.metadata.backend.lucene.LuceneConfig;
 import org.uberfire.ext.metadata.backend.lucene.LuceneConfigBuilder;
 import org.uberfire.ext.metadata.backend.lucene.analyzer.FilenameAnalyzer;
@@ -57,8 +67,28 @@ public class LuceneConfigProducer {
         return new HashMap<String, Analyzer>() {{
             put( RuleIndexTerm.TERM,
                  new RuleAttributeNameAnalyzer( LUCENE_40 ) );
+            put( RuleAttributeIndexTerm.TERM,
+                 new RuleAttributeNameAnalyzer( LUCENE_40 ) );
+            put( RuleAttributeValueIndexTerm.TERM,
+                 new RuleAttributeNameAnalyzer( LUCENE_40 ) );
+
             put( ProjectRootPathIndexTerm.TERM,
                  new FilenameAnalyzer( LUCENE_40 ) );
+
+            put( PackageNameIndexTerm.TERM,
+                 new FullyQualifiedClassNameAnalyzer( LUCENE_40 ) );
+            put( FieldTypeIndexTerm.TERM,
+                 new FullyQualifiedClassNameAnalyzer( LUCENE_40 ) );
+            put( JavaTypeIndexTerm.TERM,
+                 new FullyQualifiedClassNameAnalyzer( LUCENE_40 ) );
+            put( JavaTypeInterfaceIndexTerm.TERM,
+                 new FullyQualifiedClassNameAnalyzer( LUCENE_40 ) );
+            put( JavaTypeNameIndexTerm.TERM,
+                 new FullyQualifiedClassNameAnalyzer( LUCENE_40 ) );
+            put( JavaTypeParentIndexTerm.TERM,
+                 new FullyQualifiedClassNameAnalyzer( LUCENE_40 ) );
+            put( TypeIndexTerm.TERM,
+                 new FullyQualifiedClassNameAnalyzer( LUCENE_40 ) );
         }};
     }
 
