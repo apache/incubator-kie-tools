7e03de35369916b33b79208196e0c7652768d11f
Michael Anstis <michael.anstis@gmail.com>
2015-02-09 11:24:55 +0000

    Wires BPMN placeholder

---
+++ uberfire-extensions/uberfire-wires/uberfire-wires-bpmn/uberfire-wires-bpmn-backend/src/main/java/org/uberfire/ext/wires/bpmn/backend/ServicePlaceHolderImpl.java
@@ -0,0 +1,11 @@
+package org.uberfire.ext.wires.bpmn.backend;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.jboss.errai.bus.server.annotations.Service;
+
+@Service
+@ApplicationScoped
+public class ServicePlaceHolderImpl {
+
+}
---
+++ uberfire-extensions/uberfire-wires/uberfire-wires-bpmn/uberfire-wires-bpmn-client/src/main/java/org/uberfire/ext/wires/bpmn/client/editor/BpmnEditorView.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.uberfire.ext.wires.bpmn.client.editor;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Widget;
+import org.uberfire.client.mvp.UberView;
+import org.uberfire.ext.editor.commons.client.BaseEditorViewImpl;
+
+@Dependent
+public class BpmnEditorView
+        extends BaseEditorViewImpl
+        implements UberView<BpmnEditorPresenter> {
+
+    interface ViewBinder
+            extends
+            UiBinder<Widget, BpmnEditorView> {
+
+    }
+
+    final private static ViewBinder uiBinder = GWT.create( ViewBinder.class );
+
+    private BpmnEditorPresenter presenter;
+
+    @PostConstruct
+    public void init() {
+        initWidget( uiBinder.createAndBindUi( this ) );
+    }
+
+    @Override
+    public void init( final BpmnEditorPresenter presenter ) {
+        this.presenter = presenter;
+    }
+
+}
