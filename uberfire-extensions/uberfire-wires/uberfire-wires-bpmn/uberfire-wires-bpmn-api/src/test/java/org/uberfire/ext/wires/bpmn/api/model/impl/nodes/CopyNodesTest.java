6d3669a56b07e7bde99711a059a84c08502a7cb6
Michael Anstis <michael.anstis@gmail.com>
2015-02-17 16:51:41 +0000

    Wires BPMN placeholder. More tests for copying GraphNodes.
    (cherry picked from commit d8a7adddcf326dd7ea1aedab57bc52923f50ce19)

--- uberfire-extensions/uberfire-wires/uberfire-wires-bpmn/uberfire-wires-bpmn-api/src/test/java/org/uberfire/ext/wires/bpmn/api/model/impl/nodes/CopyNodesTest.java
+++ uberfire-extensions/uberfire-wires/uberfire-wires-bpmn/uberfire-wires-bpmn-api/src/test/java/org/uberfire/ext/wires/bpmn/api/model/impl/nodes/CopyNodesTest.java
@@ -16,8 +16,11 @@
 package org.uberfire.ext.wires.bpmn.api.model.impl.nodes;
 
 import org.junit.Test;
+import org.uberfire.ext.wires.bpmn.api.model.BpmnGraph;
+import org.uberfire.ext.wires.bpmn.api.model.BpmnGraphNode;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.jgroups.util.Util.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
@@ -62,7 +65,7 @@ public class CopyNodesTest {
     }
 
     @Test
-    public void testCopyProcessNode() {
+    public void testCopyProcessNode1() {
         final ProcessNode node = new ProcessNode();
         final ProcessNode copy = node.copy();
 
@@ -78,6 +81,55 @@ public class CopyNodesTest {
                       copy.getContent().getRoles() );
         assertEquals( node.getContent().getProperties(),
                       copy.getContent().getProperties() );
+    }
+
+    @Test
+    public void testCopyProcessNode2() {
+        final ProcessNode process = new ProcessNode();
+        final StartProcessNode startProcessNode = new StartProcessNode();
+        final EndProcessNode endProcessNode = new EndProcessNode();
+        process.addNode( startProcessNode );
+        process.addNode( endProcessNode );
+
+        final ProcessNode copyProcess = process.copy();
+
+        assertNotNull( copyProcess );
+        assertFalse( process == copyProcess );
+        assertEquals( process.getContent().getId(),
+                      copyProcess.getContent().getId() );
+        assertEquals( process.getContent().getTitle(),
+                      copyProcess.getContent().getTitle() );
+        assertEquals( process.getContent().getDescription(),
+                      copyProcess.getContent().getDescription() );
+        assertEquals( process.getContent().getRoles(),
+                      copyProcess.getContent().getRoles() );
+        assertEquals( process.getContent().getProperties(),
+                      copyProcess.getContent().getProperties() );
+
+        assertEquals( process.size(),
+                      copyProcess.size() );
+
+        final BpmnGraphNode copyStartProcessNode = getNode( copyProcess,
+                                                            StartProcessNode.class );
+        assertNotNull( copyStartProcessNode );
+        assertTrue( copyStartProcessNode instanceof StartProcessNode );
+        assertFalse( startProcessNode == copyStartProcessNode );
+
+        final BpmnGraphNode copyEndProcessNode = getNode( copyProcess,
+                                                          EndProcessNode.class );
+        assertNotNull( copyEndProcessNode );
+        assertTrue( copyEndProcessNode instanceof EndProcessNode );
+        assertFalse( endProcessNode == copyEndProcessNode );
+    }
+
+    private BpmnGraphNode getNode( final BpmnGraph process,
+                                   final Class clazz ) {
+        for ( BpmnGraphNode node : process ) {
+            if ( node.getClass().equals( clazz ) ) {
+                return node;
+            }
+        }
+        return null;
     }
 
 }
