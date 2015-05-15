12494a6d842128c592a35fae872007244c3153a4
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2015-05-15 14:21:42 -0400

    BZ-1221380: remove use of cookies (that have 4k limit) what, depending of the number of roles/groups of a user, can be reached. new solution `patches` the host page (UserHostPageFilter) with user's info that now is supported on errai client security.

--- uberfire-extensions/uberfire-security/uberfire-servlet-security/src/main/java/org/uberfire/ext/security/server/SecurityIntegrationFilter.java
+++ uberfire-extensions/uberfire-security/uberfire-servlet-security/src/main/java/org/uberfire/ext/security/server/SecurityIntegrationFilter.java
@@ -3,29 +3,17 @@ package org.uberfire.ext.security.server;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
-import javax.inject.Inject;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.jboss.errai.marshalling.server.MappingContextSingleton;
-import org.jboss.errai.security.shared.api.UserCookieEncoder;
-import org.jboss.errai.security.shared.api.identity.User;
-import org.jboss.errai.security.shared.service.AuthenticationService;
 
-/**
- * TODO: update me
- */
 public class SecurityIntegrationFilter implements Filter {
-
-    @Inject
-    private AuthenticationService authenticationService;
 
     public static final String PROBE_ROLES_INIT_PARAM = "probe-for-roles";
 
@@ -53,13 +41,6 @@ public class SecurityIntegrationFilter implements Filter {
                           ServletResponse response,
                           FilterChain chain ) throws IOException, ServletException {
         requests.set( (HttpServletRequest) request );
-        final User user = authenticationService.getUser();
-        if ( user != null ) {
-            final Cookie erraiUserCacheCookie = new Cookie(
-                    UserCookieEncoder.USER_COOKIE_NAME,
-                    UserCookieEncoder.toCookieValue( user ) );
-            ( (HttpServletResponse) response ).addCookie( erraiUserCacheCookie );
-        }
         try {
             chain.doFilter( request, response );
         } finally {
