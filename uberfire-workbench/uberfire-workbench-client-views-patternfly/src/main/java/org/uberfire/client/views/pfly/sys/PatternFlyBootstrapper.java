bfda0b71af47a78df1894e5f27d8a7f0b9294f0f
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2015-07-24 10:29:21 +1000

    Add Prettify library to ClientBundle

--- uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/sys/PatternFlyBootstrapper.java
+++ uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/sys/PatternFlyBootstrapper.java
@@ -1,8 +1,7 @@
 package org.uberfire.client.views.pfly.sys;
 
-import org.gwtbootstrap3.client.GwtBootstrap3ClientBundle;
-
 import com.google.gwt.core.client.ScriptInjector;
+import org.gwtbootstrap3.client.GwtBootstrap3ClientBundle;
 
 /**
  * Utilities for ensuring the PatternFly/BS3 system is working early enough that the app can start correctly.
@@ -14,21 +13,35 @@ public class PatternFlyBootstrapper {
      * GWTBootstrap 3 widgets should call this before creating their first such widget.
      */
     public static void ensurejQueryIsAvailable() {
-        if (!isjQueryLoaded()) {
-            ScriptInjector.fromString(GwtBootstrap3ClientBundle.INSTANCE.jQuery().getText())
-                    .setWindow(ScriptInjector.TOP_WINDOW)
+        if ( !isjQueryLoaded() ) {
+            ScriptInjector.fromString( GwtBootstrap3ClientBundle.INSTANCE.jQuery().getText() )
+                    .setWindow( ScriptInjector.TOP_WINDOW )
+                    .inject();
+        }
+    }
+
+    public static void ensurePrettifyIsAvailable() {
+        if ( !isPrettifyLoaded() ) {
+            ScriptInjector.fromString( PatternFlyClientBundle.INSTANCE.prettify().getText() )
+                    .setWindow( ScriptInjector.TOP_WINDOW )
                     .inject();
         }
     }
 
     /**
      * Checks to see if jQuery is already present.
-     *
      * @return true is jQuery is loaded, false otherwise.
      */
     private static native boolean isjQueryLoaded() /*-{
         return (typeof $wnd['jQuery'] !== 'undefined');
     }-*/;
 
+    /**
+     * Checks to see if Prettify is already present.
+     * @return true is Prettify is loaded, false otherwise.
+     */
+    private static native boolean isPrettifyLoaded() /*-{
+        return (typeof $wnd['prettyPrint'] !== 'undefined');
+    }-*/;
 }
 
