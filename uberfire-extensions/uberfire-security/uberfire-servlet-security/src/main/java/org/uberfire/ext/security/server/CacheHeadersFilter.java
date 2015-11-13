265a3a007de72a3de85772e806184993a28ec0f6
Christian Sadilek <christian.sadilek@gmail.com>
2015-11-13 14:37:29 -0500

    Enhance CacheHeadersFilter to allow for caching of .cache.* files

--- uberfire-extensions/uberfire-security/uberfire-servlet-security/src/main/java/org/uberfire/ext/security/server/CacheHeadersFilter.java
+++ uberfire-extensions/uberfire-security/uberfire-servlet-security/src/main/java/org/uberfire/ext/security/server/CacheHeadersFilter.java
@@ -17,15 +17,22 @@
 package org.uberfire.ext.security.server;
 
 import java.io.IOException;
+import java.util.Calendar;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CacheHeadersFilter implements Filter {
+    static final String EXPIRES_HEADER = "Expires";
+    static final String CACHE_CONTROL_HEADER = "Cache-Control";
+    static final String PRAGMA_HEADER = "Pragma";
+    static final int YEAR_IN_SECONDS = 365 * 24 * 60 * 60;
 
     @Override
     public void init( final FilterConfig filterConfig ) throws ServletException {
@@ -39,9 +46,23 @@ public class CacheHeadersFilter implements Filter {
     public void doFilter( final ServletRequest request,
                           final ServletResponse response,
                           final FilterChain chain ) throws IOException, ServletException {
-        ( (HttpServletResponse) response ).setHeader( "Cache-Control", "no-cache, no-store, must-revalidate" ); // HTTP 1.1.
-        ( (HttpServletResponse) response ).setHeader( "Pragma", "no-cache" ); // HTTP 1.0.
-        ( (HttpServletResponse) response ).setDateHeader( "Expires", 0 ); // Proxies.
+
+        final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        final String requestURI = httpServletRequest.getRequestURI();
+        
+        if ( requestURI.contains( ".cache." ) ) {
+            final Calendar calendar = Calendar.getInstance();
+            calendar.add( Calendar.SECOND, YEAR_IN_SECONDS );
+
+            httpServletResponse.setHeader( CACHE_CONTROL_HEADER, "max-age=" + YEAR_IN_SECONDS + ", must-revalidate" );
+            httpServletResponse.setDateHeader( EXPIRES_HEADER, calendar.getTime().getTime() );
+        } 
+        else if ( requestURI.endsWith( ".nocache.js" ) || ( requestURI.endsWith( ".html" )) ) {
+            httpServletResponse.setHeader( CACHE_CONTROL_HEADER, "no-cache, no-store, must-revalidate" );
+            httpServletResponse.setHeader( PRAGMA_HEADER, "no-cache" );
+            httpServletResponse.setDateHeader( EXPIRES_HEADER, 0 );
+        }
 
         chain.doFilter( request, response );
     }
