19b13cacbe8f55d2c81947584715fbdc0b8474c3
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2016-01-20 15:49:30 +1000

    JBPM-4847 - Fix popover not hiding after row selection
     Review implementation so showing and hiding popover is tied to GWT events.
     Clean up visible popovers once re-rendering cells.

--- uberfire-extensions/uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/tables/PopoverTextCell.java
+++ uberfire-extensions/uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/tables/PopoverTextCell.java
@@ -16,14 +16,21 @@
 
 package org.uberfire.ext.widgets.common.client.tables;
 
+import com.google.common.base.Strings;
 import com.google.gwt.cell.client.AbstractSafeHtmlCell;
+import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.text.shared.SimpleSafeHtmlRenderer;
 import com.google.gwt.user.client.DOM;
+
+import static com.google.gwt.dom.client.BrowserEvents.*;
+
 
 /**
  * An extension to the normal TextCell that renders a Bootstrap Popover when text overflows.
@@ -42,8 +49,8 @@ public class PopoverTextCell extends AbstractSafeHtmlCell<String> {
 
     private Placement placement;
 
-    public PopoverTextCell(final Placement placement ) {
-        super( SimpleSafeHtmlRenderer.getInstance() );
+    public PopoverTextCell( final Placement placement ) {
+        super( SimpleSafeHtmlRenderer.getInstance(), MOUSEOVER, MOUSEOUT );
         this.placement = placement;
     }
 
@@ -52,34 +59,74 @@ public class PopoverTextCell extends AbstractSafeHtmlCell<String> {
     }
 
     @Override
-    protected void render(Context context, SafeHtml data, SafeHtmlBuilder sb) {
+    protected void render( Context context, SafeHtml data, SafeHtmlBuilder sb ) {
+        hideAllPopover();
+        final String content = data.asString();
+        if ( Strings.isNullOrEmpty( content ) ) {
+            return;
+        }
+
         final Element div = DOM.createDiv();
         div.setId( DOM.createUniqueId() );
-        div.setInnerHTML( data.asString() );
+        div.setInnerHTML( content );
         div.getStyle().setOverflow( Style.Overflow.HIDDEN );
         div.getStyle().setTextOverflow( Style.TextOverflow.ELLIPSIS );
         div.getStyle().setWhiteSpace( Style.WhiteSpace.NOWRAP );
         final String html = div.getString();
-        sb.appendHtmlConstant(html);
+        sb.appendHtmlConstant( html );
 
-        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+        Scheduler.get().scheduleDeferred( new Scheduler.ScheduledCommand() {
             @Override
             public void execute() {
                 initPopover( div.getId(), placement.name().toLowerCase() );
             }
-        });
+        } );
     }
+
+    @Override
+    public void onBrowserEvent( final Context context, final Element parent, final String value, final NativeEvent event, final ValueUpdater<String> valueUpdater ) {
+        super.onBrowserEvent( context, parent, value, event, valueUpdater );
+
+        final Element element = Element.as( event.getEventTarget() );
+
+        if ( DivElement.is( element ) == false ) {
+            return;
+        }
+
+        if ( MOUSEOVER.equals( event.getType() ) ) {
+            Scheduler.get().scheduleDeferred( new Scheduler.ScheduledCommand() {
+                @Override
+                public void execute() {
+                    showPopover( parent.getFirstChildElement().getId() );
+                }
+            } );
+        } else if ( MOUSEOUT.equals( event.getType() ) ) {
+            hidePopover( parent.getFirstChildElement().getId() );
+        }
+    }
+
+    private native void hideAllPopover() /*-{
+        $wnd.jQuery('.popover').popover('hide');
+    }-*/;
+
+    private native void hidePopover( String id ) /*-{
+        $wnd.jQuery('#' + id).popover('hide');
+    }-*/;
+
+    private native void showPopover( String id ) /*-{
+        $wnd.jQuery('#' + id).popover('show');
+    }-*/;
 
     private native void initPopover( String id, String placement ) /*-{
         var jQueryId = '#' + id;
-        var div = $wnd.jQuery( jQueryId );
+        var div = $wnd.jQuery(jQueryId);
 
         div.popover({
-            trigger: 'hover',
+            trigger: 'manual',
             placement: placement,
-            content: function (){
-                var offsetWidth = $wnd.document.getElementById( id ).offsetWidth;
-                var scrollWidth = $wnd.document.getElementById( id ).scrollWidth;
+            content: function () {
+                var offsetWidth = $wnd.document.getElementById(id).offsetWidth;
+                var scrollWidth = $wnd.document.getElementById(id).scrollWidth;
                 return offsetWidth < scrollWidth ? div.html() : "";
             },
             container: 'body'
