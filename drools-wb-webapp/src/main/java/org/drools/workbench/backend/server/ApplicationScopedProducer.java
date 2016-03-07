df0eead151bd455dc569cae59b94e2c0d6037b8f
Michael Anstis <michael.anstis@gmail.com>
2016-03-07 13:03:33 +0000

    BZ1310893: BxMS6.2.0 Full Text search returns only 10 results even if it hits more than 10

--- drools-wb-webapp/src/main/java/org/drools/workbench/backend/server/ApplicationScopedProducer.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/backend/server/ApplicationScopedProducer.java
@@ -32,9 +32,9 @@ import org.uberfire.commons.cluster.ClusterServiceFactory;
 import org.uberfire.commons.services.cdi.Startup;
 import org.uberfire.commons.services.cdi.StartupType;
 import org.uberfire.ext.metadata.backend.lucene.LuceneConfig;
-import org.uberfire.ext.metadata.io.IOSearchIndex;
+import org.uberfire.ext.metadata.io.IOSearchServiceImpl;
 import org.uberfire.ext.metadata.io.IOServiceIndexedImpl;
-import org.uberfire.io.IOSearchService;
+import org.uberfire.ext.metadata.search.IOSearchService;
 import org.uberfire.io.IOService;
 import org.uberfire.io.attribute.DublinCoreView;
 import org.uberfire.io.impl.cluster.IOServiceClusterImpl;
@@ -82,8 +82,8 @@ public class ApplicationScopedProducer {
             ioService = new IOServiceClusterImpl( service,
                                                   clusterServiceFactory );
         }
-        ioSearchService = new IOSearchIndex( config.getSearchIndex(),
-                                             ioService );
+        ioSearchService = new IOSearchServiceImpl( config.getSearchIndex(),
+                                                   ioService );
     }
 
     @Produces
