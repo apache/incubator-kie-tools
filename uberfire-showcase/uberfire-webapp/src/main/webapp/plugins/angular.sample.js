f5caf5f94a1fb75a8c5416b1c75fdc14cb3f7b3a
Eder Ignatowicz <ignatowicz@gmail.com>
2016-04-12 09:36:02 -0300

    upgrading angular screen to work in  1.5.0 (#348)

--- uberfire-showcase/uberfire-webapp/src/main/webapp/plugins/angular.sample.js
+++ uberfire-showcase/uberfire-webapp/src/main/webapp/plugins/angular.sample.js
@@ -13,44 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-function TodoCtrl($scope) {
-
-    $scope.placeText = "MiscellaneousFeatures";
-
-    $scope.todos = [
-        {text: 'learn angular', done: true},
-        {text: 'build an angular app', done: false}
-    ];
-
-    $scope.addTodo = function () {
-        $scope.todos.push({text: $scope.todoText, done: false});
-        $scope.todoText = '';
-    };
-
-    $scope.remaining = function () {
-        var count = 0;
-        angular.forEach($scope.todos, function (todo) {
-            count += todo.done ? 0 : 1;
-        });
-        return count;
-    };
-
-    $scope.archive = function () {
-        var oldTodos = $scope.todos;
-        $scope.todos = [];
-        angular.forEach(oldTodos, function (todo) {
-            if (!todo.done) {
-                $scope.todos.push(todo);
-            }
-        });
-    };
-
-    $scope["goto"] = function () {
-        $goToPlace($scope.placeText);
-    };
-}
-
 $registerPlugin({
     id: "my_angular_js",
     type: "angularjs",
@@ -62,3 +24,44 @@ $registerPlugin({
         alert("this is a pure JS alert!");
     }
 });
+
+(function(angular) {
+    'use strict';
+    var myApp = angular.module('my_app', []);
+
+    myApp.controller('TodoCtrl', ['$scope', function($scope) {
+        $scope.placeText = "MiscellaneousFeatures";
+
+        $scope.todos = [
+            {text: 'learn angular', done: true},
+            {text: 'build an angular app', done: false}
+        ];
+
+        $scope.addTodo = function () {
+            $scope.todos.push({text: $scope.todoText, done: false});
+            $scope.todoText = '';
+        };
+
+        $scope.remaining = function () {
+            var count = 0;
+            angular.forEach($scope.todos, function (todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        $scope.archive = function () {
+            var oldTodos = $scope.todos;
+            $scope.todos = [];
+            angular.forEach(oldTodos, function (todo) {
+                if (!todo.done) {
+                    $scope.todos.push(todo);
+                }
+            });
+        };
+
+        $scope["goto"] = function () {
+            $goToPlace($scope.placeText);
+        };
+    }]);
+})(window.angular);
