b163e81b380641884ab79bfc4b1b15add8914104
Max Barkley <mbarkley@redhat.com>
2016-04-25 10:16:09 -0400

    Move CheckboxCellImpl to uberfire-widgets-table. (#274)

--- uberfire-extensions/uberfire-widgets/uberfire-widgets-table/src/main/java/org/uberfire/ext/widgets/table/client/CheckboxCellImpl.java
+++ uberfire-extensions/uberfire-widgets/uberfire-widgets-table/src/main/java/org/uberfire/ext/widgets/table/client/CheckboxCellImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.uberfire.ext.widgets.common.client.common;
+package org.uberfire.ext.widgets.table.client;
 
 import com.google.gwt.cell.client.AbstractEditableCell;
 import com.google.gwt.cell.client.Cell;
