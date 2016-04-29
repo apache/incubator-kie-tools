c0418a0aa3a1b84e4a7448a0979c971f6f768206
deanjones <deanjones@gmail.com>
2016-04-29 09:54:30 -0400

    1.0.150-RELEASE

--- src/main/java/com/ait/lienzo/client/core/shape/wires/LayoutContainer.java
+++ src/main/java/com/ait/lienzo/client/core/shape/wires/LayoutContainer.java
@@ -1,10 +1,26 @@
+/*
+   Copyright (c) 2014,2015,2016 Ahome' Innovation Technologies. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package com.ait.lienzo.client.core.shape.wires;
 
 import com.ait.lienzo.client.core.shape.Group;
 import com.ait.lienzo.client.core.shape.IPrimitive;
 
-public interface LayoutContainer {
-
+public interface LayoutContainer
+{
     enum Layout
     {
         CENTER, LEFT, TOP, RIGHT, BOTTOM;
@@ -19,7 +35,7 @@ public interface LayoutContainer {
     LayoutContainer setWidth(double width);
 
     LayoutContainer add(IPrimitive<?> child);
-    
+
     LayoutContainer add(IPrimitive<?> child, Layout layout);
 
     LayoutContainer add(IPrimitive<?> child, Layout layout, double x, double y);
@@ -29,6 +45,6 @@ public interface LayoutContainer {
     LayoutContainer remove(IPrimitive<?> child);
 
     LayoutContainer clear();
-    
+
     Group getGroup();
 }
--- src/main/java/com/ait/lienzo/client/core/shape/wires/PickerPart.java
+++ src/main/java/com/ait/lienzo/client/core/shape/wires/PickerPart.java
@@ -1,11 +1,26 @@
-package com.ait.lienzo.client.core.shape.wires;
+/*
+   Copyright (c) 2014,2015,2016 Ahome' Innovation Technologies. All rights reserved.
 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ait.lienzo.client.core.shape.wires;
 
 public class PickerPart
 {
     private final WiresShape shape;
 
-    private final ShapePart part;
+    private final ShapePart  part;
 
     public WiresShape getShape()
     {
@@ -17,7 +32,8 @@ public class PickerPart
         return part;
     }
 
-    public enum ShapePart {
+    public enum ShapePart
+    {
         BORDER, BORDER_HOTSPOT, BODY
     }
 
@@ -27,7 +43,8 @@ public class PickerPart
         this.part = part;
     }
 
-    @Override public String toString()
+    @Override
+    public String toString()
     {
         return this.part.toString() + " for " + this.shape.toString();
     }
