d9898cb9fd211eb0f912e1786c52a380978e23b4
Petr Široký <siroky.petr@gmail.com>
2016-05-13 16:48:13 +0200

    uberfire-parent directly inherits from ip-bom for easier dependency overrides (#370)
    * now we can easily override the dependency version just via property
       (since uberfire-parent extends ip-bom instead of
       importing it in depMgmt)
    
     * user BOM 'uberfire-bom' can not extend the uberfire-parent
       as that would mean polluting it with hundreds of different
       versions coming from the ip-bom -- which is not desirable
       for user BOMs
    
     * uberfire-parent-metadata was renamed to just uberfire-parent.
       uberfire-parent-with-dependencies was removed completely
       as it is no longer required. All UF modules should (transitively)
       use the uberfire-parent (except for the user BOM(s))

--- scripts/release/update-version.sh
+++ scripts/release/update-version.sh
@@ -20,6 +20,11 @@ initializeWorkingDirAndScriptDir() {
     scriptDir=`pwd -P`
 }
 
+mvnVersionsSet() {
+    newVersion=$1
+    mvn -B -N -Dfull versions:set -DnewVersion=$newVersion -DallowSnapshots=true -DgenerateBackupPoms=false
+}
+
 initializeWorkingDirAndScriptDir
 uberfireTopLevelDir="$scriptDir/../.."
 
@@ -38,7 +43,11 @@ newVersion=$1
 startDateTime=`date +%s`
 
 cd $uberfireTopLevelDir
-mvn -B -N -Dfull versions:set -DnewVersion=$newVersion -DallowSnapshots=true -DgenerateBackupPoms=false
+mvnVersionsSet $newVersion
+
+# fix leftovers not covered by the first run
+cd $uberfireTopLevelDir/uberfire-bom
+mvnVersionsSet $newVersion
 
 endDateTime=`date +%s`
 spentSeconds=`expr $endDateTime - $startDateTime`
