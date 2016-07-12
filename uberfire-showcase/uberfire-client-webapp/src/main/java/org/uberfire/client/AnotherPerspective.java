3cc6933dcb1b791c479d37d5a3b9d5d70587b367
Christian Sadilek <christian.sadilek@gmail.com>
2016-07-12 14:05:36 -0400

    UF-359: Client-only showcase/demo does not compile and is outdated (#438)

--- uberfire-showcase/uberfire-client-webapp/src/main/java/org/uberfire/client/AnotherPerspective.java
+++ uberfire-showcase/uberfire-client-webapp/src/main/java/org/uberfire/client/AnotherPerspective.java
@@ -16,8 +16,6 @@
 
 package org.uberfire.client;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 import org.uberfire.client.annotations.WorkbenchPanel;
@@ -26,7 +24,6 @@ import org.uberfire.client.workbench.panels.impl.MultiTabWorkbenchPanelPresenter
 
 import com.google.gwt.user.client.ui.Composite;
 
-@ApplicationScoped
 @WorkbenchPerspective(identifier = "AnotherPerspective")
 @Templated("another_template.html")
 public class AnotherPerspective extends Composite {
