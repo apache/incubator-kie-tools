97da67c53b6a8d50464839f08f35073df4ada9ae
Toni Rikkola <toni@rikkola.net>
2016-08-02 11:32:52 +0300

    Removing redundancy. (#258)

--- drools-wb-webapp/src/main/java/org/drools/workbench/client/perspectives/AuthoringPerspective.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/client/perspectives/AuthoringPerspective.java
@@ -21,7 +21,6 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import org.drools.workbench.client.docks.AuthoringWorkbenchDocks;
 import org.drools.workbench.client.resources.i18n.AppConstants;
 import org.guvnor.inbox.client.InboxPresenter;
 import org.kie.workbench.common.screens.examples.client.wizard.ExamplesWizard;
@@ -31,6 +30,7 @@ import org.kie.workbench.common.services.shared.preferences.ApplicationPreferenc
 import org.kie.workbench.common.widgets.client.handlers.NewResourcePresenter;
 import org.kie.workbench.common.widgets.client.handlers.NewResourcesMenu;
 import org.kie.workbench.common.widgets.client.menu.RepositoryMenu;
+import org.kie.workbench.common.workbench.client.docks.AuthoringWorkbenchDocks;
 import org.uberfire.client.annotations.Perspective;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPerspective;
