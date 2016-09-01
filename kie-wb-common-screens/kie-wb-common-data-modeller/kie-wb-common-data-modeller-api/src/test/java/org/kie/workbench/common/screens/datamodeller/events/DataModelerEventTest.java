048d923231156c1b4983c8d6d11bba459c0a1f00
Matej Čimbora <mcimbora@redhat.com>
2016-09-01 17:47:32 +0200

    Add test for DataModelerEvent creation (#433)

---
+++ kie-wb-common-screens/kie-wb-common-data-modeller/kie-wb-common-data-modeller-api/src/test/java/org/kie/workbench/common/screens/datamodeller/events/DataModelerEventTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.screens.datamodeller.events;
+
+import org.guvnor.common.services.project.model.Project;
+import org.junit.Test;
+import org.kie.workbench.common.services.datamodeller.core.DataObject;
+import org.kie.workbench.common.services.datamodeller.core.ObjectProperty;
+import org.kie.workbench.common.services.datamodeller.core.impl.DataObjectImpl;
+import org.kie.workbench.common.services.datamodeller.core.impl.ObjectPropertyImpl;
+import org.uberfire.backend.vfs.Path;
+import org.uberfire.backend.vfs.PathFactory;
+
+import static org.junit.Assert.assertEquals;
+
+public class DataModelerEventTest {
+
+    @Test
+    public void createEvent() {
+        DataObject currentDataObject = new DataObjectImpl();
+        ObjectProperty currentField = new ObjectPropertyImpl();
+        Project currentProject = new Project();
+        String source = "testSource";
+        String contextId = "testContextId";
+        Path path = new PathFactory.PathImpl();
+
+
+        DataModelerEvent event = new DataModelerEvent()
+                .withCurrentDataObject( currentDataObject )
+                .withCurrentField( currentField )
+                .withCurrentProject( currentProject )
+                .withSource( source )
+                .withContextId( contextId )
+                .withPath( path );
+
+        assertEquals( currentDataObject, event.getCurrentDataObject() );
+        assertEquals( currentField, event.getCurrentField() );
+        assertEquals( currentProject, event.getCurrentProject() );
+        assertEquals( source, event.getSource() );
+        assertEquals( contextId, event.getContextId() );
+        assertEquals( path, event.getPath() );
+    }
+
+}
