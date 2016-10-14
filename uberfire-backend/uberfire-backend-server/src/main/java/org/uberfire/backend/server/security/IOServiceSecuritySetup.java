a412e55e9492fedc35e530c01607e65a9573aff4
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2016-10-14 15:14:47 -0400

    UF-418: log login fail (#531)

--- uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/security/IOServiceSecuritySetup.java
+++ uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/security/IOServiceSecuritySetup.java
@@ -85,6 +85,7 @@ public class IOServiceSecuritySetup {
                             return null;
 
                         } catch ( final AuthenticationException loginFailed ) {
+                            LOG.warn( "Login failed", loginFailed );
                             return null;
                         }
                     }
