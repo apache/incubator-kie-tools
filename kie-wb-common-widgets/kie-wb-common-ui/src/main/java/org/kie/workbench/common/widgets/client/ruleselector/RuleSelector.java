d01a4d38087fa61fcb157ba46eb9a9d80ddd2a5f
Michael Anstis <manstis@users.noreply.github.com>
2017-05-24 19:28:30 +0100

    GUVNOR-3108: RuleSelector: Refactor to use Bootstraps Select widget (#898)

--- kie-wb-common-widgets/kie-wb-common-ui/src/main/java/org/kie/workbench/common/widgets/client/ruleselector/RuleSelector.java
+++ kie-wb-common-widgets/kie-wb-common-ui/src/main/java/org/kie/workbench/common/widgets/client/ruleselector/RuleSelector.java
@@ -15,104 +15,128 @@
 
 package org.kie.workbench.common.widgets.client.ruleselector;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasEnabled;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.InlineLabel;
+import org.gwtbootstrap3.client.ui.constants.Styles;
+import org.gwtbootstrap3.extras.select.client.ui.Option;
+import org.gwtbootstrap3.extras.select.client.ui.Select;
 import org.kie.workbench.common.widgets.client.resources.i18n.CommonConstants;
+import org.uberfire.mvp.Command;
 
 public class RuleSelector
         extends Composite
         implements HasValueChangeHandlers<String>,
                    HasEnabled {
 
-    private final HorizontalPanel panel = new HorizontalPanel();
-    private final InlineLabel ruleNamePanel = new InlineLabel();
-    private final RuleSelectorDropdown ruleSelectorDropdown = GWT.create( RuleSelectorDropdown.class );
+    public final static String NONE_SELECTED = CommonConstants.INSTANCE.LineNoneLine();
 
-    private final static String NONE_SELECTED = CommonConstants.INSTANCE.NoneSelected();
+    // It's possible for the rule name to be set before the asynchronous call to retrieve all available rule names
+    // has completed and the selector's list of rule names set. Therefore, if the rule name is set before the complete
+    // list, record a pending "set" and invoke after the list has been set.
+    private boolean ruleNamesLoaded = false;
+    private Optional<Command> onRulesLoadedCommand = Optional.empty();
+
+    private Select ruleNameSelector = GWT.create(Select.class);
 
     public RuleSelector() {
-        ruleNamePanel.setText( NONE_SELECTED );
+        ruleNameSelector.setEnabled(false);
+        ruleNameSelector.setLiveSearch(true);
+        ruleNameSelector.setLiveSearchPlaceholder(CommonConstants.INSTANCE.Choose());
+        //The form-control CSS is added by the SelectBase constructor but interferes with PatternFly's CSS.. so removing!
+        ruleNameSelector.removeStyleName(Styles.FORM_CONTROL);
 
-        ruleSelectorDropdown.addValueChangeHandler( new ValueChangeHandler<String>() {
-            @Override
-            public void onValueChange( ValueChangeEvent<String> event ) {
-                String ruleName = event.getValue();
-                if ( ruleName.isEmpty() ) {
-                    ruleNamePanel.setText( NONE_SELECTED );
-                } else {
-                    ruleNamePanel.setText( ruleName );
-                }
+        ruleNameSelector.add(makeRuleNameOption(NONE_SELECTED,
+                                                ""));
 
-                ValueChangeEvent.fire( RuleSelector.this,
-                                       ruleName );
-            }
-        } );
-
-        panel.add( ruleNamePanel );
-        panel.add( ruleSelectorDropdown );
-
-        initWidget( panel );
-        getElement().setAttribute( "data-uf-lock", "true" );
+        initWidget(ruleNameSelector);
     }
 
-    public void setRuleNames( final Collection<String> ruleNames,
-                              final String exclude ) {
-        ruleSelectorDropdown.setRuleNames( exclude( ruleNames, exclude ) );
+    public void setRuleNames(final Collection<String> ruleNames,
+                             final String exclude) {
+        setRuleNames(exclude(ruleNames,
+                             exclude));
     }
 
-    public void setRuleNames( final Collection<String> ruleNames ) {
-        ruleSelectorDropdown.setRuleNames( ruleNames );
-    }
-
-    private static Collection<String> exclude( final Collection<String> ruleNames,
-                                               final String exclude ) {
-        final Collection<String> result = new ArrayList<String>();
-        for ( String ruleName : ruleNames ) {
-            if ( !ruleName.equals( exclude ) ) {
-                result.add( ruleName );
-            }
+    public void setRuleNames(final Collection<String> ruleNames) {
+        if (!(ruleNames == null || ruleNames.isEmpty())) {
+            ruleNamesLoaded = true;
+            ruleNameSelector.setEnabled(true);
+            ruleNames.forEach((r) -> ruleNameSelector.add(makeRuleNameOption(getSimpleRuleName(r))));
+            onRulesLoadedCommand.ifPresent(Command::execute);
+        } else {
+            ruleNameSelector.setEnabled(false);
         }
-        return result;
+        ruleNameSelector.refresh();
+    }
+
+    Option makeRuleNameOption(final String ruleName) {
+        return makeRuleNameOption(ruleName,
+                                  ruleName);
+    }
+
+    Option makeRuleNameOption(final String ruleName,
+                              final String value) {
+        final Option o = GWT.create(Option.class);
+        final String simpleRuleName = getSimpleRuleName(ruleName);
+        o.setText(simpleRuleName);
+        o.setValue(value);
+        return o;
+    }
+
+    String getSimpleRuleName(final String ruleName) {
+        if (ruleName.contains(".")) {
+            return ruleName.substring(ruleName.lastIndexOf(".") + 1);
+        }
+        return ruleName;
+    }
+
+    private Collection<String> exclude(final Collection<String> ruleNames,
+                                       final String exclude) {
+        return ruleNames
+                .stream()
+                .map(this::getSimpleRuleName)
+                .filter((r) -> !r.equals(exclude)).collect(Collectors.toList());
     }
 
     public String getRuleName() {
-        if ( ruleNamePanel.getText() != null && !ruleNamePanel.getText().equals( NONE_SELECTED ) ) {
-            return "";
+        final String ruleName = ruleNameSelector.getValue();
+        if (!(ruleName == null || ruleName.equals(NONE_SELECTED))) {
+            return ruleName;
         } else {
-            return ruleNamePanel.getText();
+            return "";
         }
     }
 
-    public void setRuleName( final String ruleName ) {
-        if ( ruleName != null && !ruleName.isEmpty() ) {
-            this.ruleNamePanel.setText( ruleName );
+    public void setRuleName(final String ruleName) {
+        if (ruleName != null && !ruleName.isEmpty()) {
+            if (ruleNamesLoaded) {
+                ruleNameSelector.setValue(ruleName);
+            } else {
+                onRulesLoadedCommand = Optional.of(() -> setRuleName(ruleName));
+            }
         }
     }
 
     @Override
-    public HandlerRegistration addValueChangeHandler( final ValueChangeHandler<String> handler ) {
-        return addHandler( handler,
-                           ValueChangeEvent.getType() );
+    public HandlerRegistration addValueChangeHandler(final ValueChangeHandler<String> handler) {
+        return ruleNameSelector.addValueChangeHandler(handler);
     }
 
     @Override
     public boolean isEnabled() {
-        return ruleSelectorDropdown.isEnabled();
+        return ruleNameSelector.isEnabled();
     }
 
     @Override
-    public void setEnabled( final boolean enabled ) {
-        ruleSelectorDropdown.setEnabled( enabled );
-
+    public void setEnabled(final boolean enabled) {
+        ruleNameSelector.setEnabled(enabled);
     }
 }
