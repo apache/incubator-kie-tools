26a83f133c19696f43b474974b6e1281812831d3
Michael Anstis <manstis@users.noreply.github.com>
2017-09-19 12:26:43 +0100

    GUVNOR-3483: [Guided Decision Tree]: Unable to add field in the update node (#609)
    * GUVNOR-3483: [Guided Decision Tree]: Unable to add field in the update node
    
    * GUVNOR-3483: [Guided Decision Tree]: Unable to add field in the update node. Fix duplicate class enforcement

--- drools-wb-screens/drools-wb-guided-dtree-editor/drools-wb-guided-dtree-editor-client/src/main/java/org/drools/workbench/screens/guided/dtree/client/widget/popups/ActionFieldValueEditor.java
+++ drools-wb-screens/drools-wb-guided-dtree-editor/drools-wb-guided-dtree-editor-client/src/main/java/org/drools/workbench/screens/guided/dtree/client/widget/popups/ActionFieldValueEditor.java
@@ -22,15 +22,11 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import org.appformer.project.datamodel.oracle.DataType;
 import org.appformer.project.datamodel.oracle.ModelField;
@@ -41,7 +37,6 @@ import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.FormGroup;
 import org.gwtbootstrap3.client.ui.ListBox;
 import org.kie.workbench.common.widgets.client.datamodel.AsyncPackageDataModelOracle;
-import org.uberfire.client.callbacks.Callback;
 import org.uberfire.mvp.ParameterizedCommand;
 
 public class ActionFieldValueEditor extends Composite {
@@ -52,7 +47,7 @@ public class ActionFieldValueEditor extends Composite {
 
     }
 
-    private static ActionFieldValueEditorBinder uiBinder = GWT.create( ActionFieldValueEditorBinder.class );
+    private static ActionFieldValueEditorBinder uiBinder = GWT.create(ActionFieldValueEditorBinder.class);
 
     private final String className;
     private final ActionFieldValue afv;
@@ -63,13 +58,13 @@ public class ActionFieldValueEditor extends Composite {
     private final ValueEditorFactory valueEditorFactory = new ValueEditorFactory() {
         @Override
         protected Map<String, String> getCurrentValueMap() {
-            if ( afvs == null ) {
-                return Collections.EMPTY_MAP;
+            if (afvs == null) {
+                return Collections.emptyMap();
             }
             final Map<String, String> currentValueMap = new HashMap<String, String>();
-            for ( ActionFieldValue afv : afvs ) {
-                currentValueMap.put( afv.getFieldName(),
-                                     ValueUtilities.convertNodeValue( afv.getValue() ) );
+            for (ActionFieldValue afv : afvs) {
+                currentValueMap.put(afv.getFieldName(),
+                                    ValueUtilities.convertNodeValue(afv.getValue()));
             }
             return currentValueMap;
         }
@@ -84,12 +79,12 @@ public class ActionFieldValueEditor extends Composite {
     @UiField
     Button removeFieldValueButton;
 
-    public ActionFieldValueEditor( final String className,
-                                   final ActionFieldValue afv,
-                                   final List<ActionFieldValue> afvs,
-                                   final AsyncPackageDataModelOracle oracle,
-                                   final ParameterizedCommand<ActionFieldValue> onDeleteCallback ) {
-        initWidget( uiBinder.createAndBindUi( this ) );
+    public ActionFieldValueEditor(final String className,
+                                  final ActionFieldValue afv,
+                                  final List<ActionFieldValue> afvs,
+                                  final AsyncPackageDataModelOracle oracle,
+                                  final ParameterizedCommand<ActionFieldValue> onDeleteCallback) {
+        initWidget(uiBinder.createAndBindUi(this));
 
         this.className = className;
         this.afv = afv;
@@ -97,72 +92,69 @@ public class ActionFieldValueEditor extends Composite {
         this.oracle = oracle;
         this.onDeleteCallback = onDeleteCallback;
 
-        initializeFieldNames( afv );
+        initializeFieldNames(afv);
     }
 
-    private void initializeFieldNames( final ActionFieldValue afv ) {
+    private void initializeFieldNames(final ActionFieldValue afv) {
         fieldListBox.clear();
-        fieldListBox.setEnabled( false );
+        fieldListBox.setEnabled(false);
 
-        oracle.getFieldCompletions( className,
-                                    new Callback<ModelField[]>() {
-                                        @Override
-                                        public void callback( final ModelField[] modelFields ) {
-                                            fieldListBox.setEnabled( modelFields.length > 0 );
-                                            if ( modelFields.length == 0 ) {
-                                                fieldListBox.addItem( GuidedDecisionTreeConstants.INSTANCE.noFields() );
-                                                return;
-                                            }
+        oracle.getFieldCompletions(className,
+                                   (modelFields) -> {
+                                       fieldListBox.setEnabled(modelFields.length > 0);
+                                       if (modelFields.length == 0) {
+                                           fieldListBox.addItem(GuidedDecisionTreeConstants.INSTANCE.noFields());
+                                           return;
+                                       }
 
-                                            //Add them to the ListBox
-                                            int selectedIndex = -1;
-                                            for ( ModelField modelField : modelFields ) {
-                                                final String fieldName = modelField.getName();
-                                                if ( !fieldName.equals( DataType.TYPE_THIS ) ) {
-                                                    fieldListBox.addItem( fieldName );
-                                                    if ( afv.getFieldName().equals( fieldName ) ) {
-                                                        selectedIndex = fieldListBox.getItemCount() - 1;
-                                                    }
-                                                }
+                                       //Add them to the ListBox
+                                       int selectedIndex = -1;
+                                       for (ModelField modelField : modelFields) {
+                                           final String fieldName = modelField.getName();
+                                           if (!fieldName.equals(DataType.TYPE_THIS)) {
+                                               fieldListBox.addItem(fieldName);
+                                               if (fieldName.equals(afv.getFieldName())) {
+                                                   selectedIndex = fieldListBox.getItemCount() - 1;
+                                               }
+                                           }
+                                       }
 
-                                            }
+                                       fieldListBox.addChangeHandler((event) -> {
+                                           final String fieldName = fieldListBox.getItemText(fieldListBox.getSelectedIndex());
+                                           afv.setFieldName(fieldName);
+                                           afv.setValue(null);
+                                           initialiseValue();
+                                       });
 
-                                            fieldListBox.addChangeHandler( new ChangeHandler() {
-                                                @Override
-                                                public void onChange( final ChangeEvent event ) {
-                                                    final String fieldName = fieldListBox.getItemText( fieldListBox.getSelectedIndex() );
-                                                    afv.setFieldName( fieldName );
-                                                    afv.setValue( null );
-                                                    initialiseValue();
-                                                }
-                                            } );
-
-                                            fieldListBox.setSelectedIndex( selectedIndex );
-                                            if ( selectedIndex == -1 ) {
-                                                fieldListBox.setSelectedIndex( 0 );
-                                                final String fieldName = fieldListBox.getItemText( 0 );
-                                                afv.setFieldName( fieldName );
-                                            }
-                                            initialiseValue();
-                                        }
-                                    } );
+                                       fieldListBox.setSelectedIndex(selectedIndex);
+                                       if (selectedIndex == -1) {
+                                           fieldListBox.setSelectedIndex(0);
+                                           final String fieldName = fieldListBox.getItemText(0);
+                                           afv.setFieldName(fieldName);
+                                       }
+                                       initialiseValue();
+                                   });
     }
 
     private void initialiseValue() {
-        final Widget editor = valueEditorFactory.getValueEditor( className,
-                                                                 afv.getFieldName(),
-                                                                 afv,
-                                                                 oracle,
-                                                                 false );
+        final Widget editor = valueEditorFactory().getValueEditor(className,
+                                                                  afv.getFieldName(),
+                                                                  afv,
+                                                                  oracle,
+                                                                  false);
         valueHolder.clear();
-        if ( editor != null ) {
-            valueHolder.add( editor );
+        if (editor != null) {
+            valueHolder.add(editor);
         }
     }
 
-    @UiHandler("removeFieldValueButton")
-    void onRemoveFieldValueButtonClick( final ClickEvent event ) {
-        onDeleteCallback.execute( afv );
+    ValueEditorFactory valueEditorFactory() {
+        return valueEditorFactory;
     }
 
+    @SuppressWarnings("unused")
+    @UiHandler("removeFieldValueButton")
+    void onRemoveFieldValueButtonClick(final ClickEvent event) {
+        onDeleteCallback.execute(afv);
+    }
 }
