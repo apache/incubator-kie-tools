fea50a297bb1ce29abc15d2f1cee6e3fbd33abe7
Christopher Chianelli <cchianel@redhat.com>
2017-10-04 14:43:24 -0400

    JBPM-5808: Elements are placed in preview position (#964)
    * JBPM-5808: Preview uses top-left corner, changed mouse icon.
    
    * Formatted Code

--- kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-lienzo/src/main/java/org/kie/workbench/common/stunner/client/lienzo/components/glyph/ShapeGlyphDragHandlerImpl.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-lienzo/src/main/java/org/kie/workbench/common/stunner/client/lienzo/components/glyph/ShapeGlyphDragHandlerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,33 +60,22 @@ public class ShapeGlyphDragHandlerImpl implements ShapeGlyphDragHandler<Abstract
     }
 
     @Override
-    public DragProxy<AbstractCanvas, Item, DragProxyCallback> show(Item item,
-                                                                   int x,
-                                                                   int y,
-                                                                   DragProxyCallback dragProxyCallback) {
+    public DragProxy<AbstractCanvas, Item, DragProxyCallback> show(Item item, int x, int y, DragProxyCallback dragProxyCallback) {
 
         int width = item.getWidth();
         int height = item.getHeight();
 
-        final Group dragShape = glyphLienzoGlyphRenderer.render(item.getShape(),
-                                                                width,
-                                                                height);
-        dragShape.setX(width / 2);
-        dragShape.setY(height / 2);
-        this.dragProxyPanel = new LienzoPanel((width * 2),
-                                              (height * 2));
-        dragProxyPanel.getElement().getStyle().setCursor(Style.Cursor.MOVE);
+        final Group dragShape = glyphLienzoGlyphRenderer.render(item.getShape(), width, height);
+        dragShape.setX(0);
+        dragShape.setY(0);
+        this.dragProxyPanel = new LienzoPanel((width * 2), (height * 2));
+        dragProxyPanel.getElement().getStyle().setCursor(Style.Cursor.AUTO);
         final Layer dragProxyLayer = new Layer();
         dragProxyLayer.add(dragShape);
         dragProxyPanel.add(dragProxyLayer);
         dragProxyLayer.batch();
-        setDragProxyPosition(dragProxyPanel,
-                             width,
-                             height,
-                             x,
-                             y);
-        attachDragProxyHandlers(dragProxyPanel,
-                                dragProxyCallback);
+        setDragProxyPosition(dragProxyPanel, width, height, x, y);
+        attachDragProxyHandlers(dragProxyPanel, dragProxyCallback);
 
         addKeyboardEscHandler();
         RootPanel.get().add(dragProxyPanel);
@@ -109,66 +98,45 @@ public class ShapeGlyphDragHandlerImpl implements ShapeGlyphDragHandler<Abstract
         clear();
     }
 
-    private void setDragProxyPosition(final LienzoPanel dragProxyPanel,
-                                      final double proxyWidth,
-                                      final double proxyHeight,
-                                      final double x,
-                                      final double y) {
+    private void setDragProxyPosition(final LienzoPanel dragProxyPanel, final double proxyWidth, final double proxyHeight, final double x, final double y) {
         Style style = dragProxyPanel.getElement().getStyle();
         style.setPosition(Style.Position.ABSOLUTE);
-        style.setLeft(x - (proxyWidth / 2),
-                      Style.Unit.PX);
-        style.setTop(y - (proxyHeight / 2),
-                     Style.Unit.PX);
+        style.setLeft(x, Style.Unit.PX);
+        style.setTop(y, Style.Unit.PX);
         style.setZIndex(ZINDEX);
     }
 
-    private void attachDragProxyHandlers(final LienzoPanel floatingPanel,
-                                         final DragProxyCallback callback) {
+    private void attachDragProxyHandlers(final LienzoPanel floatingPanel, final DragProxyCallback callback) {
         final Style style = floatingPanel.getElement().getStyle();
 
         //MouseMoveEvents
-        addMouseMoveEvents(floatingPanel,
-                           callback,
-                           style);
+        addMouseMoveEvents(floatingPanel, callback, style);
         //MouseUpEvent
-        addMouseUpEvent(floatingPanel,
-                        callback);
+        addMouseUpEvent(floatingPanel, callback);
     }
 
-    private void addMouseMoveEvents(LienzoPanel floatingPanel,
-                                    DragProxyCallback callback,
-                                    Style style) {
+    private void addMouseMoveEvents(LienzoPanel floatingPanel, DragProxyCallback callback, Style style) {
         handlerRegistrations.add(RootPanel.get().addDomHandler(mouseMoveEvent -> {
-                                                                   style.setLeft(mouseMoveEvent.getX() - (floatingPanel.getWidth() / 2),
-                                                                                 Style.Unit.PX);
-                                                                   style.setTop(mouseMoveEvent.getY() - (floatingPanel.getHeight() / 2),
-                                                                                Style.Unit.PX);
-                                                                   final int x = mouseMoveEvent.getX();
-                                                                   final int y = mouseMoveEvent.getY();
-                                                                   callback.onMove(x,
-                                                                                   y);
-                                                               },
-                                                               MouseMoveEvent.getType()));
+            style.setLeft(mouseMoveEvent.getX(), Style.Unit.PX);
+            style.setTop(mouseMoveEvent.getY(), Style.Unit.PX);
+            final int x = mouseMoveEvent.getX();
+            final int y = mouseMoveEvent.getY();
+            callback.onMove(x, y);
+        }, MouseMoveEvent.getType()));
     }
 
-    private void addMouseUpEvent(LienzoPanel floatingPanel,
-                                 DragProxyCallback callback) {
+    private void addMouseUpEvent(LienzoPanel floatingPanel, DragProxyCallback callback) {
         handlerRegistrations.add(RootPanel.get().addDomHandler(mouseUpEvent -> {
-                                                                   clearHandlers();
-                                                                   RootPanel.get().remove(floatingPanel);
-                                                                   final int x = mouseUpEvent.getX();
-                                                                   final int y = mouseUpEvent.getY();
-                                                                   callback.onComplete(x,
-                                                                                       y);
-                                                               },
-                                                               MouseUpEvent.getType()));
+            clearHandlers();
+            RootPanel.get().remove(floatingPanel);
+            final int x = mouseUpEvent.getX();
+            final int y = mouseUpEvent.getY();
+            callback.onComplete(x, y);
+        }, MouseUpEvent.getType()));
     }
 
     private void addKeyboardEscHandler() {
-        handlerRegistrations.add(RootPanel.get().addDomHandler(k -> clear(),
-                                                               KeyDownEvent
-                                                                       .getType()));
+        handlerRegistrations.add(RootPanel.get().addDomHandler(k -> clear(), KeyDownEvent.getType()));
     }
 
     private void clearHandlers() {
---
+++ kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-lienzo/src/test/java/org/kie/workbench/common/stunner/client/lienzo/canvas/controls/builder/ObserverBuilderControlTest.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.stunner.client.lienzo.canvas.controls.builder;
+
+import javax.enterprise.event.Event;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.workbench.common.stunner.core.client.ShapeSet;
+import org.kie.workbench.common.stunner.core.client.api.ClientDefinitionManager;
+import org.kie.workbench.common.stunner.core.client.api.ShapeManager;
+import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvas;
+import org.kie.workbench.common.stunner.core.client.canvas.AbstractCanvasHandler;
+import org.kie.workbench.common.stunner.core.client.canvas.CanvasHandlerImpl;
+import org.kie.workbench.common.stunner.core.client.canvas.command.AddCanvasNodeCommand;
+import org.kie.workbench.common.stunner.core.client.canvas.command.UpdateElementPositionCommand;
+import org.kie.workbench.common.stunner.core.client.canvas.controls.actions.TextPropertyProviderFactory;
+import org.kie.workbench.common.stunner.core.client.canvas.controls.builder.BuilderControl;
+import org.kie.workbench.common.stunner.core.client.canvas.controls.builder.impl.ObserverBuilderControl;
+import org.kie.workbench.common.stunner.core.client.canvas.controls.builder.request.ElementBuildRequest;
+import org.kie.workbench.common.stunner.core.client.canvas.util.CanvasLayoutUtils;
+import org.kie.workbench.common.stunner.core.client.command.CanvasCommand;
+import org.kie.workbench.common.stunner.core.client.command.CanvasCommandFactory;
+import org.kie.workbench.common.stunner.core.client.command.CanvasCommandManager;
+import org.kie.workbench.common.stunner.core.client.command.RequiresCommandManager;
+import org.kie.workbench.common.stunner.core.client.service.ClientFactoryService;
+import org.kie.workbench.common.stunner.core.client.service.ClientRuntimeError;
+import org.kie.workbench.common.stunner.core.client.service.ServiceCallback;
+import org.kie.workbench.common.stunner.core.client.shape.ElementShape;
+import org.kie.workbench.common.stunner.core.client.shape.factory.ShapeFactory;
+import org.kie.workbench.common.stunner.core.command.Command;
+import org.kie.workbench.common.stunner.core.definition.adapter.AdapterManager;
+import org.kie.workbench.common.stunner.core.definition.adapter.DefinitionAdapter;
+import org.kie.workbench.common.stunner.core.definition.adapter.DefinitionSetRuleAdapter;
+import org.kie.workbench.common.stunner.core.diagram.Diagram;
+import org.kie.workbench.common.stunner.core.diagram.Metadata;
+import org.kie.workbench.common.stunner.core.graph.Edge;
+import org.kie.workbench.common.stunner.core.graph.Graph;
+import org.kie.workbench.common.stunner.core.graph.Node;
+import org.kie.workbench.common.stunner.core.graph.content.definition.DefinitionSet;
+import org.kie.workbench.common.stunner.core.graph.content.view.BoundImpl;
+import org.kie.workbench.common.stunner.core.graph.content.view.BoundsImpl;
+import org.kie.workbench.common.stunner.core.graph.content.view.View;
+import org.kie.workbench.common.stunner.core.graph.content.view.ViewImpl;
+import org.kie.workbench.common.stunner.core.graph.impl.NodeImpl;
+import org.kie.workbench.common.stunner.core.graph.processing.index.GraphIndexBuilder;
+import org.kie.workbench.common.stunner.core.graph.processing.index.MutableIndex;
+import org.kie.workbench.common.stunner.core.graph.processing.index.bounds.GraphBoundsIndexer;
+import org.kie.workbench.common.stunner.core.graph.util.GraphUtils;
+import org.kie.workbench.common.stunner.core.registry.definition.TypeDefinitionSetRegistry;
+import org.kie.workbench.common.stunner.core.rule.RuleManager;
+import org.kie.workbench.common.stunner.core.rule.RuleSet;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.uberfire.mvp.ParameterizedCommand;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ObserverBuilderControlTest {
+
+    @Mock
+    ClientDefinitionManager clientDefinitionManager;
+
+    @Mock
+    ClientFactoryService clientFactoryServices;
+
+    @Mock
+    GraphUtils graphUtils;
+
+    @Mock
+    RuleManager ruleManager;
+
+    @Mock
+    CanvasCommandFactory<AbstractCanvasHandler> canvasCommandFactory;
+
+    @Mock
+    GraphBoundsIndexer graphBoundsIndexer;
+
+    @Mock
+    CanvasLayoutUtils canvasLayoutUtils;
+
+    @Mock
+    GraphIndexBuilder graphIndexBuilder;
+
+    @Mock
+    ShapeManager shapeManager;
+
+    @Mock
+    RequiresCommandManager.CommandManagerProvider<AbstractCanvasHandler> commandManagerProvider;
+
+    private View<Object> view;
+    private AbstractCanvasHandler canvasHandler;
+    private ObserverBuilderControl tested;
+    private double x, y;
+
+    @Before
+    @SuppressWarnings("unchecked")
+    public void setup() throws Exception {
+        doAnswer(new Answer() {
+
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                ServiceCallback<Node<View<Object>, Edge>> callback = (ServiceCallback<Node<View<Object>, Edge>>) (invocationOnMock.getArguments()[2]);
+                view = new ViewImpl<>(new Object(), new BoundsImpl(new BoundImpl(0.0, 0.0), new BoundImpl(10.0, 20.0)));
+                Node<View<Object>, Edge> item = new NodeImpl<>("UUID");
+                item.setContent(view);
+                callback.onSuccess(item);
+                return null;
+            }
+        }).when(clientFactoryServices).newElement(anyString(), anyString(), any(ServiceCallback.class));
+
+        when(graphBoundsIndexer.setRootUUID(anyString())).thenReturn(graphBoundsIndexer);
+
+        AdapterManager adapters = mock(AdapterManager.class);
+        DefinitionAdapter definitionAdapter = mock(DefinitionAdapter.class);
+        DefinitionSetRuleAdapter rulesAdapter = mock(DefinitionSetRuleAdapter.class);
+        when(definitionAdapter.getId(any())).thenReturn("Object");
+        when(rulesAdapter.getRuleSet(any())).thenReturn(mock(RuleSet.class));
+        when(adapters.forDefinition()).thenReturn(definitionAdapter);
+        when(adapters.forRules()).thenReturn(rulesAdapter);
+
+        when(clientDefinitionManager.adapters()).thenReturn(adapters);
+        when(clientDefinitionManager.definitionSets()).thenReturn(mock(TypeDefinitionSetRegistry.class));
+
+        when(canvasCommandFactory.addNode(any(Node.class), anyString())).thenAnswer(new Answer<Command>() {
+
+            @Override
+            public Command answer(InvocationOnMock invocationOnMock) {
+                Node node = (Node) invocationOnMock.getArguments()[0];
+                String uid = (String) invocationOnMock.getArguments()[1];
+                return new AddCanvasNodeCommand(node, uid);
+            }
+        });
+
+        when(canvasCommandFactory.updatePosition(any(Node.class), anyDouble(), anyDouble())).thenAnswer(new Answer<Command>() {
+
+            @Override
+            public Command answer(InvocationOnMock invocationOnMock) {
+                Node node = (Node) invocationOnMock.getArguments()[0];
+                double x = (double) invocationOnMock.getArguments()[1];
+                double y = (double) invocationOnMock.getArguments()[2];
+                return new UpdateElementPositionCommand(node, x, y);
+            }
+        });
+
+        when(canvasCommandFactory.draw()).thenReturn(mock(CanvasCommand.class));
+        ShapeSet shapeSet = mock(ShapeSet.class);
+        ShapeFactory shapeFactory = mock(ShapeFactory.class);
+
+        when(shapeFactory.newShape(any())).thenReturn(mock(ElementShape.class));
+        when(shapeSet.getShapeFactory()).thenReturn(shapeFactory);
+
+        when(shapeManager.getShapeSet(anyString())).thenReturn(shapeSet);
+        when(shapeManager.getDefaultShapeSet(anyString())).thenReturn(shapeSet);
+
+        tested = new ObserverBuilderControl(clientDefinitionManager, clientFactoryServices, graphUtils, ruleManager, canvasCommandFactory, graphBoundsIndexer, canvasLayoutUtils, mock(Event.class));
+
+        Diagram diagram = mock(Diagram.class);
+        Metadata metadata = mock(Metadata.class);
+
+        when(metadata.getCanvasRootUUID()).thenReturn("ID");
+        when(diagram.getMetadata()).thenReturn(metadata);
+        MutableIndex index = mock(MutableIndex.class);
+        Graph graph = mock(Graph.class);
+        DefinitionSet graphContent = mock(DefinitionSet.class);
+        when(graphContent.getBounds()).thenReturn(new BoundsImpl(new BoundImpl(Double.MIN_VALUE, Double.MIN_VALUE), new BoundImpl(Double.MAX_VALUE, Double.MAX_VALUE)));
+        when(graph.getContent()).thenReturn(graphContent);
+        when(index.getGraph()).thenReturn(graph);
+
+        when(graphIndexBuilder.build(any(Graph.class))).thenReturn(index);
+        canvasHandler = new CanvasHandlerImpl(clientDefinitionManager, canvasCommandFactory, clientFactoryServices, ruleManager, graphUtils, graphIndexBuilder, shapeManager, mock(TextPropertyProviderFactory.class), mock(Event.class), null, null, null);
+        canvasHandler.handle(mock(AbstractCanvas.class));
+        canvasHandler.draw(diagram, mock(ParameterizedCommand.class));
+
+        CanvasCommandManager commandManager = mock(CanvasCommandManager.class);
+
+        doAnswer(new Answer() {
+
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Command command = (Command) invocationOnMock.getArguments()[1];
+                command.execute(invocationOnMock.getArguments()[0]);
+                return null;
+            }
+        }).when(commandManager).execute(any(), any(Command.class));
+        when(commandManagerProvider.getCommandManager()).thenReturn(commandManager);
+
+        tested.enable(canvasHandler);
+        tested.setCommandManagerProvider(commandManagerProvider);
+    }
+
+    @Test
+    public void testAddElement() {
+        ElementBuildRequest<AbstractCanvasHandler> request = mock(ElementBuildRequest.class);
+        when(request.getX()).thenReturn(5.0);
+        when(request.getY()).thenReturn(5.0);
+        tested.build(request, new BuilderControl.BuildCallback() {
+
+            @Override
+            public void onSuccess(String uuid) {
+                assertEquals(15.0, view.getBounds().getLowerRight().getX(), 0.00001);
+                assertEquals(5.0, view.getBounds().getUpperLeft().getX(), 0.00001);
+                assertEquals(25.0, view.getBounds().getLowerRight().getY(), 0.00001);
+                assertEquals(5.0, view.getBounds().getUpperLeft().getY(), 0.00001);
+            }
+
+            @Override
+            public void onError(ClientRuntimeError error) {
+                fail(error.getMessage());
+            }
+        });
+    }
+}
