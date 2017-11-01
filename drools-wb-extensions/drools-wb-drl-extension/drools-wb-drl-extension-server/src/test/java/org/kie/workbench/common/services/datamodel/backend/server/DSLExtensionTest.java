4e8b2d4850ab3644fae214c7eede13bace145837
Jozef Marko <jomarko@redhat.com>
2017-11-01 16:05:27 +0100

    DROOLS-2098: Prepare drools-wb-extensions module for static code analysis (#694)

--- drools-wb-extensions/drools-wb-drl-extension/drools-wb-drl-extension-server/src/test/java/org/kie/workbench/common/services/datamodel/backend/server/DSLExtensionTest.java
+++ drools-wb-extensions/drools-wb-drl-extension/drools-wb-drl-extension-server/src/test/java/org/kie/workbench/common/services/datamodel/backend/server/DSLExtensionTest.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2017 Red Hat, Inc. and/or its affiliates.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.kie.workbench.common.services.datamodel.backend.server;
 
@@ -21,14 +21,11 @@ import java.util.List;
 import org.drools.workbench.models.datamodel.oracle.DSLActionSentence;
 import org.drools.workbench.models.datamodel.oracle.DSLConditionSentence;
 import org.junit.Test;
-import org.kie.soup.project.datamodel.commons.util.RawMVELEvaluator;
 import org.kie.soup.project.datamodel.oracle.ExtensionKind;
-import org.kie.soup.project.datamodel.oracle.PackageDataModelOracle;
-import org.kie.workbench.common.services.datamodel.backend.server.builder.packages.PackageDataModelOracleBuilder;
 import org.kie.workbench.common.services.datamodel.backend.server.cache.DSLExtension;
 import org.kie.workbench.common.services.datamodel.spi.DataModelExtension.ExtensionMapping;
 
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
 public class DSLExtensionTest {
 
