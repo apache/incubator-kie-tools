78a0573e4d7e4955bbd3f5a05dba7265c6cb40fd
Jozef Marko <jomarko@redhat.com>
2017-11-27 14:32:23 +0100

    Test rendering of ExpressionEditorColumn (#8)

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/expressions/types/context/ExpressionEditorColumnRenderer.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/expressions/types/context/ExpressionEditorColumnRenderer.java
@@ -19,6 +19,7 @@ package org.kie.workbench.common.dmn.client.editors.expressions.types.context;
 import java.util.Optional;
 
 import com.ait.lienzo.client.core.shape.Group;
+import com.google.gwt.core.client.GWT;
 import org.kie.workbench.common.dmn.client.widgets.grid.model.DMNExpressionCellValue;
 import org.kie.workbench.common.dmn.client.widgets.grid.model.DMNGridColumn;
 import org.uberfire.ext.wires.core.grids.client.model.GridCell;
@@ -35,9 +36,11 @@ public class ExpressionEditorColumnRenderer extends BaseGridColumnRenderer<Optio
             return null;
         }
 
-        final Group g = new Group();
-        final DMNExpressionCellValue ecv = (DMNExpressionCellValue) cell.getValue();
-        ecv.getValue().ifPresent(editor -> g.add(editor.setX(DMNGridColumn.PADDING).setY(DMNGridColumn.PADDING)));
+        final Group g = GWT.create(Group.class);
+        if (cell.getValue() != null && cell.getValue() instanceof DMNExpressionCellValue) {
+            final DMNExpressionCellValue ecv = (DMNExpressionCellValue) cell.getValue();
+            ecv.getValue().ifPresent(editor -> g.add(editor.setX(DMNGridColumn.PADDING).setY(DMNGridColumn.PADDING)));
+        }
 
         return g;
     }
---
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/test/java/org/kie/workbench/common/dmn/client/editors/expressions/types/context/ExpressionEditorColumnRendererTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.dmn.client.editors.expressions.types.context;
+
+import java.util.Optional;
+
+import com.ait.lienzo.client.core.shape.Group;
+import com.ait.lienzo.test.LienzoMockitoTestRunner;
+import com.google.gwtmockito.GwtMockito;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.workbench.common.dmn.client.widgets.grid.model.DMNExpressionCellValue;
+import org.mockito.Mock;
+import org.uberfire.ext.wires.core.grids.client.model.GridCell;
+import org.uberfire.ext.wires.core.grids.client.model.impl.BaseGridCell;
+import org.uberfire.ext.wires.core.grids.client.model.impl.BaseGridCellValue;
+import org.uberfire.ext.wires.core.grids.client.widget.context.GridBodyCellRenderContext;
+import org.uberfire.ext.wires.core.grids.client.widget.grid.GridWidget;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@RunWith(LienzoMockitoTestRunner.class)
+public class ExpressionEditorColumnRendererTest {
+
+    @Mock
+    private GridBodyCellRenderContext context;
+
+    @Mock
+    private GridWidget widget;
+
+    @Mock
+    private Group renderedGroup;
+
+    @Mock
+    private Group editorGroup;
+
+    private GridCell<Optional<GridWidget>> cell;
+
+    private ExpressionEditorColumnRenderer renderer;
+
+    @Before
+    public void setUp() throws Exception {
+        GwtMockito.useProviderForType(Group.class, aClass -> renderedGroup);
+
+        renderer = new ExpressionEditorColumnRenderer();
+
+        doReturn(editorGroup).when(widget).setX(anyDouble());
+        doReturn(editorGroup).when(editorGroup).setY(anyDouble());
+    }
+
+    @Test
+    public void testRenderCellNoDMNExpression() throws Exception {
+        cell = new BaseGridCell<>(new BaseGridCellValue<>(Optional.of(widget)));
+        renderer.renderCell(cell, context);
+        verify(renderedGroup, never()).add(any());
+    }
+
+    @Test
+    public void testRenderCellDMNExpression() throws Exception {
+        cell = new BaseGridCell<>(new DMNExpressionCellValue(Optional.of(widget)));
+        renderer.renderCell(cell, context);
+        verify(renderedGroup).add(editorGroup);
+    }
+}
