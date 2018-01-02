5a61922091967eae2ef1b97af1689ee4180fa599
deanjones <deanjones@gmail.com>
2018-01-01 21:16:08 -0500

    2.0.295-RELEASE

--- src/main/java/com/ait/lienzo/client/core/config/LienzoCorePlugin.java
+++ src/main/java/com/ait/lienzo/client/core/config/LienzoCorePlugin.java
@@ -589,6 +589,6 @@ final class LienzoCorePlugin extends AbstractLienzoCorePlugin
     @Override
     public final String getVersion()
     {
-        return "2.0.294-RELEASE";
+        return "2.0.295-RELEASE";
     }
 }
