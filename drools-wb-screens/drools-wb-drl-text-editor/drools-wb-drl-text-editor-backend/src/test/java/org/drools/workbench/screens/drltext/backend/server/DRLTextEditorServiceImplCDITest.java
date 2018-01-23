f67049e1887779cb439a96ddeb2856f098edc725
Jozef Marko <jomarko@redhat.com>
2018-01-23 14:06:20 +0100

    BAQE-336: Cover the plain drl service
    This PR cover basic operation of the plain drl service like save, create, load, validate.

--- drools-wb-screens/drools-wb-drl-text-editor/drools-wb-drl-text-editor-backend/src/test/java/org/drools/workbench/screens/drltext/backend/server/DRLTextEditorServiceImplCDITest.java
+++ drools-wb-screens/drools-wb-drl-text-editor/drools-wb-drl-text-editor-backend/src/test/java/org/drools/workbench/screens/drltext/backend/server/DRLTextEditorServiceImplCDITest.java
@@ -20,6 +20,8 @@ import java.net.URL;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.assertj.core.api.Assertions;
+import org.drools.workbench.screens.drltext.model.DrlModelContent;
 import org.drools.workbench.screens.drltext.service.DRLTextEditorService;
 import org.guvnor.common.services.shared.validation.model.ValidationMessage;
 import org.guvnor.test.CDITestSetup;
@@ -33,6 +35,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DRLTextEditorServiceImplCDITest extends CDITestSetup {
+
+    private static final String CAR_DRIVING_LICENSE = "drl/src/main/resources/org/kiegroup/applyForCarDrivingLicense.drl";
 
     private List<ValidationMessage> validationMessages;
     private DRLTextEditorService drlService;
@@ -66,12 +70,58 @@ public class DRLTextEditorServiceImplCDITest extends CDITestSetup {
         assertTrue(validationMessages.get(2).getText().contains("mismatched input 'then'"));
     }
 
-    private void validateResource(final String resource) throws Exception {
+    @Test
+    public void testValidDRLFile() throws Exception {
+        validateResource(CAR_DRIVING_LICENSE);
+
+        assertEquals(0, validationMessages.size());
+    }
+
+    @Test
+    public void testValidDRLFileWithTwoRules() throws Exception {
+        validateResource("drl/src/main/resources/org/kiegroup/applyForCarAndBusDrivingLicense.drl");
+
+        assertEquals(0, validationMessages.size());
+    }
+
+    @Test
+    public void testDRLFileWrongConstructor() throws Exception {
+        validateResource("drl/src/main/resources/org/kiegroup/applyForCarDrivingLicenseWrongConstructor.drl");
+
+        Assertions.assertThat(validationMessages).hasSize(2);
+        Assertions.assertThat(validationMessages)
+                .allMatch(message -> message.getText()
+                        .contains("Unable to Analyse Expression drools.insert(new DrivingLicenseApplication(\"car\"))"));
+    }
+
+    @Test
+    public void testLoadContent() throws Exception {
+        final DrlModelContent content = drlService.loadContent(getPath(CAR_DRIVING_LICENSE));
+
+        Assertions.assertThat(content.getDrl()).isEqualTo(drlService.load(getPath(CAR_DRIVING_LICENSE)));
+        Assertions.assertThat(content.getFullyQualifiedClassNames()).hasSize(2);
+        Assertions.assertThat(content.getFullyQualifiedClassNames())
+                .contains("org.kiegroup.Person", "org.kiegroup.DrivingLicenseApplication");
+    }
+
+    @Test
+    public void testLoadClassFields() throws Exception {
+        final List<String> fields = drlService.loadClassFields(getPath(CAR_DRIVING_LICENSE),
+                                                               "org.kiegroup.Person");
+
+        Assertions.assertThat(fields).hasSize(3);
+        Assertions.assertThat(fields).contains("this", "age", "dummy");
+    }
+
+    private Path getPath(final String resource) throws Exception {
         final URL resourceURL = getClass().getResource(resource);
         final org.uberfire.java.nio.file.Path resourceNioPath = fileSystemProvider.getPath(resourceURL.toURI());
-        final Path resourcePath = Paths.convert(resourceNioPath);
+        return Paths.convert(resourceNioPath);
+    }
 
-        validationMessages = drlService.validate(resourcePath,
+    private void validateResource(final String resource) throws Exception {
+        final URL resourceURL = getClass().getResource(resource);
+        validationMessages = drlService.validate(getPath(resource),
                                                  IOUtils.toString(resourceURL.toURI(), "UTF-8"));
     }
 }
--- drools-wb-screens/drools-wb-drl-text-editor/drools-wb-drl-text-editor-backend/src/test/java/org/drools/workbench/screens/drltext/backend/server/DRLTextEditorServiceImplTest.java
+++ drools-wb-screens/drools-wb-drl-text-editor/drools-wb-drl-text-editor-backend/src/test/java/org/drools/workbench/screens/drltext/backend/server/DRLTextEditorServiceImplTest.java
@@ -17,49 +17,94 @@
 package org.drools.workbench.screens.drltext.backend.server;
 
 import java.net.URISyntaxException;
+import java.util.Collections;
 
+import org.guvnor.common.services.backend.metadata.MetadataServerSideService;
+import org.guvnor.common.services.backend.util.CommentedOptionFactory;
+import org.guvnor.common.services.shared.metadata.model.Metadata;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kie.workbench.common.services.shared.project.KieProjectService;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.uberfire.backend.vfs.Path;
 import org.uberfire.backend.vfs.PathFactory;
 import org.uberfire.io.IOService;
+import org.uberfire.java.nio.base.options.CommentedOption;
 import org.uberfire.java.nio.file.FileAlreadyExistsException;
 import org.uberfire.java.nio.file.OpenOption;
 
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
 @RunWith(MockitoJUnitRunner.class)
 public class DRLTextEditorServiceImplTest {
 
-    private IOService ioService = mock(IOService.class);
-    private KieProjectService projectService = mock(KieProjectService.class);
+    @Mock
+    private IOService ioService;
+
+    @Mock
+    private KieProjectService projectService;
+
+    @Mock
+    private CommentedOptionFactory commentedOptionFactory;
+
+    @Mock
+    private CommentedOption commentedOption;
+
+    @Mock
+    private MetadataServerSideService metadataService;
 
     @InjectMocks
     private DRLTextEditorServiceImpl drlService = new DRLTextEditorServiceImpl();
 
     private String filename = "test.drl";
-    private Path path2 = PathFactory.newPath("contextpath", "file:///contextpath");
+    private Path path = PathFactory.newPath("contextpath", "file:///contextpath");
 
     @Test
     public void testCreateAlreadyExisting() throws URISyntaxException {
         when(ioService.exists(any(org.uberfire.java.nio.file.Path.class))).thenReturn(true);
 
         try {
-            drlService.create(path2, filename, "", "");
+            drlService.create(path, filename, "", "");
             fail("FileAlreadyExistsException was not thrown when expected!");
         } catch (FileAlreadyExistsException faee) {
             // this is correct behavior, anz other exception is a problem
         }
         verify(ioService, never()).write(any(org.uberfire.java.nio.file.Path.class), anyString(), any(OpenOption.class));
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        final String ruleContent = "rule \"empty\" when then";
+        final String creationComment = "initial version";
+        when(commentedOptionFactory.makeCommentedOption(creationComment)).thenReturn(commentedOption);
+
+        drlService.create(path, filename, ruleContent, creationComment);
+
+        verify(ioService).write(any(org.uberfire.java.nio.file.Path.class), eq(ruleContent), eq(commentedOption));
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        final String ruleContent = "rule \"empty\" when then";
+        final String creationComment = "initial version";
+        final Metadata metadata = mock(Metadata.class);
+        when(metadataService.setUpAttributes(path, metadata)).thenReturn(Collections.EMPTY_MAP);
+        when(commentedOptionFactory.makeCommentedOption(creationComment)).thenReturn(commentedOption);
+
+        drlService.save(path, ruleContent, metadata, creationComment);
+
+        verify(ioService).write(any(org.uberfire.java.nio.file.Path.class),
+                                eq(ruleContent),
+                                eq(Collections.EMPTY_MAP),
+                                eq(commentedOption));
     }
 }
