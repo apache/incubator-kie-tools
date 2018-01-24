1dcea19a6fc8971829d6818b5ee9bd659d571c31
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2018-01-25 09:21:17 +1000

    JBPM-6243 - Move deleteServerInstance to SpecManagementService (#1383)

--- kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-backend/src/main/java/org/kie/workbench/common/screens/server/management/backend/service/SpecManagementServiceCDI.java
+++ kie-wb-common-screens/kie-wb-common-server-ui/kie-wb-common-server-ui-backend/src/main/java/org/kie/workbench/common/screens/server/management/backend/service/SpecManagementServiceCDI.java
@@ -20,14 +20,10 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.jboss.errai.bus.server.annotations.Service;
-import org.kie.server.controller.api.model.events.ServerInstanceDeleted;
-import org.kie.server.controller.api.model.runtime.ServerInstanceKey;
 import org.kie.server.controller.api.model.spec.ServerTemplate;
 import org.kie.server.controller.impl.KieServerInstanceManager;
 import org.kie.server.controller.rest.RestSpecManagementServiceImpl;
 import org.kie.workbench.common.screens.server.management.service.SpecManagementService;
-
-import static org.kie.soup.commons.validation.PortablePreconditions.checkNotNull;
 
 @Service
 @ApplicationScoped
@@ -49,23 +45,6 @@ public class SpecManagementServiceCDI extends RestSpecManagementServiceImpl impl
     @Override
     public void setTemplateStorage(org.kie.server.controller.api.storage.KieServerTemplateStorage templateStorage) {
         super.setTemplateStorage(templateStorage);
-    }
-
-    @Override
-    public void deleteServerInstance(final ServerInstanceKey serverInstanceKey) {
-        checkNotNull("serverInstanceKey",
-                     serverInstanceKey);
-        if (getKieServerInstanceManager().isAlive(serverInstanceKey)) {
-            throw new RuntimeException("Can't delete live instance.");
-        } else {
-            final String serverInstanceId = serverInstanceKey.getServerInstanceId();
-            final ServerTemplate serverTemplate = getServerTemplate(serverInstanceKey.getServerTemplateId());
-            if (serverTemplate != null) {
-                serverTemplate.deleteServerInstance(serverInstanceId);
-                getTemplateStorage().update(serverTemplate);
-                getNotificationService().notify(new ServerInstanceDeleted(serverInstanceId));
-            }
-        }
     }
 
     @Override
