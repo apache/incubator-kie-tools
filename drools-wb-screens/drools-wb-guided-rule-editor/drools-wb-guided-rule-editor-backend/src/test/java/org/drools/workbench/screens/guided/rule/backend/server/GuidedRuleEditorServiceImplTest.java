931d7e12b142d21085e688793cfdd9d555310b77
Jozef Marko <jomarko@redhat.com>
2018-03-01 15:17:57 +0100

    BAQE-339: Cover Guided Rule service for modify score action in rule (#820)

--- drools-wb-screens/drools-wb-guided-rule-editor/drools-wb-guided-rule-editor-backend/src/test/java/org/drools/workbench/screens/guided/rule/backend/server/GuidedRuleEditorServiceImplTest.java
+++ drools-wb-screens/drools-wb-guided-rule-editor/drools-wb-guided-rule-editor-backend/src/test/java/org/drools/workbench/screens/guided/rule/backend/server/GuidedRuleEditorServiceImplTest.java
@@ -16,20 +16,27 @@
 
 package org.drools.workbench.screens.guided.rule.backend.server;
 
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.enterprise.event.Event;
 import javax.enterprise.inject.Instance;
 
+import org.assertj.core.api.Assertions;
 import org.drools.workbench.models.datamodel.oracle.DSLActionSentence;
 import org.drools.workbench.models.datamodel.oracle.DSLConditionSentence;
 import org.drools.workbench.models.datamodel.rule.DSLSentence;
+import org.drools.workbench.models.datamodel.rule.FreeFormLine;
 import org.drools.workbench.models.datamodel.rule.RuleModel;
 import org.drools.workbench.screens.guided.rule.model.GuidedEditorContent;
 import org.drools.workbench.screens.guided.rule.type.GuidedRuleDSLRResourceTypeDefinition;
+import org.guvnor.common.services.backend.validation.GenericValidator;
 import org.guvnor.common.services.shared.metadata.model.Metadata;
 import org.guvnor.common.services.shared.metadata.model.Overview;
 import org.junit.Test;
@@ -40,10 +47,12 @@ import org.kie.soup.project.datamodel.oracle.PackageDataModelOracle;
 import org.kie.workbench.common.services.datamodel.backend.server.builder.packages.PackageDataModelOracleBuilder;
 import org.kie.workbench.common.services.datamodel.backend.server.builder.projects.ModuleDataModelOracleBuilder;
 import org.kie.workbench.common.services.datamodel.backend.server.service.DataModelService;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.uberfire.backend.vfs.Path;
+import org.uberfire.backend.vfs.PathFactory;
 import org.uberfire.ext.editor.commons.backend.service.SaveAndRenameServiceImpl;
 import org.uberfire.io.IOService;
 import org.uberfire.rpc.SessionInfo;
@@ -52,6 +61,8 @@ import org.uberfire.workbench.events.ResourceOpenedEvent;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -59,11 +70,16 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class GuidedRuleEditorServiceImplTest {
 
+    private static final String MODIFY_SCORE_ACTION = "scoreHolder.addMultiConstraintMatch(kcontext, 123, 456);";
+
     @Mock
     private Event<ResourceOpenedEvent> resourceOpenedEventEvent;
 
     @Mock
     private SessionInfo sessionInfo;
+
+    @Mock
+    private GenericValidator genericValidator;
 
     @InjectMocks
     GuidedRuleEditorServiceImpl service = new GuidedRuleEditorServiceImpl(sessionInfo,
@@ -124,6 +140,35 @@ public class GuidedRuleEditorServiceImplTest {
     }
 
     @Test
+    public void testValidateModifyScoreAction() throws Exception {
+        final Path path = mock(Path.class);
+        final FreeFormLine modifyAction = new FreeFormLine();
+        modifyAction.setText(MODIFY_SCORE_ACTION);
+        final RuleModel model = new RuleModel();
+        model.addRhsItem(modifyAction);
+
+        service.validate(path, model);
+
+        final ArgumentCaptor<String> capturedFileContent = ArgumentCaptor.forClass(String.class);
+        verify(genericValidator).validate(eq(path), capturedFileContent.capture());
+
+        Assertions.assertThat(capturedFileContent.getValue()).contains(MODIFY_SCORE_ACTION);
+    }
+
+    @Test
+    public void testLoadModifyScoreAction() throws Exception {
+        final Path path = getPath("modifyScore.rdrl");
+        final String ruleFileContent = getFileContent("modifyScore.rdrl");
+        doReturn(ruleFileContent).when(ioService).readAllString(any());
+
+        final RuleModel model = service.load(path);
+
+        Assertions.assertThat(model.rhs).hasSize(1);
+        final FreeFormLine action = (FreeFormLine) model.rhs[0];
+        Assertions.assertThat(action.getText()).isEqualTo(MODIFY_SCORE_ACTION);
+    }
+
+    @Test
     public void testSaveAndRename() throws Exception {
 
         final Path path = mock(Path.class);
@@ -135,5 +180,18 @@ public class GuidedRuleEditorServiceImplTest {
         service.saveAndRename(path, newFileName, metadata, content, comment);
 
         verify(saveAndRenameService).saveAndRename(path, newFileName, metadata, content, comment);
+    }
+
+    private Path getPath(final String fileName) throws Exception {
+        final URI testedFileURI = getClass().getResource(fileName).toURI();
+        return PathFactory.newPath(fileName, testedFileURI.toString());
+    }
+
+    private String getFileContent(final String fileName) throws Exception {
+        final URI testedFileURI = getClass().getResource(fileName).toURI();
+        final java.nio.file.Path testedFile = Paths.get(testedFileURI);
+        return Files.readAllLines(testedFile)
+                .stream()
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 }
