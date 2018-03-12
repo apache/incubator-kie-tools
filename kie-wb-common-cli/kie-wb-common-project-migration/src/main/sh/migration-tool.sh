69c4132ff7eb5ed744771193d17fde5e814ceb2a
Marek Novotný <hotmana76@gmail.com>
2018-03-12 16:48:21 +0100

    RHBA-514: migration tool window launcher

--- kie-wb-common-cli/kie-wb-common-project-migration/src/main/sh/migration-tool.sh
+++ kie-wb-common-cli/kie-wb-common-project-migration/src/main/sh/migration-tool.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
+SCRIPTDIR="`pwd`"
 
-SCRIPTDIR="`dirname $0`"
-export CLASSPATH="`find "$SCRIPTDIR/../lib" -name '*.jar' | tr '\n' ':' | sed -E 's/:$//'`"
+CLASSPATH="$SCRIPTDIR/lib/*"
 
-java ${mainClass} $@
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+      # IBM's JDK on AIX uses strange locations for the executables
+      JAVACMD="$JAVA_HOME/jre/sh/java"
+    else
+      JAVACMD="$JAVA_HOME/bin/java"
+    fi
+  else
+    JAVACMD="`which java`"
+  fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+  echo "Error: JAVA_HOME is not defined correctly."
+  echo "  We cannot execute $JAVACMD"
+  exit 1
+fi
+JAVAVER=`"$JAVACMD" -version 2>&1`
+
+case $JAVAVER in
+*1.[8-9]*) ;;
+*1.[1-7]*)
+	echo " Error: a Java 1.8 or higher JRE is required to run Migration tool; found [$JAVACMD -version == $JAVAVER]."
+	exit 1
+ ;;
+esac
+
+exec "$JAVACMD" -cp $CLASSPATH ${mainClass} $@
 
