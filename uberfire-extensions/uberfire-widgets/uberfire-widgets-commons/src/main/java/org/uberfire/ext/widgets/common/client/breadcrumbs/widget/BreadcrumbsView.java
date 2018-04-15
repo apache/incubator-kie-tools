1327fd6f0c35808d29ad65e780a3d0ef054ad4aa
nmirasch <neus.miras@gmail.com>
2018-04-16 01:45:14 +0200

    JBPM-6907: Last breadcrumb element should be text only (#294)

--- uberfire-extensions/uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/breadcrumbs/widget/BreadcrumbsView.java
+++ uberfire-extensions/uberfire-widgets/uberfire-widgets-commons/src/main/java/org/uberfire/ext/widgets/common/client/breadcrumbs/widget/BreadcrumbsView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package org.uberfire.ext.widgets.common.client.breadcrumbs.widget;
 import javax.inject.Inject;
 
 import org.jboss.errai.common.client.dom.Anchor;
+import org.jboss.errai.common.client.dom.ListItem;
 import org.jboss.errai.ui.client.local.api.IsElement;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
@@ -32,7 +33,12 @@ public class BreadcrumbsView implements UberElement<BreadcrumbsPresenter>,
 
     @Inject
     @DataField
-    Anchor breadcrumb;
+    ListItem breadcrumb;
+
+    @Inject
+    @DataField
+    Anchor breadcrumbLink;
+
     private BreadcrumbsPresenter presenter;
 
     @Override
@@ -43,17 +49,19 @@ public class BreadcrumbsView implements UberElement<BreadcrumbsPresenter>,
     @Override
     public void setup(String label,
                       Command clickCommand) {
-        breadcrumb.setTextContent(label);
-        breadcrumb.setOnclick((e) -> clickCommand.execute());
+        breadcrumbLink.setTextContent(label);
+        breadcrumbLink.setOnclick((e) -> clickCommand.execute());
     }
 
     @Override
     public void activate() {
         breadcrumb.setClassName("active");
+        breadcrumbLink.setClassName("breadcrumb-last");
     }
 
     @Override
     public void deactivate() {
         breadcrumb.setClassName("");
+        breadcrumbLink.setClassName("");
     }
 }
