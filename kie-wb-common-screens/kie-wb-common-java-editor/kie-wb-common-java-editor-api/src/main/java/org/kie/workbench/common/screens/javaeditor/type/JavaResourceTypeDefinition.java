6f3019ff62c3d52fd1ad161b9c5f0554f6934d8d
Jozef Marko <jomarko@redhat.com>
2018-08-24 20:50:29 +0200

    Fix licenses and imports (#2051)

--- kie-wb-common-screens/kie-wb-common-java-editor/kie-wb-common-java-editor-api/src/main/java/org/kie/workbench/common/screens/javaeditor/type/JavaResourceTypeDefinition.java
+++ kie-wb-common-screens/kie-wb-common-java-editor/kie-wb-common-java-editor-api/src/main/java/org/kie/workbench/common/screens/javaeditor/type/JavaResourceTypeDefinition.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,7 +20,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
-import org.guvnor.common.services.project.categories.Decision;
 import org.guvnor.common.services.project.categories.Model;
 import org.uberfire.backend.vfs.Path;
 import org.uberfire.workbench.annotations.VisibleAsset;
--- kie-wb-common-screens/kie-wb-common-project-imports-editor/kie-wb-common-project-imports-editor-api/src/main/java/org/kie/workbench/common/screens/projectimportsscreen/type/ProjectImportsResourceTypeDefinition.java
+++ kie-wb-common-screens/kie-wb-common-project-imports-editor/kie-wb-common-project-imports-editor-api/src/main/java/org/kie/workbench/common/screens/projectimportsscreen/type/ProjectImportsResourceTypeDefinition.java
@@ -3,24 +3,23 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package org.kie.workbench.common.screens.projectimportsscreen.type;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
 import org.uberfire.backend.vfs.Path;
-import org.uberfire.workbench.annotations.VisibleAsset;
 import org.uberfire.workbench.category.Category;
 import org.uberfire.workbench.category.Others;
 import org.uberfire.workbench.type.ResourceTypeDefinition;
