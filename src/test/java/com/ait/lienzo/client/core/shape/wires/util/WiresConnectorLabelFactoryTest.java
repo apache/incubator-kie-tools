5a7c6939fb38d0f2288efddcc36adf4762df7048
Roger Martínez <roger600@gmail.com>
2018-09-05 15:58:51 +0200

    JBPM-5566: [Wires] Labels on connectors - Tests (#41)

---
+++ src/test/java/com/ait/lienzo/client/core/shape/wires/util/WiresConnectorLabelFactoryTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ait.lienzo.client.core.shape.wires.util;
+
+import com.ait.lienzo.client.core.shape.IDirectionalMultiPointShape;
+import com.ait.lienzo.client.core.shape.Text;
+import com.ait.lienzo.client.core.shape.TextBoundsWrap;
+import com.ait.lienzo.client.core.shape.wires.WiresConnector;
+import com.ait.lienzo.client.core.shape.wires.util.WiresConnectorLabelFactory.Segment;
+import com.ait.lienzo.client.core.types.BoundingBox;
+import com.ait.lienzo.client.core.types.Point2D;
+import com.ait.lienzo.client.core.types.Point2DArray;
+import com.ait.lienzo.test.LienzoMockitoTestRunner;
+import com.ait.tooling.common.api.java.util.function.BiConsumer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(LienzoMockitoTestRunner.class)
+public class WiresConnectorLabelFactoryTest {
+
+    @Mock
+    private WiresConnector connector;
+
+    @Mock
+    private IDirectionalMultiPointShape line;
+
+    private Text text;
+    private Point2DArray points;
+
+    @Before
+    @SuppressWarnings("unchecked")
+    public void init() {
+        text = new Text("Doing some testing here");
+        points = new Point2DArray(new Point2D(0d, 0d), new Point2D(12.4d, 65.3d), new Point2D(312.4d, 432.d));
+        when(connector.getLine()).thenReturn(line);
+        when(line.getPoint2DArray()).thenReturn(points);
+    }
+
+    @Test
+    public void testSegmentLabelExecutor() {
+        Point2D start = new Point2D(10d, 10d);
+        Point2D end = new Point2D(20d, 30.5d);
+        WiresConnectorLabelFactory.SegmentLabelExecutor executor = new WiresConnectorLabelFactory.SegmentLabelExecutor();
+        Segment segment = new Segment(0, start, end);
+        executor.consumer().accept(segment, text);
+        assertNotNull(text.getWrapper());
+        TextBoundsWrap wrapper = (TextBoundsWrap) text.getWrapper();
+        assertEquals(new BoundingBox(0d, 0d, 22.808989455914087d, 11d), wrapper.getWrapBoundaries());
+        assertEquals(new Point2D(23.987684456439d, 15.865763679785857d), text.getLocation());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testFirstSegmentLabelExecutor() {
+        final boolean[] delegated = new boolean[]{false};
+        BiConsumer<Segment, Text> delegate = new BiConsumer<Segment, Text>() {
+            @Override
+            public void accept(Segment segment, Text text) {
+                assertEquals(0, segment.getIndex());
+                assertEquals(66.4669090600729d, segment.getLength(), 0d);
+                assertEquals(-4.900047462313102d, segment.getTetha(), 0d);
+                assertEquals(new Point2D(0d, 0d), segment.getStart());
+                assertEquals(new Point2D(12.4d, 65.3d), segment.getEnd());
+                delegated[0] = true;
+            }
+        };
+        WiresConnectorLabelFactory.FirstSegmentLabelExecutor executor = new WiresConnectorLabelFactory.FirstSegmentLabelExecutor(delegate);
+        executor.consumer().accept(connector, text);
+        assertTrue(delegated[0]);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testLongestSegmentLabelExecutor() {
+        final boolean[] delegated = new boolean[]{false};
+        BiConsumer<Segment, Text> delegate = new BiConsumer<Segment, Text>() {
+            @Override
+            public void accept(Segment segment, Text text) {
+                assertEquals(1, segment.getIndex());
+                assertEquals(473.7814791652371d, segment.getLength(), 0d);
+                assertEquals(-5.398073939261661d, segment.getTetha(), 0d);
+                assertEquals(new Point2D(12.4d, 65.3d), segment.getStart());
+                assertEquals(new Point2D(312.4d, 432.d), segment.getEnd());
+                delegated[0] = true;
+            }
+        };
+        WiresConnectorLabelFactory.LongestSegmentLabelExecutor executor = new WiresConnectorLabelFactory.LongestSegmentLabelExecutor(delegate);
+        executor.consumer().accept(connector, text);
+        assertTrue(delegated[0]);
+    }
+}
---
+++ src/test/java/com/ait/lienzo/client/core/shape/wires/util/WiresConnectorLabelTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ait.lienzo.client.core.shape.wires.util;
+
+import com.ait.lienzo.client.core.shape.Group;
+import com.ait.lienzo.client.core.shape.Layer;
+import com.ait.lienzo.client.core.shape.Text;
+import com.ait.lienzo.client.core.shape.wires.WiresConnector;
+import com.ait.lienzo.test.LienzoMockitoTestRunner;
+import com.ait.tooling.common.api.java.util.function.BiConsumer;
+import com.ait.tooling.common.api.java.util.function.Consumer;
+import com.ait.tooling.nativetools.client.event.HandlerRegistrationManager;
+import com.google.gwt.event.shared.HandlerRegistration;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(LienzoMockitoTestRunner.class)
+public class WiresConnectorLabelTest {
+
+    @Mock
+    private WiresConnector connector;
+
+    @Mock
+    private HandlerRegistrationManager registrationManager;
+
+    @Mock
+    private Group group;
+
+    @Mock
+    private Layer layer;
+
+    @Mock
+    private BiConsumer<WiresConnector, Text> executor;
+
+    @Mock
+    private Text text;
+
+    private WiresConnectorLabel tested;
+
+    @Before
+    public void setup() {
+        when(connector.getGroup()).thenReturn(group);
+        when(group.getLayer()).thenReturn(layer);
+        tested = new WiresConnectorLabel(text,
+                                         connector,
+                                         executor,
+                                         registrationManager);
+    }
+
+    @Test
+    public void testInit() {
+        verify(text, times(1)).setListening(eq(false));
+        verify(text, times(1)).setDraggable(eq(false));
+        verify(group, times(1)).add(eq(text));
+        verify(registrationManager, times(1)).register(any(HandlerRegistration.class));
+        verify(executor, times(1)).accept(eq(connector), eq(text));
+    }
+
+    @Test
+    public void testConfigure() {
+        final boolean[] configured = new boolean[]{false};
+        tested.configure(new Consumer<Text>() {
+            @Override
+            public void accept(Text t) {
+                assertEquals(text, t);
+                configured[0] = true;
+            }
+        });
+        assertTrue(configured[0]);
+        verifyRefresh();
+    }
+
+    @Test
+    public void testShow() {
+        tested.show();
+        verify(text, times(1)).setAlpha(eq(1d));
+        verifyRefresh();
+    }
+
+    @Test
+    public void testHide() {
+        tested.hide();
+        verify(text, times(1)).setAlpha(eq(0d));
+        verifyRefresh();
+    }
+
+    @Test
+    public void testDestroy() {
+        tested.destroy();
+        verify(text, times(1)).removeFromParent();
+        verify(registrationManager, times(1)).destroy();
+    }
+
+    private void verifyRefresh() {
+        verify(executor, atLeastOnce()).accept(eq(connector), eq(text));
+        verify(layer, atLeastOnce()).batch();
+    }
+}
