6ee9f517d947f1623821dc8b4f7472255e4ce7fb
Jan Hrcek <jhrcek@redhat.com>
2018-09-21 23:21:20 +0200

    Prevent NPE in WildflyGroupPropertiesManager (#477)

--- uberfire-extensions/uberfire-security/uberfire-security-management/uberfire-security-management-wildfly/src/main/java/org/uberfire/ext/security/management/wildfly/properties/WildflyGroupPropertiesManager.java
+++ uberfire-extensions/uberfire-security/uberfire-security-management/uberfire-security-management-wildfly/src/main/java/org/uberfire/ext/security/management/wildfly/properties/WildflyGroupPropertiesManager.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2016 Red Hat, Inc. and/or its affiliates.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package org.uberfire.ext.security.management.wildfly.properties;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -108,7 +109,9 @@ public class WildflyGroupPropertiesManager extends BaseWildflyPropertiesManager 
 
     @Override
     public void destroy() throws Exception {
-        this.groupsPropertiesFileLoader.stop();
+        if (groupsPropertiesFileLoader != null) {
+            this.groupsPropertiesFileLoader.stop();
+        }
     }
 
     @Override
@@ -230,7 +233,7 @@ public class WildflyGroupPropertiesManager extends BaseWildflyPropertiesManager 
                 }
             });
         } catch (Exception e) {
-            LOG.error("Error removing the folowing group names: " + identifiers,
+            LOG.error("Error removing the folowing group names: " + Arrays.toString(identifiers),
                       e);
             throw new SecurityManagementException(e);
         }
