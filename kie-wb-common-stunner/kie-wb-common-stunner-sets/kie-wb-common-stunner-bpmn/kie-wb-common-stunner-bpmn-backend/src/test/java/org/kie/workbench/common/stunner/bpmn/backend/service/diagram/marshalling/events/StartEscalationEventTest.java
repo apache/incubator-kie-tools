75c56d57889b51f06edaa6e9245966e1f8ab566d
Lubo Terifaj <lubo.terifaj@gmail.com>
2018-10-05 00:05:22 +0200

    JBPM-7752 Stunner - Create Marshaller tests for Escalation Start Events (#2114)
    * JBPM-7752 Stunner - Create Marshaller tests for Escalation Start Events
    
    * JBPM-7752 Stunner - change sub-process type for test cases

---
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/service/diagram/marshalling/events/StartEscalationEventTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.stunner.bpmn.backend.service.diagram.marshalling.events;
+
+import org.junit.Test;
+import org.kie.workbench.common.stunner.bpmn.backend.service.diagram.marshalling.Marshaller;
+import org.kie.workbench.common.stunner.bpmn.definition.StartEscalationEvent;
+import org.kie.workbench.common.stunner.bpmn.definition.property.event.escalation.InterruptingEscalationEventExecutionSet;
+import org.kie.workbench.common.stunner.core.diagram.Diagram;
+import org.kie.workbench.common.stunner.core.diagram.Metadata;
+import org.kie.workbench.common.stunner.core.graph.Graph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class StartEscalationEventTest extends StartEvent<StartEscalationEvent> {
+
+    private static final String BPMN_START_EVENT_FILE_PATH = "org/kie/workbench/common/stunner/bpmn/backend/service/diagram/escalationStartEvents.bpmn";
+
+    private static final String FILLED_TOP_LEVEL_EVENT_ID = "_364A2074-C369-4DB1-8934-69CF40B9E025";
+    private static final String EMPTY_TOP_LEVEL_EVENT_ID = "_CF77BB90-5F75-4246-9E17-21383063007D";
+    private static final String FILLED_SUBPROCESS_LEVEL_EVENT_ID = "_A983FF22-5128-4C06-9D47-AB7525DF8039";
+    private static final String EMPTY_SUBPROCESS_LEVEL_EVENT_ID = "_1DC85B0B-28D9-4EAC-A53C-F0C1303E2B19";
+
+    private static final int AMOUNT_OF_NODES_IN_DIAGRAM = 10;
+
+    public StartEscalationEventTest(Marshaller marshallerType) {
+        super(marshallerType);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallTopLevelEventFilledProperties() throws Exception {
+        final String EVENT_NAME = "Escalation event01 name ~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./";
+        final String EVENT_DOCUMENTATION = "Escalation event01 doc\n ~!@#$%^&*()_+`1234567890-={}|[]\\:\";'<>?,./";
+        final String ESCALATION_REF = "escalation01";
+        final String EVENT_DATA_OUTPUT = "||output:String||[dout]output->processGlobalVar";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartEscalationEvent filledTop = getStartNodeById(diagram, FILLED_TOP_LEVEL_EVENT_ID, StartEscalationEvent.class);
+        assertGeneralSet(filledTop.getGeneral(), EVENT_NAME, EVENT_DOCUMENTATION);
+        assertEscalationEventExecutionSet(filledTop.getExecutionSet(), ESCALATION_REF, INTERRUPTING);
+        assertDataIOSet(filledTop.getDataIOSet(), EVENT_DATA_OUTPUT);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallTopLevelEmptyEventProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartEscalationEvent emptyTop = getStartNodeById(diagram, EMPTY_TOP_LEVEL_EVENT_ID, StartEscalationEvent.class);
+        assertGeneralSet(emptyTop.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+        assertEscalationEventExecutionSet(emptyTop.getExecutionSet(), EMPTY_VALUE, INTERRUPTING);
+        assertDataIOSet(emptyTop.getDataIOSet(), EMPTY_VALUE);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallSubprocessLevelEventFilledProperties() throws Exception {
+        final String EVENT_NAME = "Escalation event02 name ~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./";
+        final String EVENT_DOCUMENTATION = "Escalation event02 doc\n ~!@#$%^&*()_+`1234567890-={}|[]\\:\";'<>?,./";
+        final String ESCALATION_REF = "escalation02";
+        final String EVENT_DATA_OUTPUT = "||output:String||[dout]output->processGlobalVar";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartEscalationEvent filledSubprocess = getStartNodeById(diagram, FILLED_SUBPROCESS_LEVEL_EVENT_ID, StartEscalationEvent.class);
+        assertGeneralSet(filledSubprocess.getGeneral(), EVENT_NAME, EVENT_DOCUMENTATION);
+        assertEscalationEventExecutionSet(filledSubprocess.getExecutionSet(), ESCALATION_REF, INTERRUPTING);
+        assertDataIOSet(filledSubprocess.getDataIOSet(), EVENT_DATA_OUTPUT);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallSubprocessLevelEventEmptyProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_START_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        StartEscalationEvent emptySubprocess = getStartNodeById(diagram, EMPTY_SUBPROCESS_LEVEL_EVENT_ID, StartEscalationEvent.class);
+        assertGeneralSet(emptySubprocess.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+        assertEscalationEventExecutionSet(emptySubprocess.getExecutionSet(), EMPTY_VALUE, NON_INTERRUPTING);
+        assertDataIOSet(emptySubprocess.getDataIOSet(), EMPTY_VALUE);
+    }
+
+    @Override
+    String getBpmnStartEventFilePath() {
+        return BPMN_START_EVENT_FILE_PATH;
+    }
+
+    @Override
+    String getFilledTopLevelEventId() {
+        return FILLED_TOP_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getEmptyTopLevelEventId() {
+        return EMPTY_TOP_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getFilledSubprocessLevelEventId() {
+        return FILLED_SUBPROCESS_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getEmptySubprocessLevelEventId() {
+        return EMPTY_SUBPROCESS_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    Class<StartEscalationEvent> getStartEventType() {
+        return StartEscalationEvent.class;
+    }
+
+    private void assertEscalationEventExecutionSet(InterruptingEscalationEventExecutionSet executionSet, String escalationRef, boolean isInterrupting) {
+        assertNotNull(executionSet);
+        assertNotNull(executionSet.getEscalationRef());
+        assertNotNull(executionSet.getIsInterrupting());
+        assertEquals(escalationRef, executionSet.getEscalationRef().getValue());
+        assertEquals(isInterrupting, executionSet.getIsInterrupting().getValue());
+    }
+}
