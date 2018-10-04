cd01e3279e73bd0cd4786b4ed2083fcd9465aada
Lubo Terifaj <lubo.terifaj@gmail.com>
2018-10-05 00:24:42 +0200

    JBPM-7754 Stunner - Create Marshaller tests for Escalation End Events (#2119)
    * JBPM-7754 Stunner - Create Marshaller tests for Escalation End Events
    
    * JBPM-7754 Stunner - rename test method

---
+++ kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/test/java/org/kie/workbench/common/stunner/bpmn/backend/service/diagram/marshalling/events/EndEscalationEventTest.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.stunner.bpmn.backend.service.diagram.marshalling.events;
+
+import org.junit.Test;
+import org.kie.workbench.common.stunner.bpmn.backend.service.diagram.marshalling.Marshaller;
+import org.kie.workbench.common.stunner.bpmn.definition.EndEscalationEvent;
+import org.kie.workbench.common.stunner.bpmn.definition.property.event.escalation.EscalationEventExecutionSet;
+import org.kie.workbench.common.stunner.core.diagram.Diagram;
+import org.kie.workbench.common.stunner.core.diagram.Metadata;
+import org.kie.workbench.common.stunner.core.graph.Graph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class EndEscalationEventTest extends EndEvent<EndEscalationEvent> {
+
+    private static final String BPMN_END_EVENT_FILE_PATH = "org/kie/workbench/common/stunner/bpmn/backend/service/diagram/endEscalationEvents.bpmn";
+
+    private static final String EMPTY_TOP_LEVEL_EVENT_ID = "_F666187F-57FF-40C6-8337-7EAB9F02F58D";
+    private static final String FILLED_TOP_LEVEL_EVENT_ID = "_99A5499F-FF6B-440B-83B5-256B7DAEC7A4";
+    private static final String EMPTY_SUBPROCESS_LEVEL_EVENT_ID = "_A695D9E9-0CA4-4B1F-8982-C34734D7AB89";
+    private static final String FILLED_SUBPROCESS_LEVEL_EVENT_ID = "_04E66211-0445-492E-830B-D20209E10CA8";
+
+    private static final String EMPTY_WITH_INCOME_TOP_LEVEL_EVENT_ID = "_22884D47-0B31-441D-A744-ED70AAFD0245";
+    private static final String FILLED_WITH_INCOME_TOP_LEVEL_EVENT_ID = "_DA67C887-2A9C-4C01-AD02-6CD326378456";
+    private static final String EMPTY_WITH_INCOME_SUBPROCESS_LEVEL_EVENT_ID = "_777E309C-04B8-4777-A0E1-DC0CEA537B88";
+    private static final String FILLED_WITH_INCOME_SUBPROCESS_LEVEL_EVENT_ID = "_3933F591-35FA-45A7-84DB-7DCE49FF87B7";
+
+    private static final int AMOUNT_OF_NODES_IN_DIAGRAM = 14;
+
+    public EndEscalationEventTest(Marshaller marshallerType) {
+        super(marshallerType);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallTopLevelEventFilledProperties() throws Exception {
+        final String EVENT_NAME = "Escalation event01 name ~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./";
+        final String EVENT_DOCUMENTATION = "Escalation event01 doc\n ~!@#$%^&*()_+`1234567890-={}|[]\\:\";'<>?,./";
+        final String EVENT_REF = "escEv01";
+        final String EVENT_DATA_OUTPUT = "input:String||||[din]processGlobalVar->input";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent filledTopEvent = getEndNodeById(diagram,
+                                                           FILLED_TOP_LEVEL_EVENT_ID,
+                                                           HAS_NO_INCOME_EDGE);
+        assertGeneralSet(filledTopEvent.getGeneral(), EVENT_NAME, EVENT_DOCUMENTATION);
+        assertEscalationEventExecutionSet(filledTopEvent.getExecutionSet(), EVENT_REF);
+        assertDataIOSet(filledTopEvent.getDataIOSet(), EVENT_DATA_OUTPUT);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallTopLevelEmptyEventProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent emptyTopEvent = getEndNodeById(diagram,
+                                                          EMPTY_TOP_LEVEL_EVENT_ID,
+                                                          HAS_NO_INCOME_EDGE);
+        assertGeneralSet(emptyTopEvent.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallSubprocessLevelEventFilledProperties() throws Exception {
+        final String EVENT_NAME = "Escalation event03 name ~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./";
+        final String EVENT_DOCUMENTATION = "Escalation event03 doc\n ~!@#$%^&*()_+`1234567890-={}|[]\\:\";'<>?,./";
+        final String EVENT_REF = "escEv03";
+        final String EVENT_DATA_OUTPUT = "input:String||||[din]processGlobalVar->input";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent filledSubprocessEvent = getEndNodeById(diagram,
+                                                                  FILLED_SUBPROCESS_LEVEL_EVENT_ID,
+                                                                  HAS_NO_INCOME_EDGE);
+        assertGeneralSet(filledSubprocessEvent.getGeneral(), EVENT_NAME, EVENT_DOCUMENTATION);
+        assertEscalationEventExecutionSet(filledSubprocessEvent.getExecutionSet(), EVENT_REF);
+        assertDataIOSet(filledSubprocessEvent.getDataIOSet(), EVENT_DATA_OUTPUT);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallSubprocessLevelEventEmptyProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent emptySubprocessEvent = getEndNodeById(diagram,
+                                                                 EMPTY_SUBPROCESS_LEVEL_EVENT_ID,
+                                                                 HAS_NO_INCOME_EDGE);
+        assertGeneralSet(emptySubprocessEvent.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallTopLevelEventWithIncomeFilledProperties() throws Exception {
+        final String EVENT_NAME = "Escalation event02 name ~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./";
+        final String EVENT_DOCUMENTATION = "Escalation event02 doc\n ~!@#$%^&*()_+`1234567890-={}|[]\\:\";'<>?,./";
+        final String EVENT_REF = "escEv02";
+        final String EVENT_DATA_OUTPUT = "input:String||||[din]processGlobalVar->input";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent filledSubprocessEvent = getEndNodeById(diagram,
+                                                                  FILLED_WITH_INCOME_TOP_LEVEL_EVENT_ID,
+                                                                  HAS_INCOME_EDGE);
+        assertGeneralSet(filledSubprocessEvent.getGeneral(), EVENT_NAME, EVENT_DOCUMENTATION);
+        assertEscalationEventExecutionSet(filledSubprocessEvent.getExecutionSet(), EVENT_REF);
+        assertDataIOSet(filledSubprocessEvent.getDataIOSet(), EVENT_DATA_OUTPUT);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallTopLevelEventWithIncomeEmptyProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent emptyEvent = getEndNodeById(diagram,
+                                                       EMPTY_WITH_INCOME_TOP_LEVEL_EVENT_ID,
+                                                       HAS_INCOME_EDGE);
+        assertGeneralSet(emptyEvent.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallSubprocessLevelEventWithIncomeEmptyProperties() throws Exception {
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent emptySubprocessEvent = getEndNodeById(diagram,
+                                                                 EMPTY_WITH_INCOME_SUBPROCESS_LEVEL_EVENT_ID,
+                                                                 HAS_INCOME_EDGE);
+        assertGeneralSet(emptySubprocessEvent.getGeneral(), EMPTY_VALUE, EMPTY_VALUE);
+    }
+
+    @Test
+    @Override
+    public void testUnmarshallSubprocessLevelEventWithIncomeFilledProperties() throws Exception {
+        final String EVENT_NAME = "Escalation event04 name ~!@#$%^&*()_+`-={}|[]\\:\";'<>?,./";
+        final String EVENT_DOCUMENTATION = "Escalation event04 doc\n ~!@#$%^&*()_+`1234567890-={}|[]\\:\";'<>?,./";
+        final String EVENT_REF = "escEv04";
+        final String EVENT_DATA_OUTPUT = "input:String||||[din]processGlobalVar->input";
+
+        Diagram<Graph, Metadata> diagram = unmarshall(marshaller, BPMN_END_EVENT_FILE_PATH);
+        assertDiagram(diagram, AMOUNT_OF_NODES_IN_DIAGRAM);
+
+        EndEscalationEvent filledSubprocessEvent = getEndNodeById(diagram,
+                                                                  FILLED_WITH_INCOME_SUBPROCESS_LEVEL_EVENT_ID,
+                                                                  HAS_INCOME_EDGE);
+        assertGeneralSet(filledSubprocessEvent.getGeneral(), EVENT_NAME, EVENT_DOCUMENTATION);
+        assertEscalationEventExecutionSet(filledSubprocessEvent.getExecutionSet(), EVENT_REF);
+        assertDataIOSet(filledSubprocessEvent.getDataIOSet(), EVENT_DATA_OUTPUT);
+    }
+
+    @Override
+    String getBpmnEndEventFilePath() {
+        return BPMN_END_EVENT_FILE_PATH;
+    }
+
+    @Override
+    Class<EndEscalationEvent> getEndEventType() {
+        return EndEscalationEvent.class;
+    }
+
+    @Override
+    String getFilledTopLevelEventId() {
+        return FILLED_TOP_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getEmptyTopLevelEventId() {
+        return EMPTY_TOP_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getFilledSubprocessLevelEventId() {
+        return FILLED_SUBPROCESS_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getEmptySubprocessLevelEventId() {
+        return EMPTY_SUBPROCESS_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getFilledTopLevelEventWithIncomeId() {
+        return FILLED_WITH_INCOME_TOP_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getEmptyTopLevelEventWithIncomeId() {
+        return EMPTY_WITH_INCOME_TOP_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getFilledSubprocessLevelEventWithIncomeId() {
+        return FILLED_WITH_INCOME_SUBPROCESS_LEVEL_EVENT_ID;
+    }
+
+    @Override
+    String getEmptySubprocessLevelEventWithIncomeId() {
+        return EMPTY_WITH_INCOME_SUBPROCESS_LEVEL_EVENT_ID;
+    }
+
+    private void assertEscalationEventExecutionSet(EscalationEventExecutionSet executionSet, String escalationRef) {
+        assertNotNull(executionSet);
+        assertNotNull(executionSet.getEscalationRef());
+        assertEquals(escalationRef, executionSet.getEscalationRef().getValue());
+    }
+}
