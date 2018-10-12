e8cc05625b69fc4d4f3eb7d8c054909211fb1d76
Walter Medvedeo <wmedvedeo@gmail.com>
2018-10-12 12:53:22 +0200

    JBPM-7532: Stunner - it is possible to corrupt diagram and lose all made changes (#43)

--- src/test/java/com/ait/lienzo/client/core/shape/PolyLineTest.java
+++ src/test/java/com/ait/lienzo/client/core/shape/PolyLineTest.java
@@ -22,21 +22,14 @@ import com.ait.lienzo.test.LienzoMockitoTestRunner;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 
 @RunWith(LienzoMockitoTestRunner.class)
 public class PolyLineTest {
-
-    @Mock
-    private Point2DArray point2DArray;
 
     private Attributes attributes;
 
@@ -49,28 +42,18 @@ public class PolyLineTest {
     }
 
     @Test
-    public void testParseWhenPointsIsNull() {
-
-        doReturn(null).when(attributes).getPoints();
-
-        final boolean parse = polyLine.parse(attributes);
-
-        assertFalse(parse);
-    }
-
-    @Test
     public void testParseWhenPointsSizeIsOne() {
 
-        doReturn(point2DArray).when(attributes).getPoints();
+        final Point2D point = new Point2D(1.0, 1.0);
+        final Point2DArray point2DArray = makePointArray(point);
 
-        when(point2DArray.size()).thenReturn(1);
-        when(point2DArray.noAdjacentPoints()).thenReturn(point2DArray);
+        doReturn(point2DArray).when(attributes).getPoints();
 
         final boolean parse = polyLine.parse(attributes);
 
         assertTrue(parse);
-        assertNull(polyLine.getTailOffsetPoint());
-        assertNull(polyLine.getHeadOffsetPoint());
+        assertEquals(point, polyLine.getTailOffsetPoint());
+        assertEquals(point, polyLine.getHeadOffsetPoint());
     }
 
     @Test
@@ -78,13 +61,9 @@ public class PolyLineTest {
 
         final Point2D point1 = new Point2D(1, 1);
         final Point2D point2 = new Point2D(2, 2);
+        final Point2DArray point2DArray = makePointArray(point1, point2);
 
         doReturn(point2DArray).when(attributes).getPoints();
-
-        when(point2DArray.size()).thenReturn(2);
-        when(point2DArray.noAdjacentPoints()).thenReturn(point2DArray);
-        when(point2DArray.get(0)).thenReturn(point1);
-        when(point2DArray.get(1)).thenReturn(point2);
 
         final boolean parse = polyLine.parse(attributes);
 
@@ -93,7 +72,27 @@ public class PolyLineTest {
         assertEquals(point2, polyLine.getTailOffsetPoint());
     }
 
+    @Test
+    public void testParseWhenStartAndEndpointsAreTheSame() {
+
+        final Point2D point1 = new Point2D(1, 1);
+        final Point2D point2 = new Point2D(1, 1);
+        Point2DArray point2DArray = makePointArray(point1, point2);
+
+        doReturn(point2DArray).when(attributes).getPoints();
+
+        final boolean parse = polyLine.parse(attributes);
+
+        assertTrue(parse);
+        assertEquals(point1, polyLine.getHeadOffsetPoint());
+        assertEquals(point1, polyLine.getTailOffsetPoint());
+    }
+
     private Attributes makeAttributes() {
         return spy(new Attributes(null));
+    }
+
+    private Point2DArray makePointArray(Point2D point, Point2D... points) {
+        return spy(new Point2DArray(point, points));
     }
 }
