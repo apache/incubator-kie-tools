4a5ff1365ece1a393c0c0410929e2414a7748770
Paulo Martins <paulovmr@gmail.com>
2019-04-05 08:50:00 -0400

    AF-1910: Localization of Library.properties (#2547)

--- kie-wb-common-screens/kie-wb-common-library/kie-wb-common-library-client/src/main/java/org/kie/workbench/common/screens/library/client/LibraryEntryPoint.java
+++ kie-wb-common-screens/kie-wb-common-library/kie-wb-common-library-client/src/main/java/org/kie/workbench/common/screens/library/client/LibraryEntryPoint.java
@@ -19,7 +19,7 @@ import org.jboss.errai.ioc.client.api.EntryPoint;
 import org.jboss.errai.ui.shared.api.annotations.Bundle;
 
 @EntryPoint
-@Bundle("resources/i18n/Library.properties")
+@Bundle("resources/i18n/LibraryConstants.properties")
 public class LibraryEntryPoint {
 
 }
