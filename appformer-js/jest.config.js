f50f035c4a1609f4e67e99aa077ada607b1f8259
Dominik Hanák <domhanak@users.noreply.github.com>
2019-05-21 18:37:31 +0200

    AF-1900: Make jest-junit report more jenkins friendly (#699)
    * AF-1900: Make jest-junit report more jenkins friendly
    
    * AF-1900: Improve the formatof junit.xml report
    
    * AF-1900: Remove ',' from test names
    
    * Add comment explaining the function modifying titles of test

--- appformer-js/jest.config.js
+++ appformer-js/jest.config.js
@@ -15,7 +15,32 @@
  */
 
 module.exports = {
-  reporters: ["default", "jest-junit"],
+  reporters: [
+        "default",
+                [
+                  "jest-junit",
+                   {
+                   /*
+                    * Removes spaces from test titles and makes first
+                    * letter of each word capitalized.
+                    *
+                    * unit test -> UnitTest
+                    *
+                    * See junit.xml report for resulting look.
+                    */
+                    titleTemplate: (vars) => {
+                            var str = vars.title.toLowerCase();
+                            str = str.split(' ');
+                            for (var i = 0; i < str.length; i++) {
+                                str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+                            }
+                            var result = str.join('');
+                            return result.replace(',');
+                        }
+
+                    }
+                ]
+  ],
   moduleDirectories: ["node_modules", "src"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   testRegex: "/__tests__/.*\\.test\\.(jsx?|tsx?)$",
