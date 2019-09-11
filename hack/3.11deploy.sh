9c520de14612c63c0ae43d0ce6ae640e793947f3
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2019-09-11 14:13:49 -0300

    [KOGITO-252] - Add an option to Kogito CLI to deploy the Data Index Service (#53)

--- hack/3.11deploy.sh
+++ hack/3.11deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for FILE in deploy/crds/app_v1alpha1_kogitoapp_crd.yaml deploy/role.yaml deploy/service_account.yaml deploy/role_binding.yaml deploy/operator.yaml
+for FILE in deploy/crds/app_v1alpha1_kogitoapp_crd.yaml deploy/crds/app_v1alpha1_kogitodataindex_crd.yaml deploy/role.yaml deploy/service_account.yaml deploy/role_binding.yaml deploy/operator.yaml
 do
 	oc apply -f ${FILE}
 done
