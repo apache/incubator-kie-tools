2da89771ac3a7c7ad2e9e32ee398a98ea70259f0
Tiago Bento <tiagobentofernandes@gmail.com>
2019-10-14 16:59:25 -0300

    Improve KogitoEditorIframe

--- packages/chrome-extension/src/app/components/common/KogitoEditorIframe.tsx
+++ packages/chrome-extension/src/app/components/common/KogitoEditorIframe.tsx
@@ -15,13 +15,13 @@
  */
 
 import * as React from "react";
-import { useContext, useEffect, useRef, useState } from "react";
-import { IsolatedEditorContext, IsolatedEditorContextType } from "./IsolatedEditorContext";
+import { useContext, useEffect, useMemo, useRef } from "react";
+import { IsolatedEditorContext } from "./IsolatedEditorContext";
 import { EnvelopeBusOuterMessageHandler } from "@kogito-tooling/microeditor-envelope-protocol";
 import { runScriptOnPage } from "../../utils";
 import { GlobalContext } from "./GlobalContext";
 
-const githubCodeMirrorEditorSelector = `.file-editor-textarea + .CodeMirror`;
+const GITHUB_CODEMIRROR_EDITOR_SELECTOR = `.file-editor-textarea + .CodeMirror`;
 const GITHUB_EDITOR_SYNC_POLLING_INTERVAL = 1500;
 
 export function KogitoEditorIframe(props: {
@@ -29,100 +29,81 @@ export function KogitoEditorIframe(props: {
   getFileContents: () => Promise<string | undefined>;
   readonly: boolean;
 }) {
-  const globalContext = useContext(GlobalContext);
-  const isolatedEditorContext = useContext(IsolatedEditorContext);
+  const { router, editorIndexPath } = useContext(GlobalContext);
+  const { textMode, fullscreen, onEditorReady } = useContext(IsolatedEditorContext);
   const ref = useRef<HTMLIFrameElement | null>(null);
-  const [polling, setPolling] = useState<number | undefined>();
-  const [prevIsolatedEditorContext, setPrevIsolatedEditorContext] = useState<IsolatedEditorContextType | undefined>();
-
-  const envelopeBusOuterMessageHandler = new EnvelopeBusOuterMessageHandler(
-    {
-      postMessage: msg => {
-        if (ref.current && ref.current.contentWindow) {
-          ref.current.contentWindow.postMessage(msg, globalContext.router.getTargetOrigin());
-        }
-      }
-    },
-    self => ({
-      pollInit() {
-        self.request_initResponse(window.location.origin);
-      },
-      receive_languageRequest() {
-        self.respond_languageRequest(globalContext.router.getLanguageData(props.openFileExtension));
-      },
-      receive_contentResponse(content: string) {
-        if (props.readonly) {
-          return;
-        }
-        runScriptOnPage(
-          `document.querySelector("${githubCodeMirrorEditorSelector}").CodeMirror.setValue('${content}')`
-        );
-      },
-      receive_contentRequest() {
-        props
-          .getFileContents()
-          .then(c => self.respond_contentRequest(c || ""))
-          .then(() => startPollingForChangesOnDiagram());
-      },
-      receive_setContentError() {
-        //TODO: Display a nice message with explanation why "setContent" failed
-        console.info("Set content error");
-      },
-      receive_dirtyIndicatorChange(isDirty: boolean) {
-        //TODO: Perhaps show window.alert to warn that the changes were not saved?
-        console.info(`Dirty indicator changed to ${isDirty}`);
-      },
-      receive_ready() {
-        console.info(`Editor is ready`);
-        if (isolatedEditorContext.onEditorReady) {
-          isolatedEditorContext.onEditorReady();
-        }
-      }
-    })
+  const envelopeBusOuterMessageHandler = useMemo(
+    () =>
+      new EnvelopeBusOuterMessageHandler(
+        {
+          postMessage: msg => {
+            if (ref.current && ref.current.contentWindow) {
+              ref.current.contentWindow.postMessage(msg, router.getTargetOrigin());
+            }
+          }
+        },
+        self => ({
+          pollInit() {
+            self.request_initResponse(window.location.origin);
+          },
+          receive_languageRequest() {
+            self.respond_languageRequest(router.getLanguageData(props.openFileExtension));
+          },
+          receive_contentResponse(content: string) {
+            if (props.readonly) {
+              return;
+            }
+            runScriptOnPage(
+              `document.querySelector("${GITHUB_CODEMIRROR_EDITOR_SELECTOR}").CodeMirror.setValue('${content}')`
+            );
+          },
+          receive_contentRequest() {
+            props.getFileContents().then(c => self.respond_contentRequest(c || ""));
+          },
+          receive_setContentError() {
+            //TODO: Display a nice message with explanation why "setContent" failed
+            console.info("Set content error");
+          },
+          receive_dirtyIndicatorChange(isDirty: boolean) {
+            //TODO: Perhaps show window.alert to warn that the changes were not saved?
+            console.info(`Dirty indicator changed to ${isDirty}`);
+          },
+          receive_ready() {
+            console.info(`Editor is ready`);
+            if (onEditorReady) {
+              onEditorReady();
+            }
+          }
+        })
+      ),
+    [ref, router, onEditorReady]
   );
-
-  function startPollingForChangesOnDiagram() {
-    if (props.readonly) {
-      return;
-    }
-
-    if (polling) {
-      return;
-    }
-
-    setPolling(
-      window.setInterval(
-        () => envelopeBusOuterMessageHandler.request_contentResponse(),
-        GITHUB_EDITOR_SYNC_POLLING_INTERVAL
-      )
-    );
-  }
-
-  function stopPollingForChangesOnDiagram() {
-    if (props.readonly) {
-      return;
-    }
-
-    clearInterval(polling);
-    setPolling(undefined);
-  }
 
   useEffect(
     () => {
-      console.info("OI");
-      if (isolatedEditorContext.textMode) {
-        stopPollingForChangesOnDiagram();
+      if (textMode) {
         envelopeBusOuterMessageHandler.request_contentResponse();
-      } else if (prevIsolatedEditorContext && prevIsolatedEditorContext.textMode !== isolatedEditorContext.textMode) {
-        props
-          .getFileContents()
-          .then(c => envelopeBusOuterMessageHandler.respond_contentRequest(c || ""))
-          .then(startPollingForChangesOnDiagram);
+        return;
       }
 
-      setPrevIsolatedEditorContext(isolatedEditorContext);
+      if (props.readonly) {
+        return;
+      }
+
+      let task: number;
+      Promise.resolve()
+        .then(() => props.getFileContents())
+        .then(c => envelopeBusOuterMessageHandler.respond_contentRequest(c || ""))
+        .then(() => {
+          task = window.setInterval(
+              () => envelopeBusOuterMessageHandler.request_contentResponse(),
+            GITHUB_EDITOR_SYNC_POLLING_INTERVAL
+          );
+        });
+
+      return () => clearInterval(task);
     },
-    [isolatedEditorContext]
+    [textMode, envelopeBusOuterMessageHandler]
   );
 
   useEffect(() => {
@@ -133,15 +114,14 @@ export function KogitoEditorIframe(props: {
     return () => {
       envelopeBusOuterMessageHandler.stopInitPolling();
       window.removeEventListener("message", listener);
-      stopPollingForChangesOnDiagram();
     };
   }, []);
 
   return (
     <iframe
       ref={ref}
-      className={`kogito-iframe ${isolatedEditorContext.fullscreen ? "fullscreen" : "not-fullscreen"}`}
-      src={globalContext.router.getRelativePathTo(globalContext.editorIndexPath)}
+      className={`kogito-iframe ${fullscreen ? "fullscreen" : "not-fullscreen"}`}
+      src={router.getRelativePathTo(editorIndexPath)}
     />
   );
 }
