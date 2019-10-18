40e8b887d3bdb1cdab8e591ca0369d9a9d0023b5
Tiago Bento <tiagobentofernandes@gmail.com>
2019-10-18 16:10:38 -0300

    Fix duplication on setContent

--- packages/chrome-extension/src/app/components/common/KogitoEditorIframe.tsx
+++ packages/chrome-extension/src/app/components/common/KogitoEditorIframe.tsx
@@ -86,7 +86,7 @@ const RefForwardingKogitoEditorIframe: React.RefForwardingComponent<IsolatedEdit
         })
       );
     },
-    [ref, router, onEditorReady]
+    [ref, router]
   );
 
   useEffect(
