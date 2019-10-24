7a5b9cb6d99bbd29bcb2793dee8fdf96e22f7cd7
Tiago Bento <tiagobentofernandes@gmail.com>
2019-10-24 19:57:34 -0300

    .

--- .github/actions/prerelease-tag/main.js
+++ .github/actions/prerelease-tag/main.js
@@ -1,30 +1,9 @@
-/*
- * Copyright 2019 Red Hat, Inc. and/or its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const tag = core.getInput('branch').replace("-prerelease");
+    core.setOutput("tag", tag);
 } catch (error) {
-    core.setF
+    core.setFailed(error.message);
+}
