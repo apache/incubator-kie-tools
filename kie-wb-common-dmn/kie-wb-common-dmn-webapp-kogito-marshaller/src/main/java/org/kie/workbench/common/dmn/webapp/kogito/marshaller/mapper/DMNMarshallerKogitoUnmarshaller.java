3ceddaf35ae9baa927413c3314cb5d5f65640c04
Michael Anstis <manstis@users.noreply.github.com>
2019-10-29 10:49:04 +0000

    KOGITO-404: [DMN Designer] Investigate files that cannot be opened (#2972)

--- kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-marshaller/src/main/java/org/kie/workbench/common/dmn/webapp/kogito/marshaller/mapper/DMNMarshallerKogitoUnmarshaller.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-marshaller/src/main/java/org/kie/workbench/common/dmn/webapp/kogito/marshaller/mapper/DMNMarshallerKogitoUnmarshaller.java
@@ -998,7 +998,27 @@ public class DMNMarshallerKogitoUnmarshaller {
             lrBound.setY(yOfShape(drgShape) + heightOfShape(drgShape));
         }
 
-        final JSIStyle drgStyle = Js.uncheckedCast(JsUtils.getUnwrappedElement(drgShape.getStyle()));
+        internalAugmentStyles(drgShape,
+                              bgset,
+                              fontSetSetter);
+
+        if (Objects.nonNull(drgShape.getDMNDecisionServiceDividerLine())) {
+            final JSIDMNDecisionServiceDividerLine divider = Js.uncheckedCast(drgShape.getDMNDecisionServiceDividerLine());
+            final List<JSIPoint> dividerPoints = divider.getWaypoint();
+            final JSIPoint dividerY = Js.uncheckedCast(dividerPoints.get(0));
+            decisionServiceDividerLineYSetter.accept(dividerY.getY());
+        }
+    }
+
+    private void internalAugmentStyles(final JSIDMNShape drgShape,
+                                       final BackgroundSet bgset,
+                                       final Consumer<FontSet> fontSetSetter) {
+        final JSIStyle jsiStyle = drgShape.getStyle();
+        if (Objects.isNull(jsiStyle)) {
+            return;
+        }
+
+        final JSIStyle drgStyle = Js.uncheckedCast(JsUtils.getUnwrappedElement(jsiStyle));
         final JSIDMNStyle dmnStyleOfDrgShape = JSIDMNStyle.instanceOf(drgStyle) ? Js.uncheckedCast(drgStyle) : null;
         if (Objects.nonNull(dmnStyleOfDrgShape)) {
             if (Objects.nonNull(dmnStyleOfDrgShape.getFillColor())) {
@@ -1013,6 +1033,7 @@ public class DMNMarshallerKogitoUnmarshaller {
         if (Objects.nonNull(dmnStyleOfDrgShape)) {
             mergeFontSet(fontSet, FontSetPropertyConverter.wbFromDMN(dmnStyleOfDrgShape));
         }
+
         if (Objects.nonNull(drgShape.getDMNLabel())) {
             final JSIDMNShape jsiLabel = Js.uncheckedCast(drgShape.getDMNLabel());
             final JSIStyle jsiLabelStyle = jsiLabel.getStyle();
@@ -1025,13 +1046,6 @@ public class DMNMarshallerKogitoUnmarshaller {
             }
         }
         fontSetSetter.accept(fontSet);
-
-        if (Objects.nonNull(drgShape.getDMNDecisionServiceDividerLine())) {
-            final JSIDMNDecisionServiceDividerLine divider = Js.uncheckedCast(drgShape.getDMNDecisionServiceDividerLine());
-            final List<JSIPoint> dividerPoints = divider.getWaypoint();
-            final JSIPoint dividerY = Js.uncheckedCast(dividerPoints.get(0));
-            decisionServiceDividerLineYSetter.accept(dividerY.getY());
-        }
     }
 
     private void mergeFontSet(final FontSet fontSet,
