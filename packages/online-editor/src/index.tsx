6148c948515487b5e7d11a13ed97e16e2df7994a
Paulo Martins <paulovmr@gmail.com>
2020-01-27 11:41:43 -0300

    KOGITO-860: Add error alerts for when Online Editor fails to open a file URL

--- packages/online-editor/src/index.tsx
+++ packages/online-editor/src/index.tsx
@@ -19,10 +19,26 @@ import * as ReactDOM from "react-dom";
 import { App } from "./App";
 import { EMPTY_FILE } from "./common/File";
 import { removeDirectories, removeFileExtension } from "./common/utils";
+import { Alert, AlertActionLink, AlertVariant } from "@patternfly/react-core";
 
 const urlParams = new URLSearchParams(window.location.search);
 
 if (urlParams.has("ext")) {
+  waitForEventWithFileData();
+} else if (urlParams.has("file")) {
+  openFileByUrl();
+} else {
+  openDefaultOnlineEditor();
+}
+
+function openDefaultOnlineEditor() {
+  ReactDOM.render(
+    <App iframeTemplateRelativePath={"envelope/index.html"} file={EMPTY_FILE} readonly={false} external={false} />,
+    document.getElementById("app")!
+  );
+}
+
+function waitForEventWithFileData() {
   window.addEventListener("loadOnlineEditor", (e: CustomEvent) => {
     const file = { fileName: e.detail.fileName, getFileContents: () => Promise.resolve(e.detail.fileContent) };
     ReactDOM.render(
@@ -38,11 +54,26 @@ if (urlParams.has("ext")) {
   });
 }
 
-if (urlParams.has("file")) {
+function openFileByUrl() {
   const filePath = urlParams.get("file")!;
+
+  fetch(filePath)
+    .then(response => {
+      if (response.ok) {
+        openFile(filePath, response.text());
+      } else {
+        showResponseError(response.status, response.statusText);
+      }
+    })
+    .catch(error => {
+      showFetchError(error.toString());
+    });
+}
+
+function openFile(filePath: string, getFileContent: Promise<string>) {
   const file = {
     fileName: removeFileExtension(removeDirectories(filePath)!)!,
-    getFileContents: () => fetch(filePath).then(response => Promise.resolve(response.text()))
+    getFileContents: () => getFileContent
   };
   ReactDOM.render(
     <App iframeTemplateRelativePath={"envelope/index.html"} file={file} readonly={false} external={false} />,
@@ -50,9 +81,48 @@ if (urlParams.has("file")) {
   );
 }
 
-if (!urlParams.has("ext") && !urlParams.has("file")) {
+function showResponseError(statusCode: number, description: string) {
   ReactDOM.render(
-    <App iframeTemplateRelativePath={"envelope/index.html"} file={EMPTY_FILE} readonly={false} external={false} />,
+    <div className={"kogito--alert-container"}>
+      <Alert
+        variant={AlertVariant.danger}
+        title="An error happened while fetching your file"
+        action={<AlertActionLink onClick={goToHomePage}>Go to Home Page</AlertActionLink>}
+      >
+        <br />
+        <b>Error details: </b>
+        {statusCode}
+        {statusCode && description && " - "}
+        {description}
+      </Alert>
+    </div>,
     document.getElementById("app")!
   );
+}
+
+function showFetchError(description: string) {
+  ReactDOM.render(
+    <div className={"kogito--alert-container"}>
+      <Alert
+        variant={AlertVariant.danger}
+        title="An unexpected error happened while trying to fetch your file"
+        action={<AlertActionLink onClick={goToHomePage} children={"Go to Home Page"} />}
+      >
+        <br />
+        <b>Error details: </b>
+        {description}
+        <br />
+        <br />
+        <b>Possible cause: </b>
+        The URL to your file must allow CORS in its response, which should contain the following header:
+        <br />
+        <pre>Access-Control-Allow-Origin: *</pre>
+      </Alert>
+    </div>,
+    document.getElementById("app")!
+  );
+}
+
+function goToHomePage() {
+  window.location.href = window.location.href.split("?")[0].split("#")[0];
 }
