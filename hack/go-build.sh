5c7d3f3eeb32fd9d65a73cd97e5c941ac87dfcc9
Tristan Radisson <tradisso@redhat.com>
2020-01-31 12:19:06 +0100

    [KOGITO-972] Allow to change the image-builder when building the operator (#158)

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -13,9 +13,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+SCRIPT_NAME=`basename $0`
+
+function usage(){
+  printf "Build the operator."
+  printf "\n"
+  printf "\n${SCRIPT_NAME} [options]*"
+  printf "\n"
+  printf "\nOptions:"
+  printf "\n"
+  printf "\n-h | --help\n\tPrint the usage of this script."
+  printf "\n--image_registry {IMAGE_REGISTRY}\n\tSet the Image Registry."
+  printf "\n--image_name {IMAGE_NAME}\n\tSet the Image Name."
+  printf "\n--image_tag {IMAGE_TAG}\n\tSet the Image Tag."
+  printf "\n--image_builder {IMAGE_BUILDER}\n\tSet the Image Builder to build the Operator image (docker [default], podman or buildah)."
+  printf "\n"
+}
 
 . ./hack/go-mod-env.sh
 
+# Default values
 REPO=https://github.com/kiegroup/kogito-cloud-operator
 BRANCH=master
 REGISTRY=quay.io/kiegroup
@@ -24,12 +41,46 @@ TAG=0.7.0
 TAR=${BRANCH}.tar.gz
 URL=${REPO}/archive/${TAR}
 CFLAGS="--redhat --build-tech-preview"
+BUILDER=
+
+while (( $# ))
+do
+case $1 in
+  --image_registry)
+    shift
+    if [[ ! ${1} =~ ^-.* ]] && [[ ! -z "${1}" ]]; then export REGISTRY="${1}"; shift; fi
+  ;;
+  --image_name)
+    shift
+    if [[ ! ${1} =~ ^-.* ]] && [[ ! -z "${1}" ]]; then export IMAGE="${1}"; shift; fi
+  ;;
+  --image_tag)
+    shift
+    if [[ ! ${1} =~ ^-.* ]] && [[ ! -z "${1}" ]]; then export TAG="${1}"; shift; fi
+  ;;
+  --image_builder)
+    shift
+    if [[ ! ${1} =~ ^-.* ]] && [[ ! -z "${1}" ]]; then export BUILDER="${1}"; shift; fi
+  ;;
+  -h|--help)
+    usage
+    exit 0
+  ;;
+  *)
+    echo "Unknown arguments: ${1}"
+    usage
+    exit 1
+  ;;
+esac
+done
 
 setGoModEnv
 go generate ./...
 if [[ -z ${CI} ]]; then
     ./hack/go-test.sh
-    operator-sdk build ${REGISTRY}/${IMAGE}:${TAG}
+    BUILD_PARAMS=""
+    if [[ ! -z ${BUILDER} ]]; then BUILD_PARAMS="${BUILD_PARAMS} --image-builder ${BUILDER}"; fi
+    operator-sdk build ${REGISTRY}/${IMAGE}:${TAG} ${BUILD_PARAMS}
     if [[ ${1} == "rhel" ]]; then
         if [[ ${LOCAL} != true ]]; then
             CFLAGS+=" --build-engine=osbs --build-osbs-target=??"
