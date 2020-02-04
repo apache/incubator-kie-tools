fe0b61704de9f371323e773c1e0117e2013c7ca6
Tristan Radisson <tradisso@redhat.com>
2020-02-04 12:45:43 +0100

    [KOGITO-947] CRDs files should be updated before launching the smoke tests and not during (#162)

--- hack/run-smoke.sh
+++ hack/run-smoke.sh
@@ -16,6 +16,15 @@
 
 # runs all smoke tests for the operator
 SCRIPT_NAME=`basename $0`
+SCRIPT_DIR=`dirname $0`
+
+MASTER_RAW_URL=https://raw.githubusercontent.com/kiegroup/kogito-cloud-operator/master/deploy
+CRD_FILES_TO_IMPORT=(
+  "app.kiegroup.org_kogitoapps_crd.yaml"
+  "app.kiegroup.org_kogitodataindices_crd.yaml"
+  "app.kiegroup.org_kogitoinfras_crd.yaml"
+  "app.kiegroup.org_kogitojobsservices_crd.yaml"
+  )
 
 function usage(){
   printf "Run smoke tests."
@@ -25,13 +34,42 @@ function usage(){
   printf "\nOptions:"
   printf "\n"
   printf "\n-h | --help\n\tPrint the usage of this script."
-  printf "\n--ope_name {URI}\n\tOperator image name."
-  printf "\n--ope_tag {URI}\n\tOperator image tag."
+  printf "\n--ope_name {NAME}\n\tOperator image name."
+  printf "\n--ope_tag {TAG}\n\tOperator image tag."
   printf "\n--maven_mirror {URI}\n\tMaven mirror url to be used when building app in the tests."
   printf "\n--feature {FEATURE_NAME}\n\tRun a specific feature file."
   printf "\n--local ${BOOLEAN}\n\tSpecify whether you run test in local"
   printf "\n-c or --concurrent ${NUMBER}\n\Set the number of concurrent tests. Default is 1."
   printf "\n"
+}
+
+function download_remote_crds(){
+  deploy_folder=${1}
+  url=${2}
+  echo "Download crd files into '${deploy_folder}' from ${url}"
+
+  mkdir -p "${deploy_folder}/crds"
+  for file in ${CRD_FILES_TO_IMPORT[*]}
+  do
+    curl -k -o "${deploy_folder}/crds/${file}" "${url}/crds/${file}"
+  done
+}
+
+function delete_and_apply_crds(){
+  deploy_folder=${1}
+
+  OLD_CLUSTER_CRDS=`oc get crds | grep kogito | awk -F' ' '{print $1}'`
+  for crd in ${OLD_CLUSTER_CRDS};
+  do
+    echo "Delete crds ${crd}"
+    oc delete crds ${crd}
+  done
+  for file in ${CRD_FILES_TO_IMPORT[*]}
+  do
+    crd_file="${deploy_folder}/crds/${file}"
+    echo "Apply crds file ${crd_file}"
+    oc apply -f ${crd_file}
+  done
 }
 
 FEATURE=""
@@ -78,8 +116,35 @@ done
 
 
 echo "-------- Retrieve godog"
+
 go get github.com/DATA-DOG/godog/cmd/godog
+
+
+echo "-------- Setup CRD files"
+
+deploy_folder="${SCRIPT_DIR}/../deploy"
+if [[ ! -z "${OPERATOR_DEPLOY_FOLDER}" ]]; then 
+  # Get crds files from URI if 
+  if [[ ${OPERATOR_DEPLOY_FOLDER} == http://* ]] || [[ ${OPERATOR_DEPLOY_FOLDER} == https://* ]]; then
+    url=${OPERATOR_DEPLOY_FOLDER}
+    deploy_folder=`mktemp -d`
+    download_remote_crds ${deploy_folder} ${url}
+  else
+    deploy_folder="${OPERATOR_DEPLOY_FOLDER}"
+  fi
+fi
+delete_and_apply_crds ${deploy_folder}
 
 echo "-------- Running smoke tests"
 
-cd test/smoke/ && godog -c ${CONCURRENT} --random -f progress ${FEATURE}
+cd ${SCRIPT_DIR}/../test/smoke/ && godog -c ${CONCURRENT} --random -f progress ${FEATURE}
+exit_code=$?
+echo "Tests finished with code ${exit_code}"
+
+echo "-------- Set back master CRD files"
+
+deploy_folder=`mktemp -d`
+download_remote_crds ${deploy_folder} ${MASTER_RAW_URL}
+delete_and_apply_crds ${deploy_folder}
+
+exit ${exit_code}
