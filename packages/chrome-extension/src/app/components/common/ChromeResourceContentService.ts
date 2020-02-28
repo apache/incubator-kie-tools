d192dac85df9ef6873ff0a330444262d89756e04
William <william.fatecsjc@gmail.com>
2020-02-28 14:30:37 -0300

    improvement in opts from chrome resource content service get

--- packages/chrome-extension/src/app/components/common/ChromeResourceContentService.ts
+++ packages/chrome-extension/src/app/components/common/ChromeResourceContentService.ts
@@ -53,10 +53,10 @@ class ChromeResourceContentService implements ResourceContentService {
     this.repoInfo = repoInfo;
   }
 
-  public get(path: string, opts: ResourceContentOptions): Promise<ResourceContent | undefined> {
-    opts = opts ? opts : { type: ContentType.TEXT };
-    return fetchFile(this.octokit, this.repoInfo.owner, this.repoInfo.repo, this.repoInfo.gitref, path, opts.type)
-      .then(resourceContent => new ResourceContent(path, resourceContent, opts.type))
+  public get(path: string, opts?: ResourceContentOptions): Promise<ResourceContent | undefined> {
+    opts = opts ?? { type: ContentType.TEXT };
+    return fetchFile(this.octokit, this.repoInfo.owner, this.repoInfo.repo, this.repoInfo.gitref, path, opts!.type)
+      .then(resourceContent => new ResourceContent(path, resourceContent, opts!.type))
       .catch(e => {
         console.debug(e);
         console.debug(`Error retrieving content from URI ${path}`);
