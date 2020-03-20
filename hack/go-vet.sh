848ea4b24c3b0ef2b680c2e471cb4b68326f9d13
Lucas Caparelli <lucas.caparelli112@gmail.com>
2020-03-20 09:24:32 -0300

    [KOGITO-1516] Allow "make vet" to work from any directory (#225)
    Use the canonical path ("github.com/kiegroup/kogito-cloud-operator")
    instead of the relative path as argument to the "-i" flag when running
    the openapi-gen command, allowing this script to be ran from any
    directory without making any modifications.

--- hack/go-vet.sh
+++ hack/go-vet.sh
@@ -24,8 +24,8 @@ if [[ -z ${CI} ]]; then
     which ./bin/openapi-gen > /dev/null || go build -o ./bin/openapi-gen k8s.io/kube-openapi/cmd/openapi-gen
     # generate the openapi files
     echo "Generating openapi files"
-    ./bin/openapi-gen --logtostderr=true -o "" -i ./pkg/apis/app/v1alpha1 -O zz_generated.openapi -p ./pkg/apis/app/v1alpha1 -h ./hack/boilerplate.go.txt -r "-"
-    ./bin/openapi-gen --logtostderr=true -o "" -i ./pkg/apis/kafka/v1beta1 -O zz_generated.openapi -p ./pkg/apis/kafka/v1beta1 -h ./hack/boilerplate.go.txt -r "-"
+    ./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1 -O zz_generated.openapi -p ./pkg/apis/app/v1alpha1 -h ./hack/boilerplate.go.txt -r "-"
+    ./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/kafka/v1beta1 -O zz_generated.openapi -p ./pkg/apis/kafka/v1beta1 -h ./hack/boilerplate.go.txt -r "-"
 
     operator-sdk generate csv --csv-version 0.9.0 --update-crds --operator-name kogito-operator
 fi
