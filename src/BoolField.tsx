8586374c3d6d39b1affe6a641d41bde77adcef41
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-02 11:04:54 +0100

    feat: add boolean field

---
+++ src/BoolField.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connectField, filterDOMProps } from 'uniforms';
+import { Checkbox, CheckboxProps } from '@patternfly/react-core';
+
+export type BoolFieldProps = {
+  appearance?: 'checkbox' | 'switch';
+  label?: string;
+  legend?: string;
+  onChange?: (value: any) => void;
+  transform?: (label?: string) => string;
+  disabled: boolean;
+} & Omit<CheckboxProps, 'isDisabled'>;
+
+const Bool = ({
+  disabled,
+  id,
+  inputRef,
+  label,
+  name,
+  onChange,
+  value,
+  ...props
+}) => (
+    <Checkbox
+      isChecked={!!value}
+      isDisabled={disabled}
+      id={id}
+      name={name}
+      onChange={() => disabled || onChange(!value)}
+      ref={inputRef}
+      label={label}
+      {...filterDOMProps(props)}
+    />
+  );
+
+export default connectField(Bool);
