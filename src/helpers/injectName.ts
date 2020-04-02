6e7701e4880bb59bcd1f54ed3c7c893b64aa9b20
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-02 13:29:35 +0100

    feat: add injectName unreleased changes

---
+++ src/helpers/injectName.ts
@@ -0,0 +1,22 @@
+import get from 'lodash/get';
+import { Children, ReactElement, ReactNode, cloneElement } from 'react';
+
+import { joinName } from 'uniforms';
+
+export default function injectName(
+  name: string,
+  children: ReactNode,
+  parent?: ReactNode,
+): ReactNode[] {
+  return Children.map(children, child => {
+    if (!child || typeof child !== 'object' || get(parent, 'props.name'))
+      return child;
+
+    // TODO: It's probably the only case left.
+    const element = child as ReactElement;
+    return cloneElement(element, {
+      children: injectName(name, element.props.children, child),
+      name: joinName(name, element.props.name),
+    });
+  });
+}
