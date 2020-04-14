d8a0112dd958150cc0a16324a19515d88e17987a
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-14 17:24:03 +0100

    fix: tsconfig module resolution

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,18 @@ module.exports = {
   collectCoverageFrom: ['src/*.{ts,tsx}'],
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
-    '^simpl-schema$': '<rootDir>/node_modules/simpl-schema',
+    '^uniforms$': '<rootDir>/node_modules/uniforms/src',
+    '^uniforms-bridge-simple-schema-2$': '<rootDir>/node_modules/uniforms-bridge-simple-schema-2/src',
     '^uniforms-patternfly$': '<rootDir>/src',
   },
   setupFiles: ['<rootDir>/setupEnzyme.js'],
-  testMatch: ['**/__tests__/**/!(_)*.{ts,tsx}', '!**/*.d.ts'],
+  testMatch: ['**/__tests__/**/!(_)*.{ts,tsx}', '!**/*.d.ts', '!**/helpers/*.ts'],
   moduleDirectories: [
     "node_modules",
     "<rootDir>/src"
   ],
+  preset: "ts-jest",
+  transformIgnorePatterns: ["node_modules/(?!uniforms)"],
   transform: {
     '^.+\\.(js|ts|tsx)$': './transform.js'
   }
