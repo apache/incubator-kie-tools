47f88b95771eeedf5b3054d6beabc34f51349432
Spolti <fspolti@redhat.com>
2020-04-24 12:31:42 -0300

    [KOGITO-2007] - Prepare Kogito Operator for next version iteration (#320)
    Signed-off-by: spolti <fspolti@redhat.com>

--- hack/go-vet.sh
+++ hack/go-vet.sh
@@ -27,6 +27,6 @@ if [[ -z ${CI} ]]; then
     ./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1 -O zz_generated.openapi -p ./pkg/apis/app/v1alpha1 -h ./hack/boilerplate.go.txt -r "-"
     ./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/kafka/v1beta1 -O zz_generated.openapi -p ./pkg/apis/kafka/v1beta1 -h ./hack/boilerplate.go.txt -r "-"
 
-    operator-sdk generate csv --csv-version 0.10.0 --update-crds --operator-name kogito-operator
+    operator-sdk generate csv --csv-version 0.11.0 --update-crds --operator-name kogito-operator
 fi
 go vet ./...
