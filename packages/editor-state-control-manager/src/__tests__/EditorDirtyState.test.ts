48b45ba9386d5fb0f9510a8dc49e5d7e2731e3e2
Luiz Motta <luizjoaomotta@gmail.com>
2020-05-18 13:32:42 -0300

    [KOGITO-1373] Add useEditorDirtyState tests

---
+++ packages/editor-state-control-manager/src/__tests__/EditorDirtyState.test.ts
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { renderHook } from "@testing-library/react-hooks";
+import { useEditorDirtyState } from "../EditorDirtyState";
+import { StateControl } from "../StateControl";
+import { act } from "react-test-renderer";
+
+describe("useEditorDirtyState", () => {
+  let stateControl: StateControl;
+  beforeEach(() => (stateControl = new StateControl()));
+
+  describe("false", () => {
+    it("after initialization", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      expect(result.current).toBeFalsy();
+    });
+
+    it("redo without any event to be redone", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      act(() => {
+        stateControl.redoEvent();
+      });
+
+      expect(result.current).toBeFalsy();
+    });
+
+    it("add event and save it", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      act(() => {
+        stateControl.updateEventStack("1");
+        stateControl.setSavedEvent();
+      });
+
+      expect(result.current).toBeFalsy();
+    });
+
+    it("add event and undo it", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      act(() => {
+        stateControl.updateEventStack("1");
+        stateControl.undoEvent();
+      });
+
+      expect(result.current).toBeFalsy();
+    });
+
+    it("add event, save it, undo and redo", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      act(() => {
+        stateControl.updateEventStack("1");
+        stateControl.setSavedEvent();
+        stateControl.undoEvent();
+        stateControl.redoEvent();
+      });
+
+      expect(result.current).toBeFalsy();
+    });
+  });
+
+  describe("true", () => {
+    it("add event without saving", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      act(() => {
+        stateControl.updateEventStack("1");
+      });
+
+      expect(result.current).toBeTruthy();
+    });
+
+    it("add event, undo and redo", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      act(() => {
+        stateControl.updateEventStack("1");
+        stateControl.undoEvent();
+        stateControl.redoEvent();
+      });
+
+      expect(result.current).toBeTruthy();
+    });
+
+    it("add event, save it, undo and new event", () => {
+      const { result } = renderHook(() => useEditorDirtyState(stateControl));
+
+      act(() => {
+        stateControl.updateEventStack("1");
+        stateControl.setSavedEvent();
+        stateControl.undoEvent();
+        stateControl.updateEventStack("2");
+      });
+
+      expect(result.current).toBeTruthy();
+    });
+  });
+});
