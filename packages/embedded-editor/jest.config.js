8f1989c813c2642abf54bc25b920ed7c5715756c
Michael Anstis <manstis@redhat.com>
2020-05-19 07:38:55 +0100

    Fix EmbeddedViewer tests

--- packages/embedded-editor/jest.config.js
+++ packages/embedded-editor/jest.config.js
@@ -15,10 +15,11 @@
  */
 
 module.exports = {
+  setupFiles: ["<rootDir>/jest/jest-env-setup.js"],
+  snapshotSerializers: ["<rootDir>/../../node_modules/enzyme-to-json/serializer"],
   reporters: ["default"],
   moduleDirectories: ["node_modules", "src"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-  setupFilesAfterEnv: ["./src/__tests__/jest.setup.ts"],
   testRegex: "/__tests__/.*\\.test\\.(jsx?|tsx?)$",
   transform: {
     "^.+\\.jsx?$": "babel-jest",
--- packages/embedded-editor/src/__tests__/embedded/EmbeddedViewer.test.tsx
+++ packages/embedded-editor/src/__tests__/embedded/EmbeddedViewer.test.tsx
@@ -14,91 +14,18 @@
  * limitations under the License.
  */
 
-import {
-  ChannelType,
-  ContentType,
-  LanguageData,
-  ResourceContentRequest,
-  ResourceListRequest,
-  ResourcesList,
-  ResourceContent
-} from "@kogito-tooling/core-api";
-import {
-  ResourceContentApi,
-  EditorEnvelopeController,
-  ResourceContentEditorCoordinator,
-  SpecialDomElements,
-  EnvelopeBusInnerMessageHandler
-} from "@kogito-tooling/microeditor-envelope";
-import { EnvelopeBusMessage, EnvelopeBusMessageType } from "@kogito-tooling/microeditor-envelope-protocol";
-import { render } from "@testing-library/react";
+import { ChannelType, ContentType, ResourceContentRequest, ResourceListRequest } from "@kogito-tooling/core-api";
+import { EnvelopeBusMessageType, EnvelopeBusOuterMessageHandler } from "@kogito-tooling/microeditor-envelope-protocol";
+import { mount, render } from "enzyme";
 import * as React from "react";
 import { EditorType } from "../../common/EditorTypes";
 import { File } from "../../common/File";
 import { EmbeddedEditorRouter } from "../../embedded/EmbeddedEditorRouter";
 import { EmbeddedViewer } from "../../embedded/EmbeddedViewer";
-import { DummyEditor } from "./DummyEditor";
-
-const StateControlMock = jest.fn(() => ({
-  undo: jest.fn(),
-  redo: jest.fn(),
-  registry: jest.fn()
-}));
-
-let stateControl: any;
-let resourceContentEditorCoordinator: ResourceContentEditorCoordinator;
-let loadingScreenContainer: HTMLElement;
-let envelopeContainer: HTMLElement;
-let controller: EditorEnvelopeController;
-let sentMessages: Array<EnvelopeBusMessage<any>>;
-
-beforeEach(() => {
-  loadingScreenContainer = document.body.appendChild(document.createElement("div"));
-  loadingScreenContainer.setAttribute("id", "loading-screen");
-
-  envelopeContainer = document.body.appendChild(document.createElement("div"));
-  envelopeContainer.setAttribute("id", "envelopeContainer");
-});
-
-beforeEach(() => {
-  sentMessages = [];
-  stateControl = new StateControlMock();
-  resourceContentEditorCoordinator = new ResourceContentEditorCoordinator();
-  controller = new EditorEnvelopeController(
-    {
-      postMessage: message => {
-        sentMessages.push(message);
-      }
-    },
-    {
-      createEditor(_: LanguageData) {
-        return Promise.resolve(new DummyEditor());
-      }
-    },
-    new SpecialDomElements(),
-    stateControl,
-    {
-      render: (element, container, callback) => {
-        callback();
-      }
-    },
-    resourceContentEditorCoordinator
-  );
-
-});
-
-afterEach(() => {
-  controller.stop();
-  loadingScreenContainer.remove();
-});
 
 const delay = (ms: number) => {
   return Promise.resolve().then(() => new Promise(res => setTimeout(res, ms)));
 };
-
-async function startController(): Promise<EnvelopeBusInnerMessageHandler> {
-  return controller.start(envelopeContainer);
-}
 
 async function incomingMessage(message: any) {
   window.postMessage(message, window.location.origin);
@@ -114,79 +41,54 @@ describe("EmbeddedViewer", () => {
   };
   const router: EmbeddedEditorRouter = new EmbeddedEditorRouter();
   const channelType: ChannelType = ChannelType.ONLINE;
+  const holder: HTMLElement = document.body.appendChild(document.createElement("div"));
 
-  afterEach(() => {
-    jest.clearAllMocks();
+  beforeEach(() => {
+    spyOn<any>(EnvelopeBusOuterMessageHandler, "generateRandomBusId");
   });
 
-  test("EmbeddedViewer::defaults", async () => {
+  test("EmbeddedViewer::defaults", () => {
     render(<EmbeddedViewer file={file} router={router} channelType={channelType} />);
 
     expect(document.body).toMatchSnapshot();
   });
 
-  test("EmbeddedViewer::init", async () => {
-    render(<EmbeddedViewer file={file} router={router} channelType={channelType} />);
-
-    await startController();
-
-    await incomingMessage({ type: EnvelopeBusMessageType.REQUEST_INIT, data: "test-target-origin" });
-
-    expect(sentMessages).toEqual([
-      { type: EnvelopeBusMessageType.RETURN_INIT, data: undefined },
-      { type: EnvelopeBusMessageType.REQUEST_LANGUAGE, data: undefined }
-    ]);
-
-  });
-
   test("EmbeddedViewer::onResourceContentRequest", async () => {
-    const onResourceContentRequest = jest.fn((request: ResourceContentRequest) => {
-      console.log("smsu");
-      return Promise.resolve({ path: "", type: ContentType.TEXT });
-    });
+    const onResourceContentRequest = jest.fn((request: ResourceContentRequest) =>
+      Promise.resolve({ path: "", type: ContentType.TEXT }));
 
-    render(<EmbeddedViewer
+    mount(<EmbeddedViewer
       file={file}
       router={router}
       channelType={channelType}
       onResourceContentRequest={onResourceContentRequest}
-    />);
+    />, { attachTo: holder });
 
-    //Start editor and wait for it to initialise
-    const messageBus: EnvelopeBusInnerMessageHandler = await startController();
-    await incomingMessage({ type: EnvelopeBusMessageType.REQUEST_INIT, data: "test-target-origin" });
-
-    //Make request for content and wait for it to complete
-    resourceContentEditorCoordinator.exposeApi(messageBus).get("");
-    await incomingMessage({ type: EnvelopeBusMessageType.RETURN_RESOURCE_CONTENT, data: { path: "" } });
-
-    expect(sentMessages).toEqual([
-      { type: EnvelopeBusMessageType.RETURN_INIT, data: undefined },
-      { type: EnvelopeBusMessageType.REQUEST_LANGUAGE, data: undefined },
-      { type: EnvelopeBusMessageType.REQUEST_RESOURCE_CONTENT, data: { path: "" } }
-    ]);
+    await incomingMessage({ type: EnvelopeBusMessageType.REQUEST_RESOURCE_CONTENT, data: { path: "" } });
 
     expect(onResourceContentRequest).toBeCalled();
+
+    expect(document.body).toMatchSnapshot();
   });
 
-  // test("EmbeddedViewer::onResourceListRequest", async () => {
-  //   const onResourceListRequest = jest.fn((request: ResourceListRequest) =>
-  //     Promise.resolve({ pattern: "", paths: [] })
-  //   );
+  test("EmbeddedViewer::onResourceListRequest", async () => {
+    const onResourceListRequest = jest.fn((request: ResourceListRequest) =>
+      Promise.resolve({ pattern: "", paths: [] })
+    );
 
-  //   const r = await startController(
-  //     <EmbeddedViewer
-  //       file={file}
-  //       router={router}
-  //       channelType={channelType}
-  //       onResourceListRequest={onResourceListRequest}
-  //     />
-  //   );
+    mount(
+      <EmbeddedViewer
+        file={file}
+        router={router}
+        channelType={channelType}
+        onResourceListRequest={onResourceListRequest}
+      />, { attachTo: holder }
+    );
 
-  //   await incomingMessage({ type: EnvelopeBusMessageType.RETURN_RESOURCE_LIST, data: { pattern: "", paths: [] } });
+    await incomingMessage({ type: EnvelopeBusMessageType.REQUEST_RESOURCE_LIST, data: { pattern: "", paths: [] } });
 
-  //   expect(sentMessages).toEqual([
-  //     { type: EnvelopeBusMessageType.RETURN_INIT, data: undefined }
-  //   ]);
-  // });
+    expect(onResourceListRequest).toBeCalled();
+
+    expect(document.body).toMatchSnapshot();
+  });
 });
