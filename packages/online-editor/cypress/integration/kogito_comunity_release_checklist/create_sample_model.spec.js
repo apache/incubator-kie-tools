ad61e2d90c2dabe41a47c344103fa9f542cca9e4
Jozef Marko <jomarko@redhat.com>
2020-06-16 15:31:49 +0200

    KOGITO-204: Unify locating elements in online channel test
    This PR is dependent on these PRs:
    - https://github.com/kiegroup/kogito-tooling/pull/168
    - https://github.com/kiegroup/appformer/pull/987
    - https://github.com/kiegroup/kie-wb-common/pull/3333
    
    This PR tries to unify locating elements by OUIA standard.
    
    For more details see: https://issues.redhat.com/browse/KOGITO-204

--- packages/online-editor/cypress/integration/kogito_comunity_release_checklist/create_sample_model.spec.js
+++ packages/online-editor/cypress/integration/kogito_comunity_release_checklist/create_sample_model.spec.js
@@ -1,6 +1,6 @@
 var cyOnlineEditorUtils = require('../../utils/general')
 
-context('Check sample models successfully created', () => {
+context('Check sample models successfully opened', () => {
 
   beforeEach(() => {
     // visit home page of the Online Editor
@@ -10,19 +10,22 @@ context('Check sample models successfully created', () => {
   it('Try BPMN sample', () => {
     cyOnlineEditorUtils.trySample('bpmn')
 
-    cyOnlineEditorUtils.getDiagramEditorBody().find('[data-title="Explore Diagram"]').click();
     cyOnlineEditorUtils.getDiagramEditorBody()
-      .find('[data-field="explorerPanelBody"]')
-      .contains("Process travelers")
+      .find('[data-ouia-component-id$="ProjectDiagramExplorerScreen"] button')
+      .click();
+    cyOnlineEditorUtils.getDiagramEditorBody()
+      .find('[data-ouia-component-id="diagram-tree-explorer"]')
+      .find('[data-ouia-component-id$="tree-item-Process travelers"]')
   })
 
   it('Try DMN sample', () => {
     cyOnlineEditorUtils.trySample('dmn')
 
-    cyOnlineEditorUtils.getDiagramEditorBody().find('.fa-chevron-right').click();
     cyOnlineEditorUtils.getDiagramEditorBody()
-      .find('li[data-i18n-prefix="DecisionNavigatorTreeView."]')
-      .should("have.attr", "title", "loan_pre_qualification");
+      .find('button[data-ouia-component-id$="dmn.decision.navigator"]')
+      .click();
+    cyOnlineEditorUtils.getDiagramEditorBody()
+      .find('[data-ouia-component-id$="qe-dmn-graph-navigator-node-loan_pre_qualification"]')
 
   })
 })
