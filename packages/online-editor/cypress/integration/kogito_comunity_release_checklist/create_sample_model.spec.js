f935b2feaddeaa67b84ac669438459d1fb287d73
Jozef Marko <jomarko@redhat.com>
2020-06-18 21:17:10 +0200

    Peer Review: simplify test using cypress 'within' context

--- packages/online-editor/cypress/integration/kogito_comunity_release_checklist/create_sample_model.spec.js
+++ packages/online-editor/cypress/integration/kogito_comunity_release_checklist/create_sample_model.spec.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// <reference types="Cypress" />
 context('Check sample models successfully created', () => {
 
   beforeEach(() => {
@@ -8,19 +25,20 @@ context('Check sample models successfully created', () => {
   it('Try BPMN sample', () => {
     cy.trySampleModel('bpmn')
 
-    cy.getDiagramEditorBody().find('[data-title="Explore Diagram"]').click();
-    cy.getDiagramEditorBody()
-      .find('[data-field="explorerPanelBody"]')
-      .contains("Process travelers")
+    cy.getDiagramEditorBody().within(($diagramEditor) => {
+      cy.get('[data-title="Explore Diagram"]').click();
+      cy.get('[data-field="explorerPanelBody"]')
+        .contains("Process travelers")
+    })
   })
 
   it('Try DMN sample', () => {
     cy.trySampleModel('dmn')
 
-    cy.getDiagramEditorBody().find('.fa-chevron-right').click();
-    cy.getDiagramEditorBody()
-      .find('li[data-i18n-prefix="DecisionNavigatorTreeView."]')
-      .should("have.attr", "title", "loan_pre_qualification");
-
+    cy.getDiagramEditorBody().within(($diagramEditor) => {
+      cy.get('.fa-chevron-right').click();
+      cy.get('li[data-i18n-prefix="DecisionNavigatorTreeView."]')
+        .should("have.attr", "title", "loan_pre_qualification");
+    })
   })
 })
