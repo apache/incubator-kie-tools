e11d83649430500da227eb5c8ea66db24045ab0b
Tarun Khandelwal <tarkhand@redhat.com>
2020-06-19 19:50:08 +0530

    [KOGITO-2406] - Add Test Coverage to the Operator PR check CI (#389)
    * [KOGITO-2406] - Add Test Coverage to the Operator PR check CI
    
    SEE: https://issues.redhat.com/browse/KOGITO-2406
    
    Signed-off-by: Tarun Khandelwal <tarkhand@redhat.com>

--- hack/go-lint.sh
+++ hack/go-lint.sh
@@ -20,6 +20,6 @@ fi
 rm -f golint_errors
 # The command in or will fetch the latest tag available for golangci-lint and install in $GOPATH/bin/
 which golangci-lint > /dev/null || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
-golangci-lint run ./... --enable golint
+golangci-lint run ./... --enable golint --timeout 2m0s
 
 exit ${code:0}
