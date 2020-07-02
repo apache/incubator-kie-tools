49d0852ed8358d558ca3739ff4c5792a2ec7e29a
Tarun Khandelwal <tarkhand@redhat.com>
2020-07-03 00:04:55 +0530

    Fixed Vet Test (#413)

--- hack/go-vet.sh
+++ hack/go-vet.sh
@@ -14,19 +14,18 @@
 # limitations under the License.
 
 
-if [[ -z ${CI} ]]; then
-    ./hack/go-mod.sh
-    # enforce GOROOT
-    export GOROOT=$(go env GOROOT)
-    operator-sdk generate k8s
-    operator-sdk generate crds --crd-version=v1beta1
-    # get the openapi binary
-    which ./bin/openapi-gen > /dev/null || go build -o ./bin/openapi-gen k8s.io/kube-openapi/cmd/openapi-gen
-    # generate the openapi files
-    echo "Generating openapi files"
-    ./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1 -O zz_generated.openapi -p ./pkg/apis/app/v1alpha1 -h ./hack/boilerplate.go.txt -r "-"
-    ./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/kafka/v1beta1 -O zz_generated.openapi -p ./pkg/apis/kafka/v1beta1 -h ./hack/boilerplate.go.txt -r "-"
+./hack/go-mod.sh
+# enforce GOROOT
+export GOROOT=$(go env GOROOT)
+operator-sdk generate k8s
+operator-sdk generate crds --crd-version=v1beta1
+# get the openapi binary
+which ./bin/openapi-gen > /dev/null || go build -o ./bin/openapi-gen k8s.io/kube-openapi/cmd/openapi-gen
+# generate the openapi files
+echo "Generating openapi files"
+./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/app/v1alpha1 -O zz_generated.openapi -p ./pkg/apis/app/v1alpha1 -h ./hack/boilerplate.go.txt -r "-"
+./bin/openapi-gen --logtostderr=true -o "" -i github.com/kiegroup/kogito-cloud-operator/pkg/apis/kafka/v1beta1 -O zz_generated.openapi -p ./pkg/apis/kafka/v1beta1 -h ./hack/boilerplate.go.txt -r "-"
 
-    operator-sdk generate csv --csv-version 0.12.0 --update-crds --operator-name kogito-operator
-fi
+operator-sdk generate csv --csv-version 0.12.0 --update-crds --operator-name kogito-operator
+
 go vet ./...
