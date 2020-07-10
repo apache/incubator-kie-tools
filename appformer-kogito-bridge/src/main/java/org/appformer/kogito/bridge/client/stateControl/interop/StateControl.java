f1b8b084afed85ca5c23c23eecdb965db63da87c
Tiago Bento <1584568+tiagobento@users.noreply.github.com>
2020-07-10 10:22:46 -0300

    KOGITO-2601: Move State Control API to kie-bc-editors (#997)
    * Move window.envelope.stateControl to window.gwtStateControl
    
    * Changed API a bit

--- appformer-kogito-bridge/src/main/java/org/appformer/kogito/bridge/client/stateControl/interop/StateControl.java
+++ appformer-kogito-bridge/src/main/java/org/appformer/kogito/bridge/client/stateControl/interop/StateControl.java
@@ -24,12 +24,11 @@ import org.appformer.kogito.bridge.client.stateControl.registry.interop.KogitoJS
 /**
  * Represents the TypeScript StateControl engine present on the Envelope
  */
-@JsType(isNative = true, namespace = "window", name = "envelope")
+@JsType(isNative = true, namespace = "window", name = "gwt")
 public class StateControl {
 
     /**
      * Retrieves the {@link KogitoJSCommandRegistry}
-     *
      * @param <C> Anything that can be considered a command
      * @return The {@link KogitoJSCommandRegistry}
      */
@@ -39,7 +38,6 @@ public class StateControl {
     /**
      * Sets the {@link StateControlCommand} that will be called when the StateControl engine is notified to undo last
      * command on the {@link KogitoJSCommandRegistry}
-     *
      * @param command The command to execute on undo
      */
     @JsMethod
@@ -48,12 +46,11 @@ public class StateControl {
     /**
      * Sets the {@link StateControlCommand} that will be called when the StateControl engine is notified to redo last
      * undone command on the {@link KogitoJSCommandRegistry}
-     *
      * @param command The command to execute on redo
      */
     @JsMethod
     public native void setRedoCommand(StateControlCommand command);
 
     @JsProperty(name = "stateControl")
-    public static native StateControl get();
+    public native static StateControl get();
 }
