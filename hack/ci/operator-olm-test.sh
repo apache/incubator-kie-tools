e4afaa447d720f510edb92dc22e2d658ae69982e
Tarun Khandelwal <tarkhand@redhat.com>
2020-07-21 17:37:16 +0530

    [KOGITO-2825] - Change operator-testing image to latest (#463)
    * [KOGITO-2825] - Change operator-testing image to latest
    
    * Run olm change if change in hack/ci

--- hack/ci/operator-olm-test.sh
+++ hack/ci/operator-olm-test.sh
@@ -30,7 +30,7 @@ INSTALL_MODE="SingleNamespace"
 # See: https://github.com/operator-framework/operator-courier/issues/188
 # Needs to be changed back to quay.io/operator-framework/operator-testing:latest
 # after the issue has been resolved and the new image has been built
-OPERATOR_TESTING_IMAGE="docker.io/tarun18/operator-testing:latest"
+OPERATOR_TESTING_IMAGE="quay.io/operator-framework/operator-testing:latest"
 
 if [ -z ${KUBECONFIG} ]; then
     KUBECONFIG=${HOME}/.kube/config
