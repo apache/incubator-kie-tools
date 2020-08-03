b568a5bee512770c7acfa8ab9800ec248b287407
Michael Anstis <manstis@redhat.com>
2020-08-03 16:44:41 +0100

    FAI-235: PMML: Enhance marshaller for DMN's use case

--- packages/pmml-editor-marshaller/src/marshaller/jsonata/JSON2UI.ts
+++ packages/pmml-editor-marshaller/src/marshaller/jsonata/JSON2UI.ts
@@ -13,10 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import { ANOMALY_DETECTION_MODEL } from "./json2ui/AnomalyDetectionModel";
+import { ASSOCIATION_MODEL } from "./json2ui/AssociationModel";
+import { BASELINE_MODEL } from "./json2ui/BaselineModel";
+import { BAYESIAN_NETWORK_MODEL } from "./json2ui/BayesianNetworkModel";
+import { CLUSTERING_MODEL } from "./json2ui/ClusteringModel";
 import { DATA_DICTIONARY } from "./json2ui/DataDictionary";
+import { GAUSSIAN_PROCESS_MODEL } from "./json2ui/GaussianProcessModel";
+import { GENERAL_REGRESSION_MODEL } from "./json2ui/GeneralRegressionModel";
 import { HEADER } from "./json2ui/Header";
-import { SCORE_CARD } from "./json2ui/Scorecard";
+import { MINING_MODEL } from "./json2ui/MiningModel";
+import { NAIVE_BAYES_MODEL } from "./json2ui/NaiveBayesModel";
+import { NEAREST_NEIGHBOR_MODEL } from "./json2ui/NearestNeighborModel";
+import { NEURAL_NETWORK_MODEL } from "./json2ui/NeuralNetwork";
 import { REGRESSION_MODEL } from "./json2ui/RegressionModel";
+import { RULE_SET_MODEL } from "./json2ui/RuleSetModel";
+import { SCORE_CARD } from "./json2ui/Scorecard";
+import { SEQUENCE_MODEL } from "./json2ui/SequenceModel";
+import { SUPPORT_VECTOR_MACHINE_MODEL } from "./json2ui/SupportVectorMachineModel";
+import { TEXT_MODEL } from "./json2ui/TextModel";
+import { TREE_MODEL } from "./json2ui/TreeModel";
 
 export const JSON2UI_TRANSFORMATION: string = `(
   $bootstrap := function($node) {
@@ -25,10 +42,55 @@ export const JSON2UI_TRANSFORMATION: string = `(
       ${DATA_DICTIONARY}, 
       "models": $singletonArray(
         $append(
-          [],
+          ${ANOMALY_DETECTION_MODEL},
           $append(
-              ${SCORE_CARD}, 
-              ${REGRESSION_MODEL}
+            ${ASSOCIATION_MODEL},
+            $append(
+              ${BASELINE_MODEL},
+              $append(
+                ${BAYESIAN_NETWORK_MODEL},
+                $append(
+                  ${CLUSTERING_MODEL},
+                  $append(
+                    ${GAUSSIAN_PROCESS_MODEL},
+                    $append(
+                      ${GENERAL_REGRESSION_MODEL},
+                      $append(
+                        ${MINING_MODEL},
+                        $append(
+                          ${NAIVE_BAYES_MODEL},
+                          $append(
+                            ${NEAREST_NEIGHBOR_MODEL},
+                            $append(
+                              ${NEURAL_NETWORK_MODEL},
+                              $append(
+                                ${REGRESSION_MODEL},
+                                $append(
+                                  ${RULE_SET_MODEL},
+                                  $append(
+                                    ${SCORE_CARD},
+                                    $append(
+                                      ${SEQUENCE_MODEL},
+                                      $append(
+                                        ${SUPPORT_VECTOR_MACHINE_MODEL},
+                                        $append(
+                                          ${TEXT_MODEL},
+                                          ${TREE_MODEL}
+                                        )
+                                      )
+                                    )
+                                  )
+                                )
+                              )
+                            )
+                          )
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+            )
           )
         )
       )
--- packages/pmml-editor-marshaller/src/marshaller/jsonata/json2ui/index.ts
+++ packages/pmml-editor-marshaller/src/marshaller/jsonata/json2ui/index.ts
@@ -13,14 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+export * from "./AnomalyDetectionModel";
+export * from "./AssociationModel";
+export * from "./BaselineModel";
+export * from "./BayesianNetworkModel";
+export * from "./ClusteringModel";
 export * from "./DataDictionary";
+export * from "./GaussianProcessModel";
+export * from "./GeneralRegressionModel";
 export * from "./Header";
 export * from "./LocalTransformations";
+export * from "./MiningModel";
 export * from "./MiningSchema";
 export * from "./ModelExplanation";
 export * from "./ModelStats";
 export * from "./ModelVerification";
+export * from "./NaiveBayesModel";
+export * from "./NearestNeighborModel";
+export * from "./NeuralNetwork";
 export * from "./Output";
 export * from "./RegressionModel";
+export * from "./RuleSetModel";
 export * from "./Scorecard";
+export * from "./SequenceModel";
+export * from "./SupportVectorMachineModel";
 export * from "./Targets";
+export * from "./TextModel";
+export * from "./TreeModel";
