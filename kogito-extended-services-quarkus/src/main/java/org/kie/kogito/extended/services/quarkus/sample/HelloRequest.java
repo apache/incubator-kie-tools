7ff4e76ca6905cb8bc474f6c0d671efed17a575d
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2020-08-12 10:54:42 -0300

    KOGITO-1808: Add base code

---
+++ kogito-extended-services-quarkus/src/main/java/org/kie/kogito/extended/services/quarkus/sample/HelloRequest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.kogito.extended.services.quarkus.sample;
+
+/**
+ * Sample request.
+ */
+public class HelloRequest {
+
+    private String name;
+
+    private int delay;
+
+    public HelloRequest() {
+    }
+
+    public HelloRequest(String name, int delay) {
+        this.name = name;
+        this.delay = delay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+}
