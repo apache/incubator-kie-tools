99fa4cb57a152bbf5e73195a811ce4a0f55ddbdf
Michael Anstis <manstis@users.noreply.github.com>
2020-10-01 13:48:01 +0100

    FAI-253: Score Cards: Research feasibility of reducers for undo/redo (#272)
    * FAI-253: Score Cards: Research feasibility of reducers for undo/redo

---
+++ packages/pmml-editor/src/editor/reducers/DataFieldReducer.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { ActionMap, Actions } from "./Actions";
+import { HistoryService } from "../history/HistoryProvider";
+import { DataField, FieldName } from "@kogito-tooling/pmml-editor-marshaller";
+import { Reducer } from "react";
+import { HistoryAwareReducer } from "../history/HistoryAwareReducer";
+
+interface DataFieldPayload {
+  [Actions.SetDataFieldName]: {
+    readonly index: number;
+    readonly name: FieldName;
+  };
+}
+
+export type DataFieldActions = ActionMap<DataFieldPayload>[keyof ActionMap<DataFieldPayload>];
+
+export const DataFieldReducer: HistoryAwareReducer<DataField[], DataFieldActions> = (
+  service: HistoryService
+): Reducer<DataField[], DataFieldActions> => {
+  return (state: DataField[], action: DataFieldActions) => {
+    switch (action.type) {
+      case Actions.SetDataFieldName:
+        return service.mutate(state, `DataDictionary.DataField`, draft => {
+          const index: number = action.payload.index;
+          if (index >= 0 && index < draft.length) {
+            draft[index].name = action.payload.name;
+          }
+        });
+    }
+
+    return state;
+  };
+};
