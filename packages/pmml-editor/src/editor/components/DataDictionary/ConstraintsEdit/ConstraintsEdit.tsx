bc78eafa6a655e5f384105dec167b8ca2a73eec7
roberto emanuel <kelvah@gmail.com>
2020-10-27 19:18:09 +0100

    data-dictionary: changing constraint type selection to select input, adding basic manual validation

--- packages/pmml-editor/src/editor/components/DataDictionary/ConstraintsEdit/ConstraintsEdit.tsx
+++ packages/pmml-editor/src/editor/components/DataDictionary/ConstraintsEdit/ConstraintsEdit.tsx
@@ -10,7 +10,9 @@ import {
   FormGroup,
   Grid,
   GridItem,
-  Radio,
+  Select,
+  SelectOption,
+  SelectVariant,
   Split,
   SplitItem,
   Stack,
@@ -23,6 +25,7 @@ import {
 import { Constraints, DataType } from "../DataDictionaryContainer/DataDictionaryContainer";
 import { useState } from "react";
 import "./ConstraintsEdit.scss";
+import { Validated } from "../../../types";
 
 interface ConstraintsEditProps {
   dataType: DataType;
@@ -33,6 +36,8 @@ interface ConstraintsEditProps {
 const ConstraintsEdit = (props: ConstraintsEditProps) => {
   const { dataType, onAdd, onDelete } = props;
   const [constraintType, setConstraintType] = useState<string>(dataType.constraints?.type ?? "");
+  const [typeIsOpen, setTypeIsOpen] = useState(false);
+  const constraintsTypes = [{ value: "", isPlaceholder: true }, { value: "Range" }, { value: "Enumeration" }];
   const rangeEmptyValue = {
     start: {
       value: "",
@@ -46,29 +51,60 @@ const ConstraintsEdit = (props: ConstraintsEditProps) => {
   const rangeInitialValue = dataType.constraints?.type === "Range" ? dataType.constraints : rangeEmptyValue;
   const [range, setRange] = useState(rangeInitialValue);
   const [enums, setEnums] = useState("");
-  const [validation, setValidation] = useState<"success" | "error" | "default">("default");
+  const [validation, setValidation] = useState<{ form: Validated; fields: { [key: string]: Validated } }>({
+    form: "default",
+    fields: { start: "default", end: "default" }
+  });
 
-  const handleTypeChange = (checked: boolean, event: React.FormEvent<HTMLInputElement>) => {
-    const { value } = event.currentTarget;
-    setConstraintType(value);
+  const handleTypeChange = (event: React.MouseEvent | React.ChangeEvent, value: string) => {
+    if (value !== constraintType) {
+      setConstraintType(value);
+    }
+    setTypeIsOpen(false);
   };
 
-  const handleStartChange = (value: string) => {
-    setRange({ ...range, start: { value, included: range.start.included } });
+  const handleTypeOpening = () => {
+    setTypeIsOpen(!typeIsOpen);
   };
 
-  const handleEndChange = (value: string) => {
-    setRange({ ...range, end: { value, included: range.end.included } });
+  const handleRangeChange = (value: string | boolean, event: React.FormEvent<HTMLInputElement>) => {
+    switch ((event.target as HTMLInputElement).id) {
+      case "start-value":
+        setRange({ ...range, start: { ...range.start, value: value as string } });
+        break;
+      case "start-included":
+        setRange({ ...range, start: { ...range.start, included: value as boolean } });
+        break;
+      case "end-value":
+        setRange({ ...range, end: { ...range.end, value: value as string } });
+        break;
+      case "end-included":
+        setRange({ ...range, end: { ...range.end, included: value as boolean } });
+        break;
+      default:
+        break;
+    }
   };
+
   const handleEnumsChange = (value: string) => {
     setEnums(value);
   };
 
   const validateConstraints = () => {
     // const isValid = constraintType.length > 0 ? "success" : "error";
-    const isValid = "success";
+    const isValid = { ...validation };
+    isValid.form = "success";
+    if (constraintType === "Range") {
+      isValid.fields.start = range.start.value.trim().length === 0 ? "error" : "success";
+      isValid.fields.end = range.end.value.trim().length === 0 ? "error" : "success";
+    }
+    for (const fieldsKey in isValid.fields) {
+      if (isValid.fields[fieldsKey] === "error") {
+        isValid.form = "error";
+      }
+    }
     setValidation(isValid);
-    return isValid;
+    return isValid.form;
   };
 
   const handleSubmit = (event: React.FormEvent) => {
@@ -97,105 +133,114 @@ const ConstraintsEdit = (props: ConstraintsEditProps) => {
       <Stack hasGutter={true}>
         <StackItem>
           <TextContent>
-            <Text component={TextVariants.h3}>Constraints</Text>
+            <Text component={TextVariants.h3}>Constraints for {dataType.name}</Text>
             <Text component={TextVariants.p}>Select the type of constraint and then fill in the required fields.</Text>
           </TextContent>
         </StackItem>
-        {validation === "error" && (
-          <StackItem>
-            <Alert variant="warning" isInline={true} title="Please select a constraint first" />
-          </StackItem>
-        )}
+
         <StackItem>
-          <Form onSubmit={handleSubmit} autoComplete="off">
-            <Card>
-              <CardBody>
-                <Grid>
-                  <GridItem span={3}>
-                    <Radio
-                      isChecked={constraintType === "Range"}
-                      name={`constraint-type-range`}
-                      onChange={handleTypeChange}
-                      label="Range"
-                      id={`constraint-type-range`}
-                      value={"Range"}
-                    />
-                  </GridItem>
-                  <GridItem span={8}>
-                    <Split hasGutter={true}>
-                      <SplitItem isFilled={true}>
-                        <FormGroup label="Start Value" fieldId="start-value">
-                          <TextInput
-                            type="text"
-                            id="start-value"
-                            name="start-value"
-                            value={range.start.value}
-                            isDisabled={constraintType !== "Range"}
-                            onChange={handleStartChange}
-                          />
-                        </FormGroup>
-                        <FormGroup fieldId="start-included" className="constraints__include-range">
-                          <Checkbox
-                            label="Include Start Value"
-                            aria-label="Include Start Value"
-                            id="start-included"
-                            isDisabled={constraintType !== "Range"}
-                          />
-                        </FormGroup>
-                      </SplitItem>
-                      <SplitItem isFilled={true}>
-                        <FormGroup label="End Value" fieldId="end-value">
-                          <TextInput
-                            type="text"
-                            id="end-value"
-                            name="end-value"
-                            value={range.end.value}
-                            onChange={handleEndChange}
-                            isDisabled={constraintType !== "Range"}
-                          />
-                        </FormGroup>
-                        <FormGroup fieldId="end-included" className="constraints__include-range">
-                          <Checkbox
-                            label="Include End Value"
-                            aria-label="Include End Value"
-                            id="end-included"
-                            isDisabled={constraintType !== "Range"}
-                          />
-                        </FormGroup>
-                      </SplitItem>
-                    </Split>
-                  </GridItem>
-                </Grid>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardBody>
-                <Grid>
-                  <GridItem span={3}>
-                    <Radio
-                      isChecked={constraintType === "Enumeration"}
-                      name={`constraint-type-enum`}
-                      onChange={handleTypeChange}
-                      label="Enumeration"
-                      id={`constraint-type-enum`}
-                      value={"Enumeration"}
-                    />
-                  </GridItem>
-                  <GridItem span={8}>
-                    <FormGroup label="Values" fieldId="enum-values">
-                      <TextInput
-                        type="text"
-                        id="enum-values"
-                        name="enum-values"
-                        value={enums}
-                        onChange={handleEnumsChange}
-                        isDisabled={constraintType !== "Enumeration"}
-                      />
-                    </FormGroup>
-                  </GridItem>
-                </Grid>
-              </CardBody>
-            </Card>
+          <Form onSubmit={handleSubmit} autoComplete="off" className="constraints-form">
+            <section style={{ width: 200 }}>
+              <Select
+                id="constraints-type"
+                variant={SelectVariant.single}
+                aria-label="Select Constraint Type"
+                onToggle={handleTypeOpening}
+                onSelect={handleTypeChange}
+                selections={constraintType}
+                isOpen={typeIsOpen}
+                placeholderText={"Select a type"}
+              >
+                {constraintsTypes.map((item, index) => (
+                  <SelectOption key={index} value={item.value}>
+                    {item.isPlaceholder ? "Select a type" : item.value}
+                  </SelectOption>
+                ))}
+              </Select>
+            </section>
+            {validation.form === "error" && (
+              <StackItem>
+                <Alert variant="warning" isInline={true} title="Please check the highlighted fields content" />
+              </StackItem>
+            )}
+            {constraintType === "Range" && (
+              <Card>
+                <CardBody>
+                  <Grid>
+                    <GridItem span={8}>
+                      <Split hasGutter={true}>
+                        <SplitItem isFilled={true}>
+                          <FormGroup label="Start Value*" fieldId="start-value">
+                            <TextInput
+                              type="text"
+                              id="start-value"
+                              name="start-value"
+                              value={range.start.value}
+                              onChange={handleRangeChange}
+                              validated={validation.fields.start as Validated}
+                              tabIndex={20}
+                            />
+                          </FormGroup>
+                          <FormGroup fieldId="start-included" className="constraints__include-range">
+                            <Checkbox
+                              label="Include Start Value"
+                              aria-label="Include Start Value"
+                              id="start-included"
+                              isChecked={range.start.included}
+                              onChange={handleRangeChange}
+                              tabIndex={22}
+                            />
+                          </FormGroup>
+                        </SplitItem>
+                        <SplitItem isFilled={true}>
+                          <FormGroup label="End Value*" fieldId="end-value">
+                            <TextInput
+                              type="text"
+                              id="end-value"
+                              name="end-value"
+                              value={range.end.value}
+                              onChange={handleRangeChange}
+                              validated={validation.fields.end as Validated}
+                              tabIndex={21}
+                            />
+                          </FormGroup>
+                          <FormGroup fieldId="end-included" className="constraints__include-range">
+                            <Checkbox
+                              label="Include End Value"
+                              aria-label="Include End Value"
+                              id="end-included"
+                              isChecked={range.end.included}
+                              onChange={handleRangeChange}
+                              tabIndex={23}
+                            />
+                          </FormGroup>
+                        </SplitItem>
+                      </Split>
+                    </GridItem>
+                  </Grid>
+                </CardBody>
+              </Card>
+            )}
+            {constraintType === "Enumeration" && (
+              <Card>
+                <CardBody>
+                  <Grid>
+                    <GridItem span={8}>
+                      <FormGroup label="Values" fieldId="enum-values">
+                        <TextInput
+                          type="text"
+                          id="enum-values"
+                          name="enum-values"
+                          value={enums}
+                          onChange={handleEnumsChange}
+                          isDisabled={constraintType !== "Enumeration"}
+                        />
+                      </FormGroup>
+                    </GridItem>
+                  </Grid>
+                </CardBody>
+              </Card>
+            )}
             <ActionGroup>
               <Button variant="primary" type="submit">
                 Done
