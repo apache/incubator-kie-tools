d079c35921d904f829957e3d4303896b5e930d0e
Michael Anstis <manstis@redhat.com>
2020-11-13 15:36:19 +0000

    Basic auto-complete for True/False predicates

--- packages/pmml-editor/src/editor/components/EditorScorecard/molecules/AttributesTableEditRow.tsx
+++ packages/pmml-editor/src/editor/components/EditorScorecard/molecules/AttributesTableEditRow.tsx
@@ -32,6 +32,8 @@ import { ExclamationCircleIcon } from "@patternfly/react-icons";
 import { toText } from "../../../reducers";
 import MonacoEditor, { EditorWillMount } from "react-monaco-editor";
 import { CancellationToken, editor, languages, Position } from "monaco-editor/esm/vs/editor/editor.api";
+import CompletionItemKind = languages.CompletionItemKind;
+import CompletionItemInsertTextRule = languages.CompletionItemInsertTextRule;
 
 interface AttributesTableEditRowProps {
   index: number | undefined;
@@ -91,9 +93,18 @@ export const AttributesTableEditRow = (props: AttributesTableEditRowProps) => {
           token: "sc-string",
           foreground: "2A9343",
           fontStyle: "bold"
+        },
+        {
+          token: "sc-operator",
+          foreground: "3232E8"
+        },
+        {
+          token: "sc-keyword",
+          foreground: "0000ff",
+          fontStyle: "bold"
         }
       ],
-      colors: { "editorLineNumber.foreground": "123456" }
+      colors: { "editorLineNumber.foreground": "00ff00" }
     };
     const tokens: languages.IMonarchLanguage = {
       tokenizer: {
@@ -107,8 +118,16 @@ export const AttributesTableEditRow = (props: AttributesTableEditRowProps) => {
             action: "sc-boolean"
           },
           {
+            regex: "True|False",
+            action: "sc-keyword"
+          },
+          {
             regex: '(?:\\"(?:.*?)\\")',
             action: "sc-string"
+          },
+          {
+            regex: "==|!=|<|<=|>|>=|isMissing|isNotMissing",
+            action: "sc-operator"
           }
         ]
       }
@@ -120,7 +139,24 @@ export const AttributesTableEditRow = (props: AttributesTableEditRowProps) => {
         context: languages.CompletionContext,
         token: CancellationToken
       ): languages.ProviderResult<languages.CompletionList> {
-        return { suggestions: [] };
+        return {
+          suggestions: [
+            {
+              label: "True",
+              insertText: "True",
+              kind: CompletionItemKind.Keyword,
+              insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet,
+              range: { startLineNumber: 1, endLineNumber: 1, startColumn: 1, endColumn: 1 }
+            },
+            {
+              label: "False",
+              insertText: "False",
+              kind: CompletionItemKind.Keyword,
+              insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet,
+              range: { startLineNumber: 1, endLineNumber: 1, startColumn: 1, endColumn: 1 }
+            }
+          ]
+        };
       }
     };
     monaco.editor.defineTheme("scorecards", theme);
