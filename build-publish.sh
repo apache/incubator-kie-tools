f64929d367b567fc4ca15ca017d28387e04c9030
ginxo <emingora@redhat.com>
2020-11-16 14:22:44 +0100

    publish all, build:prod and run init

--- build-publish.sh
+++ build-publish.sh
@@ -2,6 +2,6 @@
 # input parameter NPM Registry URL instead of default ones (npm, yarn)
 npm run install-locktt
 npm run locktt --registry=$1
-yarn install
-yarn build
-yarn publish
+yarn run init
+yarn build:prod
+yarn publish:all
