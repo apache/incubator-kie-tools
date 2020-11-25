c3a6c221060629d4e8c8d17176851ca6eeb41fb2
Michael Anstis <manstis@redhat.com>
2020-11-25 16:23:23 +0000

    Ensure Monaco is only setup once

--- packages/pmml-editor/src/editor/components/EditorScorecard/molecules/PredicateEditor.tsx
+++ packages/pmml-editor/src/editor/components/EditorScorecard/molecules/PredicateEditor.tsx
@@ -16,12 +16,10 @@
 import * as React from "react";
 import { useRef } from "react";
 import { ValidatedType } from "../../../types";
-import MonacoEditor, { EditorWillMount } from "react-monaco-editor";
-import { CancellationToken, editor, languages, Position } from "monaco-editor/esm/vs/editor/editor.api";
+import MonacoEditor from "react-monaco-editor";
 import { EditorDidMount } from "react-monaco-editor/src/types";
 import * as monacoEditor from "monaco-editor";
-import CompletionItemKind = languages.CompletionItemKind;
-import CompletionItemInsertTextRule = languages.CompletionItemInsertTextRule;
+import { bootstrapMonaco } from "./PredicateEditorSetup";
 
 interface PredicateEditorProps {
   text: ValidatedType<string | undefined>;
@@ -29,97 +27,12 @@ interface PredicateEditorProps {
   validateText: (text: string | undefined) => boolean;
 }
 
+bootstrapMonaco();
+
 export const PredicateEditor = (props: PredicateEditorProps) => {
   const { text, setText, validateText } = props;
 
   const monaco = useRef<MonacoEditor>(null);
-
-  const editorWillMount: EditorWillMount = _monaco => {
-    const theme: editor.IStandaloneThemeData = {
-      base: "vs",
-      inherit: false,
-      rules: [
-        { token: "sc-numeric", foreground: "3232E7" },
-        {
-          token: "sc-boolean",
-          foreground: "26268D",
-          fontStyle: "bold"
-        },
-        {
-          token: "sc-string",
-          foreground: "2A9343",
-          fontStyle: "bold"
-        },
-        {
-          token: "sc-operator",
-          foreground: "3232E8"
-        },
-        {
-          token: "sc-keyword",
-          foreground: "0000ff",
-          fontStyle: "bold"
-        }
-      ],
-      colors: { "editorLineNumber.foreground": "00ff00" }
-    };
-    const tokens: languages.IMonarchLanguage = {
-      tokenizer: {
-        root: [
-          {
-            regex: "[0-9]+",
-            action: "sc-numeric"
-          },
-          {
-            regex: "(?:(\\btrue\\b)|(\\bfalse\\b))",
-            action: "sc-boolean"
-          },
-          {
-            regex: "True|False",
-            action: "sc-keyword"
-          },
-          {
-            regex: '(?:\\"(?:.*?)\\")',
-            action: "sc-string"
-          },
-          {
-            regex: "==|!=|<|<=|>|>=|isMissing|isNotMissing",
-            action: "sc-operator"
-          }
-        ]
-      }
-    };
-    const provider: languages.CompletionItemProvider = {
-      provideCompletionItems(
-        model: editor.ITextModel,
-        position: Position,
-        context: languages.CompletionContext,
-        token: CancellationToken
-      ): languages.ProviderResult<languages.CompletionList> {
-        return {
-          suggestions: [
-            {
-              label: "True",
-              insertText: "True",
-              kind: CompletionItemKind.Keyword,
-              insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet,
-              range: { startLineNumber: 1, endLineNumber: 1, startColumn: 1, endColumn: 1 }
-            },
-            {
-              label: "False",
-              insertText: "False",
-              kind: CompletionItemKind.Keyword,
-              insertTextRules: CompletionItemInsertTextRule.InsertAsSnippet,
-              range: { startLineNumber: 1, endLineNumber: 1, startColumn: 1, endColumn: 1 }
-            }
-          ]
-        };
-      }
-    };
-    _monaco.editor.defineTheme("scorecards", theme);
-    _monaco.languages.register({ id: "scorecards" });
-    _monaco.languages.setMonarchTokensProvider("scorecards", tokens);
-    _monaco.languages.registerCompletionItemProvider("scorecards", provider);
-  };
 
   const editorDidMount: EditorDidMount = (editor: monacoEditor.editor.IStandaloneCodeEditor) => {
     editor.focus();
@@ -142,7 +55,6 @@ export const PredicateEditor = (props: PredicateEditorProps) => {
           valid: validateText(e)
         })
       }
-      editorWillMount={editorWillMount}
       editorDidMount={editorDidMount}
     />
   );
