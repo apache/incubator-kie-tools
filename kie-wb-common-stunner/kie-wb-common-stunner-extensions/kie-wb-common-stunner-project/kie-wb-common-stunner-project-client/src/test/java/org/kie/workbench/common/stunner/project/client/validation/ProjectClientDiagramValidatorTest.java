53d2a362484eca533049308a8fa99c365e040d23
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2020-11-26 08:07:22 -0300

    AF-1799: Fix more tests after mockito upgrade.

--- kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-project/kie-wb-common-stunner-project-client/src/test/java/org/kie/workbench/common/stunner/project/client/validation/ProjectClientDiagramValidatorTest.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-project/kie-wb-common-stunner-project-client/src/test/java/org/kie/workbench/common/stunner/project/client/validation/ProjectClientDiagramValidatorTest.java
@@ -48,7 +48,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class ProjectClientDiagramValidatorTest {
 
     private ProjectClientDiagramValidator clientDiagramValidator;
