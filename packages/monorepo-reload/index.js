d2e8ba89e0b9b91d39498003d17827718458a2c3
Tiago Bento <tiagobentofernandes@gmail.com>
2020-12-13 15:43:47 -0300

    Update copyright

--- packages/monorepo-reload/index.js
+++ packages/monorepo-reload/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
