feb72c63ec884ed570a6805e97bf22df09d9be20
Enrique Mingorance Cano <ginxaco@gmail.com>
2020-12-14 08:18:09 +0100

    Update build-publish.sh

--- build-publish.sh
+++ build-publish.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # input parameter NPM Registry URL instead of default ones (npm, yarn)
+npm install -g yarn
 npm run install-locktt
-npm run locktt --registry=$1
+npm run locktt -- --registry=$1
+yarn install
 yarn run init
-yarn build:prod
-yarn publish:all
+yarn build:productization
+yarn run publish --yes --cd-version patch 
