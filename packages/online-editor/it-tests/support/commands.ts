acf39eac60f0169ff139f9bfd6dcc255a716f6a2
Tomas David <tdavid@redhat.com>
2021-01-06 09:23:44 +0100

    Add integration tests for kogito-online

---
+++ packages/online-editor/it-tests/support/commands.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/// <reference types="cypress-iframe" />
+
+declare namespace Cypress {
+    interface Chainable {
+        getEditor(): Chainable<JQuery<HTMLBodyElement>>;
+        waitUntilEditorIsLoaded(): void;
+    }
+}
+
+Cypress.Commands.add("getEditor", () => {
+    return cy.iframe("iframe#kogito-iframe");
+});
+
+Cypress.Commands.add("waitUntilEditorIsLoaded", () => {
+    cy.getEditor().within(() => {
+        cy.get(".pf-l-bullseye", { timeout: 30000 }).should("be.visible");
+        cy.get(".pf-l-bullseye", { timeout: 60000 }).should("not.exist");
+    });
+});
