2dc481318d6abcda921ce09aae416205283f42d9
Tomas David <tdavid@redhat.com>
2021-01-08 08:48:15 +0100

    Remove cypress iframe reference

--- packages/online-editor/it-tests/support/commands.ts
+++ packages/online-editor/it-tests/support/commands.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/// <reference types="cypress-iframe" />
 
 declare namespace Cypress {
     interface Chainable {
