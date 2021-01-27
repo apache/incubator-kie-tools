cf853f72463d2e62cd04c0a691042122b0a5428e
Jozef Marko <jomarko@redhat.com>
2021-01-27 16:44:21 +0100

    KOGITO-204 Update docks locators in chrome extension and standalone editor tests (#382)
    * KOGITO-204 Update docks locators in chrome extension tests
    
    Due to changes introduced in [1] and [2] the docks locators needs to be updated
    [1] - https://github.com/kiegroup/kie-wb-common/pull/3333
    [2] - https://github.com/kiegroup/appformer/pull/987
    
    * Update Editors to 7.49.0-SNAPSHOT-20210126.222335
    
    * KOGITO-204 Update docks locators
    
    Due to changes introduced in [1] and [2] the docks locators needs to be updated
    [1] - https://github.com/kiegroup/kie-wb-common/pull/3333
    [2] - https://github.com/kiegroup/appformer/pull/987
    
    * replace class locators with ouia attribute locators
    
    * Upgrade chromedriver version
    
    * Point to public package registry
    
    Co-authored-by: Kogito Tooling Bot (kiegroup) <kietooling@gmail.com>

--- packages/kie-editors-standalone/it-tests/cypress/integration/dmn-readonly.spec.ts
+++ packages/kie-editors-standalone/it-tests/cypress/integration/dmn-readonly.spec.ts
@@ -26,20 +26,21 @@ describe.skip("Dmn Read Only.", () => {
       .should("not.be.visible");
 
     cy.editor("dmn-read-only")
-      .find(".qe-docks-bar-W", { timeout: 10000 })
+      .ouiaId("collapsed-docks-bar", "collapsed-docks-bar-W", { timeout: 10000 })
       .should("be.visible");
 
     cy.uploadFile("call centre drd.dmn");
     cy.viewFile("call centre drd.dmn");
 
     cy.editor("dmn-read-only")
-      .find(".qe-docks-bar-W button")
+      .ouiaId("collapsed-docks-bar", "collapsed-docks-bar-W")
+      .find("button")
       .first()
       .should("be.visible")
       .click(); // open DecisionNavigator
 
     cy.editor("dmn-read-only")
-      .find(".qe-docks-bar-expanded-W")
+      .ouiaId("expanded-docks-bar", "expanded-docks-bar-W")
       .within($navigator => {
         cy.wrap($navigator)
           .find("[data-field='item'][title='DRG']")
