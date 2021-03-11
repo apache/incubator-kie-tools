e3fb003bcbfe568a5220d8a8d2c48c49f19e9cce
Jozef Marko <jomarko@redhat.com>
2021-03-11 10:42:01 +0100

    Formatted packages/external-assets-base/

--- packages/external-assets-base/index.js
+++ packages/external-assets-base/index.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 const nodePath = require("path");
 const nodeFs = require("fs");
 const glob = require("glob");
@@ -25,17 +25,23 @@ const glob = require("glob");
  * (2) When developing for kogito-tooling,
  *     external-assets-base is accessed directly so nodeModulesDir needs node_modules appended.
  */
-const nodeModulesDir = "../.." + (__dirname.includes("node_modules") ? "" : "/node_modules");
+const nodeModulesDir =
+  "../.." + (__dirname.includes("node_modules") ? "" : "/node_modules");
 
 module.exports = {
-  dmnEditorPath: argv => {
+  dmnEditorPath: (argv) => {
     const path =
       (argv && argv["EXTERNAL_RESOURCE_PATH__dmnEditor"]) ||
       process.env["EXTERNAL_RESOURCE_PATH__dmnEditor"] ||
-      nodePath.resolve(__dirname, nodeModulesDir + "/@kogito-tooling/dmn-editor-unpacked/target/dmn");
+      nodePath.resolve(
+        __dirname,
+        nodeModulesDir + "/@kogito-tooling/dmn-editor-unpacked/target/dmn"
+      );
 
     if (!nodeFs.existsSync(path)) {
-      throw new Error(`External asset :: DMN Editor path doesn't exist: ${path}`);
+      throw new Error(
+        `External asset :: DMN Editor path doesn't exist: ${path}`
+      );
     }
 
     console.info(`External asset :: DMN Editor path: ${path}`);
@@ -43,14 +49,19 @@ module.exports = {
     return path;
   },
 
-  bpmnEditorPath: argv => {
+  bpmnEditorPath: (argv) => {
     const path =
       (argv && argv["EXTERNAL_RESOURCE_PATH__bpmnEditor"]) ||
       process.env["EXTERNAL_RESOURCE_PATH__bpmnEditor"] ||
-      nodePath.resolve(__dirname, nodeModulesDir + "/@kogito-tooling/bpmn-editor-unpacked/target/bpmn");
+      nodePath.resolve(
+        __dirname,
+        nodeModulesDir + "/@kogito-tooling/bpmn-editor-unpacked/target/bpmn"
+      );
 
     if (!nodeFs.existsSync(path)) {
-      throw new Error(`External asset :: BPMN Editor path doesn't exist: ${path}`);
+      throw new Error(
+        `External asset :: BPMN Editor path doesn't exist: ${path}`
+      );
     }
 
     console.info(`External asset :: BPMN Editor path: ${path}`);
@@ -58,14 +69,19 @@ module.exports = {
     return path;
   },
 
-  scesimEditorPath: argv => {
+  scesimEditorPath: (argv) => {
     const path =
       (argv && argv["EXTERNAL_RESOURCE_PATH__scesimEditor"]) ||
       process.env["EXTERNAL_RESOURCE_PATH__scesimEditor"] ||
-      nodePath.resolve(__dirname, nodeModulesDir + "/@kogito-tooling/scesim-editor-unpacked/target/scesim");
+      nodePath.resolve(
+        __dirname,
+        nodeModulesDir + "/@kogito-tooling/scesim-editor-unpacked/target/scesim"
+      );
 
     if (!nodeFs.existsSync(path)) {
-      throw new Error(`External asset :: SceSim Editor path doesn't exist: ${path}`);
+      throw new Error(
+        `External asset :: SceSim Editor path doesn't exist: ${path}`
+      );
     }
 
     console.info(`External asset :: SceSim Editor path: ${path}`);
@@ -73,7 +89,7 @@ module.exports = {
     return path;
   },
 
-  quarkusRunnerPath: argv => {
+  quarkusRunnerPath: (argv) => {
     const path =
       (argv && argv["EXTERNAL_RESOURCE_PATH__quarkusRunner"]) ||
       process.env["EXTERNAL_RESOURCE_PATH__quarkusRunner"] ||
@@ -85,12 +101,20 @@ module.exports = {
 
     const matches = glob.sync(path);
 
-    console.info(`Found ${matches.length} match(es) for External asset :: Quarkus Runner.`);
+    console.info(
+      `Found ${matches.length} match(es) for External asset :: Quarkus Runner.`
+    );
 
     if (matches.length <= 0) {
-      throw new Error(`External asset :: Quarkus Runner path found no matches: ${path}`);
+      throw new Error(
+        `External asset :: Quarkus Runner path found no matches: ${path}`
+      );
     } else if (matches.length > 1) {
-      throw new Error(`External asset :: Quarkus Runner path found multiple matches: ${matches.join(", ")}`);
+      throw new Error(
+        `External asset :: Quarkus Runner path found multiple matches: ${matches.join(
+          ", "
+        )}`
+      );
     }
 
     console.info(`External asset :: Quarkus Runner path: ${matches[0]}`);
