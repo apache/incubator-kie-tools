1b458cf198a8a4970290da6e56d50c609f4769bc
Tiago Bento <tiagobentofernandes@gmail.com>
2021-05-05 15:12:33 -0300

    Fix Monaco on PMML Editor dev webapp

--- packages/pmml-editor/webpack.config.js
+++ packages/pmml-editor/webpack.config.js
@@ -55,6 +55,14 @@ module.exports = [
         ...pfWebpackOptions.patternflyRules,
       ],
     },
+    resolve: {
+      alias: {
+        // `react-monaco-editor` points to the `monaco-editor` package by default, therefore doesn't use our minified
+        // version. To solve that, we fool webpack, saying that every import for Monaco directly should actually point to
+        // `@kiegroup/monaco-editor`. This way, everything works as expected.
+        "monaco-editor/esm/vs/editor/editor.api": path.resolve(__dirname, "../../node_modules/@kiegroup/monaco-editor"),
+      },
+    },
     devServer: {
       historyApiFallback: true,
       disableHostCheck: true,
