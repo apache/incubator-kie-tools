663db8cc07a5d8e2379240d2ee6085351f6fce66
Jozef Marko <jomarko@redhat.com>
2021-05-26 17:31:19 +0200

    KOGITO-3171: Do not show DMN guided tour for invalid models (#516)
    * KOGITO-3171: Do not show DMN guided tour for invalid models
    
    For more details see:
    - https://issues.redhat.com/browse/KOGITO-3171
    
    * Add cypress test
    
    * Rename test - PR feedback
    
    * Do not verify alert content - PR feedback
    
    * Introduce ouia attribute for dmn guided tour
    
    * Remove data-kgt-close attribute
    
    * Format codebase

--- packages/online-editor/it-tests/integration/OpenFromSourceTest.ts
+++ packages/online-editor/it-tests/integration/OpenFromSourceTest.ts
@@ -80,7 +80,7 @@ describe("Open from source test", () => {
     cy.get("[aria-label='Edit file name']").should("have.value", "testModel");
 
     // close DMN guided tour dialog
-    cy.get("[data-kgt-close='true']").click();
+    cy.get("[data-ouia-component-id='dmn-guided-tour'] button[aria-label='Close']").click();
 
     cy.getEditor().within(() => {
       // open properties panel and check values
