c3fc4c311a94144200e569b431d45bff32512576
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2021-07-06 15:21:16 -0300

    KOGITO-5072: Setup Google Tag Manager.

--- packages/dmn-dev-sandbox-form-webapp/webpack.config.js
+++ packages/dmn-dev-sandbox-form-webapp/webpack.config.js
@@ -19,13 +19,60 @@ const CopyPlugin = require("copy-webpack-plugin");
 const pfWebpackOptions = require("@kogito-tooling/patternfly-base/patternflyWebpackOptions");
 const { merge } = require("webpack-merge");
 const common = require("../../webpack.common.config");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
+
+function getGtmResource(argv) {
+  const gtmId = argv["KOGITO_ONLINE_EDITOR_GTM_ID"] ?? process.env["KOGITO_ONLINE_EDITOR_GTM_ID"];
+  console.info("Google Tag Manager :: ID: " + gtmId);
+  return {
+    id: gtmId,
+    header: `<!-- Google Tag Manager -->
+    <script>
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${gtmId}');
+    </script>
+    <!-- End Google Tag Manager -->`,
+    body: `<!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+        height="0"
+        width="0"
+        style="display:none;visibility:hidden"
+      >
+      </iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->`,
+  };
+}
 
 module.exports = async (env, argv) => {
+  const gtmResource = getGtmResource(argv);
   return merge(common(env, argv), {
     entry: {
       index: "./src/index.tsx",
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: "./static/index.html",
+        inject: false,
+        minify: false,
+      }),
+      new HtmlReplaceWebpackPlugin([
+        {
+          pattern: /(<!-- gtm):([\w-\/]+)(\s*-->)?/g,
+          replacement: (match, gtm, type) => {
+            if (gtmResource.id) {
+              return gtmResource[type] ?? `${match}`;
+            }
+            return `${match}`;
+          },
+        },
+      ]),
       new CopyPlugin({
         patterns: [
           { from: "./static/resources", to: "./resources" },
