e041d2b8093fe6d49401ad2677711a58b2d9a45b
Jozef Marko <jomarko@redhat.com>
2021-07-08 06:51:03 +0200

    KOGITO-5479: DMN Runner - Wizard step for running
    For more details see:
    - https://issues.redhat.com/browse/KOGITO-5479

--- packages/online-editor/src/editor/DmnRunner/DmnRunnerModal.tsx
+++ packages/online-editor/src/editor/DmnRunner/DmnRunnerModal.tsx
@@ -535,7 +535,7 @@ export function DmnRunnerModal() {
                   <Text component={TextVariants.p}>
                     {i18n.dmnRunner.modal.wizard.linux.start.runCommand}
                     <Text component={TextVariants.p} className={"kogito--code"}>
-                      ./kie-tooling-extended-services
+                      ./kie_tooling_extended_services
                     </Text>
                   </Text>
                 </TextContent>
@@ -559,7 +559,7 @@ export function DmnRunnerModal() {
                 <br />
                 <TextContent>
                   <Text component={TextVariants.p} className={"kogito--code"}>
-                    ./kie-tooling-extended-services -p {dmnRunner.port}
+                    ./kie_tooling_extended_services -p {dmnRunner.port}
                   </Text>
                 </TextContent>
                 <br />
