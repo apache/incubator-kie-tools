1ed5ec1df97780251a5bcb02429d96461f3aa926
Rafael T. C. Soares (A.K.A Tuelho) <rafaeltuelho@users.noreply.github.com>
2021-08-23 16:45:02 -0400

    Configure ReverseProxy to tolerate TLS connections using Self Signed Certificates. (#21)
    This patche makes possible to connect with openshift clusters using Self Signed TLS certificates.

--- pkg/kogito/server.go
+++ pkg/kogito/server.go
@@ -3,6 +3,8 @@ package kogito
 import (
 	"bufio"
 	"context"
+	"crypto/tls"
+	"net"
 
 	"encoding/json"
 	"fmt"
@@ -197,6 +199,24 @@ func devSandboxHandler() func(w http.ResponseWriter, r *http.Request) {
 		r.Host = r.URL.Host
 
 		proxy := httputil.NewSingleHostReverseProxy(targetUrl)
+
+		// tolerate self-signed certificates
+		proxy.Transport = &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			ForceAttemptHTTP2:     true,
+			MaxIdleConns:          10,
+			IdleConnTimeout:       60 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		}
+
 		proxy.ModifyResponse = func(resp *http.Response) error {
 			resp.Header.Add("Access-Control-Allow-Origin", "*")
 			resp.Header.Add("Access-Control-Allow-Methods", "*")
