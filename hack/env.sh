8729dcededf92c2ebe54eb6db583f167ad61ea30
Karel Suta <ksuta@redhat.com>
2021-09-22 10:03:12 +0200

    Specify timeout for cluster resource delete (#1027)
    Signed-off-by: Karel Suta <ksuta@redhat.com>

--- hack/env.sh
+++ hack/env.sh
@@ -84,6 +84,6 @@ clean_cluster_resources() {
   for resourceName in $2
   do
     echo "Delete cluster $1 ${resourceName}"
-    oc delete $1 ${resourceName}
+    oc delete $1 ${resourceName} --timeout=30s
   done
 }
