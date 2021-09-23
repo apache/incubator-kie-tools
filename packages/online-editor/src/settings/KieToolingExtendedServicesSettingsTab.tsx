7e70c5ebe5cf75122d4d91044b7d21e39d3de54e
Tiago Bento <tiagobentofernandes@gmail.com>
2021-09-23 17:02:40 -0300

    Add indication of connection on KIE Tooling Extended Services settings tab

--- packages/online-editor/src/settings/KieToolingExtendedServicesSettingsTab.tsx
+++ packages/online-editor/src/settings/KieToolingExtendedServicesSettingsTab.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import { useCallback, useState } from "react";
-import { ActionGroup, Form, FormGroup } from "@patternfly/react-core/dist/js/components/Form";
+import { ActionGroup, Form, FormAlert, FormGroup } from "@patternfly/react-core/dist/js/components/Form";
 import { InputGroup } from "@patternfly/react-core/dist/js/components/InputGroup";
 import { TextInput } from "@patternfly/react-core/dist/js/components/TextInput";
 import { Page, PageSection } from "@patternfly/react-core/dist/js/components/Page";
 import { useSettings } from "./SettingsContext";
 import { Button } from "@patternfly/react-core/dist/js/components/Button";
+import { Alert } from "@patternfly/react-core/dist/js/components/Alert";
+import { useKieToolingExtendedServices } from "../editor/KieToolingExtendedServices/KieToolingExtendedServicesContext";
+import { KieToolingExtendedServicesStatus } from "../editor/KieToolingExtendedServices/KieToolingExtendedServicesStatus";
 
 export function KieToolingExtendedServicesSettingsTab() {
   const settings = useSettings();
+  const kieToolingExtendedServices = useKieToolingExtendedServices();
   const [port, setPort] = useState(settings.kieToolingExtendedServices.port.get);
 
   const onSubmit = useCallback(
@@ -25,6 +29,24 @@ export function KieToolingExtendedServicesSettingsTab() {
         <PageSection>
           <PageSection variant={"light"} isFilled={true} style={{ height: "100%" }}>
             <Form onSubmit={onSubmit}>
+              <FormAlert>
+                {kieToolingExtendedServices.status === KieToolingExtendedServicesStatus.RUNNING && (
+                  <Alert
+                    variant="success"
+                    title={"You are connected to KIE Tooling Extended Services"}
+                    aria-live="polite"
+                    isInline
+                  />
+                )}
+                {kieToolingExtendedServices.status !== KieToolingExtendedServicesStatus.RUNNING && (
+                  <Alert
+                    variant="danger"
+                    title={"You are not connected to KIE Tooling Extended Services"}
+                    aria-live="polite"
+                    isInline
+                  />
+                )}
+              </FormAlert>
               <FormGroup
                 isRequired={true}
                 helperTextInvalid={""}
