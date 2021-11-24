9b513e71fa755cc21c5bc498a0a28131cac1cc89
Adriel Paredes <adriel.paredes@gmail.com>
2021-11-24 11:57:45 -0300

    KOGITO-795: Added documentation to JavaCodeCompletionApi

--- packages/vscode-java-code-completion/src/api/JavaCodeCompletionApi.ts
+++ packages/vscode-java-code-completion/src/api/JavaCodeCompletionApi.ts
@@ -18,7 +18,23 @@ import { JavaCodeCompletionAccessor } from "./JavaCodeCompletionAccessor";
 import { JavaCodeCompletionClass } from "./JavaCodeCompletionClass";
 
 export interface JavaCodeCompletionApi {
+  /**
+   * Return a list of accessors for a given class. If the class does not exist or if it can't find anything
+   * with
+   * @param fqcn Class name in FQCN format
+   * @param query A string query that matches accessor names.
+   * @returns A List of accessors or empty list if nothing found
+   */
   getAccessors(fqcn: string, query: string): Promise<JavaCodeCompletionAccessor[]>;
+  /**
+   * Return a list of classes for given query. If the query is empty or there is not matching result,
+   * an empty list is returned
+   * @param query A class name or a FQCN string
+   * @returns A list of Classes or empty list if nothing found
+   */
   getClasses(query: string): Promise<JavaCodeCompletionClass[]>;
+  /**
+   * Returns if the language server is available or not.
+   */
   isLanguageServerAvailable(): Promise<boolean>;
 }
