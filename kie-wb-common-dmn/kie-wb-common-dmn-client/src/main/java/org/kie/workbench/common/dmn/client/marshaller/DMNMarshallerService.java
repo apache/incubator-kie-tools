59d1fa303de1b550ccb07f00a7301cc01d198243
Jozef Marko <jomarko@redhat.com>
2021-11-29 14:24:59 +0100

    KOGITO-6144: DMN Editor missing xml-prolog for UTF-8 encoding (#154)
    * KOGITO-6144: add xml declaration prolog
    
    For more details see:
    - https://issues.redhat.com/browse/KOGITO-6144
    
    * Fix sonar issues

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/marshaller/DMNMarshallerService.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/marshaller/DMNMarshallerService.java
@@ -128,16 +128,7 @@ public class DMNMarshallerService {
 
     public void marshall(final Diagram diagram,
                          final ServiceCallback<String> contentServiceCallback) {
-        final DMN12MarshallCallback jsCallback = result -> {
-            final String xml;
-            final String prefix = "<?xml version=\"1.0\" ?>";
-            if (result.startsWith(prefix)) {
-                xml = result;
-            } else {
-                xml = prefix + result;
-            }
-            contentServiceCallback.onSuccess(xml);
-        };
+        final DMN12MarshallCallback jsCallback = contentServiceCallback::onSuccess;
 
         if (Objects.isNull(diagram)) {
             contentServiceCallback.onError(new ClientRuntimeError("The Diagram cannot be null."));
