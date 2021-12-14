cccab7f449ae122b372306b63c8f071821fb5598
Jozef Marko <jomarko@redhat.com>
2021-12-14 13:59:39 +0100

    KOGITO-6193: Reactivate tests for DMN's new Boxed Expression component (#662)
    * Revert "KOGITO-6188: [DMN Designer] The keyboard shortcuts modal CSS from the DMN Editor is broken (#655)"
    
    This reverts commit 03c5cb7a9b80929af3b9c5fa44bafc75eacbea10.
    
    KOGITO-6097: Activate decision table tests
    
    * trigger CI

---
+++ packages/online-editor/it-tests/integration/DMNExpressionEditor.ts
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as buildEnv from "@kogito-tooling/build-env";
+
+describe("DMN Expression Editor Test", () => {
+  beforeEach(() => {
+    cy.visit(`https://localhost:${buildEnv.onlineEditor.dev.port}/`);
+  });
+
+  it("Test New Expresssion editor - context", () => {
+    // click Create new decision model button (new DMN)
+    cy.ouiaId("try-dmn-sample-button").click();
+
+    // wait until loading dialog disappears
+    cy.loadEditor();
+
+    // close DMN guided tour dialog
+    cy.ouiaId("dmn-guided-tour").children("button[aria-label='Close']").click();
+
+    cy.getEditor().within(() => {
+      // open decision navigator and check new expression editor for expressions
+      cy.ouiaId("docks-item-org.kie.dmn.decision.navigator").children("button").click();
+
+      cy.get("li[data-i18n-prefix='DecisionNavigatorTreeView.']").within(($navigator) => {
+        cy.get("[title='Back End Ratio'] div span").contains("Context").click();
+      });
+      // check using beta version
+      cy.get("[data-field='beta-boxed-expression-toggle'] [data-field='try-it']").click();
+      cy.ouiaId("expression-container").contains("Back End Ratio").should("be.visible");
+      cy.get(".expression-title").contains("Back End Ratio").should("be.visible");
+      cy.get(".expression-type").contains("Context").should("be.visible");
+    });
+  });
+
+  it("Test New Expresssion editor - function", () => {
+    // click Create new decision model button (new DMN)
+    cy.ouiaId("try-dmn-sample-button").click();
+
+    // wait until loading dialog disappears
+    cy.loadEditor();
+
+    // close DMN guided tour dialog
+    cy.ouiaId("dmn-guided-tour").children("button[aria-label='Close']").click();
+
+    cy.getEditor().within(() => {
+      // open decision navigator and check new expression editor for expressions
+      cy.ouiaId("docks-item-org.kie.dmn.decision.navigator").children("button").click();
+
+      cy.get("li[data-i18n-prefix='DecisionNavigatorTreeView.']").within(($navigator) => {
+        cy.get("[title='PITI'] div span").contains("Function").click();
+      });
+      // check using beta version
+      cy.get("[data-field='beta-boxed-expression-toggle'] [data-field='try-it']").click();
+      cy.ouiaId("expression-container").contains("PITI").should("be.visible");
+      cy.get(".expression-title").contains("PITI").should("be.visible");
+      cy.get(".expression-type").contains("Function").should("be.visible");
+    });
+  });
+
+  // TODO - unskip once kogito-editors-java bump is available
+  it.skip("Test New Expresssion editor - decision table", () => {
+    // click Create new decision model button (new DMN)
+    cy.ouiaId("try-dmn-sample-button").click();
+
+    // wait until loading dialog disappears
+    cy.loadEditor();
+
+    // close DMN guided tour dialog
+    cy.ouiaId("dmn-guided-tour").children("button[aria-label='Close']").click();
+
+    cy.getEditor().within(() => {
+      // open decision navigator and check new expression editor for expressions
+      cy.ouiaId("docks-item-org.kie.dmn.decision.navigator").children("button").click();
+
+      cy.get("li[data-i18n-prefix='DecisionNavigatorTreeView.']").within(($navigator) => {
+        cy.get("[title='Credit Score Rating'] div span").contains("Decision Table").click();
+      });
+      // check using beta version
+      cy.get("[data-field='beta-boxed-expression-toggle'] [data-field='try-it']").click();
+      cy.ouiaId("expression-container").contains("Credit Score Rating").should("be.visible");
+      cy.get(".expression-title").contains("Credit Score Rating").should("be.visible");
+      cy.get(".expression-type").contains("Decision Table").should("be.visible");
+    });
+  });
+});
