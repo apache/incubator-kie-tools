2cdc6b9c8bb6e36c4b42fd03a11c3fe78390e6c0
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2021-12-19 10:06:15 -0300

    Add image-env-to-json package

--- packages/kie-sandbox-image/entrypoint.sh
+++ packages/kie-sandbox-image/entrypoint.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-cd /var/www/html
-
 ENV_VAR_NAMES=(
   "KIE_TOOLING_EXTENDED_SERVICES_HOST"
   "KIE_TOOLING_EXTENDED_SERVICES_PORT"
 )
 
-if [ ! -e env.json ]; then
-  echo "{}" >env.json
-fi
-
-for env_var_name in "${ENV_VAR_NAMES[@]}"; do
-  if env | grep -q ^$env_var_name=; then
-    env_var_value="${!env_var_name}"
-    jq --arg key "$env_var_name" --arg value "$env_var_value" '.[$key] = $value' env.json >env.tmp && mv env.tmp env.json
-  fi
-done
+/kogito/image-env-to-json.js -d /var/www/html -n "${ENV_VAR_NAMES[@]}"
 
 httpd -D FOREGROUND
