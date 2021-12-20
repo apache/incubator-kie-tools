df3518f8dd961d6b9eb0ad70171485c538af2130
Tiago Bento <tiagobentofernandes@gmail.com>
2021-12-20 12:11:56 -0300

    Move to new folder

---
+++ packages/stunner-editors/errai-reflections/reflections/src/main/java/org/jboss/errai/reflections/scanners/FieldAnnotationsScanner.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.errai.reflections.scanners;
+
+import java.util.List;
+
+/** scans for field's annotations */
+@SuppressWarnings({"unchecked"})
+public class FieldAnnotationsScanner extends AbstractScanner {
+    public void scan(final Object cls) {
+        final String className = getMetadataAdapter().getClassName(cls);
+        List<Object> fields = getMetadataAdapter().getFields(cls);
+        for (final Object field : fields) {
+            List<String> fieldAnnotations = getMetadataAdapter().getFieldAnnotationNames(field);
+            for (String fieldAnnotation : fieldAnnotations) {
+
+                if (acceptResult(fieldAnnotation)) {
+                    String fieldName = getMetadataAdapter().getFieldName(field);
+                    getStore().put(fieldAnnotation, String.format("%s.%s", className, fieldName));
+                }
+            }
+        }
+    }
+}
---
+++ packages/stunner-editors/lienzo-core/src/main/java/com/ait/lienzo/shared/core/types/Direction.java
@@ -0,0 +1,82 @@
+/*
+   Copyright (c) 2017 Ahome' Innovation Technologies. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ait.lienzo.shared.core.types;
+
+import java.util.List;
+
+public enum Direction implements EnumWithValue {
+    NORTH("north"),
+    SOUTH("south"),
+    EAST("east"),
+    WEST("west"),
+    NONE("none"),
+    NORTH_EAST("north_east"),
+    SOUTH_EAST("south_east"),
+    SOUTH_WEST("south_west"),
+    NORTH_WEST("north_west");
+
+    private final String m_value;
+
+    private static final EnumStringMap<Direction> LOOKUP_MAP = Statics.build(Direction.values());
+
+    private Direction(final String value) {
+        m_value = value;
+    }
+
+    /**
+     * Return String representation.
+     *
+     * @return
+     */
+    @Override
+    public final String getValue() {
+        return m_value;
+    }
+
+    @Override
+    public final String toString() {
+        return m_value;
+    }
+
+    /**
+     * Used to safely convert from a String to an enum.
+     *
+     * @param key
+     * @return
+     */
+    public static final Direction lookup(final String key) {
+        return Statics.lookup(key, LOOKUP_MAP, NONE);
+    }
+
+    /**
+     * Return list of enum keys.
+     *
+     * @return
+     */
+    public static final List<String> getKeys() {
+        return Statics.getKeys(Direction.values());
+    }
+
+    /**
+     * Return list of enum values.
+     *
+     * @return
+     */
+    public static final List<Direction> getValues() {
+        return Statics.getValues(Direction.values());
+    }
+}
