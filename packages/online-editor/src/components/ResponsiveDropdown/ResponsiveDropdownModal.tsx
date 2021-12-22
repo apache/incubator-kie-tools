a278b7481aac44e3e79acede88a5a6f430e8a67f
Thiago Lugli <thiagoelg@gmail.com>
2021-12-22 11:54:41 -0300

    Add file headers

--- packages/online-editor/src/components/ResponsiveDropdown/ResponsiveDropdownModal.tsx
+++ packages/online-editor/src/components/ResponsiveDropdown/ResponsiveDropdownModal.tsx
@@ -1,4 +1,20 @@
-import React from "react";
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { ReactNode } from "react";
 import { Modal, ModalVariant } from "@patternfly/react-core/dist/js/components/Modal";
 
 export type ResponsiveDropdownModalProps = {
@@ -6,14 +22,10 @@ export type ResponsiveDropdownModalProps = {
   className?: string;
   title?: string;
   onClose?: () => void;
+  children?: ReactNode;
 };
 
-export const ResponsiveDropdownModal: React.FunctionComponent<ResponsiveDropdownModalProps> = ({
-  isOpen,
-  onClose,
-  title,
-  children,
-}) => {
+export function ResponsiveDropdownModal({ isOpen, onClose, title, children }: ResponsiveDropdownModalProps) {
   return (
     <Modal
       isOpen={isOpen ?? false}
@@ -31,4 +43,4 @@ export const ResponsiveDropdownModal: React.FunctionComponent<ResponsiveDropdown
       </div>
     </Modal>
   );
-};
+}
--- packages/online-editor/src/components/ResponsiveDropdown/ResponsiveDropdownToggle.tsx
+++ packages/online-editor/src/components/ResponsiveDropdown/ResponsiveDropdownToggle.tsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from "react";
-import {
-  Dropdown,
-  DropdownGroup,
-  DropdownItem,
-  DropdownSeparator,
-  DropdownToggle,
-  DropdownPosition,
-  DropdownDirection,
-  DropdownProps,
-  DropdownToggleProps,
-} from "@patternfly/react-core/dist/js/components/Dropdown";
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import { DropdownToggle, DropdownToggleProps } from "@patternfly/react-core/dist/js/components/Dropdown";
 import { ResponsiveDropdownContext } from "./ResponsiveDropdownContext";
 import { Button, ButtonVariant } from "@patternfly/react-core/dist/js/components/Button";
 
-export interface ResponsiveDropdownToggleProps extends DropdownToggleProps {
-  /** Indicates where menu will be aligned horizontally */
-  switchingBreakpoint?: "sm" | "md" | "lg" | "xl" | "2xl";
-}
-
-export function ResponsiveDropdownToggle(args: ResponsiveDropdownToggleProps) {
+export function ResponsiveDropdownToggle(args: DropdownToggleProps) {
   return (
     <ResponsiveDropdownContext.Consumer>
       {({ isModal }) => {
