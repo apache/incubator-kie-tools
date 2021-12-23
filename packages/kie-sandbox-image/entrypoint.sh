1e318e118eadc683d59c3d55c75cbae702bd96d4
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2021-12-23 13:32:40 -0300

    Rename: /kogito -> /kie-sandbox

--- packages/kie-sandbox-image/entrypoint.sh
+++ packages/kie-sandbox-image/entrypoint.sh
@@ -4,6 +4,6 @@ ENV_VAR_NAMES=(
   "KIE_TOOLING_EXTENDED_SERVICES_URL"
 )
 
-/kogito/image-env-to-json-standalone -d /var/www/html -n "${ENV_VAR_NAMES[@]}"
+/kie-sandbox/image-env-to-json-standalone -d /var/www/html -n "${ENV_VAR_NAMES[@]}"
 
 httpd -D FOREGROUND
