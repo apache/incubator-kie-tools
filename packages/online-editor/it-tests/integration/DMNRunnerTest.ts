b503e51633f81c26fa93902333493a0857a0f926
Luiz Motta <luizjoaomotta@gmail.com>
2021-12-23 16:11:02 -0300

    Change to find

--- packages/online-editor/it-tests/integration/DMNRunnerTest.ts
+++ packages/online-editor/it-tests/integration/DMNRunnerTest.ts
@@ -118,7 +118,7 @@ describe("DMN Runner Test", () => {
 
     // check DMN Runner outputs in table
     cy.get(".dmn-runner-table.id2")
-      .get("[data-ouia-component-id='expression-row-0']")
+      .find("[data-ouia-component-id='expression-row-0']")
       .within(($table) => {
         cy.get("[data-ouia-component-id='expression-column-1']").contains("Sufficient").should("be.visible");
         cy.get("[data-ouia-component-id='expression-column-2']").contains("Sufficient").should("be.visible");
