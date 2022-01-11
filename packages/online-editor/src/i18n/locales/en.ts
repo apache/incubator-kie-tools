8bb81120927699ae46e2254958e6de6a8ff76bdd
Thiago Lugli <thiagoelg@users.noreply.github.com>
2022-01-11 12:41:38 -0300

    KOGITO-6249: Check if there are new changes in the Gist before force-pushing (#741)
    * Force push gist
    
    * Force push and check if has changes
    
    * Update message
    
    * Check sync status
    
    * Revert hasChanges from createSavePoint
    
    * Add return back
    
    * Don't use heavy hook
    
    * Add remote head check before push
    
    * Check for gist updates when returning to home page

--- packages/online-editor/src/i18n/locales/en.ts
+++ packages/online-editor/src/i18n/locales/en.ts
@@ -32,6 +32,8 @@ export const en: OnlineI18n = {
       copy: "Content copied to clipboard",
       updateGist: "Gist successfully updated.",
       createGist: "Gist successfully created.",
+      errorPushingGist: "Failed to push an update to your current Gist. Attempt to force push?",
+      forcePushWarning: "WARNING: This will overwrite your Gist with the local changes!",
       invalidCurrentGist: `Your current gist ${en_common.names.url} is invalid. If you've updated its filename, it's necessary to update your ${en_common.names.url} as well.`,
       invalidGistFilename: "Invalid filename. This gist already has a file with this name.",
       error: `An error occurred trying to perform the last operation. Check if your ${en_common.names.github} token is still valid and try again later.`,
