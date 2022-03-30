0b52735dc83cb4ef7c7c47c58d5e1ba5aee564a9
yesamer <yamer@redhat.com>
2022-03-30 11:21:32 +0200

    KOGITO-6859: Fixing useCallback deps and message

--- packages/import-java-classes-component/src/components/ImportJavaClasses/ImportJavaClassesWizardSecondStep.tsx
+++ packages/import-java-classes-component/src/components/ImportJavaClasses/ImportJavaClassesWizardSecondStep.tsx
@@ -42,6 +42,27 @@ export const ImportJavaClassesWizardSecondStep = ({
   selectedJavaClasses,
 }: ImportJavaClassesWizardSecondStepProps) => {
   const getPrefix = "get";
+
+  const isGetterMethod = useCallback((accessorName: string) => {
+    return accessorName.includes("()") && accessorName.startsWith(getPrefix);
+  }, []);
+
+  const isMethod = useCallback((accessorName: string) => {
+    return accessorName.includes("(") && accessorName.includes(")");
+  }, []);
+
+  const renameAccessorName = useCallback(
+    (originalName: string) => {
+      if (isGetterMethod(originalName)) {
+        const name = originalName.substring(originalName.indexOf(getPrefix) + 3, originalName.lastIndexOf("("));
+        return name.charAt(0).toLowerCase() + name.slice(1);
+      } else {
+        return originalName;
+      }
+    },
+    [isGetterMethod]
+  );
+
   const generateJavaClassField = useCallback((name: string, type: string, javaClasses: JavaClass[]) => {
     let dmnTypeRef: string = JAVA_TO_DMN_MAP.get(getJavaClassSimpleName(type)) || DMNSimpleType.ANY;
     if (dmnTypeRef === DMNSimpleType.ANY && javaClasses.some((javaClass) => javaClass.name === type)) {
@@ -78,7 +99,15 @@ export const ImportJavaClassesWizardSecondStep = ({
         console.error(error);
       }
     },
-    [generateJavaClassField, javaCodeCompletionService, onSelectedJavaClassedFieldsLoaded, selectedJavaClasses]
+    [
+      generateJavaClassField,
+      javaCodeCompletionService,
+      onSelectedJavaClassedFieldsLoaded,
+      selectedJavaClasses,
+      isGetterMethod,
+      isMethod,
+      renameAccessorName,
+    ]
   );
 
   useEffect(
@@ -88,23 +117,6 @@ export const ImportJavaClassesWizardSecondStep = ({
         .forEach((javaClass) => loadJavaFields(javaClass.name)),
     [selectedJavaClasses, loadJavaFields]
   );
-
-  const isGetterMethod = useCallback((accessorName: string) => {
-    return accessorName.includes("()") && accessorName.startsWith(getPrefix);
-  }, []);
-
-  const isMethod = useCallback((accessorName: string) => {
-    return accessorName.includes("(") && accessorName.includes(")");
-  }, []);
-
-  const renameAccessorName = useCallback((originalName: string) => {
-    if (isGetterMethod(originalName)) {
-      const name = originalName.substring(originalName.indexOf(getPrefix) + 3, originalName.lastIndexOf("("));
-      return name.charAt(0).toLowerCase() + name.slice(1);
-    } else {
-      return originalName;
-    }
-  }, []);
 
   return (
     <>
