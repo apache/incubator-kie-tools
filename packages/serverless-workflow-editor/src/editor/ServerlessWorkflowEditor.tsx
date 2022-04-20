5c564f24aac1c03399d2e755f1acdf4598cd7329
Paulo Martins <paulovmr@gmail.com>
2022-04-20 15:59:15 -0300

    KOGITO-6972: Add zoom control buttons to the mermaid generated SVG

--- packages/serverless-workflow-editor/src/editor/ServerlessWorkflowEditor.tsx
+++ packages/serverless-workflow-editor/src/editor/ServerlessWorkflowEditor.tsx
@@ -79,6 +79,7 @@ const RefForwardingServerlessWorkflowEditor: React.ForwardRefRenderFunction<
   const [initialContent, setInitialContent] = useState({ originalContent: "", path: "" });
   const [diagramOutOfSync, setDiagramOutOfSync] = useState<boolean>(false);
   const svgContainer = useRef<HTMLDivElement>(null);
+  const svgPreviewHiddenContainer = useRef<HTMLDivElement>(null);
   const swfMonacoEditorRef = useRef<SwfMonacoEditorApi>(null);
 
   useImperativeHandle(
@@ -101,8 +102,15 @@ const RefForwardingServerlessWorkflowEditor: React.ForwardRefRenderFunction<
           return Promise.resolve(swfMonacoEditorRef.current?.getContent() || "");
         },
         getPreview: (): Promise<string> => {
+          svgPreviewHiddenContainer.current!.innerHTML = svgContainer.current!.innerHTML;
+          svgPreviewHiddenContainer.current!.getElementsByTagName("svg")[0].removeAttribute("style");
+
+          // Remove zoom controls from SVG
+          svgPreviewHiddenContainer.current!.getElementsByTagName("svg")[0].lastChild?.remove();
+
           // Line breaks replaced due to https://github.com/mermaid-js/mermaid/issues/1766
-          const svgContent = svgContainer.current!.innerHTML.replaceAll("<br>", "<br/>");
+          const svgContent = svgPreviewHiddenContainer.current!.innerHTML.replaceAll("<br>", "<br/>");
+
           return Promise.resolve(svgContent);
         },
         undo: (): Promise<void> => {
@@ -171,6 +179,7 @@ const RefForwardingServerlessWorkflowEditor: React.ForwardRefRenderFunction<
           ref={svgContainer}
           className={"mermaid"}
         />
+        <div ref={svgPreviewHiddenContainer} className={"hidden"} />
       </DrawerPanelBody>
     </DrawerPanelContent>
   );
