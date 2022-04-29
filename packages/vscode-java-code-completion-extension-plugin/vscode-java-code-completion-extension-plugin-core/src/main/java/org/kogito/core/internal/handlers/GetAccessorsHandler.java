3a2e43c3d2843fa4cf47c4dbd83fa08e0360d639
yesamer <yamer@redhat.com>
2022-04-29 14:40:13 +0200

    KOGITO-7040: Improving Activator Management

--- packages/vscode-java-code-completion-extension-plugin/vscode-java-code-completion-extension-plugin-core/src/main/java/org/kogito/core/internal/handlers/GetAccessorsHandler.java
+++ packages/vscode-java-code-completion-extension-plugin/vscode-java-code-completion-extension-plugin-core/src/main/java/org/kogito/core/internal/handlers/GetAccessorsHandler.java
@@ -79,11 +79,13 @@ public class GetAccessorsHandler extends AutoCompletionHandler<List<GetPublicRes
             result.setAccessor(label[0].trim());
             String type = label[1].trim();
             Map<String,String> data = (Map<String, String>) item.getData();
-            String fqcnType = data.get(DATA_FIELD_SIGNATURE);
-            /* The DATA_FIELD_SIGNATURE format is: `method()Ljava.lang.String;` */
-            if (fqcnType != null && fqcnType.contains(")L")) {
-                type = fqcnType.split("\\)L")[1];
-                type = type.replaceAll(";$", "");
+            if (data != null && data.containsKey(DATA_FIELD_SIGNATURE)) {
+                String fqcnType = data.get(DATA_FIELD_SIGNATURE);
+                /* The DATA_FIELD_SIGNATURE format is: `method()Ljava.lang.String;` */
+                if (fqcnType != null && fqcnType.contains(")L")) {
+                    type = fqcnType.split("\\)L")[1];
+                    type = type.replaceAll(";$", "");
+                }
             }
             result.setType(type);
         } else {
