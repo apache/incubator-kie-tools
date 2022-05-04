9e852cd69f72727f9b9bc92d599e59c68bf90ecd
Handrey Cunha <handrey.cunha@gmail.com>
2022-05-04 18:22:03 -0400

    Remove Eclipse Layout Kernel library/associated classes, generate a dummy layout instead

--- packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-api/src/main/java/org/kie/workbench/common/stunner/sw/autolayout/AutoLayout.java
+++ packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-api/src/main/java/org/kie/workbench/common/stunner/sw/autolayout/AutoLayout.java
@@ -19,8 +19,8 @@ package org.kie.workbench.common.stunner.sw.autolayout;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiPredicate;
 
-import elemental2.dom.DomGlobal;
 import elemental2.promise.Promise;
 import org.kie.workbench.common.stunner.core.command.impl.CompositeCommand;
 import org.kie.workbench.common.stunner.core.graph.Edge;
@@ -28,30 +28,16 @@ import org.kie.workbench.common.stunner.core.graph.Graph;
 import org.kie.workbench.common.stunner.core.graph.Node;
 import org.kie.workbench.common.stunner.core.graph.command.DirectGraphCommandExecutionContext;
 import org.kie.workbench.common.stunner.core.graph.command.GraphCommandExecutionContext;
-import org.kie.workbench.common.stunner.core.graph.command.impl.AddControlPointCommand;
 import org.kie.workbench.common.stunner.core.graph.command.impl.UpdateElementPositionCommand;
 import org.kie.workbench.common.stunner.core.graph.content.Bounds;
 import org.kie.workbench.common.stunner.core.graph.content.definition.DefinitionSet;
 import org.kie.workbench.common.stunner.core.graph.content.relationship.Child;
-import org.kie.workbench.common.stunner.core.graph.content.view.ControlPoint;
 import org.kie.workbench.common.stunner.core.graph.content.view.Point2D;
 import org.kie.workbench.common.stunner.core.graph.content.view.View;
 import org.kie.workbench.common.stunner.core.graph.processing.traverse.content.ChildrenTraverseCallback;
 import org.kie.workbench.common.stunner.core.graph.processing.traverse.content.ChildrenTraverseProcessorImpl;
-import org.kie.workbench.common.stunner.core.graph.processing.traverse.content.ContentTraverseCallback;
-import org.kie.workbench.common.stunner.core.graph.processing.traverse.content.ViewTraverseProcessorImpl;
 import org.kie.workbench.common.stunner.core.graph.processing.traverse.tree.TreeWalkTraverseProcessorImpl;
 import org.kie.workbench.common.stunner.core.rule.RuleViolation;
-import org.kie.workbench.common.stunner.sw.autolayout.elkjs.ELKEdge;
-import org.kie.workbench.common.stunner.sw.autolayout.elkjs.ELKNode;
-import org.kie.workbench.common.stunner.sw.autolayout.elkjs.ELKUtils;
-import org.kie.workbench.common.stunner.sw.autolayout.elkjs.ELKWrapper;
-import org.kie.workbench.common.stunner.sw.definition.CompensationTransition;
-import org.kie.workbench.common.stunner.sw.definition.DefaultConditionTransition;
-import org.kie.workbench.common.stunner.sw.definition.ErrorTransition;
-import org.kie.workbench.common.stunner.sw.definition.EventConditionTransition;
-import org.kie.workbench.common.stunner.sw.definition.StartTransition;
-import org.kie.workbench.common.stunner.sw.definition.Transition;
 import org.uberfire.client.promise.Promises;
 
 public class AutoLayout {
@@ -62,278 +48,121 @@ public class AutoLayout {
                                             Promises promises,
                                             DirectGraphCommandExecutionContext context,
                                             boolean isSubset) {
-        //TODO Temporary solution to inject ELK lib into canvas frame
-        new ELKWrapper().injectScript();
 
-        //Get ELK processed layout promise
-        final Promise<Object> elkLayoutPromise = ELKUtils.
-                processGraph(buildElkInputNode(graph,
-                                               parentNode,
-                                               ELKUtils.getCanvasTopDownLayoutOptionsObject(),
-                                               ELKUtils.getContainerLeftToRightDownLayoutOptionsObject(),
-                                               isSubset).sortEdges());
-
-        //Apply ELK layout to graph
-        return promises.create((resolve, reject) -> elkLayoutPromise
-                .then(elkGraph -> {
-                    final ELKNode elkRoot = ELKUtils.parse(elkGraph);
+                return promises.create((resolve, reject) -> {
                     final CompositeCommand.Builder layoutCommands = new CompositeCommand.Builder();
 
-                    //Update node sizes in the graph
-                    updateGraphNodeSizes(elkRoot, graph);
-
-                    //Apply ELKNodes layout values into graph structure
-                    updateNodesPosition(elkRoot, graph, layoutCommands);
-
-                    //Create ELKBendPoints in graph structure
-                    createControlPoints(elkRoot, layoutCommands);
+                    updateNodesPosition(parentNode.getUUID(), graph, layoutCommands);
 
                     final CompositeCommand<GraphCommandExecutionContext, RuleViolation> all =
                             new CompositeCommand.Builder<>()
                                     .addCommand(layoutCommands.build())
                                     .build();
 
-                    // TODO: Check errors...
                     all.execute(context);
+                    resolve.onInvoke(parentNode);
+                });
+    }
 
-                    resolve.onInvoke(parentNode);
-                    return null;
-                }, error -> {
-                    //TODO Handle ELK error
-                    DomGlobal.console.error("Error while performing layout: " + error);
-                    resolve.onInvoke(parentNode);
-                    return null;
-                }));
+    private static BiPredicate<String, String> isCanvasRoot() {
+        return (rootUUID, uuid) -> uuid.equals(rootUUID);
     }
 
     @SuppressWarnings("all")
-    public static ELKNode buildElkInputNode(Graph graph,
-                                            Node parentNode,
-                                            Object parentLayoutOptions,
-                                            Object nestedParentLayoutOptions,
-                                            boolean isSubset) {
-        final String rootUUID = parentNode.getUUID();
-        //ELK root node definition
-        final ELKNode[] elkRoot = new ELKNode[]{new ELKNode(rootUUID, parentLayoutOptions)};
+    private static void updateNodesPosition(
+            String rootUUID,
+            final Graph graph,
+            final CompositeCommand.Builder layoutCommands) {
 
-        final Map<String, String> nodeContainment = new HashMap<>();
+        new ChildrenTraverseProcessorImpl(new TreeWalkTraverseProcessorImpl()).traverse(graph, new ChildrenTraverseCallback<Node<View, Edge>, Edge<Child, Node>>() {
+            private Map<String, Boolean> processedNodes = new HashMap<>();
+            private double x = 0d;
+            private double y = 0d;
 
-        new ChildrenTraverseProcessorImpl(new TreeWalkTraverseProcessorImpl())
-                .setRootUUID(rootUUID)
-                .traverse(graph, new ChildrenTraverseCallback<Node<View, Edge>, Edge<Child, Node>>() {
 
-                    private Map<String, Boolean> processedNodes = new HashMap<>();
+            @Override
+            public boolean startNodeTraversal(List<Node<View, Edge>> parents, Node<View, Edge> node) {
+                final Node parent = parents.get(parents.size() - 1);
 
-                    @Override
-                    public boolean startNodeTraversal(List<Node<View, Edge>> parents, Node<View, Edge> node) {
-                        final Node parent = parents.get(parents.size() - 1);
-
-                        if (parent.getUUID().equals(rootUUID)) {
-                            addNode(node);
-                        } else {
-                            //Containment
-                            addChildNode(parent, node, nestedParentLayoutOptions);
-                        }
-
-                        return true;
-                    }
-
-                    private void addNode(final Node<View, Edge> node) {
-                        //skip in case the node was already processed
-                        if (processedNodes.containsKey(node.getUUID())) {
-                            return;
-                        }
-
-                        Bounds bounds = node.getContent().getBounds();
-                        elkRoot[0].addNode(new ELKNode(node.getUUID(),
-                                                       bounds.getWidth(),
-                                                       bounds.getHeight()));
-
-                        addProcessedNode(node);
-                    }
-
+                if (isCanvasRoot().test(rootUUID, parent.getUUID())) {
+                    addNode(node);
+                } else {
                     //Containment
-                    private void addChildNode(final Node<View, Edge> parent,
-                                              final Node<View, Edge> node,
-                                              final Object nestedParentLayoutOptions) {
-                        //skip in case the node was already processed
-                        if (processedNodes.containsKey(node.getUUID()) || parent.getUUID().equals(rootUUID)) {
-                            return;
-                        }
+                    addChildNode(parent, node);
+                }
 
-                        //check whether the parent was processed, is must be processed before child node
-                        if (!processedNodes.containsKey(parent.getUUID())) {
-                            addNode(parent);
-                        }
+                return true;
+            }
 
-                        final ELKNode elkParent = elkRoot[0].getChild(parent.getUUID());
-                        //final Bound bound = getBound(node);
-                        Bounds bounds = node.getContent().getBounds();
-                        elkParent.addNode(new ELKNode(node.getUUID(), bounds.getWidth(), bounds.getHeight()));
+            private void addNode(final Node<View, Edge> node) {
+                //skip in case the node was already processed
+                if (processedNodes.containsKey(node.getUUID())) {
+                    return;
+                }
 
-                        //Set horizontal layout for containers
-                        elkParent.setLayoutOptions(nestedParentLayoutOptions);
+                Bounds bounds = node.getContent().getBounds();
 
-                        addNodeParentReference(node, parent);
-                        addProcessedNode(node);
-                    }
+                layoutCommands.addCommand(new UpdateElementPositionCommand(graph.getNode(node.getUUID()),
+                                                                           new Point2D(getNextX(), getNextY())));
 
-                    private void addProcessedNode(Node<View, Edge> node) {
-                        processedNodes.put(node.getUUID(), true);
-                    }
+                addProcessedNode(node);
+            }
 
-                    private void addNodeParentReference(Node<View, Edge> node, Node<View, Edge> parent) {
-                        nodeContainment.put(node.getUUID(), parent.getUUID());
-                    }
+            private double getNextY() {
+                return y += 150d;
+            }
 
-                    @Override
-                    public void startGraphTraversal(Graph<DefinitionSet, Node<View, Edge>> graph) {
-                    }
+            private double getNextX() {
+                return x += 150d;
+            }
 
-                    @Override
-                    public void startEdgeTraversal(Edge<Child, Node> edge) {
-                    }
+            //Containment
+            private void addChildNode(final Node<View, Edge> parent,
+                                      final Node<View, Edge> node) {
+                //skip in case the node was already processed
+                if (processedNodes.containsKey(node.getUUID()) || isCanvasRoot().test(rootUUID, parent.getUUID())) {
+                    return;
+                }
 
-                    @Override
-                    public void endEdgeTraversal(Edge<Child, Node> edge) {
-                    }
+                //check whether the parent was processed, is must be processed before child node
+                if (!processedNodes.containsKey(parent.getUUID())) {
+                    addNode(parent);
+                }
 
-                    @Override
-                    public void startNodeTraversal(Node<View, Edge> node) {
-                    }
+//                layoutCommands.addCommand(new UpdateElementPositionCommand(graph.getNode(node.getUUID()),
+//                                                                           new Point2D(getNextX(), getNextY())));
 
-                    @Override
-                    public void endNodeTraversal(Node<View, Edge> node) {
-                    }
+                addProcessedNode(node);
+            }
 
-                    @Override
-                    public void endGraphTraversal() {
-                    }
-                });
+            private void addProcessedNode(Node<View, Edge> node) {
+                processedNodes.put(node.getUUID(), true);
+            }
 
-        new ViewTraverseProcessorImpl(new TreeWalkTraverseProcessorImpl()).traverse(graph, new ContentTraverseCallback<View<?>, Node<View, Edge>, Edge<View<?>, Node>>() {
             @Override
             public void startGraphTraversal(Graph<DefinitionSet, Node<View, Edge>> graph) {
-
             }
 
             @Override
-            public void startEdgeTraversal(Edge<View<?>, Node> edge) {
-                final ELKEdge elkEdge = new ELKEdge(edge.getUUID(),
-                                                    edge.getSourceNode().getUUID(),
-                                                    edge.getTargetNode().getUUID());
-                String sourceParent = nodeContainment.get(edge.getSourceNode().getUUID());
-                String targetParent = nodeContainment.get(edge.getTargetNode().getUUID());
-                final Class<?> type = ((View<?>) edge.getContent()).getDefinition().getClass();
-
-                // Transition priority in the same level
-                if (EventConditionTransition.class.equals(type)) {
-                    elkEdge.setPriority(0);
-                } else if (ErrorTransition.class.equals(type)) {
-                    elkEdge.setPriority(1);
-                } else if (StartTransition.class.equals(type)) {
-                    elkEdge.setPriority(2);
-                } else if (Transition.class.equals(type)) {
-                    elkEdge.setPriority(3);
-                } else if (CompensationTransition.class.equals(type)) {
-                    elkEdge.setPriority(4);
-                } else if (DefaultConditionTransition.class.equals(type)) {
-                    elkEdge.setPriority(5);
-                }
-
-                //if nodes have same parent add edge into parent node structure
-                if (null != sourceParent && sourceParent.equals(targetParent)) {
-                    if (isSubset) {
-                        elkRoot[0].getChild(sourceParent).addEdgeWithFilter(elkEdge);
-                    } else {
-                        elkRoot[0].getChild(sourceParent).addEdge(elkEdge);
-                    }
-                } else {
-                    if (isSubset) {
-                        elkRoot[0].addEdgeWithFilter(elkEdge);
-                    } else {
-                        elkRoot[0].addEdge(elkEdge);
-                    }
-                }
+            public void startEdgeTraversal(Edge<Child, Node> edge) {
             }
 
             @Override
-            public void endEdgeTraversal(Edge<View<?>, Node> edge) {
-
+            public void endEdgeTraversal(Edge<Child, Node> edge) {
             }
 
             @Override
             public void startNodeTraversal(Node<View, Edge> node) {
-
             }
 
             @Override
             public void endNodeTraversal(Node<View, Edge> node) {
-
             }
 
             @Override
             public void endGraphTraversal() {
-
             }
         });
-
-        //add container into new root
-        if (isSubset) {
-            final ELKNode newElkRoot = new ELKNode("root", parentLayoutOptions);
-            newElkRoot.addNode(elkRoot[0].setLayoutOptions(nestedParentLayoutOptions));
-
-            //DomGlobal.console.log("----->" + Global.JSON.stringify(newElkRoot));
-
-            return newElkRoot;
-        }
-
-        //DomGlobal.console.log("----->" + Global.JSON.stringify(elkRoot[0]));
-
-        return elkRoot[0];
     }
 
-    //Update node sizes after ELK processing
-    @SuppressWarnings("all")
-    public static void updateGraphNodeSizes(ELKNode elkRoot, Graph graph) {
-        for (ELKNode elkNode : elkRoot.getChildren().asList()) {
-            ((Node<View, Edge>) graph.getNode(elkNode.getId())).getContent()
-                    .setBounds(Bounds.create(elkNode.getX(),
-                                             elkNode.getY(),
-                                             elkNode.getX() + elkNode.getWidth(),
-                                             elkNode.getY() + elkNode.getHeight()));
-
-            if (elkNode.getChildren().length > 0) {
-                updateGraphNodeSizes(elkNode, graph);
-            }
-        }
-    }
-
-    @SuppressWarnings("all")
-    public static void updateNodesPosition(ELKNode elkRoot,
-                                           Graph graph,
-                                           CompositeCommand.Builder layoutCommands) {
-        for (ELKNode elkNode : elkRoot.getChildren().asList()) {
-            layoutCommands.addCommand(new UpdateElementPositionCommand(graph.getNode(elkNode.getId()),
-                                                                       new Point2D(elkNode.getX(), elkNode.getY())));
-            if (elkNode.getChildren().length > 0) {
-                updateNodesPosition(elkNode, graph, layoutCommands);
-            }
-        }
-    }
-
-    @SuppressWarnings("all")
-    public static void createControlPoints(final ELKNode elkNode,
-                                           final CompositeCommand.Builder layoutCommands) {
-        for (ELKEdge elkEdge : elkNode.getEdges().asList()) {
-            int index = -1;
-            for (Point2D point : elkEdge.getBendPoints().asList()) {
-                layoutCommands.addCommand(new AddControlPointCommand(elkEdge.getId(), new ControlPoint(point), ++index));
-            }
-        }
-
-        for (ELKNode elkClildNode : elkNode.getChildren().asList()) {
-            createControlPoints(elkClildNode, layoutCommands);
-        }
-    }
 }
