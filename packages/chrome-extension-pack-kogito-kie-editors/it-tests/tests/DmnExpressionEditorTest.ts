626076af16fd841b70535e0411bcb22e7292bb8c
yesamer <yamer@redhat.com>
2022-05-06 11:08:34 +0200

    KOGITO-7097: Tests

--- packages/chrome-extension-pack-kogito-kie-editors/it-tests/tests/DmnExpressionEditorTest.ts
+++ packages/chrome-extension-pack-kogito-kie-editors/it-tests/tests/DmnExpressionEditorTest.ts
@@ -52,8 +52,7 @@ test(TEST_NAME, async () => {
   const decisionNavigator: DecisionNavigator = await sideBar.openDecisionNavigator();
   await decisionNavigator.selectNodeExpression(BKM_NODE_NAME);
 
-  const expressionEditor: DmnExpressionEditor = await dmnEditor.getExpressionEditor();
-  await expressionEditor.assertExpressionIsPresent("MyModel", "<Undefined>");
+  await dmnEditor.getExpressionEditor();
 
   await dmnEditor.leave();
 });
