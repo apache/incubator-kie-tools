f1f4324ef35f4932103f606b07479a6d79b599fc
William Siqueira <william.fatecsjc@gmail.com>
2022-05-09 18:44:38 -0300

    Adding timeseries component

--- packages/dashbuilder-component-echarts/src/EChartsComponent.tsx
+++ packages/dashbuilder-component-echarts/src/EChartsComponent.tsx
@@ -17,14 +17,12 @@
 import * as React from "react";
 import { ComponentController, DataSet } from "@kie-tools/dashbuilder-component-api";
 import { useState, useEffect, useCallback, useRef } from "react";
-import { EChartOption, EChartsType, init } from "echarts";
+import { ECharts, Props as EChartsProps } from "@kie-tools/dashbuilder-component-echarts-base";
 
 interface Props {
   controller: ComponentController;
 }
 
-const OPTION_PARAM = "option";
-const NOT_MERGE_PARAM = "notMerge";
 const INIT_OPTIONS = {
   tooltip: {},
   xAxis: { type: "category" },
@@ -33,78 +31,25 @@ const INIT_OPTIONS = {
 };
 
 export function EChartsComponent(props: Props) {
-  const container = useRef<HTMLDivElement>(null);
-  const [chart, setChart] = useState<EChartsType | undefined>();
-
+  const [echartsProps, setEchartsProps] = useState<EChartsProps>();
   useEffect(() => {
     props.controller.setOnInit((params: Map<string, any>) => {
-      chart?.setOption(INIT_OPTIONS as EChartOption, true);
+      setEchartsProps({ option: INIT_OPTIONS, params: params });
     });
 
     props.controller.setOnDataSet((_dataset: DataSet, params: Map<string, any>) => {
-      const options = fillProperties(params);
-      const notMerge = params.get(NOT_MERGE_PARAM) === true;
-      params.delete(NOT_MERGE_PARAM);
-      options.dataset = { source: [_dataset.columns.map((c) => c.settings.columnName), ..._dataset.data] };
-      // LIMITATION: have to set all series for the same type because the parameters is in a map
-      if (options.series && !options.series.length && _dataset.columns.length > 1) {
-        const series = Array(_dataset.columns.length - 1).fill(options.series);
-        options.series = series;
-      }
-      chart?.setOption(options, notMerge);
+      const option: any = {
+        dataset: {
+          source: [_dataset.columns.map((c) => c.settings.columnName), ..._dataset.data],
+        },
+      };
+      setEchartsProps({ option: option, params: params });
     });
-  }, [props.controller, chart]);
+  }, [props.controller]);
 
-  useEffect(() => {
-    if (container.current) {
-      const chart = init(container.current);
-      setChart(chart);
-    }
-  }, []);
-
-  window.onresize = useCallback(() => {
-    if (chart) chart.resize();
-  }, [chart]);
   return (
     <>
-      <div style={{ width: "100%", height: "100%" }} ref={container}></div>
+      <ECharts {...echartsProps} />
     </>
   );
 }
-
-const fillProperties = (props: Map<string, any>): any => {
-  let option = {};
-  const optionStr = props.get(OPTION_PARAM);
-  if (optionStr) {
-    try {
-      option = JSON.parse(optionStr);
-    } catch (e) {
-      console.log("Not able to parse option property");
-    }
-    props.delete(OPTION_PARAM);
-  }
-  props.forEach((value, key) => {
-    if (key !== "dataSet") {
-      setPropertyOnObject(key, value, option);
-    }
-  });
-  return option;
-};
-
-const setPropertyOnObject = (prop: string, value: any, obj: any) => {
-  if (!prop || !value) {
-    return obj;
-  }
-  const props = prop.split(".");
-  let parent = obj;
-  for (let i = 0; i < props.length; i++) {
-    const name = props[i];
-    if (i === props.length - 1) {
-      parent[name] = value;
-    } else {
-      parent[name] = parent[name] || {};
-      parent = parent[name];
-    }
-  }
-  return obj;
-};
