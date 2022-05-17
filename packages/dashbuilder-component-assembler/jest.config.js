05da9237aff186590288886dccb499e76c87c73d
William Siqueira <william.fatecsjc@gmail.com>
2022-05-17 17:44:41 -0300

    Removing unused files and fixing tests location

--- packages/dashbuilder-component-assembler/jest.config.js
+++ packages/dashbuilder-component-assembler/jest.config.js
@@ -18,8 +18,8 @@ module.exports = {
   reporters: ["default"],
   moduleDirectories: ["node_modules", "src"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-  setupFilesAfterEnv: ["./src/__tests__/jest.setup.ts"],
-  testRegex: "/__tests__/.*\\.test\\.(jsx?|tsx?)$",
+  setupFilesAfterEnv: ["./tests/jest.setup.ts"],
+  testRegex: "/tests/.*\\.test\\.(jsx?|tsx?)$",
   transform: {
     "^.+\\.jsx?$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest",
