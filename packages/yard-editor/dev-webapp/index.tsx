76ae536a5ab78770630ede9e2081de6b723cf9b1
yesamer <yamer@redhat.com>
2022-06-06 18:58:44 +0200

    KOGITO-7220: First draft of YARD editor - Changes Requests

--- packages/yard-editor/dev-webapp/index.tsx
+++ packages/yard-editor/dev-webapp/index.tsx
@@ -17,5 +17,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./App";
+import "@patternfly/react-core/dist/styles/base.css";
 
 ReactDOM.render(<App />, document.getElementById("app")!);
