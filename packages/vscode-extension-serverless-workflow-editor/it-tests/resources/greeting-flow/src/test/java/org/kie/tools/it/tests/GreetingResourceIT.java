e3f32cf742aa763fea00d510ddfa8d26a65058a4
Dominik Hanák <domhanak@users.noreply.github.com>
2022-06-30 14:56:26 +0200

    KOGITO-7119: Establish test runner for serverless tooling in vscode (#1048)
    * KOGITO-7119: Establish test runner for serverless tooling in vscode
    
    Adds first testing project greeting-flow
    Adds first smoke test with example
    Add helpers to work with serverless files in VSCode
    Add helpers for asserts and SWF editor and text editor.
    
    * Remove extra files, fix unused variables
    
    * Remove useless try-catch
    
    * Update packages/vscode-extension-serverless-workflow-editor/it-tests/serverless-workflow-editor-extension-smoke.test.ts
    
    Co-authored-by: Tomáš David <tdavid@redhat.com>
    
    * Run test:it in build:dev
    
    * Add test:it to build:prod
    
    * Adjust smoke test to be more high-level
    
    Only check for existence of crucial elements now.
    
    * Omit validation of TextEditor
    
    There is an issue with locator in framework, will
    wait for it to fix and add this back.
    
    * Change order of commands, test it is last
    
    * Disable test:it in build:X scripts due to pending multi-arch integration
    
    Co-authored-by: Tomáš David <tdavid@redhat.com>

---
+++ packages/vscode-extension-serverless-workflow-editor/it-tests/resources/greeting-flow/src/test/java/org/kie/tools/it/tests/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.kie.tools.it.tests;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
