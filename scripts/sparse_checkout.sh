db4da502929f6b93625872a6cd238840ef5b5dc5
Tiago Bento <tiagobentofernandes@gmail.com>
2022-08-05 11:18:47 -0300

    Make sparse_checkout work for multiple packages

--- scripts/sparse_checkout.sh
+++ scripts/sparse_checkout.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+ARGV=("$@")
+echo "${A[@]:2}"
 KIE_TOOLS_FORK_USER=$1
 GIT_REPO_BRANCH=$2
-SRC_PKG_NAME=$3 #TODO: Make it work for more than one package name
+SRC_PKG_NAMES=("${ARGV[@]:2}")
+echo ${SRC_PKG_NAMES[@]}
+echo ${#SRC_PKG_NAMES[@]}
+PNPM_FILTER=$(echo ${SRC_PKG_NAMES[@]} | xargs -n1 -I{} echo -n "-F {}... " | xargs)
+echo $PNPM_FILTER
 GIT_REPO_REMOTE_URL="https://github.com/$KIE_TOOLS_FORK_USER/kie-tools"
 GIT_REPO_CLONE_DIR='kie-tools'
 ALWAYS_INCLUDED_DIRS='scripts repo docs'
@@ -12,12 +18,12 @@ git sparse-checkout init --cone
 git checkout $GIT_REPO_BRANCH
 git sparse-checkout set $ALWAYS_INCLUDED_DIRS
 pnpm install-dependencies -F . --frozen-lockfile
-echo "Discovering which packages to fetch for $SRC_PKG_NAME..."
-PKG_DEPS=$(pnpm run --silent list-packages-dependencies repo $SRC_PKG_NAME)
+echo "Discovering which packages to fetch for (${SRC_PKG_NAMES[@]})..."
+PKG_DEPS=$(pnpm run --silent list-packages-dependencies repo "${SRC_PKG_NAMES[@]}")
 echo $PKG_DEPS
 eval "git sparse-checkout set $ALWAYS_INCLUDED_DIRS $PKG_DEPS"
 ls -la packages
-pnpm install-dependencies -F $SRC_PKG_NAME... -F . --frozen-lockfile && pnpm link-packages-with-self
-pnpm -F "$SRC_PKG_NAME..." build:dev
+eval "pnpm install-dependencies $PNPM_FILTER -F . --frozen-lockfile && pnpm link-packages-with-self"
+eval "pnpm $PNPM_FILTER build:dev"
 pnpm pretty-quick
 git status
