e2b038d127979315472921e6d93e7ead1caab03a
Davide Salerno <dsalerno@redhat.com>
2022-08-26 09:09:03 +0200

    Removing wrong reference to v08 apiVersion
    Signed-off-by: Davide Salerno <dsalerno@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,6 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	serverlessv08 "github.com/davidesalerno/kogito-serverless-operator/api/v08"
 	serverlessv1alpha1 "github.com/davidesalerno/kogito-serverless-operator/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
@@ -66,9 +65,6 @@ var _ = BeforeSuite(func() {
 	Expect(cfg).NotTo(BeNil())
 
 	err = serverlessv1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	err = serverlessv08.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
