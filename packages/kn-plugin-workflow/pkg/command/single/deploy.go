7412a4e68ca290f821ed5f318553d2c2445605d6
Luiz Motta <luizjoaomotta@gmail.com>
2022-09-16 18:01:23 -0300

    Add deploy

--- packages/kn-plugin-workflow/pkg/command/single/deploy.go
+++ packages/kn-plugin-workflow/pkg/command/single/deploy.go
@@ -17,9 +17,20 @@
 package single
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
 	"github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/common"
+	"github.com/ory/viper"
 	"github.com/spf13/cobra"
 )
+
+type DeployCmdConfig struct {
+	// Deploy options
+	Path string // service name
+}
 
 func NewDeployCommand() *cobra.Command {
 	var cmd = &cobra.Command{
@@ -28,12 +39,14 @@ func NewDeployCommand() *cobra.Command {
 		Long:       ``,
 		Example:    ``,
 		SuggestFor: []string{"delpoy", "deplyo"},
-		PreRunE:    common.BindEnv("name"),
+		PreRunE:    common.BindEnv("path"),
 	}
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		return runDeploy(cmd, args)
 	}
+
+	cmd.Flags().StringP("path", "p", "./kubernetes", fmt.Sprintf("%s path to knative deployment files", cmd.Name()))
 
 	cmd.SetHelpFunc(common.DefaultTemplatedHelp)
 
@@ -41,5 +54,56 @@ func NewDeployCommand() *cobra.Command {
 }
 
 func runDeploy(cmd *cobra.Command, args []string) error {
+	start := time.Now()
+
+	cfg, err := runDeployCmdConfig(cmd)
+	if err != nil {
+		return fmt.Errorf("initializing deploy config: %w", err)
+	}
+
+	if _, err := exec.LookPath("kubectl"); err != nil {
+		fmt.Println("ERROR: kubectl is required for deploy")
+		fmt.Println("Download from https://kubectl.docs.kubernetes.io/installation/kubectl/")
+		os.Exit(1)
+	}
+
+	createService := common.ExecCommand("kubectl", "apply", "-f", fmt.Sprintf("%s/knative.yml", cfg.Path))
+	if err := common.RunCommand(
+		createService,
+		"deploy",
+	); err != nil {
+		return err
+	}
+	fmt.Println("✅ Knative service sucessufully created")
+
+	// Check if kogito.yml file exists
+	if exists, err := checkIfKogitoFileExists(cfg); exists && err == nil {
+		deploy := common.ExecCommand("kubectl", "apply", "-f", fmt.Sprintf("%s/kogito.yml", cfg.Path))
+		if err := common.RunCommand(
+			deploy,
+			"deploy",
+		); err != nil {
+			return err
+		}
+		fmt.Println("✅ Knative Eventing bindings successfully created")
+	}
+
+	finish := time.Since(start)
+	fmt.Printf("🚀 Deploy took: %s \n", finish)
 	return nil
+}
+
+func runDeployCmdConfig(cmd *cobra.Command) (cfg DeployCmdConfig, err error) {
+	cfg = DeployCmdConfig{
+		Path: viper.GetString("path"),
+	}
+	return
+}
+
+func checkIfKogitoFileExists(cfg DeployCmdConfig) (bool, error) {
+	if _, err := os.Stat(fmt.Sprintf("%s/kogito.yml", cfg.Path)); err == nil {
+		return true, nil
+	} else {
+		return false, err
+	}
 }
