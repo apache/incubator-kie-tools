e8c3ca124a42dddf7b9c61679b6a84f712c434d8
Tiago <tiagobentofernandes@gmail.com>
2022-09-19 12:09:28 -0300

    Fix CI for publishing emscripten-fs and jitexecutor-native

--- .github/supporting-files/publish_emscripten_fs/build.sh
+++ .github/supporting-files/publish_emscripten_fs/build.sh
@@ -3,4 +3,4 @@
 rm -rf dist
 mkdir dist
 docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) \
-    emscripten/emsdk emcc src/fsMain.c -o dist/fsMain.js -lidbfs.js --post-js post.js -sEXPORTED_RUNTIME_METHODS=['FS'] -Oz
+    emscripten/emsdk:3.1.21 emcc src/fsMain.c -o dist/fsMain.js -lidbfs.js --post-js post.js -sEXPORTED_RUNTIME_METHODS=['FS'] -Oz
