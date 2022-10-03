aac9889edb431a8a0659a7ef5ac336ca842844f3
William Antônio Siqueira <william.fatecsjc@gmail.com>
2022-10-03 15:58:59 -0300

    KOGITO-7937: Dashbuilder Editor Viewer (#1235)
    * KOGITO-7937: Dashbuilder Editor Viewer
    
    * Renaming to DashbuilderViewer
    
    * building dashbuilder runtime in dev as well
    
    * Removing duplicated script
    
    * Exposing content loading in Javascript as well
    
    * Now the envelope is also used as Dashbuilder container
    
    * Dynamically loading resources
    
    * Components in buil:dev
    
    * Fixes and updating lock and graph files
    
    * Version update
    
    * Version fix
    
    * License headers update

---
+++ packages/dashbuilder-viewer/dev-webapp/webpack.config.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("@kie-tools-core/webpack-base/webpack.common.config");
+const patternflyBase = require("@kie-tools-core/patternfly-base");
+
+module.exports = (env) =>
+  merge(common(env), {
+    mode: "development",
+    entry: {
+      index: path.resolve(__dirname, "./index.tsx"),
+      "dashbuilder-viewer-envelope": path.resolve(__dirname, "./envelope/DashbuilderViewerEnvelopeApp.ts"),
+    },
+    output: {
+      path: path.resolve("../dist-dev"),
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: path.resolve(__dirname, "./static/resources"), to: "./resources" },
+          { from: path.resolve(__dirname, "./static/index.html"), to: "./index.html" },
+          { from: path.resolve(__dirname, "./static/favicon.ico"), to: "./favicon.ico" },
+          {
+            from: path.resolve(__dirname, "../dist/dashbuilder-client"),
+            to: "./",
+          },
+          {
+            from: path.resolve(__dirname, "./static/envelope/dashbuilder-viewer-envelope.html"),
+            to: "./dashbuilder-viewer-envelope.html",
+          },
+        ],
+      }),
+    ],
+    module: {
+      rules: [...patternflyBase.webpackModuleRules],
+    },
+    ignoreWarnings: [/Failed to parse source map/],
+    resolve: {
+      alias: {},
+    },
+    devServer: {
+      historyApiFallback: false,
+      static: [{ directory: path.join(__dirname) }],
+      compress: true,
+      port: 9007,
+    },
+  });
--- packages/dashbuilder/dashbuilder-runtime-parent/dashbuilder-runtime-client/src/main/java/org/dashbuilder/client/RuntimeClientLoader.java
+++ packages/dashbuilder/dashbuilder-runtime-parent/dashbuilder-runtime-client/src/main/java/org/dashbuilder/client/RuntimeClientLoader.java
@@ -248,15 +248,19 @@ public class RuntimeClientLoader {
     }
 
     public void loadClientModel(String content) {
-        if (content == null || content.trim().isEmpty()) {
-            clientModel = null;
-            router.doRoute();
-        } else {
-            var parser = parserFactory.getEditorParser(content);
-            var runtimeModel = parser.parse(content);
-            registerModel(runtimeModel);
-            this.clientModel = runtimeModel;
-            updatedRuntimeModelEvent.fire(new UpdatedRuntimeModelEvent(""));
+        try {
+            if (content == null || content.trim().isEmpty()) {
+                clientModel = null;
+                router.doRoute();
+            } else {
+                var parser = parserFactory.getEditorParser(content);
+                var runtimeModel = parser.parse(content);
+                registerModel(runtimeModel);
+                this.clientModel = runtimeModel;
+                updatedRuntimeModelEvent.fire(new UpdatedRuntimeModelEvent(""));
+            }
+        } catch (Exception e) {
+            router.goToContentError(e);
         }
     }
 
