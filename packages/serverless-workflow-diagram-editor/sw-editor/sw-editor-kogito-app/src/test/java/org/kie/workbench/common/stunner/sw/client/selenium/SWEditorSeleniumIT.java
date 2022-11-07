6da242dafc372fee95855f6b8fcfdf06082c26ac
Jaime Enriquez <jaime.enriquez@inodesoft.com>
2022-11-07 15:29:46 -0700

    KOGITO-7954 : SWF Editor - Some basic automated integration tests (#1261)

--- packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-kogito-app/src/test/java/org/kie/workbench/common/stunner/sw/client/selenium/SWEditorSeleniumIT.java
+++ packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-kogito-app/src/test/java/org/kie/workbench/common/stunner/sw/client/selenium/SWEditorSeleniumIT.java
@@ -18,88 +18,21 @@ package org.kie.workbench.common.stunner.sw.client.selenium;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.util.stream.Collectors;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.IOUtils;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
 import static org.apache.commons.io.FileUtils.copyFile;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.openqa.selenium.By.className;
-import static org.openqa.selenium.By.xpath;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
-public class SWEditorSeleniumIT {
+public class SWEditorSeleniumIT extends SWEditorSeleniumBase {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SWEditorSeleniumIT.class);
-
-    private static final String SET_CONTENT_TEMPLATE =
-            "gwtEditorBeans.get(\"SWDiagramEditor\").get().setContent(\"\", '%s')";
-    private static final String GET_CONTENT_TEMPLATE =
-            "return gwtEditorBeans.get(\"SWDiagramEditor\").get().getContent()";
-
-    private static final String CONTENT_REGULAR_CHAR = "\\\"";
-    private static final String CONTENT_EXECUTOR_CHAR = "&quot;";
-
-    private static final String INDEX_HTML = "target/sw-editor-kogito-app/index.html";
-    private static final String INDEX_HTML_PATH = "file:///" + new File(INDEX_HTML).getAbsolutePath();
-
-    private static final String DIAGRAM_PANEL = "qe-static-workbench-panel-view";
-    private static final String CANVAS_PANEL = "canvas-panel";
-    private static final Boolean HEADLESS = Boolean.valueOf(System.getProperty("org.kie.sw.editor.browser.headless"));
-    private static final String SCREENSHOTS_DIR = System.getProperty("org.kie.sw.editor.screenshots.dir");
-
-    /**
-     * Selenium web driver
-     */
-    private WebDriver driver;
-
-    @BeforeClass
-    public static void setupClass() {
-        WebDriverManager.firefoxdriver().useMirror().setup();
-    }
-
-    @Before
-    public void openSWEditor() {
-        final FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.setHeadless(HEADLESS);
-        driver = new FirefoxDriver(firefoxOptions);
-
-        driver.manage().window().maximize();
-
-        driver.get(INDEX_HTML_PATH);
-
-        final WebElement designer = waitOperation()
-                .until(presenceOfElementLocated(className(DIAGRAM_PANEL)));
-        assertThat(designer)
-                .as("Diagram panel is a prerequisite for all tests. " +
-                            "its absence is indicator of designer load fail.")
-                .isNotNull();
-    }
-
-    private final File screenshotDirectory = initScreenshotDirectory();
+    protected static final String SCREENSHOTS_DIR = System.getProperty("org.kie.sw.editor.screenshots.dir");
 
     @Rule
     public TestWatcher takeScreenShotAndCleanUp = new TestWatcher() {
@@ -177,7 +110,7 @@ public class SWEditorSeleniumIT {
     @Test
     public void testSendCloudEventOnProvisionExample() throws Exception {
         testExample("SendCloudEventOnProvisionExample.sw.json");
-    }  
+    }
 
     @Test
     public void testApplicantRequestDecisionExample() throws Exception {
@@ -193,17 +126,17 @@ public class SWEditorSeleniumIT {
     public void testFinalizeCollegeExample() throws Exception {
         testExample("FinalizeCollegeApplicationExample.sw.json");
     }
-    
+
     @Test
     public void testHandleCarAuctionBidExample() throws Exception {
         testExample("HandleCarAuctionBidExample.sw.json");
-    } 
+    }
 
     @Test
     public void testCheckInboxPeriodicallyExample() throws Exception {
         testExample("CheckInboxPeriodicallyExample.sw.json");
     }
-    
+
     @Test
     public void testEventBasedServiceInvocation() throws Exception {
         testExample("EventBasedServiceInvocationExample.sw.json");
@@ -258,6 +191,9 @@ public class SWEditorSeleniumIT {
         final String actual = getContent();
         assertThatJson(json(actual)).isEqualTo(json(expected));
     }
+
+    protected final File screenshotDirectory = initScreenshotDirectory();
+
     private File initScreenshotDirectory() {
         if (SCREENSHOTS_DIR == null) {
             throw new IllegalStateException(
@@ -274,54 +210,5 @@ public class SWEditorSeleniumIT {
             throw new IllegalStateException("The screenshotDir must be writable" + scd);
         }
         return scd;
-    }
-
-    private void waitCanvasPanel() {
-        final WebElement canvasPanelDiv = waitOperation()
-                .until(visibilityOfElementLocated(className(CANVAS_PANEL)));
-        assertThat(canvasPanelDiv)
-                .as("Once content is set canvas panel visibility is a prerequisite" +
-                            "for further test execution.")
-                .isNotNull();
-    }
-
-    private void setContent(final String xml) {
-        try {
-            String content = String.format(SET_CONTENT_TEMPLATE, xml);
-            content = content.replace(CONTENT_REGULAR_CHAR, CONTENT_EXECUTOR_CHAR);
-            ((JavascriptExecutor) driver).executeScript(content);
-
-        } catch (Exception e) {
-            LOG.error("Exception during JS execution. Ex: {}", e.getMessage());
-        }
-    }
-
-    private String getContent() {
-        final Object result = ((JavascriptExecutor) driver).executeScript(String.format(GET_CONTENT_TEMPLATE));
-        assertThat(result).isInstanceOf(String.class);
-
-        String content = (String) result;
-        content = content.replace(CONTENT_EXECUTOR_CHAR, CONTENT_REGULAR_CHAR);
-        return content;
-    }
-
-    /**
-     * Use this for loading serverless workflow definitions placed in src/test/resources
-     * @param filename
-     * @return Text content of the file
-     * @throws IOException
-     */
-    private String loadResource(final String filename) throws IOException {
-        return IOUtils.readLines(this.getClass().getResourceAsStream(filename), StandardCharsets.UTF_8)
-                .stream()
-                .collect(Collectors.joining(""));
-    }
-
-    private ExpectedCondition<WebElement> element(final String xpathLocator, final String... parameters) {
-        return visibilityOfElementLocated(xpath(String.format(xpathLocator, parameters)));
-    }
-
-    private WebDriverWait waitOperation() {
-        return new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds());
     }
 }
