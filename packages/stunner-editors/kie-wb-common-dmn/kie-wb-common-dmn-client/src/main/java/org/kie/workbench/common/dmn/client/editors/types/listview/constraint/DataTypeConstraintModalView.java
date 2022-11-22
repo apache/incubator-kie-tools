33b59f530bdeb3fe83ded04e671e697441be7c6a
Jozef Marko <Jozef.Marko@ibm.com>
2022-11-22 08:52:12 +0100

    DROOLS-3703: Disable Enumeration and Range constraint for Any DataType (#1323)
    * DROOLS-3703: Disable Enumeration and Range constraint for Any DataType
    
    * Fix unit tests
    
    * Incorporate peer review feedback - local variable
    
    Co-authored-by: Jozef Marko <jomarko@redhat.com>

--- packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/types/listview/constraint/DataTypeConstraintModalView.java
+++ packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/types/listview/constraint/DataTypeConstraintModalView.java
@@ -16,6 +16,8 @@
 
 package org.kie.workbench.common.dmn.client.editors.types.listview.constraint;
 
+import java.util.Objects;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
@@ -27,12 +29,16 @@ import elemental2.dom.HTMLAnchorElement;
 import elemental2.dom.HTMLButtonElement;
 import elemental2.dom.HTMLDivElement;
 import elemental2.dom.HTMLElement;
+import elemental2.dom.HTMLOptionElement;
+import elemental2.dom.HTMLOptionsCollection;
+import elemental2.dom.HTMLSelectElement;
 import elemental2.dom.Node;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 import org.kie.workbench.common.dmn.api.definition.model.ConstraintType;
 import org.kie.workbench.common.dmn.client.editors.common.RemoveHelper;
+import org.kie.workbench.common.dmn.client.editors.types.common.DataType;
 import org.uberfire.client.views.pfly.selectpicker.JQuery;
 import org.uberfire.client.views.pfly.selectpicker.JQuerySelectPicker;
 import org.uberfire.client.views.pfly.selectpicker.JQuerySelectPickerEvent;
@@ -82,6 +88,7 @@ public class DataTypeConstraintModalView implements DataTypeConstraintModal.View
     private final HTMLButtonElement closeConstraintWarningMessage;
 
     private DataTypeConstraintModal presenter;
+    private DataType dataType;
 
     @Inject
     public DataTypeConstraintModalView(final HTMLDivElement header,
@@ -165,6 +172,11 @@ public class DataTypeConstraintModalView implements DataTypeConstraintModal.View
         this.type.textContent = type;
     }
 
+    @Override
+    public void setDataType(DataType dataType) {
+        this.dataType = dataType;
+    }
+
     void onSelectChange(final JQuerySelectPickerEvent event) {
 
         final String constraintType = event.target.value;
@@ -191,7 +203,18 @@ public class DataTypeConstraintModalView implements DataTypeConstraintModal.View
 
     @Override
     public void onShow() {
-        setPickerValue(getSelectPicker(), getConstraintType());
+        final HTMLSelectElement selectElement = (HTMLSelectElement) getSelectPicker();
+        final HTMLOptionsCollection options = getSelectOptionsElement();
+        if (Objects.equals("Any", dataType.getType())) {
+            disableOptionElement(options.getAt(1));
+            disableOptionElement(options.getAt(3));
+        } else {
+            enableOptionElement(options.getAt(1));
+            enableOptionElement(options.getAt(3));
+        }
+
+        triggerPickerAction(selectElement, "refresh");
+        setPickerValue(selectElement, getConstraintType());
     }
 
     @Override
@@ -248,9 +271,21 @@ public class DataTypeConstraintModalView implements DataTypeConstraintModal.View
         return body.querySelector(".selectpicker");
     }
 
+    HTMLOptionsCollection getSelectOptionsElement() {
+        return ((HTMLSelectElement) getSelectPicker()).options;
+    }
+
     void triggerPickerAction(final Element element,
                              final String method) {
         JQuerySelectPicker.$(element).selectpicker(method);
+    }
+
+    void disableOptionElement(final HTMLOptionElement htmlOptionElement) {
+        htmlOptionElement.disabled = true;
+    }
+
+    void enableOptionElement(final HTMLOptionElement htmlOptionElement) {
+        htmlOptionElement.disabled = false;
     }
 
     /**
