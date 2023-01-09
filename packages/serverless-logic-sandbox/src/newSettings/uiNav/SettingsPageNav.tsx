1d1126a82468ab69a906e2f4e1eded9749959f43
fantonangeli <fabrizio.antonangeli@gmail.com>
2023-01-09 14:52:51 +0100

    Sidebar title same as OpenShift Console

--- packages/serverless-logic-sandbox/src/newSettings/uiNav/SettingsPageNav.tsx
+++ packages/serverless-logic-sandbox/src/newSettings/uiNav/SettingsPageNav.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { Nav, NavItem, NavList, Title } from "@patternfly/react-core";
+import { Nav, NavItem, NavList } from "@patternfly/react-core";
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { useRoutes } from "../../navigation/Hooks";
@@ -23,55 +23,53 @@ export function SettingsPageNav(props: { pathname: string }) {
   const routes = useRoutes();
 
   return (
-    <Nav aria-label="Global NAV" theme="dark">
-      <NavList>
-        <NavItem itemId={0} key={"Settings-title-nav"} isActive={false} disabled>
-          <Title headingLevel="h2" size="md">
-            Settings
-          </Title>
-        </NavItem>
-        <NavItem itemId={0} key={`Settings-github-nav`} isActive={props.pathname === routes.settings.github.path({})}>
-          <Link to={routes.settings.github.path({})}>GitHub</Link>
-        </NavItem>
-        <NavItem
-          itemId={0}
-          key={`Settings-kie_sandbox_extended_services-nav`}
-          isActive={props.pathname === routes.settings.kie_sandbox_extended_services.path({})}
-        >
-          <Link to={routes.settings.kie_sandbox_extended_services.path({})}>KIE Sandbox Extended Services</Link>
-        </NavItem>
-        <NavItem
-          itemId={0}
-          key={`Settings-openshift-nav`}
-          isActive={props.pathname === routes.settings.openshift.path({})}
-        >
-          <Link to={routes.settings.openshift.path({})}>OpenShift</Link>
-        </NavItem>
-        <NavItem
-          itemId={0}
-          key={`Settings-service_account-nav`}
-          isActive={props.pathname === routes.settings.service_account.path({})}
-        >
-          <Link to={routes.settings.service_account.path({})}>Service Account</Link>
-        </NavItem>
-        <NavItem
-          itemId={0}
-          key={`Settings-service_registry-nav`}
-          isActive={props.pathname === routes.settings.service_registry.path({})}
-        >
-          <Link to={routes.settings.service_registry.path({})}>Service Registry</Link>
-        </NavItem>
-        <NavItem itemId={0} key={`Settings-kafka-nav`} isActive={props.pathname === routes.settings.kafka.path({})}>
-          <Link to={routes.settings.kafka.path({})}>Streams for Apache Kafka</Link>
-        </NavItem>
-        <NavItem
-          itemId={0}
-          key={`Settings-feature_preview-nav`}
-          isActive={props.pathname === routes.settings.feature_preview.path({})}
-        >
-          <Link to={routes.settings.feature_preview.path({})}>Feature Preview</Link>
-        </NavItem>
-      </NavList>
-    </Nav>
+    <>
+      <div className="chr-c-app-title">Settings</div>
+      <Nav aria-label="Global NAV" theme="dark">
+        <NavList>
+          <NavItem itemId={0} key={`Settings-github-nav`} isActive={props.pathname === routes.settings.github.path({})}>
+            <Link to={routes.settings.github.path({})}>GitHub</Link>
+          </NavItem>
+          <NavItem
+            itemId={0}
+            key={`Settings-kie_sandbox_extended_services-nav`}
+            isActive={props.pathname === routes.settings.kie_sandbox_extended_services.path({})}
+          >
+            <Link to={routes.settings.kie_sandbox_extended_services.path({})}>KIE Sandbox Extended Services</Link>
+          </NavItem>
+          <NavItem
+            itemId={0}
+            key={`Settings-openshift-nav`}
+            isActive={props.pathname === routes.settings.openshift.path({})}
+          >
+            <Link to={routes.settings.openshift.path({})}>OpenShift</Link>
+          </NavItem>
+          <NavItem
+            itemId={0}
+            key={`Settings-service_account-nav`}
+            isActive={props.pathname === routes.settings.service_account.path({})}
+          >
+            <Link to={routes.settings.service_account.path({})}>Service Account</Link>
+          </NavItem>
+          <NavItem
+            itemId={0}
+            key={`Settings-service_registry-nav`}
+            isActive={props.pathname === routes.settings.service_registry.path({})}
+          >
+            <Link to={routes.settings.service_registry.path({})}>Service Registry</Link>
+          </NavItem>
+          <NavItem itemId={0} key={`Settings-kafka-nav`} isActive={props.pathname === routes.settings.kafka.path({})}>
+            <Link to={routes.settings.kafka.path({})}>Streams for Apache Kafka</Link>
+          </NavItem>
+          <NavItem
+            itemId={0}
+            key={`Settings-feature_preview-nav`}
+            isActive={props.pathname === routes.settings.feature_preview.path({})}
+          >
+            <Link to={routes.settings.feature_preview.path({})}>Feature Preview</Link>
+          </NavItem>
+        </NavList>
+      </Nav>
+    </>
   );
 }
