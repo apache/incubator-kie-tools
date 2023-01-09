2763252cae6510a8d3378e3c11d954f06b21eb3e
Tiago <tiagobentofernandes@gmail.com>
2023-01-09 10:37:17 -0300

    Context menu scroll improvements

--- packages/boxed-expression-component/src/contextMenu/Hooks.ts
+++ packages/boxed-expression-component/src/contextMenu/Hooks.ts
@@ -26,8 +26,12 @@ export function useCustomContextMenuHandler(domEventTargetRef: React.RefObject<H
 } {
   const { setCurrentlyOpenContextMenu, currentlyOpenContextMenu } = useBoxedExpressionEditor();
 
-  const [xPos, setXPos] = useState(0);
-  const [yPos, setYPos] = useState(0);
+  // FIXME: Tiago -> Maybe using window.scrollX/Y is not the best, as other elements might have the scroll added to it.
+  const [scroll, setScroll] = useState({ x: 0, y: 0 });
+
+  // FIXME: Tiago -> Need to calculate the context menu length for it to not be hidden by the borders of the screen.
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
   const [isOpen, setOpen] = useState(false);
 
   const id = useMemo(() => generateUuid(), []);
@@ -48,12 +52,26 @@ export function useCustomContextMenuHandler(domEventTargetRef: React.RefObject<H
     (e: MouseEvent) => {
       e.preventDefault();
       e.stopPropagation();
-      setXPos(e.pageX);
-      setYPos(e.pageY);
+      setPosition({ x: e.pageX, y: e.pageY });
+      setScroll({ x: window.scrollX, y: window.scrollY });
       setCurrentlyOpenContextMenu(id);
     },
     [id, setCurrentlyOpenContextMenu]
   );
+
+  useEffect(() => {
+    function onScroll(e: UIEvent) {
+      if (currentlyOpenContextMenu === id) {
+        setScroll({ x: window.scrollX, y: window.scrollY });
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [currentlyOpenContextMenu, id]);
 
   useEffect(() => {
     setOpen(id === currentlyOpenContextMenu);
@@ -93,8 +111,8 @@ export function useCustomContextMenuHandler(domEventTargetRef: React.RefObject<H
   }, [domEventTargetRef, hide, currentlyOpenContextMenu, isOpen, show]);
 
   return {
-    xPos: `${xPos + 1}px`, // Leave some margin for clicking without moving the mouse.
-    yPos: `${yPos + 1}px`, // Leave some margin for clicking without moving the mouse.
+    xPos: `${position.x - scroll.x + 1}px`, // Leave some margin for clicking without moving the mouse.
+    yPos: `${position.y - scroll.y + 1}px`, // Leave some margin for clicking without moving the mouse.
     isOpen,
   };
 }
