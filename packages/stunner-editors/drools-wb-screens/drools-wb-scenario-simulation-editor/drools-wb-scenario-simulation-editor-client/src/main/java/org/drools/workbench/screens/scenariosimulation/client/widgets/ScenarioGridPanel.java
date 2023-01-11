6f9d73e7b66520533fe68e59488e22ecb845f73d
Yeser Amer <yamer@redhat.com>
2023-01-11 08:46:15 +0100

    kie-issues#49: Stunner - Scrolling outside the editing cell closes the Text Editor losing any change (#1372)

--- packages/stunner-editors/drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/main/java/org/drools/workbench/screens/scenariosimulation/client/widgets/ScenarioGridPanel.java
+++ packages/stunner-editors/drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/main/java/org/drools/workbench/screens/scenariosimulation/client/widgets/ScenarioGridPanel.java
@@ -103,7 +103,7 @@ public class ScenarioGridPanel extends GridLienzoPanel implements NodeMouseOutHa
 
     @Override
     public void onNodeMouseWheel(NodeMouseWheelEvent nodeMouseWheelEvent) {
-        getScenarioGrid().getModel().destroyAllTextAreaDOMElementFactoryResources();
+        getScenarioGrid().getModel().flushAllTextAreaDOMElementFactoryResources();
     }
 
     @Override
