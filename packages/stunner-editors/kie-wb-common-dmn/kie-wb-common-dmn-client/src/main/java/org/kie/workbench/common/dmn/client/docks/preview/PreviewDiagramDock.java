1452ed60c202e303731bc0f03fb955c718dcda71
Yeser Amer <yeser.amer@ibm.com>
2023-01-12 14:39:53 +0100

    kie-issue#31 Reorganizing docks assets

--- packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/docks/preview/PreviewDiagramDock.java
+++ packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/docks/preview/PreviewDiagramDock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,30 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.kie.workbench.common.dmn.client.docks.navigator;
+package org.kie.workbench.common.dmn.client.docks.preview;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.jboss.errai.ui.client.local.spi.TranslationService;
+import org.kie.workbench.common.dmn.client.resources.i18n.DMNEditorConstants;
 import org.kie.workbench.common.stunner.client.widgets.editor.DiagramEditorDock;
 import org.uberfire.client.workbench.docks.UberfireDock;
 import org.uberfire.client.workbench.docks.UberfireDockPosition;
 import org.uberfire.client.workbench.docks.UberfireDocks;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
 
-import static org.kie.workbench.common.dmn.client.resources.i18n.DMNEditorConstants.DecisionNavigatorPresenter_DecisionNavigator;
-
 @ApplicationScoped
-public class DecisionNavigatorDock implements DiagramEditorDock {
+public class PreviewDiagramDock implements DiagramEditorDock {
 
     protected static final double DOCK_SIZE = 400d;
 
     protected UberfireDocks uberfireDocks;
-
-    protected DecisionNavigatorPresenter decisionNavigatorPresenter;
 
     protected TranslationService translationService;
 
@@ -44,16 +40,14 @@ public class DecisionNavigatorDock implements DiagramEditorDock {
 
     protected boolean isOpened = false;
 
-    public DecisionNavigatorDock() {
+    public PreviewDiagramDock() {
         // CDI proxy
     }
 
     @Inject
-    public DecisionNavigatorDock(final UberfireDocks uberfireDocks,
-                                 final DecisionNavigatorPresenter decisionNavigatorPresenter,
-                                 final TranslationService translationService) {
+    public PreviewDiagramDock(final UberfireDocks uberfireDocks,
+                              final TranslationService translationService) {
         this.uberfireDocks = uberfireDocks;
-        this.decisionNavigatorPresenter = decisionNavigatorPresenter;
         this.translationService = translationService;
     }
 
@@ -74,17 +68,8 @@ public class DecisionNavigatorDock implements DiagramEditorDock {
         }
     }
 
-    public void reload() {
-        decisionNavigatorPresenter.refresh();
-    }
-
-    public void resetContent() {
-        decisionNavigatorPresenter.removeAllElements();
-    }
-
     @Override
     public void open() {
-
         if (isOpened()) {
             return;
         }
@@ -95,7 +80,6 @@ public class DecisionNavigatorDock implements DiagramEditorDock {
 
     @Override
     public void close() {
-
         if (!isOpened()) {
             return;
         }
@@ -109,19 +93,9 @@ public class DecisionNavigatorDock implements DiagramEditorDock {
         return isOpened;
     }
 
-    protected void setOpened(final boolean opened) {
-        isOpened = opened;
-    }
-
     protected UberfireDock makeUberfireDock() {
-
         final UberfireDock uberfireDock = new UberfireDock(position(), icon(), placeRequest());
-
         return uberfireDock.withSize(DOCK_SIZE).withLabel(dockLabel());
-    }
-
-    protected UberfireDock getUberfireDock() {
-        return uberfireDock;
     }
 
     protected UberfireDockPosition position() {
@@ -129,14 +103,18 @@ public class DecisionNavigatorDock implements DiagramEditorDock {
     }
 
     protected String icon() {
-        return IconType.MAP.toString();
-    }
-
-    protected String dockLabel() {
-        return translationService.format(DecisionNavigatorPresenter_DecisionNavigator);
+        return IconType.EYE.toString();
     }
 
     protected DefaultPlaceRequest placeRequest() {
-        return new DefaultPlaceRequest(DecisionNavigatorPresenter.IDENTIFIER);
+        return new DefaultPlaceRequest(PreviewDiagramScreen.SCREEN_ID);
+    }
+
+    protected UberfireDock getUberfireDock() {
+        return uberfireDock;
+    }
+
+    protected String dockLabel() {
+        return translationService.getTranslation(DMNEditorConstants.DMNPreviewDiagramDock_Title);
     }
 }
