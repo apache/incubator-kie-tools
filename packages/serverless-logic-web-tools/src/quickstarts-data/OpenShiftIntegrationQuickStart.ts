01c4b45e4002dbec1a350eb98ea707d17e474d2b
fantonangeli <fabrizio.antonangeli@gmail.com>
2023-01-26 16:23:21 +0100

    Rewrite GitHubTokenQuickStart and OpenShiftIntegrationQuickStart in html for better rendering

--- packages/serverless-logic-web-tools/src/quickstarts-data/OpenShiftIntegrationQuickStart.ts
+++ packages/serverless-logic-web-tools/src/quickstarts-data/OpenShiftIntegrationQuickStart.ts
@@ -18,7 +18,7 @@ import { QuickStart } from "@patternfly/quickstarts";
 
 export const OpenShiftIntegrationQuickStart: QuickStart = {
   metadata: {
-    name: "open-shift-integration-quick-start",
+    name: "serverless-logic-web-tools-openshift-integration",
     instructional: true,
   },
   spec: {
@@ -35,41 +35,138 @@ If you find an issue or any misleading information, please feel free to report i
 
     tasks: [
       {
+        title: "Setting KIE Sandbox Extended Services",
+        description: `<div class="sectionbody">
+          <div class="paragraph">
+          <p>The KIE Sandbox Extended Services tool is required to proxy requests to an OpenShift instance. Therefore, setting the KIE Sandbox Extended Services enables you to deploy and monitor your Serverless Workflow projects.</p>
+          </div>
+          <div class="olist arabic">
+          <div class="title"><em><strong>Procedure</strong></em></div>
+          <ol class="arabic">
+          <li>
+          <p>In the Serverless Logic Web Tools web application, click on the <strong>Cog wheel</strong> (⚙️) on the top-right corner and go to the <strong>KIE Sandbox Extended Services</strong> tab.</p>
+          </li>
+          <li>
+          <p>In case you are not connected to KIE Sandbox Extended Services, you see <strong>You are not connected to KIE Sandbox Extended Services. Click to setup</strong> mesaage along with <strong>Host</strong> and <strong>Port</strong> fields, containing the values as <code><a class="bare" href="http://localhost" aria-invalid="true">http://localhost</a></code> and <code>21345</code> respectively.</p>
+          </li>
+          <li>
+          <p>Click on the link in the message.</p>
+          <div class="paragraph">
+          <p>A window appears containing your current operating system as selected and a link to download to the latest version of KIE Sandbox Extended Services.</p>
+          </div>
+          </li>
+          <li>
+          <p>Download the KIE Sandbox Extended Services and execute it.</p>
+          <div class="openblock">
+          <div class="content">
+          <div class="paragraph">
+          <p>You might need to grant permissions to run the file depending on your operating system and settings.</p>
+          </div>
+          <div class="paragraph">
+          <p>After executing the KIE Sandbox Extended Services the content in the <strong>KIE Sandbox Extended Services</strong> is updated and displays that you are connected to the KIE Sandbox Extended Services.</p>
+          </div>
+          </div>
+          </div>
+          </li>
+          </ol>
+          </div>
+          </div>`,
+      },
+      {
         title: "Connecting to OpenShift instance using Serverless Logic Web Tools",
-        description: `After setting the KIE Sandbox Extended Services, you can connect to your OpenShift instance to deploy your Serverless Workflow projects with Serverless Logic Web Tools.
-
-You can create a free developer sandbox. For more information, see [OpenShift Developer Sandbox](https://developers.redhat.com/developer-sandbox).
-
-
-_**Procedure**_
-
-1.  Log in to your OpenShift instance console interface.
-2.  In the OpenShift instance console interface, you need your OpenShift project name (also known as namespace), API server, and an access token.
-
-	*   For the OpenShift project name, go to the **Topology** tab and in the top-left corner you see your project name.
-
-	![serverless logic web tools openshift project](images/quickstarts/serverless-logic-web-tools-openshift-project.png)
-
-	Figure 1. OpenShift project name in OpenShift instance console
-
-	*   To obtain the API server and access token, click on your username and **Copy login command**.  
-		A new page opens containing your new API token along with \`oc cli\` login command. From the \`oc cli\` command, copy the value of \`--server=\`.
-
-	![serverless logic web tools openshift info](https://kiegroup.github.io/kogito-docs/serverlessworkflow/latest/_images/tooling/serverless-logic-web-tools/serverless-logic-web-tools-openshift-info.png)
-
-	Figure 2. OpenShift access token and API server
-
-3.  Go to the Serverless Logic Web Tools web application, click on the **Cog wheel** (⚙️) on the top-right corner and go to the **OpenShift** tab.
-4.  Enter your OpenShift project name in the **Namespace (project)** field.
-5.  Enter the value copied value of \`--server\` flag in the **Host** field.
-6.  Enter the value of API token in the **Token** field.
-7.  Click **Connect**.
-
-    If the entered values are correct, then the tab updates and displays **You’re connected to OpenShift** message.
-
-After connecting to OpenShift, you are ready to deploy your Serverless Workflow projects using Serverless Logic Web Tools. For more information about deploying your projects, see [Deploying your Serverless Workflow projects using Serverless Logic Web Tools](https://kiegroup.github.io/kogito-docs/serverlessworkflow/latest/tooling/serverless-logic-web-tools/serverless-logic-web-tools-deploy-projects.html).
-
-<table border="1"><tbody><tr><td>If your OpenShift instance uses self-signed certificates, then you might need to enable <code>InsecureSkipVerify</code> on KIE Sandbox Extended Services.</td></tr></tbody></table>`,
+        description: `<div class="sect1">
+          <h2 id="proc-connecting-openshift-instance-serverless-logic-web-tools">Connecting to OpenShift instance using Serverless Logic Web Tools</h2>
+          <div class="sectionbody">
+          <div class="paragraph">
+          <p>After setting the KIE Sandbox Extended Services, you can connect to your OpenShift instance to deploy your Serverless Workflow projects with Serverless Logic Web Tools.</p>
+          </div>
+          <div class="ulist">
+          <div class="title"><em><strong>Prerequisites</strong></em></div>
+          <ul>
+          <li>
+          <p>KIE Sandbox Extended Services tool installed and running.</p>
+          </li>
+          <li>
+          <p>OpenShift instance is active.</p>
+          <div class="paragraph">
+          <p>You can create a free developer sandbox. For more information, see <a href="https://developers.redhat.com/developer-sandbox">OpenShift Developer Sandbox</a>.</p>
+          </div>
+          </li>
+          </ul>
+          </div>
+          <div class="olist arabic">
+          <div class="title"><em><strong>Procedure</strong></em></div>
+          <ol class="arabic">
+          <li>
+          <p>Log in to your OpenShift instance console interface.</p>
+          </li>
+          <li>
+          <p>In the OpenShift instance console interface, you need your OpenShift project name (also known as namespace), API server, and an access token.</p>
+          <div class="openblock">
+          <div class="content">
+          <div class="ulist">
+          <ul>
+          <li>
+          <p>For the OpenShift project name, go to the <strong>Topology</strong> tab and in the top-left corner you see your project name.</p>
+          <div class="imageblock">
+          <div class="content"><img src="images/quickstarts/serverless-logic-web-tools-openshift-project.png" alt="serverless logic web tools openshift project"></div>
+          <div class="title">Figure 1. OpenShift project name in OpenShift instance console</div>
+          </div>
+          </li>
+          <li>
+          <p>To obtain the API server and access token, click on your username and <strong>Copy login command</strong>.</p>
+          <div class="paragraph">
+          <p>A new page opens containing your new API token along with <code>oc cli</code> login command. From the <code>oc cli</code> command, copy the value of <code>--server=</code>.</p>
+          </div>
+          <div class="imageblock">
+          <div class="content"><img src="images/quickstarts/serverless-logic-web-tools-openshift-info.png" alt="serverless logic web tools openshift info"></div>
+          <div class="title">Figure 2. OpenShift access token and API server</div>
+          </div>
+          </li>
+          </ul>
+          </div>
+          </div>
+          </div>
+          </li>
+          <li>
+          <p>Go to the Serverless Logic Web Tools web application, click on the <strong>Cog wheel</strong> (⚙️) on the top-right corner and go to the <strong>OpenShift</strong> tab.</p>
+          </li>
+          <li>
+          <p>Enter your OpenShift project name in the <strong>Namespace (project)</strong> field.</p>
+          </li>
+          <li>
+          <p>Enter the value copied value of <code>--server</code> flag in the <strong>Host</strong> field.</p>
+          </li>
+          <li>
+          <p>Enter the value of API token in the <strong>Token</strong> field.</p>
+          </li>
+          <li>
+          <p>Click <strong>Connect</strong>.</p>
+          <div class="paragraph">
+          <p>If the entered values are correct, then the tab updates and displays <strong>You&rsquo;re connected to OpenShift</strong> message.</p>
+          </div>
+          </li>
+          </ol>
+          </div>
+          <div class="paragraph">
+          <p>After connecting to OpenShift, you are ready to deploy your Serverless Workflow projects using Serverless Logic Web Tools. For more information about deploying your projects, see <a class="xref page" href="serverless-logic-web-tools-deploy-projects.html" aria-invalid="true">Deploying your Serverless Workflow projects using Serverless Logic Web Tools</a>.</p>
+          </div>
+          <div class="admonitionblock note">
+          <table>
+          <tbody>
+          <tr>
+          <td class="icon">&nbsp;</td>
+          <td class="content">
+          <div class="paragraph">
+          <p><b>NOTE</b><br />If your OpenShift instance uses self-signed certificates, then you might need to enable <code>InsecureSkipVerify</code> on KIE Sandbox Extended Services.</p>
+          </div>
+          </td>
+          </tr>
+          </tbody>
+          </table>
+          </div>
+          </div>
+          </div>`,
       },
     ],
   },
