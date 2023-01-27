6d247d63950df58b55a6c36c039fe82f8cfb58a1
Luiz Motta <luizjoaomotta@gmail.com>
2023-01-27 12:07:18 -0300

    Change to work with any "import" tag

--- packages/dmn-language-service/src/index.ts
+++ packages/dmn-language-service/src/index.ts
@@ -17,18 +17,22 @@
 import { ResourceContent } from "@kie-tools-core/workspace/dist/api";
 import { WorkspacesContextType } from "@kie-tools-core/workspaces-git-fs/dist/context/WorkspacesContext";
 
-const DMN_IMPORT = "dmn:import";
-const DMN_INPUT_DATA = "dmn:inputData";
+const IMPORT = "import";
+const INPUT_DATA = "inputData";
 const XML_MIME = "text/xml";
 const LOCATION_URI_ATTRIBUTE = "locationURI";
 
 export class DmnLanguageService {
   private readonly parser = new DOMParser();
+  private readonly importTagRegExp = new RegExp(`([a-z]*:)?(${IMPORT})`);
+  private readonly inputDataRegEx = new RegExp(`([a-z]*:)?(${INPUT_DATA})`);
+
   DmnLanguageService() {}
 
   private getImportedModel(model: string): string[] {
     const xmlContent = this.parser.parseFromString(model, XML_MIME);
-    const importedModels = xmlContent.getElementsByTagName(DMN_IMPORT);
+    const importTag = this.importTagRegExp.exec(model);
+    const importedModels = xmlContent.getElementsByTagName(importTag?.at(0) ?? IMPORT);
     return Array.from(importedModels)
       .map((importedModel) => importedModel.getAttribute(LOCATION_URI_ATTRIBUTE))
       .filter((e) => e !== null) as string[];
@@ -46,7 +50,8 @@ export class DmnLanguageService {
   public getPathFromNodeId(resourceContents: { path: string; content?: string }[], nodeId: string): string {
     for (const resourceContent of resourceContents) {
       const xmlContent = this.parser.parseFromString(resourceContent.content ?? "", XML_MIME);
-      const inputs = xmlContent.getElementsByTagName(DMN_INPUT_DATA);
+      const inputDataTag = this.inputDataRegEx.exec(resourceContent.content ?? "");
+      const inputs = xmlContent.getElementsByTagName(inputDataTag?.at(0) ?? INPUT_DATA);
       for (const input of Array.from(inputs)) {
         if (input.id === nodeId) {
           return resourceContent.path;
