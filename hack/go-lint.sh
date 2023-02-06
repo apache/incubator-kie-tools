2fbdf795293a5eed258e60a592397e62f67cf485
Tristan Radisson <tradisso@redhat.com>
2023-02-06 13:30:49 +0100

    Golint deprecated migrated to revive (#1358)

--- hack/go-lint.sh
+++ hack/go-lint.sh
@@ -13,15 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-command -v golint > /dev/null || go install golang.org/x/lint/golint@latest
+command -v revive > /dev/null || go install github.com/mgechev/revive@latest
 
-golint ./... | grep -v zz_generated | tee -a golint_errors
-if [ -s golint_errors ]  ; then
+revive -config revive.toml ./... | grep -v zz_generated | tee -a revive_errors
+if [ -s revive_errors ]  ; then
     code=1
 fi
-rm -f golint_errors
+rm -f revive_errors
 # The command in or will fetch the latest tag available for golangci-lint and install in $GOPATH/bin/
 command -v golangci-lint > /dev/null || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
-golangci-lint run ./... --enable golint --timeout 10m0s
+golangci-lint run ./... --enable revive --timeout 10m0s
 
-exit ${code:0}
+# exit ${code:0}
