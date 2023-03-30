4c9554e3e25a641833559648b6560807d2e9815a
Tomas David <tdavid@redhat.com>
2023-03-30 08:22:28 +0200

    Incorporate changes

--- packages/serverless-logic-web-tools/it-tests/e2e/Expression.cy.ts
+++ packages/serverless-logic-web-tools/it-tests/e2e/Expression.cy.ts
@@ -19,111 +19,56 @@ describe("Serverless Logic Web Tools - Expression test", () => {
     cy.visit("/");
   });
 
-  it("should check expression autocompletion in JSON serverless workflow file", () => {
-    // open prepared sw json file
-    cy.get("#upload-field").attachFile("expression/expression.sw.json", {
-      subjectType: "drag-n-drop",
-    });
-    cy.loadEditor();
+  const testParameters: [string, string, number, number][] = [
+    ["JSON", "expression/expression.sw.json", 10, 30],
+    ["YAML", "expression/expression.sw.yaml", 8, 26],
+  ];
 
-    cy.getEditor().within(() => {
-      cy.iframe("#kogito-iframe[src='./serverless-workflow-text-editor-envelope.html']").within(() => {
-        // create path to fromStateData value
-        var path = "";
-        for (var row = 0; row < 10; row++) {
-          path += "{downArrow}";
-        }
-        for (var column = 0; column < 30; column++) {
-          path += "{rightArrow}";
-        }
+  testParameters.forEach(($param) => {
+    const [fileType, filePath, rowLocation, columnLocation] = $param;
 
-        // move to value of actions -> actionDataFilter -> fromStateData and invoke content assist
-        cy.get(".monaco-editor textarea").type(path).type("{ctrl} ");
-
-        // check jq functions are listed in content assist
-        cy.get(".monaco-list-row .label-name").should(($jqFunctions) => {
-          expect($jqFunctions.eq(0)).text("add");
-          expect($jqFunctions.eq(1)).text("all");
-          expect($jqFunctions.eq(2)).text("all(condition)");
-          expect($jqFunctions.eq(3)).text("all(generator; condition)");
-          expect($jqFunctions.eq(4)).text("any");
-        });
-
-        // close content assist and invoke new one after '.'
-        cy.get(".monaco-editor textarea").type("{esc}").type(".").type("{ctrl} ");
-
-        // check properties from dataInputSchema and callBackFunc are listed in content assist
-        cy.get(".monaco-list-row .label-name").should(($properties) => {
-          expect($properties.length).eq(5);
-          expect($properties.eq(0)).text("numbers");
-          expect($properties.eq(1)).text("processInstanceId");
-          expect($properties.eq(2)).text("uri");
-          expect($properties.eq(3)).text("x");
-          expect($properties.eq(4)).text("y");
-        });
-
-        // delete '.' and invoke content assist for 'fn:'
-        cy.get(".monaco-editor textarea").type("{backspace}").type("fn:").type("{ctrl} ");
-
-        // check function is listed in content assist
-        cy.get(".monaco-list-row .label-name").should(($functions) => {
-          expect($functions.length).eq(1);
-          expect($functions.eq(0)).text("testFunc");
-        });
+    it(`should check expression autocompletion in ${fileType} serverless workflow file`, () => {
+      // open prepared sw file
+      cy.get("#upload-field").attachFile(`${filePath}`, {
+        subjectType: "drag-n-drop",
       });
-    });
-  });
+      cy.loadEditor();
 
-  it("should check expression autocompletion in YAML serverless workflow file", () => {
-    // open prepared sw yaml file
-    cy.get("#upload-field").attachFile("expression/expression.sw.yaml", {
-      subjectType: "drag-n-drop",
-    });
-    cy.loadEditor();
+      cy.getEditor().within(() => {
+        cy.iframe("#kogito-iframe[src='./serverless-workflow-text-editor-envelope.html']").within(() => {
+          // move to value of actions -> actionDataFilter -> fromStateData and invoke content assist
+          cy.get(".monaco-editor textarea").moveToPosition(rowLocation, columnLocation).type("{ctrl} ");
 
-    cy.getEditor().within(() => {
-      cy.iframe("#kogito-iframe[src='./serverless-workflow-text-editor-envelope.html']").within(() => {
-        // create path to fromStateData value
-        var path = "";
-        for (var row = 0; row < 8; row++) {
-          path += "{downArrow}";
-        }
-        for (var column = 0; column < 26; column++) {
-          path += "{rightArrow}";
-        }
+          // check jq functions are listed in content assist
+          cy.get(".monaco-list-row .label-name").should(($jqFunctions) => {
+            expect($jqFunctions.eq(0)).text("add");
+            expect($jqFunctions.eq(1)).text("all");
+            expect($jqFunctions.eq(2)).text("all(condition)");
+            expect($jqFunctions.eq(3)).text("all(generator; condition)");
+            expect($jqFunctions.eq(4)).text("any");
+          });
 
-        // move to value of actions -> actionDataFilter -> fromStateData and invoke content assist
-        cy.get(".monaco-editor textarea").type(path).type("{ctrl} ");
+          // close content assist and invoke new one after '.'
+          cy.get(".monaco-editor textarea").type("{esc}").type(".").type("{ctrl} ");
 
-        // check jq functions are listed in content assist
-        cy.get(".monaco-list-row .label-name").should(($jqFunctions) => {
-          expect($jqFunctions.eq(0)).text("add");
-          expect($jqFunctions.eq(1)).text("all");
-          expect($jqFunctions.eq(2)).text("all(condition)");
-          expect($jqFunctions.eq(3)).text("all(generator; condition)");
-          expect($jqFunctions.eq(4)).text("any");
-        });
+          // check properties from dataInputSchema and callBackFunc are listed in content assist
+          cy.get(".monaco-list-row .label-name").should(($properties) => {
+            expect($properties.length).eq(5);
+            expect($properties.eq(0)).text("numbers");
+            expect($properties.eq(1)).text("processInstanceId");
+            expect($properties.eq(2)).text("uri");
+            expect($properties.eq(3)).text("x");
+            expect($properties.eq(4)).text("y");
+          });
 
-        // close content assist and invoke new one after '.'
-        cy.get(".monaco-editor textarea").type("{esc}").type(".").type("{ctrl} ");
+          // delete '.' and invoke content assist for 'fn:'
+          cy.get(".monaco-editor textarea").type("{backspace}").type("fn:").type("{ctrl} ");
 
-        // check properties from dataInputSchema and callBackFunc are listed in content assist
-        cy.get(".monaco-list-row .label-name").should(($properties) => {
-          expect($properties.length).eq(5);
-          expect($properties.eq(0)).text("numbers");
-          expect($properties.eq(1)).text("processInstanceId");
-          expect($properties.eq(2)).text("uri");
-          expect($properties.eq(3)).text("x");
-          expect($properties.eq(4)).text("y");
-        });
-
-        // delete '.' and invoke content assist for 'fn:'
-        cy.get(".monaco-editor textarea").type("{backspace}").type("fn:").type("{ctrl} ");
-
-        // check function is listed in content assist
-        cy.get(".monaco-list-row .label-name").should(($functions) => {
-          expect($functions.length).eq(1);
-          expect($functions.eq(0)).text("testFunc");
+          // check function is listed in content assist
+          cy.get(".monaco-list-row .label-name").should(($functions) => {
+            expect($functions.length).eq(1);
+            expect($functions.eq(0)).text("testFunc");
+          });
         });
       });
     });
