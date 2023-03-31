42771812077f2cd35ffa0688c36887ba0a25f8a4
Kirill Gaevskii <kgaevski@redhat.com>
2023-03-31 16:24:59 +0200

    KOGITO-8316: SWF Viewer - Parallel state requires more details (#1541)

--- packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-client/src/main/java/org/kie/workbench/common/stunner/sw/client/shapes/ParallelStateShape.java
+++ packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-client/src/main/java/org/kie/workbench/common/stunner/sw/client/shapes/ParallelStateShape.java
@@ -21,18 +21,21 @@ import org.kie.workbench.common.stunner.core.client.shape.MutationContext;
 import org.kie.workbench.common.stunner.core.graph.Edge;
 import org.kie.workbench.common.stunner.core.graph.Node;
 import org.kie.workbench.common.stunner.core.graph.content.view.View;
+import org.kie.workbench.common.stunner.sw.client.shapes.icons.BottomText;
 import org.kie.workbench.common.stunner.sw.client.shapes.icons.CornerIcon;
 import org.kie.workbench.common.stunner.sw.definition.ParallelState;
 import org.kie.workbench.common.stunner.sw.definition.State;
 
+import static org.kie.workbench.common.stunner.sw.client.shapes.icons.IconPath.BRANCH;
 import static org.kie.workbench.common.stunner.sw.client.shapes.icons.IconPath.CLOCK;
 import static org.kie.workbench.common.stunner.sw.client.shapes.icons.IconPath.FILTER;
 import static org.kie.workbench.common.stunner.sw.client.shapes.icons.IconPosition.BOTTOM_FROM_RIGHT_TOP_CORNER;
+import static org.kie.workbench.common.stunner.sw.client.shapes.icons.IconPosition.LEFT_FROM_RIGHT_TOP_CORNER;
 import static org.kie.workbench.common.stunner.sw.client.shapes.icons.IconPosition.RIGHT_TOP_CORNER;
 import static org.kie.workbench.common.stunner.sw.resources.i18n.SWConstants.TIMEOUT_BRANCH;
 import static org.kie.workbench.common.stunner.sw.resources.i18n.SWConstants.TIMEOUT_STATE;
 
-public class ParallelStateShape extends StateShape implements HasDataFilter {
+public class ParallelStateShape extends StateShape implements HasDataFilter, HasBranches {
 
     public static final String ICON_COLOR = "#4CB140";
     public static final String ICON_SVG = "M50.54,.01c2.34,.16,4.24,2.31,4.24,4.65V59.43c0,2.64-2.23,4.75-4.91,4.56-2.33-.17-4.23-2.31-4.23-4.65V4.57c0-2.63,2.22-4.75,4.9-4.56M13.46,63.99c-2.34-.16-4.24-2.31-4.24-4.65V4.57c0-2.52,2.04-4.57,4.57-4.57s4.57,2.05,4.57,4.57V59.43c0,2.64-2.22,4.75-4.9,4.56Z";
@@ -47,16 +50,25 @@ public class ParallelStateShape extends StateShape implements HasDataFilter {
         ParallelState state = (ParallelState) element.getContent().getDefinition();
         if (state.getTimeouts() != null) {
             getView().addChild(new CornerIcon(CLOCK,
-                                              RIGHT_TOP_CORNER,
+                                              LEFT_FROM_RIGHT_TOP_CORNER,
                                               getTranslation(TIMEOUT_BRANCH) + ": " + state.getTimeouts().getBranchExecTimeout() + "\r\n"
                                                       + getTranslation(TIMEOUT_STATE) + ": " + state.getTimeouts().getStateExecTimeout()));
         }
+
+        getView().addChild(new CornerIcon(BRANCH,
+                                          RIGHT_TOP_CORNER,
+                                          getBranchesString(state.getBranches())));
 
         if (state.getStateDataFilter() != null) {
             getView().addChild(new CornerIcon(FILTER,
                                               BOTTOM_FROM_RIGHT_TOP_CORNER,
                                               getStateDataFilter(state.getStateDataFilter())));
         }
+
+        String completionType = "atleast".equalsIgnoreCase(state.getCompletionType())
+                ? getTranslation("Enum.atleast") + ": " + state.getNumCompleted()
+                : getTranslation("Enum.allof");
+        getView().addChild(new BottomText(completionType));
     }
 
     @Override
