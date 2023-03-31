a14cdb89dfd81878bc1c567ab7142fdc75b40f18
William Siqueira <william.fatecsjc@gmail.com>
2023-03-31 15:38:39 -0300

    Removing Properties Editor

--- packages/dashbuilder/appformer/uberfire-extensions/uberfire-layout-editor/uberfire-layout-editor-client/src/main/java/org/uberfire/ext/layout/editor/client/infra/LayoutEditorCssHelper.java
+++ packages/dashbuilder/appformer/uberfire-extensions/uberfire-layout-editor/uberfire-layout-editor-client/src/main/java/org/uberfire/ext/layout/editor/client/infra/LayoutEditorCssHelper.java
@@ -16,7 +16,6 @@
 package org.uberfire.ext.layout.editor.client.infra;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,139 +25,11 @@ import javax.enterprise.context.ApplicationScoped;
 import org.uberfire.ext.layout.editor.api.css.CssAllowedValue;
 import org.uberfire.ext.layout.editor.api.css.CssProperty;
 import org.uberfire.ext.layout.editor.api.css.CssValue;
-import org.uberfire.ext.layout.editor.api.css.CssValueType;
-import org.uberfire.ext.layout.editor.client.api.LayoutElementWithProperties;
 import org.uberfire.ext.layout.editor.client.resources.i18n.CssAllowedValueConstants;
-import org.uberfire.ext.layout.editor.client.resources.i18n.PropertiesConstants;
-import org.uberfire.ext.layout.editor.client.validator.LayoutEditorPropertyLengthValidator;
-import org.uberfire.ext.layout.editor.client.validator.LayoutEditorPropertyNumberValidator;
-import org.uberfire.ext.properties.editor.model.PropertyEditorCategory;
-import org.uberfire.ext.properties.editor.model.PropertyEditorFieldInfo;
-import org.uberfire.ext.properties.editor.model.PropertyEditorType;
-import org.uberfire.ext.properties.editor.model.validators.PropertyFieldValidator;
 
 @ApplicationScoped
 public class LayoutEditorCssHelper {
-
-    private Map<CssValueType, PropertyFieldValidator> valueTypeValidatorMap = new HashMap<>();
-    private Map<String, String> propertyCategoryMapI18n = new HashMap<>();
-    private Map<CssProperty, String> propertyNameMapI18n = new HashMap<>();
-    private Map<CssProperty, String> propertyHelpMapI18n = new HashMap<>();
-
-    public static final String CSS_CATEGORY_PANEL = "panel";
-    public static final String CSS_CATEGORY_TEXT = "text";
-    public static final String CSS_CATEGORY_MARGIN = "margin";
-    public static final String CSS_CATEGORY_PADDING = "padding";
-
-    public LayoutEditorCssHelper() {
-        valueTypeValidatorMap.put(CssValueType.LENGTH, new LayoutEditorPropertyLengthValidator());
-        valueTypeValidatorMap.put(CssValueType.NUMBER, new LayoutEditorPropertyNumberValidator());
-
-        propertyCategoryMapI18n.put(CSS_CATEGORY_PANEL, PropertiesConstants.INSTANCE.panel());
-        propertyCategoryMapI18n.put(CSS_CATEGORY_TEXT, PropertiesConstants.INSTANCE.text());
-        propertyCategoryMapI18n.put(CSS_CATEGORY_MARGIN, PropertiesConstants.INSTANCE.margin());
-        propertyCategoryMapI18n.put(CSS_CATEGORY_PADDING, PropertiesConstants.INSTANCE.padding());
-
-        propertyNameMapI18n.put(CssProperty.WIDTH, PropertiesConstants.INSTANCE.width());
-        propertyNameMapI18n.put(CssProperty.HEIGHT, PropertiesConstants.INSTANCE.height());
-        propertyNameMapI18n.put(CssProperty.FLOAT, PropertiesConstants.INSTANCE.float_property());
-        propertyNameMapI18n.put(CssProperty.BACKGROUND_COLOR, PropertiesConstants.INSTANCE.background_color());
-        propertyNameMapI18n.put(CssProperty.MARGIN_TOP, PropertiesConstants.INSTANCE.margin_top());
-        propertyNameMapI18n.put(CssProperty.MARGIN_BOTTOM, PropertiesConstants.INSTANCE.margin_bottom());
-        propertyNameMapI18n.put(CssProperty.MARGIN_LEFT, PropertiesConstants.INSTANCE.margin_left());
-        propertyNameMapI18n.put(CssProperty.MARGIN_RIGHT, PropertiesConstants.INSTANCE.margin_right());
-        propertyNameMapI18n.put(CssProperty.PADDING_TOP, PropertiesConstants.INSTANCE.padding_top());
-        propertyNameMapI18n.put(CssProperty.PADDING_BOTTOM, PropertiesConstants.INSTANCE.padding_bottom());
-        propertyNameMapI18n.put(CssProperty.PADDING_LEFT, PropertiesConstants.INSTANCE.padding_left());
-        propertyNameMapI18n.put(CssProperty.PADDING_RIGHT, PropertiesConstants.INSTANCE.padding_right());
-        propertyNameMapI18n.put(CssProperty.TEXT_ALIGN, PropertiesConstants.INSTANCE.text_align());
-        propertyNameMapI18n.put(CssProperty.TEXT_DECORATION, PropertiesConstants.INSTANCE.text_decoration());
-        propertyNameMapI18n.put(CssProperty.COLOR, PropertiesConstants.INSTANCE.color());
-        propertyNameMapI18n.put(CssProperty.FONT_SIZE, PropertiesConstants.INSTANCE.font_size());
-        propertyNameMapI18n.put(CssProperty.FONT_WEIGHT, PropertiesConstants.INSTANCE.font_weight());
-
-        propertyHelpMapI18n.put(CssProperty.WIDTH, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.HEIGHT, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.MARGIN_TOP, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.MARGIN_BOTTOM, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.MARGIN_LEFT, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.MARGIN_RIGHT, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.PADDING_TOP, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.PADDING_BOTTOM, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.PADDING_LEFT, PropertiesConstants.INSTANCE.length_help());
-        propertyHelpMapI18n.put(CssProperty.PADDING_RIGHT, PropertiesConstants.INSTANCE.length_help());
-    }
-
-    public String formatCssAllowedValue(CssProperty cssProperty) {
-        return propertyNameMapI18n.get(cssProperty);
-    }
-
-    public String getHelpText(CssProperty cssProperty) {
-        return propertyHelpMapI18n.get(cssProperty);
-    }
-
-    public List<PropertyFieldValidator> getValidators(CssProperty property) {
-        List<PropertyFieldValidator> result = new ArrayList<>();
-        for (CssValueType cssValueType : property.getSupportedValueTypes()) {
-            PropertyFieldValidator validator = valueTypeValidatorMap.get(cssValueType);
-            if (validator != null) {
-                result.add(validator);
-            }
-        }
-        return result;
-    }
-
-    public PropertyEditorType resolveFieldType(CssProperty cssProperty) {
-        if (cssProperty.supportsValueType(CssValueType.COLOR)) {
-            return PropertyEditorType.COLOR;
-        }
-        if (!cssProperty.getAllowedValues().isEmpty()) {
-            return PropertyEditorType.COMBO;
-        }
-        return PropertyEditorType.TEXT;
-    }
-
-    public PropertyEditorCategory createCategory(String categoryCode) {
-        String name = propertyCategoryMapI18n.get(categoryCode);
-        return new PropertyEditorCategory(name);
-    }
-
-    public PropertyEditorFieldInfo createField(LayoutElementWithProperties element, CssProperty property) {
-        return createField(element.getProperties(), property);
-    }
-
-    public PropertyEditorFieldInfo createField(Map<String, String> propertyMap, CssProperty property) {
-        String propValue = propertyMap.get(property.getName());
-        return createPropertyField(property, propValue == null ? "" : propValue);
-    }
-
-    public PropertyEditorFieldInfo createPropertyField(CssProperty property, String value) {
-        String displayName = propertyNameMapI18n.get(property);
-        String helpText = propertyHelpMapI18n.get(property);
-        PropertyEditorType propType = resolveFieldType(property);
-        List<PropertyFieldValidator> validators = getValidators(property);
-        List<CssAllowedValue> allowedValues = property.getAllowedValues();
-        CssAllowedValue allowedValue = parseCssAllowedValue(property, value);
-        String currentValue = allowedValue != null ? formatCssAllowedValue(property, allowedValue) : value;
-
-        PropertyEditorFieldInfo field = new PropertyEditorFieldInfo(displayName, currentValue, propType)
-                .withKey(property.getName());
-
-        if (helpText != null) {
-            field.withHelpInfo("", helpText);
-        }
-        if (validators != null && !validators.isEmpty()) {
-            field.withValidators(validators);
-        }
-        if (allowedValues != null && !allowedValues.isEmpty()) {
-            field.withComboValues(allowedValues.stream()
-                    .map(v -> formatCssAllowedValue(property, v))
-                    .collect(Collectors.toList()));
-        }
-
-        return field;
-    }
-
+    
     public String formatCssAllowedValue(CssProperty property, CssAllowedValue value) {
         String i18nKey = property.toString() + "__" + value.toString();
         return CssAllowedValueConstants.INSTANCE.getString(i18nKey);
