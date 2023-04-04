4bb956999052381f794f9776a9e19b4aa0954e27
Saravana Balaji Srinivasan <40579476+Sara4994@users.noreply.github.com>
2023-04-04 23:12:52 +0530

    KOGITO-8875: [SWF Editor] Integrate samples from kie-samples (#1535)
    Co-authored-by: Guilherme Caponetto <638737+caponetto@users.noreply.github.com>

---
+++ packages/serverless-logic-web-tools/src/home/sample/SampleCard.tsx
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from "react";
+import { useMemo } from "react";
+import { Card, CardTitle, CardFooter, CardBody } from "@patternfly/react-core/dist/js/components/Card";
+import { Grid, GridItem } from "@patternfly/react-core/dist/js/layouts/Grid";
+import { Button, ButtonVariant } from "@patternfly/react-core/dist/js/components/Button";
+import { useRoutes } from "../../navigation/Hooks";
+import { Link } from "react-router-dom";
+import { Text } from "@patternfly/react-core/dist/js/components/Text";
+import { Label, LabelProps } from "@patternfly/react-core/dist/js/components/Label";
+import { FolderIcon, FileIcon, MonitoringIcon } from "@patternfly/react-icons/dist/js/icons";
+import { Sample, SampleCategory } from "./sampleApi";
+import { Tooltip } from "@patternfly/react-core/dist/js/components/Tooltip";
+
+const tagMap: Record<SampleCategory, { label: string; icon: React.ComponentClass; color: LabelProps["color"] }> = {
+  ["serverless-workflow"]: {
+    label: "Serverless Workflow",
+    icon: FileIcon,
+    color: "orange",
+  },
+  ["serverless-decision"]: {
+    label: "Serverless Decision",
+    icon: FolderIcon,
+    color: "blue",
+  },
+  ["dashbuilder"]: {
+    label: "Dashboard",
+    icon: MonitoringIcon,
+    color: "purple",
+  },
+};
+
+export function RenderSvg(props: { svg: string }) {
+  const modifiedContent = useMemo(() => {
+    try {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(props.svg, "image/svg+xml");
+      const svg = xml.getElementsByTagName("svg")[0];
+      if (svg) {
+        svg.setAttribute("width", "400px");
+        svg.setAttribute("height", "300px");
+      }
+      const serializer = new XMLSerializer();
+      return serializer.serializeToString(xml);
+    } catch (e) {
+      return "Cannot render SVG!";
+    }
+  }, [props.svg]);
+
+  return (
+    <div
+      style={{ height: "100%", maxWidth: "100%", maxHeight: "400px", paddingTop: "30px" }}
+      dangerouslySetInnerHTML={{ __html: modifiedContent }}
+    />
+  );
+}
+
+export function SampleCard(props: { sample: Sample }) {
+  const routes = useRoutes();
+
+  const tag = useMemo(() => tagMap[props.sample.definition.category], [props.sample.definition.category]);
+
+  return (
+    <Card isCompact={true} isFullHeight={true}>
+      <Grid style={{ height: "100%" }}>
+        <GridItem
+          md={6}
+          style={{ overflow: "hidden", textAlign: "center", verticalAlign: "middle", position: "relative" }}
+        >
+          <div style={{ position: "absolute", bottom: "16px", right: 0, left: 0, margin: "auto" }}>
+            <Label color={tag.color}>
+              <tag.icon />
+              &nbsp;&nbsp;<b>{tag.label}</b>
+            </Label>
+          </div>
+          <RenderSvg svg={props.sample.svgContent} />
+        </GridItem>
+        <GridItem md={6} style={{ display: "flex", flexDirection: "column" }}>
+          <CardTitle data-ouia-component-type="sample-title">{props.sample.definition.title}</CardTitle>
+          <CardBody isFilled={true}>
+            <Tooltip content={<div>{props.sample.definition.description}</div>}>
+              <Text
+                component="p"
+                style={{
+                  display: "-webkit-box",
+                  WebkitBoxOrient: "vertical",
+                  WebkitLineClamp: 5,
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  whiteSpace: "pre-wrap",
+                }}
+              >
+                {props.sample.definition.description}
+              </Text>
+            </Tooltip>
+          </CardBody>
+          <CardFooter style={{ alignItems: "baseline" }}>
+            <Link
+              to={{
+                pathname: routes.sampleShowcase.path({}),
+                search: routes.sampleShowcase.queryString({ sampleId: props.sample.sampleId }),
+              }}
+            >
+              <Button variant={ButtonVariant.tertiary} ouiaId={props.sample.sampleId + `-try-swf-sample-button`}>
+                Try it out!
+              </Button>
+            </Link>
+          </CardFooter>
+        </GridItem>
+      </Grid>
+    </Card>
+  );
+}
---
+++ packages/serverless-logic-web-tools/src/home/sample/Showcase.tsx
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { TextContent, Text } from "@patternfly/react-core/dist/js/components/Text";
+import { SampleCard } from "./SampleCard";
+import { Gallery } from "@patternfly/react-core/dist/js/layouts/Gallery";
+import { Sample } from "./sampleApi";
+import { SampleCardSkeleton } from "./SampleCardSkeleton";
+import { SamplesLoadError } from "./SamplesLoadError";
+import { useSampleDispatch } from "./hooks/SampleContext";
+
+const priority = {
+  ["serverless-workflow"]: 1,
+  ["dashbuilder"]: 2,
+  ["serverless-decision"]: 3,
+};
+
+export function Showcase() {
+  const sampleDispatch = useSampleDispatch();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [samples, setSamples] = useState<Sample[]>([]);
+  const [sampleLoadingError, setSampleLoadingError] = useState("");
+
+  useEffect(() => {
+    sampleDispatch
+      .getSamples()
+      .then((data) => {
+        const sortedSamples = data.sort(
+          (a: Sample, b: Sample) => priority[a.definition.category] - priority[b.definition.category]
+        );
+        setSamples([...sortedSamples]);
+      })
+      .catch((e) => {
+        setSampleLoadingError(e.toString());
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [sampleDispatch]);
+
+  return (
+    <>
+      {sampleLoadingError && <SamplesLoadError errors={[sampleLoadingError]} />}
+      {!sampleLoadingError && (
+        <>
+          <TextContent>
+            <Text component="h1">Samples Showcase</Text>
+          </TextContent>
+          <br />
+          {loading ? (
+            <SampleCardSkeleton numberOfCards={4} />
+          ) : (
+            <Gallery
+              hasGutter={true}
+              minWidths={{ sm: "calc(100%/3.1 - 16px)", default: "100%" }}
+              style={{
+                overflowX: "auto",
+                gridAutoFlow: "column",
+                gridAutoColumns: "minmax(calc(100%/3.1 - 16px),1fr)",
+                paddingBottom: "8px",
+                paddingRight: "var(--pf-c-page__main-section--xl--PaddingRight)",
+              }}
+            >
+              {samples.map((sample) => (
+                <SampleCard sample={sample} key={`sample-${sample.sampleId}`} />
+              ))}
+            </Gallery>
+          )}
+        </>
+      )}
+    </>
+  );
+}
