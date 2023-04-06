e51588bd9f22ebe2a30a3737a80b1ec6fe790c81
Tiago Bento <tiagobentofernandes@gmail.com>
2023-04-05 21:41:29 -0300

    Improvements to dev-deployments-upload-service

--- packages/dev-deployments-upload-service/README.md
+++ packages/dev-deployments-upload-service/README.md
@@ -1,16 +1,16 @@
-## Dev deployments upload service
+# Dev deployments upload service
 
 This package used on KIE Sandbox's Dev deployments feature, and should be the first command to run when a deployed container spins up.
 
 The "Dev deployments upload service" runs an HTTP server that accepts ZIP file uploads to the `/upload` endpoint. When an upload is done, the application will unzip the file at the configured location and exit with a 0 code. If anything goes wrong during execution, it will exit with code 1 and a helpful message will be printed to stderr.
 
-#### Usage:
+### Usage:
 
 `dmn-dev-deployments-upload-service --unzip-at [unzip dir path] --port [port number]`.
 
 Both `--unzip-at` and `--port` arguments are required.
 
-#### Example:
+### Example:
 
 For a Dev deployment that runs a Quarkus application, the intended use is:
 
@@ -22,8 +22,8 @@ EXPOSE [port number]
 CMD ["/bin/bash", "-c", "dmn-dev-deployments-upload-service --unzip-at [unzip dir path] --port [port number] && cd [unzip dir path] && mvn quarkus:dev"]
 ```
 
-#### Develop:
+### Develop:
 
 - On macOS:
-  `pnpm build:dev && ./dist/darwin/dev-deployments-upload-service --unzip-at 'some/dir/path' --port 8091`
-  `open test.html`
+  - `pnpm build:dev && ./dist/darwin/dev-deployments-upload-service --unzip-at 'some/dir/path' --port 8091`
+  - `open test.html`
--- packages/dev-deployments-upload-service/main.go
+++ packages/dev-deployments-upload-service/main.go
@@ -10,14 +10,18 @@ import (
 	"strconv"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
-func printUsage() {
-	fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] USAGE: dev-deployments-upload-service --unzip-at [dir path] --port [port number]\n")
-}
+var GLOBAL__UPLOAD_CAPTURED = false;
+var MAX_UPLOADED_FILE_SIZE_IN_BYTES int64 = 200 << 20; // 200 MiB
+
+var SHUTDOWN_CHANNEL = make(chan int);
+
+var LOG_PREFIX = "[dev-deployments-upload-service] ";
 
 func main() {
-	
+
 	unzipAtArgString := "";
 	portArgString := ""
 
@@ -39,89 +43,127 @@ func main() {
 	// Validate --port
 	port, err := strconv.Atoi(portArgString);
 	if (err != nil) {
-		fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: '%s' is not a valid port number.\n", portArgString)
+		fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: '%s' is not a valid port number.\n", portArgString)
 		os.Exit(1);
 	}
 	
 	unzipAtPath := unzipAtArgString
 	// Validate --unzip-at
 	if _, err := os.Stat(unzipAtArgString); err == nil {
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Found directory '%s'.\n", unzipAtArgString)
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "Found directory '%s'.\n", unzipAtArgString)
 	} else if err := os.MkdirAll(unzipAtArgString, os.ModePerm); err != nil {  // os.ModePerm == chmod 777
-		fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Creating directory '%s' failed:\n", unzipAtArgString)
-		fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: %+v\n", err)
+		fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Creating directory '%s' failed:\n", unzipAtArgString)
+		fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
 		os.Exit(1);
 	} else {
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Created directory '%s'.\n", unzipAtArgString)
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "Created directory '%s'.\n", unzipAtArgString)
 	}
 
+
+
     http.HandleFunc("/upload", func (w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Upload arrived...\n")
+		if (GLOBAL__UPLOAD_CAPTURED) {
+			fmt.Fprintf(os.Stdout, LOG_PREFIX + "Upload arrived, but another arrived earlier. Server should be in the process of gracefully shutting down.\n")
+			w.WriteHeader(http.StatusConflict)
+		    w.Write([]byte("409: Another upload arrived first. Can't accept this one. Server is in the process of shutting down."));
+			return;
+		}
+
+		GLOBAL__UPLOAD_CAPTURED = true;
+
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "Upload arrived...\n")
 	
-		// Parse the multipart form, 200 << 20 specifies a maximum upload of 200 MiB files.
-		req.ParseMultipartForm(200 << 20) //FIXME: Extract this to a constant
+		req.ParseMultipartForm(MAX_UPLOADED_FILE_SIZE_IN_BYTES)
 
 		// FormFile returns the first file for the given key `myFile`
 		// it also returns the FileHeader so we can get the Filename,
 		// the Header and the size of the file
 		uploadedFile, handler, err := req.FormFile("myFile") //FIXME: Is there a way to not need this?
 		if (err != nil) {
-			fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Reading uploaded file failed:\n")
-			fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: %+v\n", err)
-			os.Exit(1)
+			fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Reading uploaded file failed:\n")
+			fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500: Reading uploaded file failed."));
+
+			signalShutdown(1)
+			return;
 		}
 		defer uploadedFile.Close()
 
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Uploaded File: %+v\n", handler.Filename)
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] File Size: %+v\n", handler.Size)
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] MIME Header: %+v\n", handler.Header)
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "Uploaded File: %+v\n", handler.Filename)
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "File Size: %+v\n", handler.Size)
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "MIME Header: %+v\n", handler.Header)
 	
 		// Read all the contents of the uploaded file into a byte array
 		uploadedZipBytes, err := ioutil.ReadAll(uploadedFile)
 		if (err != nil) {
-			fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Reading bytes of uploaded file failed:\n")
-			fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: %+v\n", err)
-			os.Exit(1);
+			fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Reading bytes of uploaded zip file failed:\n")
+			fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500: Reading bytes of uploaded zip file failed."));
+
+			signalShutdown(1)
+			return;
 		}
 
 		// Extract zip contents to `extractedZippedFilePath`
 		zipReader, err := zip.NewReader(bytes.NewReader(uploadedZipBytes), int64(len(uploadedZipBytes)))
 		if (err != nil) {
-			fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Creating zip reader failed.\n");
-			fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: %+v\n", err)
-			os.Exit(1);
+			fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Creating zip reader failed.\n");
+			fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500: Creating zip reader failed."));
+
+			signalShutdown(1)
+			return;
 		}
 		for _, zipFile := range zipReader.File {
-			fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Reading zipped file '%s'...\n", zipFile.Name)
+			fmt.Fprintf(os.Stdout, LOG_PREFIX + "Reading zipped file '%s'...\n", zipFile.Name)
 			unzippedFileBytes, err := readZipFile(zipFile)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Reading zipped file '%s' failed:\n", zipFile.Name);
-				fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: %+v\n", err)
-				os.Exit(1);
+				fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Reading zipped file '%s' failed:\n", zipFile.Name);
+				fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
+				w.WriteHeader(http.StatusInternalServerError)
+			    w.Write([]byte("500: Reading zipped file. " + zipFile.Name));
+
+				signalShutdown(1)
+				return;
 			}
 
 			// Check for Zip Slip (directory traversal)
 			extractedZippedFilePath := filepath.Join(unzipAtPath, zipFile.Name)
 			if (!strings.HasPrefix(extractedZippedFilePath, filepath.Clean(unzipAtPath) + string(os.PathSeparator))) {
-				fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Illegal zipped file path '%s'.\n", zipFile.Name);
-				os.Exit(1);
+				fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Illegal zipped file path '%s'.\n", zipFile.Name);
+				w.WriteHeader(http.StatusInternalServerError)
+			    w.Write([]byte("500: Illegal zipped file path. " + extractedZippedFilePath));
+
+				signalShutdown(1)
+				return;
 			}
 
 			// Always try and write the dirs where the files are going to be written to.
 			if err := os.MkdirAll(filepath.Dir(extractedZippedFilePath), os.ModePerm); err != nil {  // os.ModePerm == chmod 777
-				fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Creating directory '%s' failed:\n", filepath.Dir(extractedZippedFilePath))
-				fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: %+v\n", err)
-				os.Exit(1);
+				fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Creating directory '%s' failed:\n", filepath.Dir(extractedZippedFilePath))
+				fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
+				w.WriteHeader(http.StatusInternalServerError)
+			    w.Write([]byte("500: Creating directory failed. " + filepath.Dir(extractedZippedFilePath)));
+
+				signalShutdown(1)
+				return;
 			}
 
 			// Only write the file if it's not a dir
 			if (!zipFile.FileInfo().IsDir()) {
-				fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Writing zipped file '%s'...\n", zipFile.Name)
+				fmt.Fprintf(os.Stdout, LOG_PREFIX + "Writing zipped file '%s'...\n", zipFile.Name)
 				f, err := os.Create(extractedZippedFilePath)
 				if (err != nil) {
-					fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: Creating file '%s' failed:\n", extractedZippedFilePath);
-					fmt.Fprintf(os.Stderr, "[dev-deployments-upload-service] ERROR: %+v\n", err)
-					os.Exit(1);
+					fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Creating file '%s' failed:\n", extractedZippedFilePath);
+					fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
+					w.WriteHeader(http.StatusInternalServerError)
+			   		w.Write([]byte("500: Creating file failed. " + extractedZippedFilePath));
+
+					signalShutdown(1)
+					return;
 				}
 				f.Write(unzippedFileBytes);
 				defer f.Close();
@@ -129,27 +171,45 @@ func main() {
 		}
 
 		// Return that we have successfully uploaded the zip file
-		fmt.Fprintf(w, "[dev-deployments-upload-service] Successfully extracted '%s' to '%s'.\n", handler.Filename, unzipAtPath);
-		if f, ok := w.(http.Flusher); ok {
-			f.Flush()
-		}
-
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("200: Successfully extracted '%s' to '%s'.\n", handler.Filename, unzipAtPath)))
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "Successfully extracted '%s' to '%s'.\n", handler.Filename, unzipAtPath);
+		
 		// End the program gracefully.
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Successfully extracted '%s' to '%s'.\n", handler.Filename, unzipAtPath);
-		fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Done.");
-
-		// FIXME: Gracefully shutdown the HTTP Server before exiting.
-		//        We need to make sure that the HTTP request is correctly closed so we get a nice 200 on the frontend.
-		os.Exit(0);
-	})
+		signalShutdown(0)
+		return;
+	});
 
 
-	fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Starting HTTP server...\n");
-	fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Running at port %d.\n", port);
-	fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Uploaded zip will be unziped to '%s'.\n", unzipAtPath);
-	fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] Waiting for upload to arrive...\n");
-	fmt.Fprintf(os.Stdout, "[dev-deployments-upload-service] -------------------------------\n");
-    http.ListenAndServe(":" + portArgString, nil)
+	fmt.Fprintf(os.Stdout, LOG_PREFIX + "Starting HTTP server...\n");
+	fmt.Fprintf(os.Stdout, LOG_PREFIX + "Running at port %d.\n", port);
+	fmt.Fprintf(os.Stdout, LOG_PREFIX + "Uploaded zip will be unziped to '%s'.\n", unzipAtPath);
+	fmt.Fprintf(os.Stdout, LOG_PREFIX + "Waiting for upload to arrive...\n");
+	fmt.Fprintf(os.Stdout, LOG_PREFIX + "-------------------------------\n");
+
+	httpServer := http.Server { Addr: ":" + portArgString, 	}
+
+	// Start a new thread waiting for the shutdown signal.
+	go func() {
+		signaledExitCode := <- SHUTDOWN_CHANNEL
+		fmt.Fprintf(os.Stdout, LOG_PREFIX + "Signal captured. Gracefully shutting down HTTP Server with code '%d'.\n", signaledExitCode);
+
+		// FIXME: Alright, this is not ideal, but it does give some extra time for the first upload request to have its connection closed,
+		// and give the client a nice 200. Ideally, we should:
+		// 1. Stop accepting new requests.
+		// 2. Wait for the existing requests to be completed, making their connections closed.
+		// 3. os.Exit(signaledExitCode)
+		// 4. Done.
+		time.Sleep(2 * time.Second)
+		os.Exit(signaledExitCode)
+	}()
+
+	// 
+	if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+		fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: Starting HTTP server failed::\n");
+		fmt.Fprintf(os.Stderr, LOG_PREFIX + "ERROR: %+v\n", err)
+		os.Exit(1);
+	}
 }
 
 func readZipFile(zf *zip.File) ([]byte, error) {
@@ -160,4 +220,13 @@ func readZipFile(zf *zip.File) ([]byte, error) {
 
     defer f.Close()
     return ioutil.ReadAll(f);
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, LOG_PREFIX + "USAGE: dev-deployments-upload-service --unzip-at [dir path] --port [port number]\n")
+}
+
+func signalShutdown(exitCode int) {
+	fmt.Fprintf(os.Stdout, LOG_PREFIX + "Signaling the HTTP Server to gracefully shutdown with code '%d'...\n", exitCode);
+	SHUTDOWN_CHANNEL <- exitCode
 }
