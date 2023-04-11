d94bec71d08fd0a9adb686496249bdaf9ea06b06
Yeser Amer <yeser.amer@ibm.com>
2023-04-11 12:13:10 +0200

    Improved integration with Property Panel and UNDO/REDO actions

--- packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/expressions/ExpressionEditor.java
+++ packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/expressions/ExpressionEditor.java
@@ -163,7 +163,7 @@ public class ExpressionEditor implements ExpressionEditorView.Presenter {
                 hasExpression.ifPresent(e -> {
                     if (Objects.equals(e.asDMNModelInstrumentedBase(), definition.getDefinition())) {
                         view.setExpressionNameText(Optional.ofNullable((HasName) definition.getDefinition()));
-                        view.refresh();
+                        view.reloadEditor();
                     }
                 });
             }
