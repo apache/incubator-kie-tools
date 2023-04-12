2f31b20de15d2d744e1fdea48757c5fc45955b79
Dmitrii Tikhomirov <chani.liet@gmail.com>
2023-04-11 22:17:55 -0700

    post rebase fixies

--- packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-api/src/main/java/org/kie/workbench/common/stunner/sw/definition/Metadata.java
+++ packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-api/src/main/java/org/kie/workbench/common/stunner/sw/definition/Metadata.java
@@ -1,10 +1,9 @@
 package org.kie.workbench.common.stunner.sw.definition;
 
-import jsinterop.annotations.JsType;
-
 import jakarta.json.GwtIncompatible;
 import jakarta.json.bind.annotation.JsonbTypeDeserializer;
 import jakarta.json.bind.annotation.JsonbTypeSerializer;
+import jsinterop.annotations.JsType;
 import org.kie.workbench.common.stunner.client.yaml.mapper.api.annotation.YamlTypeDeserializer;
 import org.kie.workbench.common.stunner.client.yaml.mapper.api.annotation.YamlTypeSerializer;
 import org.kie.workbench.common.stunner.sw.definition.custom.json.MetadataJsonSerializer;
--- packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-kogito-app/src/main/java/org/kie/workbench/common/stunner/sw/client/editor/DiagramEditor.java
+++ packages/serverless-workflow-diagram-editor/sw-editor/sw-editor-kogito-app/src/main/java/org/kie/workbench/common/stunner/sw/client/editor/DiagramEditor.java
@@ -28,7 +28,6 @@ import com.ait.lienzo.client.core.types.JsCanvas;
 import com.ait.lienzo.client.widget.panel.impl.ScrollablePanel;
 import com.ait.lienzo.client.widget.panel.util.PanelTransformUtils;
 import com.google.gwt.user.client.ui.IsWidget;
-import elemental2.core.Global;
 import elemental2.core.JsRegExp;
 import elemental2.core.RegExpResult;
 import elemental2.dom.DomGlobal;
@@ -151,16 +150,13 @@ public class DiagramEditor {
     }
 
     public Promise<Void> setContent(final String path, final String value) {
-        if(value == null || value.isEmpty()) {
+        if(value == null || value.trim().isEmpty()) {
             return setContent(path, "{}", DocType.JSON);
         }
-
-        try{
-            Global.JSON.parse(value); //check if it is a valid json, if now, we can assume it is a yaml
+        if(value.trim().charAt(0) == '{') {
             return setContent(path, value, DocType.JSON);
-        } catch (Exception e) {
-            return setContent(path, value, DocType.YAML);
         }
+        return setContent(path, value, DocType.YAML);
     }
 
     private Promise<Void> setContent(final String path, final String value, final DocType docType) {
