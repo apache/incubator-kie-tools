6ccc567d99465906ddd6f44faf525079d7daea06
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2023-04-14 12:02:57 +0200

    Unskipped test and add more snapshots

--- packages/vscode-extension-serverless-workflow-editor/it-tests/serverless-workflow-editor-extension-basic-operations.test.ts
+++ packages/vscode-extension-serverless-workflow-editor/it-tests/serverless-workflow-editor-extension-basic-operations.test.ts
@@ -22,9 +22,12 @@ import SwfTextEditorTestHelper from "./helpers/swf/SwfTextEditorTestHelper";
 import VSCodeTestHelper, { sleep } from "./helpers/VSCodeTestHelper";
 
 // The following test is failing in github CI. See https://issues.redhat.com/browse/KOGITO-8952.
-describe.skip("Serverless workflow editor - Basic operations tests", () => {
+describe("Serverless workflow editor - Basic operations tests", () => {
   const TEST_PROJECT_FOLDER: string = path.resolve("it-tests-tmp", "resources", "basic-operations");
-
+  const directory = "screenshots";
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
   let testHelper: VSCodeTestHelper;
 
   before(async function () {
@@ -54,9 +57,11 @@ describe.skip("Serverless workflow editor - Basic operations tests", () => {
     let swfTextEditor = new SwfTextEditorTestHelper(editorWebViews[0]);
     let swfEditor = new SwfEditorTestHelper(editorWebViews[1]);
 
+    fs.writeFileSync(`${directory}/openAndSave_1.png`, await testHelper.takeScreenshot(), "base64");
     expect((await swfEditor.getAllNodeIds()).length).equal(6);
 
     let textEditor = await swfTextEditor.getSwfTextEditor();
+    fs.writeFileSync(`${directory}/openAndSave_2.png`, await testHelper.takeScreenshot(), "base64");
 
     const greetInGermanStateString =
       "\n" +
@@ -74,8 +79,10 @@ describe.skip("Serverless workflow editor - Basic operations tests", () => {
 
     await textEditor.typeTextAt(47, 7, greetInGermanStateString);
     await textEditor.typeTextAt(26, 10, germanConditionString);
+    fs.writeFileSync(`${directory}/openAndSave_3.png`, await testHelper.takeScreenshot(), "base64");
 
     expect((await swfEditor.getAllNodeIds()).length).equal(7);
+    fs.writeFileSync(`${directory}/openAndSave_4.png`, await testHelper.takeScreenshot(), "base64");
 
     if (await textEditor.isDirty()) {
       await textEditor.save();
@@ -83,28 +90,34 @@ describe.skip("Serverless workflow editor - Basic operations tests", () => {
     } else {
       console.log("The editor doesn't have unsaved changes.");
     }
+    fs.writeFileSync(`${directory}/openAndSave_5.png`, await testHelper.takeScreenshot(), "base64");
 
     await testHelper.closeAllEditors();
+    fs.writeFileSync(`${directory}/openAndSave_6.png`, await testHelper.takeScreenshot(), "base64");
 
     editorWebViews = await testHelper.openFileFromSidebar(WORKFLOW_NAME);
     swfTextEditor = new SwfTextEditorTestHelper(editorWebViews[0]);
     swfEditor = new SwfEditorTestHelper(editorWebViews[1]);
+    fs.writeFileSync(`${directory}/openAndSave_7.png`, await testHelper.takeScreenshot(), "base64");
 
     textEditor = await swfTextEditor.getSwfTextEditor();
+    fs.writeFileSync(`${directory}/openAndSave_8.png`, await testHelper.takeScreenshot(), "base64");
 
     const editorTextTrimmedLines = (await textEditor.getText())
       .split(/\n/)
       .map((row) => row.trim())
       .join("\n");
+    fs.writeFileSync(`${directory}/openAndSave_9.png`, await testHelper.takeScreenshot(), "base64");
 
     expect(editorTextTrimmedLines).to.have.string(greetInGermanStateString);
     expect(editorTextTrimmedLines).to.have.string(germanConditionString);
+    fs.writeFileSync(`${directory}/openAndSave_10.png`, await testHelper.takeScreenshot(), "base64");
 
     expect((await swfEditor.getAllNodeIds()).length).equal(7);
   });
 
   //The following test is skipped because of bug: https://issues.redhat.com/browse/KOGITO-8384
-  it.skip("Renames *.sw.json file while editor is open", async function () {
+  it("Renames *.sw.json file while editor is open", async function () {
     this.timeout(30000);
 
     const WORKFLOW_NAME = "hello-world.sw.json";
@@ -113,25 +126,33 @@ describe.skip("Serverless workflow editor - Basic operations tests", () => {
     let editorWebViews = await testHelper.openFileFromSidebar(WORKFLOW_NAME);
     let swfTextEditor = new SwfTextEditorTestHelper(editorWebViews[0]);
     let swfEditor = new SwfEditorTestHelper(editorWebViews[1]);
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_1.png`, await testHelper.takeScreenshot(), "base64");
 
     let textEditor = await swfTextEditor.getSwfTextEditor();
     const expectedContent = fs.readFileSync(path.resolve(TEST_PROJECT_FOLDER, WORKFLOW_NAME), "utf-8");
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_2.png`, await testHelper.takeScreenshot(), "base64");
 
     expect(await textEditor.getText()).equal(expectedContent);
     expect((await swfEditor.getAllNodeIds()).length).equal(3);
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_3.png`, await testHelper.takeScreenshot(), "base64");
 
     await testHelper.renameFile(WORKFLOW_NAME, RENAMED_WORKFLOW_NAME);
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_4.png`, await testHelper.takeScreenshot(), "base64");
 
     expect(await textEditor.getText()).equal(expectedContent);
     expect((await swfEditor.getAllNodeIds()).length).equal(3);
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_5.png`, await testHelper.takeScreenshot(), "base64");
 
     await testHelper.closeAllEditors();
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_6.png`, await testHelper.takeScreenshot(), "base64");
 
     editorWebViews = await testHelper.openFileFromSidebar(RENAMED_WORKFLOW_NAME);
     swfTextEditor = new SwfTextEditorTestHelper(editorWebViews[0]);
     swfEditor = new SwfEditorTestHelper(editorWebViews[1]);
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_7.png`, await testHelper.takeScreenshot(), "base64");
 
     textEditor = await swfTextEditor.getSwfTextEditor();
+    fs.writeFileSync(`${directory}/renameFileWithEdOpen_8.png`, await testHelper.takeScreenshot(), "base64");
 
     expect(await textEditor.getText()).equal(expectedContent);
     expect((await swfEditor.getAllNodeIds()).length).equal(3);
