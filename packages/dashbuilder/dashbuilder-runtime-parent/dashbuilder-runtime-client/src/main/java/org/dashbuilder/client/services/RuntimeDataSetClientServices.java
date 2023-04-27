6cc2a185c5a8eb58e918528766c0e8034af42900
William Siqueira <william.fatecsjc@gmail.com>
2023-04-27 17:54:22 -0300

    KOGITO-9076: Retrieve Dashbuilder DataSet Columns information for CSV and Metrics

--- packages/dashbuilder/dashbuilder-runtime-parent/dashbuilder-runtime-client/src/main/java/org/dashbuilder/client/services/RuntimeDataSetClientServices.java
+++ packages/dashbuilder/dashbuilder-runtime-parent/dashbuilder-runtime-client/src/main/java/org/dashbuilder/client/services/RuntimeDataSetClientServices.java
@@ -15,32 +15,19 @@
  */
 package org.dashbuilder.client.services;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
 
-import elemental2.dom.DomGlobal;
-import elemental2.dom.Headers;
-import elemental2.dom.RequestInit;
-import elemental2.dom.Response;
-import elemental2.dom.XMLHttpRequest;
-import elemental2.promise.IThenable;
 import org.dashbuilder.client.RuntimeClientLoader;
 import org.dashbuilder.client.error.DefaultRuntimeErrorCallback;
-import org.dashbuilder.client.error.DefaultRuntimeErrorCallback.DefaultErrorType;
 import org.dashbuilder.client.error.ErrorResponseVerifier;
 import org.dashbuilder.client.external.ExternalDataSetClientProvider;
 import org.dashbuilder.client.marshalling.ClientDataSetMetadataJSONMarshaller;
-import org.dashbuilder.common.client.error.ClientRuntimeError;
 import org.dashbuilder.dataprovider.DataSetProviderType;
-import org.dashbuilder.dataset.DataSet;
 import org.dashbuilder.dataset.DataSetLookup;
 import org.dashbuilder.dataset.DataSetMetadata;
 import org.dashbuilder.dataset.client.ClientDataSetManager;
@@ -50,18 +37,11 @@ import org.dashbuilder.dataset.client.DataSetMetadataCallback;
 import org.dashbuilder.dataset.client.DataSetReadyCallback;
 import org.dashbuilder.dataset.def.DataSetDef;
 import org.dashbuilder.dataset.events.DataSetDefRemovedEvent;
-import org.dashbuilder.dataset.json.DataSetJSONMarshaller;
-import org.dashbuilder.dataset.json.DataSetLookupJSONMarshaller;
 import org.jboss.errai.common.client.api.RemoteCallback;
-import org.jboss.resteasy.util.HttpResponseCodes;
-
-import static elemental2.dom.DomGlobal.fetch;
 
 @Alternative
 @ApplicationScoped
 public class RuntimeDataSetClientServices implements DataSetClientServices {
-
-    private static final String LOOKUP_ENDPOINT = "/rest/dataset/lookup";
 
     @Inject
     ClientDataSetMetadataJSONMarshaller dataSetMetadataJsonMarshaller;
@@ -81,8 +61,6 @@ public class RuntimeDataSetClientServices implements DataSetClientServices {
     @Inject
     ExternalDataSetClientProvider externalDataSetClientProvider;
 
-    Map<String, DataSetMetadata> metadataCache = new HashMap<>();
-
     public RuntimeDataSetClientServices() {
         // empty
     }
@@ -94,46 +72,13 @@ public class RuntimeDataSetClientServices implements DataSetClientServices {
 
     @Override
     public void fetchMetadata(String uuid, DataSetMetadataCallback listener) throws Exception {
-        if (metadataCache.containsKey(uuid)) {
-            listener.callback(metadataCache.get(uuid));
-            return;
-        }
-        fetch(LOOKUP_ENDPOINT).then((Response response) -> {
-            verifier.verify(response);
-            response.text().then(responseText -> {
-                if (response.status == HttpResponseCodes.SC_INTERNAL_SERVER_ERROR) {
-                    listener.onError(new ClientRuntimeError("Not able to retrieve dataset metadata",
-                            new Exception(responseText)));
-                } else {
-                    var meta = parseMetadata(responseText);
-                    listener.callback(meta);
-                    metadataCache.put(uuid, meta);
-                }
-                return null;
-            }, error -> {
-                listener.onError(new ClientRuntimeError("Not able to read dataset metadata"));
-                return null;
-            });
-
-            return null;
-        }).catch_(this::handleError);
+        // empty
     }
 
     @Override
     public DataSetMetadata getMetadata(String uuid) {
-        if (metadataCache.containsKey(uuid)) {
-            return metadataCache.get(uuid);
-        }
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "/rest/dataset/" + uuid + "/metadata", false);
-        xhr.send();
-        verifier.verify(xhr);
-        if (xhr.status == HttpResponseCodes.SC_INTERNAL_SERVER_ERROR) {
-            throw new RuntimeException("Not able to retrieve data set metadata: " + xhr.responseText);
-        }
-        var metadata = parseMetadata(xhr.responseText);
-        metadataCache.put(uuid, metadata);
-        return metadata;
+        // empty
+        return null;
     }
 
     @Override
@@ -144,31 +89,7 @@ public class RuntimeDataSetClientServices implements DataSetClientServices {
             return;
         }
 
-        externalDataSetClientProvider.fetchAndRegister(lookup.getDataSetUUID(),
-                lookup,
-                new DataSetReadyCallback() {
-
-                    @Override
-                    public boolean onError(ClientRuntimeError error) {
-                        if (loader.isEditor() || loader.isClient()) {
-                            listener.onError(error);
-                        } else {
-                            DomGlobal.console.debug("Error retrieving dataset from client, trying from backend");
-                            backendLookup(def, lookup, listener);
-                        }
-                        return false;
-                    }
-
-                    @Override
-                    public void notFound() {
-                        backendLookup(def, lookup, listener);
-                    }
-
-                    @Override
-                    public void callback(DataSet dataSet) {
-                        listener.callback(dataSet);
-                    }
-                });
+        externalDataSetClientProvider.fetchAndRegister(lookup.getDataSetUUID(), lookup, listener);
     }
 
     private boolean isAccumulate(String uuid) {
@@ -200,88 +121,13 @@ public class RuntimeDataSetClientServices implements DataSetClientServices {
         // ignored in runtime
     }
 
-    private void backendLookup(DataSetDef def, DataSetLookup lookup, DataSetReadyCallback listener) {
-        var request = RequestInit.create();
-        var headers = new Headers();
-        request.setMethod("POST");
-        request.setBody(toJson(lookup));
-        headers.append(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
-        request.setHeaders(headers);
-        fetch(LOOKUP_ENDPOINT, request).then((Response response) -> {
-            verifier.verify(response);
-            response.text().then(responseText -> handleResponseText(def, lookup, listener, response, responseText),
-                    error -> {
-                        listener.onError(new ClientRuntimeError("Error reading data set content: " + error));
-                        return null;
-                    });
-            return null;
-        }).catch_(this::handleError);
-    }
-
     void onDataSetDefRemovedEvent(@Observes DataSetDefRemovedEvent evt) {
         if (evt.getDataSetDef() != null) {
             var uuid = evt.getDataSetDef().getUUID();
-            metadataCache.remove(uuid);
             externalDataSetClientProvider.unregister(uuid);
             clientDataSetManager.removeDataSet(uuid);
         }
 
-    }
-
-    private IThenable<Object> handleResponseText(DataSetDef def,
-                                                 DataSetLookup lookup,
-                                                 DataSetReadyCallback listener,
-                                                 Response response,
-                                                 String responseText) {
-
-        switch (response.status) {
-            case (HttpResponseCodes.SC_INTERNAL_SERVER_ERROR):
-                listener.onError(buildError("Not able to retrieve data set: " + getName(lookup, def),
-                        responseText));
-                break;
-            case (HttpResponseCodes.SC_NOT_FOUND):
-                listener.onError(buildError("Data Set not found: " + getName(lookup, def), responseText));
-                break;
-            default:
-                DataSet dataSet = null;
-                try {
-                    dataSet = parseDataSet(responseText);
-                } catch (Exception e) {
-                    listener.onError(new ClientRuntimeError("Error reading data set content", e));
-                }
-
-                if (dataSet != null) {
-                    listener.callback(dataSet);
-                } else {
-                    listener.onError(new ClientRuntimeError("Not able to retrieve the dataset. Content is not valid."));
-                }
-        }
-        return null;
-    }
-
-    private ClientRuntimeError buildError(String message, String responseText) {
-        return new ClientRuntimeError(message, new Exception(responseText));
-    }
-
-    private DataSetMetadata parseMetadata(String jsonContent) {
-        return dataSetMetadataJsonMarshaller.fromJSON(jsonContent);
-    }
-
-    private String toJson(DataSetLookup lookup) {
-        return DataSetLookupJSONMarshaller.get().toJson(lookup).toJson();
-    }
-
-    private DataSet parseDataSet(String dataSetJson) {
-        return DataSetJSONMarshaller.get().fromJson(dataSetJson);
-    }
-
-    private IThenable<?> handleError(Object e) {
-        errorCallback.error(DefaultErrorType.OTHER, "Unexpected error retrieving data set metadata:" + e);
-        return null;
-    }
-
-    private String getName(DataSetLookup lookup, DataSetDef def) {
-        return def == null ? lookup.getDataSetUUID() : (def.getName() == null ? def.getUUID() : def.getName());
     }
 
 }
