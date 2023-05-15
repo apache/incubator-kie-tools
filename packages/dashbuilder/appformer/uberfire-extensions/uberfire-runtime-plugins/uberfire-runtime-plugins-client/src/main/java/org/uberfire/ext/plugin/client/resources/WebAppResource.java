3cf2d732f159ce11c88ecf266e747823bdb5f3cc
William Siqueira <william.fatecsjc@gmail.com>
2023-05-15 15:26:05 -0300

    KOGITO-9151: Remove old Dashbuilder dependencies leftovers

--- packages/dashbuilder/appformer/uberfire-extensions/uberfire-runtime-plugins/uberfire-runtime-plugins-client/src/main/java/org/uberfire/ext/plugin/client/resources/WebAppResource.java
+++ packages/dashbuilder/appformer/uberfire-extensions/uberfire-runtime-plugins/uberfire-runtime-plugins-client/src/main/java/org/uberfire/ext/plugin/client/resources/WebAppResource.java
@@ -19,21 +19,24 @@ package org.uberfire.ext.plugin.client.resources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.DataResource;
+import com.google.gwt.resources.client.TextResource;
 
 public interface WebAppResource
-        extends
-        ClientBundle {
+                                extends
+                                ClientBundle {
 
     WebAppResource INSTANCE = GWT.create(WebAppResource.class);
 
     @Source("css/plugin.css")
     PluginCss CSS();
-    
+
     @Source("images/handle-v.png")
     DataResource handleVertical();
 
     @Source("images/handle-h.png")
     DataResource handleHorizontal();
 
+    @Source("js/marked.min.js")
+    TextResource markedJs();
 
 }
