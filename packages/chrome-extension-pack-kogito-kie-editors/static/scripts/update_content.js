808301ec33731dd64c6a1682f740e787e81898a6
AJAY_JAGANATHAN <ajayjeganathan@gmail.com>
2023-06-08 16:27:37 +0530

    fix the edit mode

--- packages/chrome-extension-pack-kogito-kie-editors/static/scripts/update_content.js
+++ packages/chrome-extension-pack-kogito-kie-editors/static/scripts/update_content.js
@@ -1,5 +1,10 @@
 // Updates the GitHub editor with the content on the `kogito-content` `pre` tag.
-
 document
-  .querySelector(".file-editor-textarea + .CodeMirror")
+  .querySelector(".CodeMirror")
   .CodeMirror.setValue(`${document.getElementById("kogito-content").textContent ?? ""}`);
+
+// Enables the commit changes... button
+document.querySelector(".CodeMirror-code").dispatchEvent(new Event("keypress", { bubbles: true }));
+
+// Undo the special character which gets added due to the keypress event.
+document.querySelector(".CodeMirror").CodeMirror.undo();
--- packages/chrome-extension-serverless-workflow-editor/static/scripts/update_content.js
+++ packages/chrome-extension-serverless-workflow-editor/static/scripts/update_content.js
@@ -1,7 +1,10 @@
-// enables the commit changes... button
-document.querySelector(".CodeMirror-code").dispatchEvent(new Event("keypress"));
-
 // Updates the GitHub editor with the content on the `kogito-content` `pre` tag.
 document
   .querySelector(".CodeMirror")
-  .CodeMirror.setValue(`${document.getElementById("kogito-content").textContent.trim() ?? ""}`);
+  .CodeMirror.setValue(`${document.getElementById("kogito-content").textContent ?? ""}`);
+
+// Enables the commit changes... button
+document.querySelector(".CodeMirror-code").dispatchEvent(new Event("keypress", { bubbles: true }));
+
+// Undo the special character which gets added due to the keypress event.
+document.querySelector(".CodeMirror").CodeMirror.undo();
