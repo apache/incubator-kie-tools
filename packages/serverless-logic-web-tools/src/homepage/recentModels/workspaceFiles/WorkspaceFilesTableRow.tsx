cc2b154238ebdf20050c59559f5e6266677920df
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2023-06-09 06:07:44 -0300

    KOGITO-9282: Reflect changes from other tabs in Recent Models page (#1717)

--- packages/serverless-logic-web-tools/src/homepage/recentModels/workspaceFiles/WorkspaceFilesTableRow.tsx
+++ packages/serverless-logic-web-tools/src/homepage/recentModels/workspaceFiles/WorkspaceFilesTableRow.tsx
@@ -19,12 +19,13 @@ import { BanIcon, CheckCircleIcon } from "@patternfly/react-icons/dist/js/icons"
 import { TaskIcon } from "@patternfly/react-icons/dist/js/icons/task-icon";
 import { ActionsColumn, Td, Tr } from "@patternfly/react-table/dist/esm";
 import { TdSelectType } from "@patternfly/react-table/dist/esm/components/Table/base";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { routes } from "../../../navigation/Routes";
 import "../../../table/Table.css";
 import { FileLabel } from "../../../workspace/components/FileLabel";
 import { columnNames, WorkspaceFilesTableRowData } from "./WorkspaceFilesTable";
+import { Tooltip } from "@patternfly/react-core/dist/js/components/Tooltip";
 
 export const workspacesTableRowErrorContent = "Error obtaining workspace information";
 
@@ -50,6 +51,7 @@ export type WorkspaceFilesTableRowProps = {
 export function WorkspaceFilesTableRow(props: WorkspaceFilesTableRowProps) {
   const { isSelected, rowIndex, totalFilesCount } = props;
   const { name, extension, isEditable, fileDescriptor } = props.rowData;
+  const downloadRef = useRef<HTMLAnchorElement>(null);
   const workspaces = useWorkspaces();
   const history = useHistory();
 
@@ -67,41 +69,62 @@ export function WorkspaceFilesTableRow(props: WorkspaceFilesTableRowProps) {
     if (totalFilesCount > 1) {
       await workspaces.deleteFile({ file: fileDescriptor });
     } else {
-      workspaces.deleteWorkspace({ workspaceId: fileDescriptor.workspaceId });
+      await workspaces.deleteWorkspace({ workspaceId: fileDescriptor.workspaceId });
       history.push({ pathname: routes.recentModels.path({}) });
     }
     props.onDelete(fileDescriptor);
   }, [fileDescriptor, history, workspaces, totalFilesCount, props]);
 
+  const onDownload = useCallback(async () => {
+    if (!downloadRef.current) {
+      return;
+    }
+    const content = await fileDescriptor.getFileContentsAsString();
+    const fileBlob = new Blob([content], { type: "text/plain" });
+    downloadRef.current.download = `${fileDescriptor.name}`;
+    downloadRef.current.href = URL.createObjectURL(fileBlob);
+    downloadRef.current.click();
+  }, [fileDescriptor]);
+
   return (
-    <Tr key={name}>
-      <Td
-        select={{
-          rowIndex,
-          onSelect: (_event, checked) => props.onToggle(checked),
-          isSelected,
-        }}
-      />
-      <Td dataLabel={columnNames.name}>
-        <TaskIcon />
-        &nbsp;&nbsp;&nbsp;<Link to={linkTo}>{name}</Link>
-      </Td>
-      <Td dataLabel={columnNames.type}>
-        <FileLabel extension={extension} />
-      </Td>
-      <Td dataLabel={columnNames.isEditable}>
-        {isEditable ? <CheckCircleIcon className="success-icon"></CheckCircleIcon> : <BanIcon></BanIcon>}
-      </Td>
-      <Td isActionCell>
-        <ActionsColumn
-          items={[
-            {
-              title: "Delete",
-              onClick: onDelete,
-            },
-          ]}
+    <>
+      <Tr key={fileDescriptor.relativePath}>
+        <Td
+          select={{
+            rowIndex,
+            onSelect: (_event, checked) => props.onToggle(checked),
+            isSelected,
+          }}
         />
-      </Td>
-    </Tr>
+        <Td dataLabel={columnNames.name}>
+          <TaskIcon />
+          &nbsp;&nbsp;&nbsp;
+          <Tooltip content={fileDescriptor.relativePath}>
+            <Link to={linkTo}>{name}</Link>
+          </Tooltip>
+        </Td>
+        <Td dataLabel={columnNames.type}>
+          <FileLabel extension={extension} />
+        </Td>
+        <Td dataLabel={columnNames.isEditable}>
+          {isEditable ? <CheckCircleIcon className="success-icon"></CheckCircleIcon> : <BanIcon></BanIcon>}
+        </Td>
+        <Td isActionCell>
+          <ActionsColumn
+            items={[
+              {
+                title: "Delete",
+                onClick: onDelete,
+              },
+              {
+                title: "Download",
+                onClick: onDownload,
+              },
+            ]}
+          />
+        </Td>
+      </Tr>
+      <a ref={downloadRef} />
+    </>
   );
 }
