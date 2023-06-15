0cff1eebb38701605fd0304e65e08a3365da8486
Tiago Bento <1584568+tiagobento@users.noreply.github.com>
2023-06-15 19:06:51 -0400

    NO-ISSUE: Remove gtmId from `dmn-dev-deployment-base-image` and add a description to its README (#1763)

--- packages/dmn-dev-deployment-form-webapp/webpack.config.js
+++ packages/dmn-dev-deployment-form-webapp/webpack.config.js
@@ -21,12 +21,9 @@ const { merge } = require("webpack-merge");
 const common = require("@kie-tools-core/webpack-base/webpack.common.config");
 const { env } = require("./env");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
 const buildEnv = env;
 
-module.exports = async (env, argv) => {
-  const gtmResource = getGtmResource(argv);
-
+module.exports = async (env) => {
   return merge(common(env), {
     entry: {
       index: "./src/index.tsx",
@@ -37,17 +34,6 @@ module.exports = async (env, argv) => {
         inject: false,
         minify: false,
       }),
-      new HtmlReplaceWebpackPlugin([
-        {
-          pattern: /(<!-- gtm):([\w-\/]+)(\s*-->)?/g,
-          replacement: (match, gtm, type) => {
-            if (gtmResource) {
-              return gtmResource[type] ?? `${match}`;
-            }
-            return `${match}`;
-          },
-        },
-      ]),
       new CopyPlugin({
         patterns: [
           { from: "./static/resources", to: "./resources" },
@@ -68,36 +54,3 @@ module.exports = async (env, argv) => {
     },
   });
 };
-
-function getGtmResource() {
-  const gtmId = buildEnv.dmnDevDeploymentFormWebapp.gtmId;
-  console.info(`Google Tag Manager :: ID: ${gtmId}`);
-
-  if (!gtmId) {
-    return undefined;
-  }
-
-  return {
-    id: gtmId,
-    header: `<!-- Google Tag Manager -->
-    <script>
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${gtmId}');
-    </script>
-    <!-- End Google Tag Manager -->`,
-    body: `<!-- Google Tag Manager (noscript) -->
-    <noscript>
-      <iframe
-        src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
-        height="0"
-        width="0"
-        style="display:none;visibility:hidden"
-      >
-      </iframe>
-    </noscript>
-    <!-- End Google Tag Manager (noscript) -->`,
-  };
-}
