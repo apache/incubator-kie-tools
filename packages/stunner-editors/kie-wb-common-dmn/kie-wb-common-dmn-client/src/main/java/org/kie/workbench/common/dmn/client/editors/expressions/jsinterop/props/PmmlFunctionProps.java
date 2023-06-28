92ac72e98d7314cb2d57a34995dcffa4959b98e5
Yeser Amer <yeser.amer@ibm.com>
2023-06-28 13:04:09 +0200

    kie-issue#372: Removing GWT-based autocompletion feature

--- packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/expressions/jsinterop/props/PmmlFunctionProps.java
+++ packages/stunner-editors/kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/expressions/jsinterop/props/PmmlFunctionProps.java
@@ -28,7 +28,7 @@ public class PmmlFunctionProps extends FunctionProps {
     public final String modelFieldId;
 
     public PmmlFunctionProps(final String id, final String name, final String dataType, final EntryInfo[] formalParameters, final Double parametersWidth, final String document, final String model, final String documentFieldId, final String modelFieldId) {
-        super(id, name, dataType, formalParameters, parametersWidth, FunctionDefinition.Kind.FEEL.getValue());
+        super(id, name, dataType, formalParameters, parametersWidth, FunctionDefinition.Kind.PMML.getValue());
         this.document = document;
         this.model = model;
         this.documentFieldId = documentFieldId;
