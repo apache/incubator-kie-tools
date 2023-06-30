55e397861bac7f5f9ad36df90b2fcd82b67a85bc
Zbyněk Drápela <zbynek.drapela@gmail.com>
2023-06-30 12:36:02 +0200

    KOGITO-9299: Cover kn-workflow-plugin CLI with integration tests II.

--- packages/kn-plugin-workflow/it-tests/helper_test.go
+++ packages/kn-plugin-workflow/it-tests/helper_test.go
@@ -21,15 +21,17 @@ package it_tests
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"syscall"
 	"testing"
 
 	"github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/command/quarkus"
 	"github.com/spf13/cobra"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // ExecuteCommand executes a command with the given arguments and returns an error if the command fails.
@@ -42,26 +44,44 @@ func ExecuteCommand(command string, args ...string) error {
 
 // ExecuteKnWorkflow executes the 'kn-workflow' CLI tool with the given arguments and returns the command's output and possible error message.
 func ExecuteKnWorkflow(args ...string) (string, error) {
-	return executeCommandWithOutput(KnExecutable, args...)
+	cmd := exec.Command(KnExecutable)
+	return executeCommandWithOutput(cmd, args...)
 }
 
 // ExecuteKnWorkflowQuarkus executes the 'kn-workflow' CLI tool with 'quarkus' command with the given arguments and returns the command's output and possible error message.
 func ExecuteKnWorkflowQuarkus(args ...string) (string, error) {
 	newArgs := append([]string{"quarkus"}, args...)
-	return executeCommandWithOutput(KnExecutable, newArgs...)
+	cmd := exec.Command(KnExecutable)
+	return executeCommandWithOutput(cmd, newArgs...)
 }
 
-// executeCommandWithOutput executes a command with the given arguments and captures its standard output and error streams.
+// ExecuteKnWorkflowWithCmd executes the 'kn-workflow' CLI tool with the given arguments using the provided command and returns the command's output and possible error message.
+func ExecuteKnWorkflowWithCmd(cmd *exec.Cmd, args ...string) (string, error) {
+	return executeCommandWithOutput(cmd, args...)
+}
+
+// ExecuteKnWorkflowQuarkusWithCmd executes the 'kn-workflow' CLI tool with 'quarkus' command with the given arguments using the provided command and returns the command's output and possible error message.
+func ExecuteKnWorkflowQuarkusWithCmd(cmd *exec.Cmd, args ...string) (string, error) {
+	newArgs := append([]string{"quarkus"}, args...)
+	return executeCommandWithOutput(cmd, newArgs...)
+}
+
+// executeCommandWithOutput executes a command with the given arguments using the provided command and captures its standard output and error streams.
 // It returns the combined standard output as a string and an error if the command fails.
-func executeCommandWithOutput(command string, args ...string) (string, error) {
-	cmd := exec.Command(command, args...)
+// It also prints out the standard output to the console.
+func executeCommandWithOutput(cmd *exec.Cmd, args ...string) (string, error) {
+	cmd.Args = append([]string{cmd.Path}, args...)
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
+	if os.Getenv("KN_PLUGIN_WORKFLOW__testPrintCmdOutput") == "true" {
+		cmd.Stdout = io.MultiWriter(os.Stdout, &stdout)
+	} else {
+		cmd.Stdout = &stdout
+	}
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		return stderr.String(), fmt.Errorf("command failed: %v", err)
+		return stdout.String(), err
 	}
 	return stdout.String(), nil
 }
@@ -69,15 +89,15 @@ func executeCommandWithOutput(command string, args ...string) (string, error) {
 // VerifyFileContent verifies that the content of a file matches the expected content.
 func VerifyFileContent(t *testing.T, filePath string, expected string) {
 	actual, err := os.ReadFile(filePath)
-	assert.NoErrorf(t, err, "Failed to read file: %s", filePath)
-	assert.Equalf(t, expected, string(actual), "The content of the file '%s' is different than expected", filePath)
+	require.NoErrorf(t, err, "Failed to read file: %s", filePath)
+	require.Equalf(t, expected, string(actual), "The content of the file '%s' is different than expected", filePath)
 }
 
 // VerifyDirectoriesExist verifies that the specified directories exist within the given base directory.
 func VerifyDirectoriesExist(t *testing.T, baseDir string, directories []string) {
 	for _, dir := range directories {
 		dirPath := filepath.Join(baseDir, dir)
-		assert.DirExistsf(t, dirPath, "Expected directory '%s' to be present", dirPath)
+		require.DirExistsf(t, dirPath, "Expected directory '%s' to be present", dirPath)
 	}
 }
 
@@ -85,7 +105,7 @@ func VerifyDirectoriesExist(t *testing.T, baseDir string, directories []string) 
 func VerifyFilesExist(t *testing.T, baseDir string, files []string) {
 	for _, file := range files {
 		filePath := filepath.Join(baseDir, file)
-		assert.FileExistsf(t, filePath, "Expected file '%s' to be present", filePath)
+		require.FileExistsf(t, filePath, "Expected file '%s' to be present", filePath)
 	}
 }
 
@@ -115,4 +135,37 @@ func LookupFlagDefaultValue(flagName string, createCmd *cobra.Command) (string, 
 		return "", fmt.Errorf("flag '%s' not found", flagName)
 	}
 	return flag.DefValue, nil
+}
+
+// IsSignalInterrupt checks if the given error is caused by a signal interrupt.
+func IsSignalInterrupt(err error) bool {
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+			return status.Signaled() && status.Signal() == os.Interrupt
+		}
+	}
+	return false
+}
+
+// cleanUpFolder removes the folder at the given path and recreates it with permissions set to 0750.
+func cleanUpFolder(t *testing.T, path string) {
+	var err error
+	err = os.RemoveAll(path)
+	if err != nil {
+		t.Errorf("failed to delete '%s' folder: %v", path, err)
+	}
+	err = os.Mkdir(path, 0750)
+	if err != nil {
+		t.Errorf("failed to create '%s' create: %v", path, err)
+	}
+}
+
+// CleanUpAndChdirTemp cleans up the 'temp-tests' folder by removing its contents and changes the current directory to it.
+func CleanUpAndChdirTemp(t *testing.T) {
+	cleanUpFolder(t, TempTestsPath)
+	err := os.Chdir(TempTestsPath)
+	if err != nil {
+		t.Errorf("failed to change directory to temp: %v", err)
+		os.Exit(1)
+	}
 }
