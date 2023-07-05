02cb8b72a646d1d7989c886a51810f1e1ae0d91a
Thiago Lugli <thiagoelg@gmail.com>
2023-07-05 19:25:19 -0300

    Use jest to test kie-sandbox-distribution

---
+++ packages/kie-sandbox-distribution/tests/integrationTest.test.js
@@ -0,0 +1,24 @@
+const kieSandboxDistributionEnv = require("../env");
+
+const env = kieSandboxDistributionEnv.env;
+const kieSandboxUrl = `http://localhost:${env.kieSandboxDistribution.kieSandbox.port}`;
+const gitCorsProxyUrl = `http://localhost:${env.kieSandboxDistribution.gitCorsProxy.port}`;
+const extendedServicesUrl = `http://localhost:${env.kieSandboxDistribution.extendedServices.port}`;
+
+describe("Test built images individually", async () => {
+  it("git-cors-proxy homepage", async () => {
+    expect(await (await fetch(gitCorsProxyUrl)).text()).toMatchSnapshot();
+  });
+  it("git-cors-proxy ping", async () => {
+    expect(await (await fetch(`${gitCorsProxyUrl}/ping`)).text()).toBe("pong");
+  });
+  it("extended-services homepage", async () => {
+    expect(await (await fetch(extendedServicesUrl)).text()).toMatchSnapshot();
+  });
+  it("extended-services ping", async () => {
+    expect(await (await fetch(`${extendedServicesUrl}/ping`)).json()).toHaveProperty("proxy");
+  });
+  it("kie-sandbox homepage", async () => {
+    expect(await (await fetch(kieSandboxUrl)).text()).toMatchSnapshot();
+  });
+});
