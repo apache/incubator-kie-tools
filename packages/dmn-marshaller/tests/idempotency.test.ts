209026ea1b54e9199c8f211f98086cd476d86b42
Tiago Bento <1584568+tiagobento@users.noreply.github.com>
2023-07-18 23:38:35 -0400

    kie-issues#220: Create new Marshallers for BPMN, DMN, and SceSim, based in TypeScript and able to run in the browser and Node.js (#1854)

---
+++ packages/dmn-marshaller/tests/idempotency.test.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from "fs";
+import * as path from "path";
+import { getMarshaller } from "@kie-tools/dmn-marshaller";
+
+const files = [
+  "../tests-data--manual/other/attachment.dmn",
+  "../tests-data--manual/other/empty13.dmn",
+  "../tests-data--manual/other/sample12.dmn",
+  "../tests-data--manual/other/list.dmn",
+  "../tests-data--manual/other/list2.dmn",
+  "../tests-data--manual/other/external.dmn",
+  "../tests-data--manual/other/weird.dmn",
+  "../tests-data--manual/dmn-1_4--examples/Chapter 11 Example 1 Originations/Chapter 11 Example.dmn",
+];
+
+describe("idempotency", () => {
+  for (const file of files) {
+    test(path.basename(file), () => {
+      const xml_original = fs.readFileSync(path.join(__dirname, file), "utf-8");
+
+      const { parser, builder } = getMarshaller(xml_original);
+      const json = parser.parse();
+
+      const xml_firstPass = builder.build(json);
+      const xml_secondPass = builder.build(getMarshaller(xml_firstPass).parser.parse());
+
+      expect(xml_firstPass).toStrictEqual(xml_secondPass);
+    });
+  }
+});
---
+++ packages/scesim-marshaller/tests/versions.test.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from "fs";
+import * as path from "path";
+import { getMarshaller } from "@kie-tools/scesim-marshaller";
+
+const files = [
+  { path: "../tests-data--manual/simple.scesim", version: "1.8" },
+  { path: "../tests-data--manual/TrafficViolationTest.scesim", version: "1.8" },
+];
+
+describe("versions", () => {
+  for (const file of files) {
+    test(path.basename(file.path), () => {
+      const xml = fs.readFileSync(path.join(__dirname, file.path), "utf-8");
+      const { version } = getMarshaller(xml);
+      expect(version).toStrictEqual(file.version);
+    });
+  }
+});
