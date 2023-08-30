b78ebf1e7fa631d8ecd4d8bd5f230b8dec69be2a
Dmitrii Tikhomirov <chani.liet@gmail.com>
2023-08-30 08:40:23 -0700

    removed widgets

---
+++ packages/serverless-workflow-diagram-editor/kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-lienzo-extensions/src/main/java/org/kie/workbench/common/stunner/lienzo/flowpanel/FlowPanel.java
@@ -0,0 +1,43 @@
+package org.kie.workbench.common.stunner.lienzo.flowpanel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import elemental2.dom.DomGlobal;
+import elemental2.dom.HTMLDivElement;
+import elemental2.dom.HTMLElement;
+import org.jboss.errai.ui.client.local.api.IsElement;
+
+public class FlowPanel implements IsElement {
+
+    private final Map<IsElement, HTMLElement> children = new HashMap<>();
+
+    private final HTMLDivElement root = (HTMLDivElement) DomGlobal.document.createElement("div");
+
+    public void add(IsElement element) {
+        HTMLElement child = element.getElement();
+        children.put(element, child);
+        root.appendChild(child);
+    }
+
+    public void remove(IsElement element) {
+        HTMLElement child = children.remove(element);
+        if (child != null) {
+            root.removeChild(child);
+        }
+    }
+
+    public void clear() {
+        children.forEach((element, htmlElement) -> root.removeChild(htmlElement));
+        children.clear();
+    }
+
+    @Override
+    public HTMLElement getElement() {
+        return root;
+    }
+
+    public boolean isVisible() {
+        return !root.style.display.equals("none");
+    }
+}
--- packages/serverless-workflow-diagram-editor/kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-lienzo-extensions/src/main/java/org/kie/workbench/common/stunner/lienzo/primitive/PrimitivePopup.java
+++ packages/serverless-workflow-diagram-editor/kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-lienzo-extensions/src/main/java/org/kie/workbench/common/stunner/lienzo/primitive/PrimitivePopup.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License. 
+ * under the License.
  */
 
 
@@ -24,12 +24,15 @@ import com.ait.lienzo.client.core.shape.IPrimitive;
 import com.ait.lienzo.client.core.shape.Layer;
 import com.ait.lienzo.client.widget.panel.LienzoPanel;
 import com.ait.lienzo.client.widget.panel.impl.LienzoFixedPanel;
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.RootPanel;
-import org.jboss.errai.common.client.ui.ElementWrapperWidget;
+import elemental2.dom.HTMLElement;
+import org.jboss.errai.ui.client.local.api.IsElement;
+import org.kie.workbench.common.stunner.lienzo.flowpanel.FloatingWidgetView;
 
-public class PrimitivePopup extends FlowPanel {
+import static elemental2.dom.CSSProperties.ZIndexUnionType;
+
+public class PrimitivePopup {
+
+    private FloatingWidgetView floatingWidgetView = new FloatingWidgetView();
 
     protected Layer canvasLayer = new Layer();
     protected LienzoFixedPanel lienzoPanel;
@@ -77,22 +80,26 @@ public class PrimitivePopup extends FlowPanel {
                           final double y) {
         reset();
         lienzoPanel = LienzoFixedPanel.newPanel((int) width, (int) height);
-        this.add(ElementWrapperWidget.getWidget(lienzoPanel.getElement()));
+        floatingWidgetView.add(new IsElement() {
+            @Override
+            public HTMLElement getElement() {
+                return lienzoPanel.getElement();
+            }
+        });
         this.lienzoPanel.add(canvasLayer);
         if (null != _primitive) {
             canvasLayer.add(_primitive);
         }
-        this.getElement().getStyle().setLeft(x,
-                                             Style.Unit.PX);
-        this.getElement().getStyle().setTop(y,
-                                            Style.Unit.PX);
-        this.getElement().getStyle().setZIndex(zIndex);
-        this.getElement().getStyle().setDisplay(Style.Display.INLINE);
+        floatingWidgetView.getPanel().getElement().style.left = x + "px";
+        floatingWidgetView.getPanel().getElement().style.top = y + "px";
+        floatingWidgetView.getPanel().getElement().style.zIndex = ZIndexUnionType.of(zIndex);
+        floatingWidgetView.getPanel().getElement().style.display = "inline";
+        floatingWidgetView.show();
     }
 
     public PrimitivePopup hide() {
         reset();
-        this.getElement().getStyle().setDisplay(Style.Display.NONE);
+        floatingWidgetView.hide();
         return this;
     }
 
@@ -111,18 +118,17 @@ public class PrimitivePopup extends FlowPanel {
     }
 
     protected void attach() {
-        RootPanel.get().add(this);
-        this.getElement().getStyle().setPosition(Style.Position.FIXED);
-        this.getElement().getStyle().setZIndex(zIndex);
-        this.getElement().getStyle().setDisplay(Style.Display.NONE);
+        floatingWidgetView.getPanel().getElement().style.position = "fixed";
+        floatingWidgetView.getPanel().getElement().style.zIndex = ZIndexUnionType.of(zIndex);
+        floatingWidgetView.getPanel().getElement().style.display = "none";
     }
 
     protected void deattach() {
-        RootPanel.get().remove(this);
+        floatingWidgetView.destroy();
     }
 
     protected void reset() {
-        this.clear();
+        floatingWidgetView.clear();
         canvasLayer = new Layer();
         lienzoPanel = null;
     }
