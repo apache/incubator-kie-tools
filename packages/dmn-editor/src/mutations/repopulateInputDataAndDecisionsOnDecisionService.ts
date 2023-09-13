be50014341f336cb0753f2a610550e38ea45cb91
Tiago Bento <tiagobentofernandes@gmail.com>
2023-09-13 15:35:29 -0400

    Oops. Forgot to add the new file

---
+++ packages/dmn-editor/src/mutations/repopulateInputDataAndDecisionsOnDecisionService.ts
@@ -0,0 +1,57 @@
+import {
+  DMN15__tDecisionService,
+  DMN15__tDefinitions,
+} from "@kie-tools/dmn-marshaller/dist/schemas/dmn-1_5/ts-gen/types";
+import { idFromHref } from "../diagram/maths/DmnMaths";
+
+export function repopulateInputDataAndDecisionsOnDecisionService({
+  definitions,
+  decisionService,
+}: {
+  definitions: DMN15__tDefinitions;
+  decisionService: DMN15__tDecisionService;
+}) {
+  decisionService.inputData = [];
+  decisionService.inputDecision = [];
+
+  const hrefsToDecisionsInsideDecisionService = new Set([
+    ...(decisionService.outputDecision ?? []).map((s) => s["@_href"]),
+    ...(decisionService.encapsulatedDecision ?? []).map((s) => s["@_href"]),
+  ]);
+
+  const requirements = new Array<{ id: string; type: "decisionIr" | "inputDataIr" }>();
+  for (let i = 0; i < definitions.drgElement!.length; i++) {
+    const drgElement = definitions.drgElement![i];
+    if (!hrefsToDecisionsInsideDecisionService.has(`#${drgElement["@_id"]}`) || drgElement.__$$element !== "decision") {
+      continue;
+    }
+
+    (drgElement.informationRequirement ?? []).flatMap((ir) => {
+      if (ir.requiredDecision) {
+        requirements.push({ id: idFromHref(ir.requiredDecision["@_href"]), type: "decisionIr" });
+      } else if (ir.requiredInput) {
+        requirements.push({ id: idFromHref(ir.requiredInput["@_href"]), type: "inputDataIr" });
+      }
+    });
+  }
+
+  const inputDatas = new Set<string>(); // Using Set for uniqueness
+  const inputDecisions = new Set<string>(); // Using Set for uniqueness
+
+  const requirementsArray = [...requirements];
+  for (let i = 0; i < requirementsArray.length; i++) {
+    const r = requirementsArray[i];
+    if (r.type === "inputDataIr") {
+      inputDatas.add(r.id);
+    } else if (r.type === "decisionIr") {
+      inputDecisions.add(r.id);
+    } else {
+      throw new Error(`Invalid type of element to be referenced by DecisionService: '${r.type}'`);
+    }
+  }
+
+  decisionService.inputData = [...inputDatas].map((i) => ({ "@_href": `#${i}` }));
+  decisionService.inputDecision = [...inputDecisions].flatMap(
+    (d) => (hrefsToDecisionsInsideDecisionService.has(`#${d}`) ? [] : { "@_href": `#${d}` }) // Makes sure output and encapsulated Decisions are not listed as inputDecisions
+  );
+}
