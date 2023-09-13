7124560ec620f30d9c71c8a4af05563f53c4dc30
Luiz Motta <luizjoaomotta@gmail.com>
2023-09-13 19:23:30 -0300

    Add apache header

--- packages/boxed-expression-component/stories/misc/StandaloneTable/StandaloneTable.stories.tsx
+++ packages/boxed-expression-component/stories/misc/StandaloneTable/StandaloneTable.stories.tsx
@@ -1,8 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import * as React from "react";
 import { useRef } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { ResizerStopBehavior } from "../../../src/resizing/ResizingWidthsContext";
-import { DmnBuiltInDataType, BeeTableHeaderVisibility, BeeTableProps, generateUuid } from "../../../src/api";
+import { BeeTableHeaderVisibility, BeeTableProps, generateUuid } from "../../../src/api";
 import { StandaloneBeeTable } from "../../../src/table/BeeTable";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
--- packages/online-editor/tests/e2e/editorPage/dmnRunner.spec.ts
+++ packages/online-editor/tests/e2e/editorPage/dmnRunner.spec.ts
@@ -1,7 +1,4 @@
 /*
-<<<<<<< HEAD:packages/online-editor/tests/e2e/editorPage/dmnRunner.spec.ts
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
-=======
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -9,7 +6,6 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
->>>>>>> origin/main:packages/online-editor/it-tests/support/e2e.ts
  *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
