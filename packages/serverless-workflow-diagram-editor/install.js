7da07c171c1bda1c90f2df392224f1ddcca71567
Dmitrii Tikhomirov <chani.liet@gmail.com>
2023-10-18 23:51:21 -0700

    process resources with bootstrap install.js

--- packages/serverless-workflow-diagram-editor/install.js
+++ packages/serverless-workflow-diagram-editor/install.js
@@ -20,6 +20,136 @@
 const buildEnv = require("./env");
 const { setup } = require("@kie-tools/maven-config-setup-helper");
 
+const fs = require("fs").promises;
+const path = require("path");
+const constants = require("./package.json").constants;
+const download = require("mvn-artifact-download").default;
+const AdmZip = require("adm-zip");
+
+const stunner_lienzo_path = "kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-lienzo/";
+const tempPath = stunner_lienzo_path + "target/external-deps";
+const resources_path =
+  stunner_lienzo_path + "src/main/resources/org/kie/workbench/common/stunner/client/lienzo/resources";
+
 setup(`
     -Drevision=${buildEnv.env.swfDiagramEditor.version}
 `);
+
+console.info("[serverless-workflow-diagram-editor] Download maven resources...");
+
+const artifacts = new Map([
+  [
+    {
+      groupId: "org.uberfire",
+      artifactId: "uberfire-workbench-client-views-patternfly",
+      version: constants.UBERFIRE_VERSION,
+    },
+    [
+      {
+        path: "org/uberfire/client/views/static/js/patternfly.min.js",
+        type: "js",
+      },
+      {
+        path: "org/uberfire/client/views/static/bootstrap-select/js/bootstrap-select.min.js",
+        type: "js",
+      },
+      {
+        path: "org/uberfire/client/views/static/css/patternfly-additions.min.css",
+        type: "css",
+      },
+      {
+        path: "org/uberfire/client/views/static/css/patternfly.min.css",
+        type: "css",
+      },
+      {
+        path: "org/uberfire/client/views/static/uberfire-patternfly.css",
+        type: "css",
+      },
+    ],
+  ],
+  [
+    {
+      groupId: "org.webjars",
+      artifactId: "font-awesome",
+      version: constants.FONT_AWESOME_VERSION,
+    },
+    [
+      {
+        path: "META-INF/resources/webjars/font-awesome/" + constants.FONT_AWESOME_VERSION + "/css/font-awesome.min.css",
+        type: "css",
+      },
+    ],
+  ],
+  [
+    {
+      groupId: "org.gwtbootstrap3",
+      artifactId: "gwtbootstrap3",
+      version: constants.GWTBOOTSTRAP3_VERSION,
+    },
+    [
+      {
+        path: "org/gwtbootstrap3/client/resource/js/jquery-1.12.4.min.cache.js",
+        type: "js",
+      },
+      {
+        path: "org/gwtbootstrap3/client/resource/js/gwtbootstrap3.js",
+        type: "js",
+      },
+    ],
+  ],
+  [
+    {
+      groupId: "org.webjars",
+      artifactId: "bootstrap",
+      version: constants.BOOTSTRAP_VERSION,
+    },
+    [
+      {
+        path: "META-INF/resources/webjars/bootstrap/" + constants.BOOTSTRAP_VERSION + "/js/bootstrap.min.js",
+        type: "js",
+      },
+    ],
+  ],
+  [
+    {
+      groupId: "org.webjars",
+      artifactId: "animate.css",
+      version: constants.ANIMATE_CSS_VERSION,
+    },
+    [
+      {
+        path: "META-INF/resources/webjars/animate.css/" + constants.ANIMATE_CSS_VERSION + "/animate.min.css",
+        type: "css",
+      },
+    ],
+  ],
+]);
+
+processMavenDependencies();
+
+async function prepareFolders() {
+  try {
+    await fs.mkdir(tempPath, { recursive: true });
+    await fs.mkdir(resources_path + "/js", { recursive: true });
+    await fs.mkdir(resources_path + "/css", { recursive: true });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function processMavenDependencies() {
+  await prepareFolders(tempPath);
+  artifacts.forEach((resources, artifact) => {
+    download(artifact, tempPath).then((jarPath) => {
+      var zip = new AdmZip(jarPath);
+      resources.forEach((resource) => {
+        let fileName = path.basename(resource.path);
+        if (resource.type === "js") {
+          zip.extractEntryTo(resource.path, resources_path + "/js/", false, true, false, fileName + ".noproc");
+        } else if (resource.type === "css") {
+          zip.extractEntryTo(resource.path, resources_path + "/css/", false, true);
+        }
+      });
+    });
+  });
+}
--- packages/serverless-workflow-diagram-editor/kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-lienzo/src/main/java/org/kie/workbench/common/stunner/client/lienzo/resources/StunnerLienzoCoreResources.java
+++ packages/serverless-workflow-diagram-editor/kie-wb-common-stunner/kie-wb-common-stunner-client/kie-wb-common-stunner-lienzo/src/main/java/org/kie/workbench/common/stunner/client/lienzo/resources/StunnerLienzoCoreResources.java
@@ -28,87 +28,34 @@ public interface StunnerLienzoCoreResources extends ClientBundle {
 
     StunnerLienzoCoreResources INSTANCE = StunnerLienzoCoreResourcesImpl.INSTANCE;
 
-    @MavenArtifactSource(
-            group = "org.gwtbootstrap3",
-            artifact = "gwtbootstrap3",
-            version = "1.0.1",
-            path = "org/gwtbootstrap3/client/resource/js/jquery-1.12.4.min.cache.js",
-            copyTo = "org/kie/workbench/common/stunner/client/lienzo/resources/js/jquery-1.12.4.min.js.noproc"
-    )
+    @Source("js/jquery-1.12.4.min.cache.js.noproc")
     TextResource jquery();
 
-    @MavenArtifactSource(
-            group = "org.webjars",
-            artifact = "bootstrap",
-            version = "3.4.1",
-            path = "META-INF/resources/webjars/bootstrap/3.4.1/js/bootstrap.min.js",
-            copyTo = "org/kie/workbench/common/stunner/client/lienzo/resources/js/bootstrap.min.js.noproc")
+    @Source("js/bootstrap.min.js.noproc")
     TextResource bootstrapJs();
 
-    @MavenArtifactSource(
-            group = "org.gwtbootstrap3",
-            artifact = "gwtbootstrap3",
-            version = "1.0.1",
-            path = "org/gwtbootstrap3/client/resource/js/gwtbootstrap3.js",
-            copyTo = "org/kie/workbench/common/stunner/client/lienzo/resources/js/gwtbootstrap3.min.js.noproc"
-    )
+    @Source("js/gwtbootstrap3.js.noproc")
     TextResource gwtbootstrap3();
 
-    @MavenArtifactSource(
-            group = "org.uberfire",
-            artifact = "uberfire-workbench-client-views-patternfly",
-            version = "7.74.1.Final",
-            path = "org/uberfire/client/views/static/js/patternfly.min.js",
-            copyTo = "org/kie/workbench/common/stunner/client/lienzo/resources/js/patternfly.min.js.noproc"
-    )
+    @Source("js/patternfly.min.js.noproc")
     TextResource patternfly();
 
-    @MavenArtifactSource(
-            group = "org.uberfire",
-            artifact = "uberfire-workbench-client-views-patternfly",
-            version = "7.74.1.Final",
-            path = "org/uberfire/client/views/static/bootstrap-select/js/bootstrap-select.min.js",
-            copyTo = "org/kie/workbench/common/stunner/client/lienzo/resources/js/bootstrap-select.min.js.noproc"
-    )
+    @Source("js/bootstrap-select.min.js.noproc")
     TextResource bootstrapSelectJs();
 
-    @MavenArtifactSource(
-            group = "org.webjars",
-            artifact = "animate.css",
-            version = "3.5.2",
-            path = "META-INF/resources/webjars/animate.css/3.5.2/animate.min.css",
-            copyTo = "org/kie/workbench/common/stunner/client/lienzo/resources/css/animate.min.css"
-    )
+    @Source("css/animate.min.css")
     TextResource animate();
 
-    @MavenArtifactSource(
-            group = "org.uberfire",
-            artifact = "uberfire-workbench-client-views-patternfly",
-            version = "7.74.1.Final",
-            path = "org/uberfire/client/views/static/css/patternfly-additions.min.css")
+    @Source("css/patternfly-additions.min.css")
     TextResource patternflyStyleAdditionsMin();
 
-    @MavenArtifactSource(
-            group = "org.uberfire",
-            artifact = "uberfire-workbench-client-views-patternfly",
-            version = "7.74.1.Final",
-            path = "org/uberfire/client/views/static/css/patternfly.min.css")
+    @Source("css/patternfly.min.css")
     TextResource patternflyStyleMin();
 
-    @MavenArtifactSource(
-            group = "org.uberfire",
-            artifact = "uberfire-workbench-client-views-patternfly",
-            version = "7.74.1.Final",
-            path = "org/uberfire/client/views/static/uberfire-patternfly.css")
+    @Source("css/uberfire-patternfly.css")
     TextResource uberfirePatternfly();
 
-    @MavenArtifactSource(
-            group = "org.webjars",
-            artifact = "font-awesome",
-            version = "4.7.0",
-            path = "META-INF/resources/webjars/font-awesome/4.7.0/css/font-awesome.min.css",
-            copyTo = "org/kie/workbench/common/stunner/client/lienzo/resources/fonts/font-awesome.min.css"
-    )
+    @Source("css/font-awesome.min.css")
     TextResource fontAwesome();
 
     @Source("css/fonts.css")
