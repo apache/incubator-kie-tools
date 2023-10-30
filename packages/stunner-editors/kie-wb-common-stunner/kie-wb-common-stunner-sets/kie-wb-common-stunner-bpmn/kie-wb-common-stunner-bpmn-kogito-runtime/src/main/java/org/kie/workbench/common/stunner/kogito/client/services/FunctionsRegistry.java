1f704561685c22f66ccf7db21fbc09b15a9d4ebd
Халтобин Евгений <evgeny.khaltobine@gmail.com>
2023-10-30 20:18:10 +0300

    added isNull function to String, Date, Number, ArrayList, Boolean

--- packages/stunner-editors/kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-kogito-runtime/src/main/java/org/kie/workbench/common/stunner/kogito/client/services/FunctionsRegistry.java
+++ packages/stunner-editors/kie-wb-common-stunner/kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-kogito-runtime/src/main/java/org/kie/workbench/common/stunner/kogito/client/services/FunctionsRegistry.java
@@ -17,7 +17,9 @@
 package org.kie.workbench.common.stunner.kogito.client.services;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -71,6 +73,13 @@ public class FunctionsRegistry {
     }
 
     private void initRegistry() {
+
+        String[] types = new String[] {Object.class.getName(), Number.class.getName(), String.class.getName(),
+                Date.class.getName(), Boolean.class.getName(), ArrayList.class.getName()
+        };
+        Arrays.stream(types).map(type -> FunctionDef.FunctionDefBuilder.newFunction(IS_NULL)
+                .withParam("param1", type)
+                .build()).forEach(func -> registry.add(func));
 
         FunctionDef isNull = FunctionDef.FunctionDefBuilder.newFunction(IS_NULL)
                 .withParam("param1", Object.class.getName())
