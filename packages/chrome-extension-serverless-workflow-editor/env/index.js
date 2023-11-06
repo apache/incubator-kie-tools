77cd3177b5b2cbe732042062ea9b2c33874247dd
Tomáš David <tdavid@redhat.com>
2023-11-06 14:10:28 +0100

    KOGITO-9907: Enable to use GitHub token without logged in user (#2019)

--- packages/chrome-extension-serverless-workflow-editor/env/index.js
+++ packages/chrome-extension-serverless-workflow-editor/env/index.js
@@ -33,6 +33,10 @@ module.exports = composeEnv([require("@kie-tools/root-env/env")], {
       default: "manifest.dev.json",
       description: "",
     },
+    SWF_CHROME_EXTENSION__e2eTestingToken: {
+      default: "",
+      description: "",
+    },
   }),
   get env() {
     return {
@@ -44,6 +48,7 @@ module.exports = composeEnv([require("@kie-tools/root-env/env")], {
         routerTargetOrigin: getOrDefault(this.vars.SWF_CHROME_EXTENSION__routerTargetOrigin),
         routerRelativePath: getOrDefault(this.vars.SWF_CHROME_EXTENSION__routerRelativePath),
         manifestFile: getOrDefault(this.vars.SWF_CHROME_EXTENSION__manifestFile),
+        e2eTestingToken: getOrDefault(this.vars.SWF_CHROME_EXTENSION__e2eTestingToken),
       },
     };
   },
