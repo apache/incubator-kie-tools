eeb7c60eeb935fd0e2df020ae7777a2f9abe5065
Yeser Amer <yeser.amer@ibm.com>
2023-11-07 20:51:24 +0100

    WIP

--- packages/editor/tests/embedded/embedded/EmbeddedEditor.test.tsx
+++ packages/editor/tests/embedded/embedded/EmbeddedEditor.test.tsx
@@ -66,11 +66,10 @@ describe("EmbeddedEditor::ONLINE", () => {
       />
     );
 
-    /*
     expect(getByTestId("kogito-iframe")).toBeVisible();
     expect(getByTestId("kogito-iframe")).toHaveAttribute("data-envelope-channel", ChannelType.ONLINE);
     expect(getByTestId("kogito-iframe")).toHaveAttribute("src", "envelope/envelope.html");
-    */
+
     expect(container.firstChild).toMatchSnapshot();
   });
 
