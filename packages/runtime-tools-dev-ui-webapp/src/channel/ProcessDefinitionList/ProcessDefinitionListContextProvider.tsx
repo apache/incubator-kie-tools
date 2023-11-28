e82ead544cf2853eee8f0ceab3d32e6e730f82d1
fantonangeli <fabrizio.antonangeli@gmail.com>
2023-11-28 10:18:15 +0100

    Copyed DevUI src

---
+++ packages/runtime-tools-dev-ui-webapp/src/channel/ProcessDefinitionList/ProcessDefinitionListContextProvider.tsx
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useMemo } from "react";
+import { DevUIAppContext, useDevUIAppContext } from "../../components/contexts/DevUIAppContext";
+import ProcessDefinitionListContext from "./ProcessDefinitionListContext";
+import { ProcessDefinitionListGatewayApiImpl } from "./ProcessDefinitionListGatewayApi";
+
+interface ProcessDefinitionListContextProviderProps {
+  children;
+}
+
+const ProcessDefinitionListContextProvider: React.FC<ProcessDefinitionListContextProviderProps> = ({ children }) => {
+  const runtimeToolsApi: DevUIAppContext = useDevUIAppContext();
+
+  const gatewayApiImpl = useMemo(() => {
+    return new ProcessDefinitionListGatewayApiImpl(runtimeToolsApi.getDevUIUrl(), runtimeToolsApi.getOpenApiPath());
+  }, []);
+
+  return (
+    <ProcessDefinitionListContext.Provider value={gatewayApiImpl}>{children}</ProcessDefinitionListContext.Provider>
+  );
+};
+
+export default ProcessDefinitionListContextProvider;
---
+++ packages/runtime-tools-dev-ui-webapp/src/components/contexts/DevUIAppContextProvider.tsx
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import { User } from "@kogito-apps/consoles-common/dist/environment/auth";
+import RuntimeToolsDevUIAppContext, { DevUIAppContextImpl } from "./DevUIAppContext";
+import { CustomLabels } from "../../api/CustomLabels";
+import { DiagramPreviewSize } from "@kogito-apps/process-details/dist/api";
+
+interface IOwnProps {
+  users: User[];
+  devUIUrl: string;
+  openApiPath: string;
+  isProcessEnabled: boolean;
+  isTracingEnabled: boolean;
+  availablePages: string[];
+  customLabels: CustomLabels;
+  omittedProcessTimelineEvents: string[];
+  diagramPreviewSize: DiagramPreviewSize;
+  isStunnerEnabled: boolean;
+}
+
+const DevUIAppContextProvider: React.FC<IOwnProps> = ({
+  users,
+  devUIUrl,
+  openApiPath,
+  isProcessEnabled,
+  isTracingEnabled,
+  availablePages,
+  customLabels,
+  omittedProcessTimelineEvents,
+  diagramPreviewSize,
+  isStunnerEnabled,
+  children,
+}) => {
+  return (
+    <RuntimeToolsDevUIAppContext.Provider
+      value={
+        new DevUIAppContextImpl({
+          users,
+          devUIUrl,
+          openApiPath,
+          isProcessEnabled,
+          isTracingEnabled,
+          availablePages,
+          customLabels,
+          omittedProcessTimelineEvents,
+          diagramPreviewSize,
+          isStunnerEnabled,
+        })
+      }
+    >
+      {children}
+    </RuntimeToolsDevUIAppContext.Provider>
+  );
+};
+
+export default DevUIAppContextProvider;
