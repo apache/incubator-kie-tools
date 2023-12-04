69091927063e44ac9e9cb31713a2539f3f8b9e6a
Tiago Bento <tiagobentofernandes@gmail.com>
2023-12-04 15:54:48 -0500

    Apache headers on TS/TSX files

--- packages/dmn-editor/src/Dmn15Spec.ts
+++ packages/dmn-editor/src/Dmn15Spec.ts
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { ns as dmn10ns } from "@kie-tools/dmn-marshaller/dist/schemas/dmn-1_0/ts-gen/meta";
 import { ns as dmn11ns } from "@kie-tools/dmn-marshaller/dist/schemas/dmn-1_1/ts-gen/meta";
 import { ns as dmn12ns } from "@kie-tools/dmn-marshaller/dist/schemas/dmn-1_2/ts-gen/meta";
