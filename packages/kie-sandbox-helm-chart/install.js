eda0ebd9990ff2179fe433ac7416b55edb2bad26
Thiago Lugli <thiagoelg@gmail.com>
2023-12-18 13:05:25 -0300

    Format docs

--- packages/kie-sandbox-helm-chart/install.js
+++ packages/kie-sandbox-helm-chart/install.js
@@ -1,7 +1,6 @@
 const buildEnv = require("./env");
 const yaml = require("js-yaml");
 const fs = require("fs");
-const prettier = require("prettier");
 
 const file = "src/Chart.yaml";
 const doc = yaml.load(fs.readFileSync(file, "utf8"));
@@ -14,8 +13,5 @@ const chartReadmeSections = chartReadme.split("## Values");
 const readmeSections = readme.split("<!-- CHART_VALUES_README -->");
 readmeSections[1] = chartReadmeSections[1];
 
-const newContent = prettier.format(readmeSections.join("<!-- CHART_VALUES_README -->"), {
-  parser: "markdown",
-  printWidth: 120,
-});
+const newContent = readmeSections.join("<!-- CHART_VALUES_README -->");
 fs.writeFileSync("./README.md", newContent);
