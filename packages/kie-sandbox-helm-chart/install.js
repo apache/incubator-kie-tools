fea2ff88f07c0e08b94ad2aa9b7c2509ce28d6e3
Thiago Lugli <thiagoelg@gmail.com>
2023-12-28 12:16:11 -0300

    Update install script to change all versions

--- packages/kie-sandbox-helm-chart/install.js
+++ packages/kie-sandbox-helm-chart/install.js
@@ -2,16 +2,34 @@ const buildEnv = require("./env");
 const yaml = require("js-yaml");
 const fs = require("fs");
 
-const file = "src/Chart.yaml";
-const doc = yaml.load(fs.readFileSync(file, "utf8"));
-doc.version = buildEnv.env.kieSandboxHelmChart.tag;
-fs.writeFileSync(file, yaml.dump(doc), "utf8");
+// Set version for the Chart (and its dependencies) and Subcharts
+const files = [
+  "src/Chart.yaml",
+  "src/charts/extended_services/Chart.yaml",
+  "src/charts/cors_proxy/Chart.yaml",
+  "src/charts/kie_sandbox/Chart.yaml",
+];
+files.forEach((file) => {
+  const doc = yaml.load(fs.readFileSync(file, "utf8"));
+  doc.version = buildEnv.env.kieSandboxHelmChart.tag;
+  doc.appVersion = buildEnv.env.kieSandboxHelmChart.tag;
+  if (doc.dependencies) {
+    doc.dependencies = doc.dependencies.map((dep) => {
+      if (["extended_services", "cors_proxy", "kie_sandbox"].includes(dep.name)) {
+        return { ...dep, version: buildEnv.env.kieSandboxHelmChart.tag };
+      }
+      return dep;
+    });
+  }
+  console.log(doc);
+  fs.writeFileSync(file, yaml.dump(doc), "utf8");
+});
 
+// Update Values table on README
 const readme = fs.readFileSync("./README.md").toString();
 const chartReadme = fs.readFileSync("./src/README.md").toString();
 const chartReadmeSections = chartReadme.split("## Values");
 const readmeSections = readme.split("<!-- CHART_VALUES_README -->");
 readmeSections[1] = chartReadmeSections[1];
-
 const newContent = readmeSections.join("<!-- CHART_VALUES_README -->");
 fs.writeFileSync("./README.md", newContent);
