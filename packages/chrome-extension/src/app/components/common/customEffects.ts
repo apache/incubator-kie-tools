15a2b2ca44791853f3f435f6b09f41bbe564dad9
Kennedy Bowers <kennedy.bowers@ibm.com>
2024-01-08 21:04:47 +0100

    Ignoring ESLint warning

--- packages/chrome-extension/src/app/components/common/customEffects.ts
+++ packages/chrome-extension/src/app/components/common/customEffects.ts
@@ -27,7 +27,7 @@ export function useEffectAfterFirstRender(func: () => ReturnType<EffectCallback>
     } else {
       firstRender.current = false;
     }
-    //Using deps as an extension mechanism to allow callers to define their own custom dependencies.
+    // Using deps as an extension mechanism to allow callers to define their own custom dependencies.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [func, ...deps]);
 }
