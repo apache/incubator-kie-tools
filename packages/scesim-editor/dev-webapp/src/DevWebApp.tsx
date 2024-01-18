1e7190124ba33476a6bcce4248b8db71910070da
Yeser Amer <yeser.amer@ibm.com>
2024-01-18 11:49:30 +0100

    Added an example Dropdrown in the DevWebApp

--- packages/scesim-editor/dev-webapp/src/DevWebApp.tsx
+++ packages/scesim-editor/dev-webapp/src/DevWebApp.tsx
@@ -18,10 +18,12 @@
  */
 
 import * as React from "react";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import { TestScenarioEditor, TestScenarioEditorRef } from "../../src/TestScenarioEditor";
 
+import { Button } from "@patternfly/react-core/dist/js/components/Button";
+import { Dropdown, DropdownToggle, DropdownItem } from "@patternfly/react-core/dist/js/components/Dropdown";
 import { Flex, FlexItem } from "@patternfly/react-core/dist/js/layouts/Flex";
 import { Page, PageSection } from "@patternfly/react-core/dist/js/components/Page";
 
@@ -29,6 +31,7 @@ import "./DevWebApp.css";
 
 export function DevWebApp() {
   const ref = useRef<TestScenarioEditorRef>(null);
+  const [isExampleDropdownOpen, setExampleDropdownIsOpen] = useState(false);
 
   useEffect(() => {
     /* Simulating a call from "Foundation" code */
@@ -77,6 +80,40 @@ export function DevWebApp() {
     }
   }, []);
 
+  const onOpenStaticScesimExample = useCallback(async (fileName: string) => {
+    const res = await fetch("/examples/" + fileName);
+    const content = await res.text();
+    ref.current?.setContent(fileName, content);
+  }, []);
+
+  const dropdownExamplesItems = [
+    <DropdownItem
+      key="action"
+      component="button"
+      onClick={() => onOpenStaticScesimExample("TrafficViolationTest.scesim")}
+    >
+      DMN-Based: TrafficViolation
+    </DropdownItem>,
+    <DropdownItem
+      key="action"
+      component="button"
+      onClick={() => onOpenStaticScesimExample("Are they old enough.scesim")}
+    >
+      Rule-Based: Are they old enough
+    </DropdownItem>,
+  ];
+
+  const onExampleDropdownToggle = useCallback((isOpen: boolean) => {
+    console.log(isOpen);
+    setExampleDropdownIsOpen(isOpen);
+  }, []);
+
+  const onExampleDropdownSelect = useCallback(() => {
+    setExampleDropdownIsOpen(false);
+    const element = document.getElementById("toggle-basic");
+    element?.focus();
+  }, []);
+
   return (
     <>
       <Page onDragOver={onDragOver} onDrop={onDrop}>
@@ -89,12 +126,30 @@ export function DevWebApp() {
               <h5>(Drag & drop a file anywhere to open it)</h5>
             </FlexItem>
             <FlexItem shrink={{ default: "shrink" }}>
-              &nbsp; &nbsp; | &nbsp; &nbsp;
-              <button onClick={reset}>Reset</button>
               &nbsp; &nbsp;
-              <button onClick={copyAsXml}>Copy as XML</button>
+              <Dropdown
+                className="dev-webapp--example-dropdown"
+                onSelect={onExampleDropdownSelect}
+                toggle={
+                  <DropdownToggle id="toggle-basic" onToggle={onExampleDropdownToggle}>
+                    Examples
+                  </DropdownToggle>
+                }
+                isOpen={isExampleDropdownOpen}
+                dropdownItems={dropdownExamplesItems}
+              />
               &nbsp; &nbsp;
-              <button onClick={downloadAsXml}>Download as XML</button>
+              <Button onClick={reset} variant="tertiary">
+                Reset
+              </Button>
+              &nbsp; &nbsp;
+              <Button onClick={copyAsXml} variant="tertiary">
+                Copy as XML
+              </Button>
+              &nbsp; &nbsp;
+              <Button onClick={downloadAsXml} variant="tertiary">
+                Download as XML
+              </Button>
             </FlexItem>
           </Flex>
           <a ref={downloadRef} />
