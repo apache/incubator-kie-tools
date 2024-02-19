3aefc2181dcf0331e22940d34ab262d2563d403c
Wagner Scholl Lemos <josephblt@gmail.com>
2024-02-19 12:03:44 -0300

    VS Code Extension finalized. BPMN and DMN validation fully implemented.

---
+++ packages/extended-services-vscode-extension/src/watchers/kie-files/kie-file.ts
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import * as vscode from "vscode";
+
+export class KIEFile {
+  public readonly fsPath: string;
+
+  private kieFileChangedEventHandler: KIEFileChangedHandler | null;
+  private fileWatcher: vscode.FileSystemWatcher;
+
+  constructor(textDocument: vscode.TextDocument) {
+    this.fsPath = textDocument.uri.fsPath;
+    this.fileWatcher = vscode.workspace.createFileSystemWatcher(this.fsPath);
+    this.fileWatcher.onDidChange(this.fireKIEFileChangedEvent, this);
+  }
+
+  public subscribeKIEFileChanged(handler: KIEFileChangedHandler) {
+    this.kieFileChangedEventHandler = handler;
+  }
+
+  private fireKIEFileChangedEvent() {
+    if (this.kieFileChangedEventHandler) {
+      this.kieFileChangedEventHandler();
+    }
+  }
+
+  public unsubscribeKIEFileChanged() {
+    this.kieFileChangedEventHandler = null;
+  }
+
+  public dispose() {
+    this.unsubscribeKIEFileChanged();
+    this.fileWatcher.dispose();
+  }
+}
+
+interface KIEFileChangedHandler {
+  (): void;
+}
