8f351399887fd4cfea0001b02fd6170bf35c468d
Thiago Lugli <thiagoelg@gmail.com>
2024-02-26 21:25:50 -0300

    Fix yard-editor webpack.config

--- packages/yard-editor/dev-webapp/webpack.config.js
+++ packages/yard-editor/dev-webapp/webpack.config.js
@@ -47,7 +47,10 @@ module.exports = (env) =>
             to: "./yard-editor-envelope.html",
           },
           {
-            from: path.resolve(__dirname, "../node_modules/@kie-tools/yard-validator/dist/yard-validator-worker.js"),
+            from: path.join(
+              path.dirname(require.resolve("@kie-tools/yard-validator")),
+              "/dist/yard-validator-worker.js"
+            ),
             to: "./yard-validator-worker.js",
           },
         ],
