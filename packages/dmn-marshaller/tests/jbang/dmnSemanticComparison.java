f7a13deda419384530d5864a0dc66872f9095dea
Yeser Amer <yeser.amer@ibm.com>
2024-04-09 21:42:50 +0200

    WIP

--- packages/dmn-marshaller/tests/jbang/dmnSemanticComparison.java
+++ packages/dmn-marshaller/tests/jbang/dmnSemanticComparison.java
@@ -1,16 +1,30 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //REPOS mavencentral,apache=https://repository.apache.org/content/groups/public/
-//DEPS org.kie:kie-dmn-validation:${kogito-runtime.version:LATEST}
+//DEPS org.kie:kie-dmn-api:${kogito-runtime.version:LATEST}
+//DEPS org.kie:kie-dmn-core:${kogito-runtime.version:LATEST}
+//DEPS org.kie:kie-dmn-model:${kogito-runtime.version:LATEST}
+//DEPS org.kie:kie-api:${kogito-runtime.version:LATEST}
+//DEPS org.kie:kie-internal:${kogito-runtime.version:LATEST}
 
 import java.io.File;
+import java.io.FileReader;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.kie.dmn.api.core.DMNMessage;
-import org.kie.dmn.core.compiler.profiles.ExtendedDMNProfile;
-import org.kie.dmn.validation.DMNValidator;
-import org.kie.dmn.validation.DMNValidator.Validation;
-import org.kie.dmn.validation.DMNValidatorFactory;
+import org.kie.api.io.Resource;
+import org.kie.dmn.api.core.DMNContext;
+import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNResult;
+import org.kie.dmn.api.core.DMNRuntime;
+import org.kie.dmn.model.api.Definitions;
+import org.kie.dmn.model.api.NamedElement;
+import org.kie.dmn.core.internal.utils.DMNRuntimeBuilder;
+import org.kie.dmn.core.internal.utils.DynamicDMNContextBuilder;
+import org.kie.internal.io.ResourceFactory;
 
 /**
  * JBang script that performs DMN files' XML (in string format) validation relying on KIE DMN Validator
@@ -30,8 +44,24 @@ class dmnSemanticComparison {
             throw new IllegalArgumentException("Comparison requires 2 DMN files");
         }
 
-        System.out.println(args[0]);
-        System.out.println(args[1]);
+        File[] models = Stream.of(args)
+                              .map(File::new)
+                              .toArray(File[]::new);
+
+        Resource modelResource = ResourceFactory.newReaderResource(new FileReader(models[0]), "UTF-8");
+        DMNRuntime dmnRuntime = DMNRuntimeBuilder.fromDefaults().buildConfiguration()
+                .fromResources(Collections.singletonList(modelResource)).getOrElseThrow(RuntimeException::new);
+        DMNModel originalModel = dmnRuntime.getModels().get(0);
+
+        Resource modelResource1 = ResourceFactory.newReaderResource(new FileReader(models[1]), "UTF-8");
+        DMNRuntime dmnRuntime1 = DMNRuntimeBuilder.fromDefaults().buildConfiguration()
+                .fromResources(Collections.singletonList(modelResource1)).getOrElseThrow(RuntimeException::new);
+        DMNModel parsedModel = dmnRuntime1.getModels().get(0);
+
+        compareDMNModels(originalModel, parsedModel);
+
+        //System.out.println(args[0]);
+        //System.out.println(args[1]);
 
 
 
@@ -60,5 +90,32 @@ class dmnSemanticComparison {
             System.out.println("=============================");
             return 1;
         } */
+    }
+
+    private static void compareDMNModels(DMNModel originalModel, DMNModel parsedModel) {
+
+        Definitions originalModelDefinitions = originalModel.getDefinitions();
+        Definitions parsedModelDefinitions = parsedModel.getDefinitions();
+
+        checkElements(originalModelDefinitions.getDecisionService(), parsedModelDefinitions.getDecisionService());
+        checkElements(originalModelDefinitions.getBusinessContextElement(), parsedModelDefinitions.getBusinessContextElement());
+        checkElements(originalModelDefinitions.getDrgElement(), parsedModelDefinitions.getDrgElement());
+        checkElements(originalModelDefinitions.getImport(), parsedModelDefinitions.getImport());
+        checkElements(originalModelDefinitions.getItemDefinition(), parsedModelDefinitions.getItemDefinition());
+    }
+
+    static <T extends NamedElement> List<String> checkElements(Collection<T> target, Collection<T> source) {
+        return source.map(sourceElement -> {
+            boolean isAbsent = checkIfAbsent(target, sourceElement);
+            return isAbsent ? "sourceElement " + sourceElement.getName() + "is missing!!!" : true;
+            System.out.println(isAbsent);
+        }).collect(Collectors.toList());
+    }
+
+    static <T extends NamedElement> boolean checkIfAbsent(Collection<T> target, T source) {
+        return target.stream().noneMatch(namedElement -> {
+            System.out.println(namedElement.getName() + " - " + source.getName());
+            return Objects.equals(namedElement.getName(), source.getName());
+        });
     }
 }
