2ab2b8776dde406f949677e6a90d66bc3457f24e
Yeser Amer <yeser.amer@ibm.com>
2024-05-06 23:37:32 +0200

    WIP

--- packages/serverless-logic-web-tools-swf-dev-mode-image/env/index.js
+++ packages/serverless-logic-web-tools-swf-dev-mode-image/env/index.js
@@ -28,7 +28,7 @@ module.exports = composeEnv(
         description: "",
       },
       SERVERLESS_LOGIC_WEB_TOOLS_DEVMODE_IMAGE__kogitoBaseBuilderImageTag: {
-        default: "999-20240417",
+        default: "latest",
         description: "",
       },
     }),
