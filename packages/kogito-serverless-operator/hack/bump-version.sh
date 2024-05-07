79aa789bc1e293041400b410fe38508980425d0a
Thiago Lugli <thiagoelg@gmail.com>
2024-05-07 17:52:56 -0300

    kogito-serverless-opeator: Use environment variables when bumping versions

--- packages/kogito-serverless-operator/hack/bump-version.sh
+++ packages/kogito-serverless-operator/hack/bump-version.sh
@@ -20,48 +20,42 @@ set -e
 script_dir_path=$(dirname "${BASH_SOURCE[0]}")
 source "${script_dir_path}"/env.sh
 
-imageTag=$(pnpm build-env kogitoServerlessOperator.registry)/$(pnpm build-env kogitoServerlessOperator.account)/$(pnpm build-env kogitoServerlessOperator.name)
-# shellcheck disable=SC2034
-old_version=$(getOperatorVersion)
-latest_version=$(getOperatorLatestVersion)
-new_version=$(pnpm build-env kogitoServerlessOperator.version)
+imageName=$(pnpm build-env kogitoServerlessOperator.registry)/$(pnpm build-env kogitoServerlessOperator.account)/$(pnpm build-env kogitoServerlessOperator.name)
+imageTag=$(pnpm build-env kogitoServerlessOperator.tag)
+version=$(pnpm build-env kogitoServerlessOperator.version)
 
-if [ -z "${new_version}" ]; then
+targetKogitoSwfBuilderImage=$(pnpm build-env kogitoServerlessOperator.kogitoSwfBuilderImage)
+targetKogitoSwfDevModeImage=$(pnpm build-env kogitoServerlessOperator.kogitoSwfDevModeImage)
+
+if [ -z "${version}" ]; then
   echo "Please inform the new version"
   exit 1
 fi
 
-imageSuffix=$(if [[ "${new_version}" == 0.0.0 ]]; then echo '-nightly'; else echo ''; fi)
+newMajorMinorVersion=${version%.*}
 
-oldMajorMinorVersion=${old_version%.*}
-newMajorMinorVersion=${new_version%.*}
-if [ "${old_version}" = "${latest_version}" ]; then
-  oldMajorMinorVersion='latest'
-fi
-if [ "${new_version}" = "${latest_version}" ]; then
-  newMajorMinorVersion='latest'
-fi
+targetKogitoServerlessOperatorImage="${imageName}:${imageTag}"
 
-echo "Set new version to ${new_version} (img_suffix = '${imageSuffix}', majorMinor = ${newMajorMinorVersion})"
+echo "Set new version to ${version} (majorMinor = ${newMajorMinorVersion}, imageName:imageTag = ${targetKogitoServerlessOperatorImage})"
 
-
-node -p "require('replace-in-file').sync({ from: /\bVERSION\ \?=.*\b/g, to: 'VERSION ?= ${new_version}', files: ['./Makefile'] });"
+node -p "require('replace-in-file').sync({ from: /\bVERSION\ \?=.*\b/g, to: 'VERSION ?= ${version}', files: ['./Makefile'] });"
 node -p "require('replace-in-file').sync({ from: /\bREDUCED_VERSION\ \?=.*\b/g, to: 'REDUCED_VERSION ?= ${newMajorMinorVersion}', files: ['./Makefile'] });"
-node -p "require('replace-in-file').sync({ from: /\bIMAGE_TAG_BASE\ \?=.*\b/g, to: 'IMAGE_TAG_BASE ?= ${imageTag}${imageSuffix}', files: ['./Makefile'] });"
+node -p "require('replace-in-file').sync({ from: /\bIMAGE_TAG\ \?=.*\b/g, to: 'IMAGE_TAG ?= ${imageTag}', files: ['./Makefile'] });"
+node -p "require('replace-in-file').sync({ from: /\bIMAGE_TAG_BASE\ \?=.*\b/g, to: 'IMAGE_TAG_BASE ?= ${imageName}', files: ['./Makefile'] });"
 
-node -p "require('replace-in-file').sync({ from: /\bnewTag:.*\b/g, to: 'newTag: ${new_version}', files: ['./config/manager/kustomization.yaml'] });"
-node -p "require('replace-in-file').sync({ from: /\bnewName:.*\b/g, to: 'newName: ${imageTag}${imageSuffix}', files: ['./config/manager/kustomization.yaml'] });"
+node -p "require('replace-in-file').sync({ from: /\bnewTag:.*\b/g, to: 'newTag: ${version}', files: ['./config/manager/kustomization.yaml'] });"
+node -p "require('replace-in-file').sync({ from: /\bnewName:.*\b/g, to: 'newName: ${imageName}', files: ['./config/manager/kustomization.yaml'] });"
 
-node -p "require('replace-in-file').sync({ from: /\bversion: .*\b/g, to: 'version: ${new_version}', files: ['./images/bundle.yaml'] });"
-node -p "require('replace-in-file').sync({ from: /\bversion: .*\b/g, to: 'version: ${new_version}', files: ['./images/manager.yaml'] });"
+node -p "require('replace-in-file').sync({ from: /\bversion: .*\b/g, to: 'version: ${version}', files: ['./images/bundle.yaml'] });"
+node -p "require('replace-in-file').sync({ from: /\bversion: .*\b/g, to: 'version: ${version}', files: ['./images/manager.yaml'] });"
 
 # Update kogito-swf-* images
-node -p "require('replace-in-file').sync({ from: 'quay.io/kiegroup/kogito-swf-builder${imageSuffix}:${oldMajorMinorVersion}', to: 'quay.io/kiegroup/kogito-swf-builder${imageSuffix}:${newMajorMinorVersion}', files: ['**/*.yaml', '**/*.containerfile', '**/*.dockerfile', '**/*.Dockerfile', '**/*.go'] });"
-node -p "require('replace-in-file').sync({ from: 'quay.io/kiegroup/kogito-swf-devmode${imageSuffix}:${oldMajorMinorVersion}', to: 'quay.io/kiegroup/kogito-swf-devmode${imageSuffix}:${newMajorMinorVersion}', files: ['**/*.yaml', '**/*.containerfile', '**/*.dockerfile', '**/*.Dockerfile', '**/*.go'] });"
-node -p "require('replace-in-file').sync({ from: 'quay.io/kiegroup/kogito-serverless-operator${imageSuffix}:${oldMajorMinorVersion}', to: 'quay.io/kiegroup/kogito-serverless-operator${imageSuffix}:${newMajorMinorVersion}', files: ['**/*.yaml'] });"
+node -p "require('replace-in-file').sync({ from: '/quay.io\/kiegroup\/kogito-swf-builder.*/g', to: '${targetKogitoSwfBuilderImage}', files: ['**/*.yaml', '**/*.containerfile', '**/*.dockerfile', '**/*.Dockerfile', '**/*.go'] });"
+node -p "require('replace-in-file').sync({ from: '/quay.io\/kiegroup\/kogito-swf-devmode.*/g', to: '${targetKogitoSwfDevModeImage}', files: ['**/*.yaml', '**/*.containerfile', '**/*.dockerfile', '**/*.Dockerfile', '**/*.go'] });"
+node -p "require('replace-in-file').sync({ from: '/quay.io\/kiegroup\/kogito-serverless-operator.*/g', to: '${targetKogitoServerlessOperatorImage}', files: ['**/*.yaml'] });"
 
-node -p "require('replace-in-file').sync({ from: /\bOperatorVersion = .*/g, to: 'OperatorVersion = \"${new_version}\"', files: ['version/version.go'] });"
-node -p "require('replace-in-file').sync({ from: /\bcontainerImage:.*\b/g, to: 'containerImage: ${imageTag}${imageSuffix}:${newMajorMinorVersion}', files: ['$(getCsvFile)'] });"
+node -p "require('replace-in-file').sync({ from: /\bOperatorVersion = .*/g, to: 'OperatorVersion = \"${version}\"', files: ['version/version.go'] });"
+node -p "require('replace-in-file').sync({ from: /\bcontainerImage:.*\b/g, to: 'containerImage: ${targetKogitoServerlessOperatorImage}', files: ['$(getCsvFile)'] });"
 
 make generate-all
 make vet
