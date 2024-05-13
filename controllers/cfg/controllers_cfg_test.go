6db8d3417b9f89f8fd25e44972afadacda7312ce
Walter Medvedeo <wmedvedeo@gmail.com>
2024-05-13 18:25:24 +0200

    kie-kogito-serverless-operator-459: Update operator managed persistence extensions to quarkus 3.8.4 (#460)

--- controllers/cfg/controllers_cfg_test.go
+++ controllers/cfg/controllers_cfg_test.go
@@ -37,13 +37,13 @@ func TestInitializeControllersCfgAt_ValidFile(t *testing.T) {
 	assert.Equal(t, GAV{
 		GroupId:    "io.quarkus",
 		ArtifactId: "quarkus-jdbc-postgresql",
-		Version:    "3.2.10.Final",
+		Version:    "3.8.4",
 	}, postgresExtensions[0])
 
 	assert.Equal(t, GAV{
 		GroupId:    "io.quarkus",
 		ArtifactId: "quarkus-agroal",
-		Version:    "3.2.10.Final",
+		Version:    "3.8.4",
 	}, postgresExtensions[1])
 
 	assert.Equal(t, GAV{
