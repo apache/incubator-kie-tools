6ffb903d3777c863fcc0c9c90f2dd10bcfd94ff7
Thiago Lugli <thiagoelg@gmail.com>
2024-05-17 19:14:07 -0300

    Oops

--- packages/kie-sandbox-helm-chart/scripts/update-readmes.js
+++ packages/kie-sandbox-helm-chart/scripts/update-readmes.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 // Install helm-docs if not installed
 if (process.platform === "win32") {
-  execSync("where /q helm-docs || go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.31.1");
+  execSync("where /q helm-docs || go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.13.1");
 } else {
-  execSync("which helm-docs || go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.31.1");
+  execSync("which helm-docs || go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.13.1");
 }
 
 const srcPath = path.resolve(path.join(__dirname, "../src"));
