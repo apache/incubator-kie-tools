5c024ecdfd71f81f4348a70d0290d688b5498800
Thiago Lugli <thiagoelg@gmail.com>
2024-05-17 16:11:57 -0300

    update-readmes script for kie-sandbox-helm-chart

---
+++ packages/kie-sandbox-helm-chart/scripts/update-readmes.js
@@ -0,0 +1,15 @@
+const { execSync } = require("child_process");
+const path = require("path");
+
+// Install helm-docs if not installed
+if (process.platform === "win32") {
+  execSync("where /q helm-docs || go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.31.1");
+} else {
+  execSync("which helm-docs || go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.31.1");
+}
+
+const srcPath = path.resolve(path.join(__dirname, "../src"));
+
+console.log(srcPath);
+
+execSync(`helm-docs --document-dependency-values=true -c ${srcPath}`);
