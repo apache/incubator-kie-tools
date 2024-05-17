534e0b83f0c70647b474d2ccfde8b478f1a3cbb8
Thiago Lugli <thiagoelg@gmail.com>
2024-05-17 17:35:33 -0300

    Add header to helm-chart READMEs

--- packages/kie-sandbox-helm-chart/scripts/update-readmes.js
+++ packages/kie-sandbox-helm-chart/scripts/update-readmes.js
@@ -9,7 +9,8 @@ if (process.platform === "win32") {
 }
 
 const srcPath = path.resolve(path.join(__dirname, "../src"));
+const readmeTemplatePath = path.resolve(path.join(__dirname, "../src/templates/README.md.gotmpl"));
 
 console.log(srcPath);
 
-execSync(`helm-docs --document-dependency-values=true -c ${srcPath}`);
+execSync(`helm-docs --document-dependency-values=true -c ${srcPath} -t ${readmeTemplatePath}`);
