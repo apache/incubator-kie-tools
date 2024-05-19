493c10a3c5a4f97ed03158232407e845a7d62f6e
Thiago Lugli <thiagoelg@gmail.com>
2024-05-19 17:52:33 -0300

    Fix script on windows

--- packages/kie-sandbox-helm-chart/scripts/update-readmes.js
+++ packages/kie-sandbox-helm-chart/scripts/update-readmes.js
@@ -10,18 +10,17 @@ if (process.platform === "win32") {
   execSync("which helm-docs || go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.13.1");
 }
 
-const packageRootPath = path.resolve(path.join(__dirname, ".."));
-const srcPath = path.join(packageRootPath, "src");
-const readmeTemplatePath = path.join(packageRootPath, "src/templates/README.md.gotmpl");
+const packageRootPath = path.join(__dirname, "..");
+const srcPath = path.join(packageRootPath, "./src");
 
 console.log(srcPath);
 
 console.log("[kie-sandbox-helm-chart scripts/update-readmes.js] Updating Charts README.md files...");
-execSync(`helm-docs --document-dependency-values=true -c ${srcPath} -t ${readmeTemplatePath}`);
+execSync(`cd ${srcPath} && helm-docs --document-dependency-values=true -t templates/README.md.gotmpl`);
 
 console.log("[kie-sandbox-helm-chart scripts/update-readmes.js] Updating package README.md file");
-const srcReadmePath = path.join(packageRootPath, "src/README.md");
-const rootReadmePath = path.join(packageRootPath, "README.md");
+const srcReadmePath = path.resolve(path.join(packageRootPath, "./src/README.md"));
+const rootReadmePath = path.resolve(path.join(packageRootPath, "./README.md"));
 const chartReadmeSections = fs.readFileSync(srcReadmePath).toString().split("## Values");
 const readmeSections = fs.readFileSync(rootReadmePath).toString().split("<!-- CHART_VALUES_README -->");
 readmeSections[1] = chartReadmeSections[1];
