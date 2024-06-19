98bf023980902c70fee1f43f816369fe3942f818
Pere Fernández <pefernan@users.noreply.github.com>
2024-06-19 15:56:05 +0200

    incubator-kie-issues#1338: 'Thread blocked' Exception in console when starting 'jbpm-quarkus-devui' & 'sonataflow-quarkus-devui'

--- packages/jbpm-quarkus-devui/jbpm-quarkus-devui-runtime/src/main/java/org/jbpm/quarkus/devui/runtime/rpc/JBPMDevuiJsonRPCService.java
+++ packages/jbpm-quarkus-devui/jbpm-quarkus-devui-runtime/src/main/java/org/jbpm/quarkus/devui/runtime/rpc/JBPMDevuiJsonRPCService.java
@@ -19,61 +19,95 @@
 
 package org.jbpm.quarkus.devui.runtime.rpc;
 
-import java.util.function.Supplier;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.jbpm.quarkus.devui.runtime.dataindex.DataIndexClient;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import jakarta.annotation.PostConstruct;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jbpm.quarkus.devui.runtime.forms.FormsStorage;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.smallrye.common.annotation.NonBlocking;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class JBPMDevuiJsonRPCService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JBPMDevuiJsonRPCService.class);
+
+    public static final String PROCESS_INSTANCES = "ProcessInstances";
+    public static final String USER_TASKS = "UserTaskInstances";
+    public static final String JOBS = "Jobs";
+
     public static final String ALL_TASKS_IDS_QUERY = "{ \"operationName\": \"getAllTasksIds\", \"query\": \"query getAllTasksIds{  UserTaskInstances{ id } }\" }";
     public static final String ALL_PROCESS_INSTANCES_IDS_QUERY = "{ \"operationName\": \"getAllProcessesIds\", \"query\": \"query getAllProcessesIds{  ProcessInstances{ id } }\" }";
     public static final String ALL_JOBS_IDS_QUERY = "{ \"operationName\": \"getAllJobsIds\", \"query\": \"query getAllJobsIds{  Jobs{ id } }\" }";
 
-    private final ObjectMapper mapper;
-    private final DataIndexClient dataIndexClient;
+    private WebClient dataIndexWebClient;
+
+    private final String dataIndexURL;
+    private final Vertx vertx;
     private final FormsStorage formsStorage;
 
     @Inject
-    public JBPMDevuiJsonRPCService(ObjectMapper mapper, @RestClient DataIndexClient dataIndexClient, FormsStorage formsStorage) {
-        this.mapper = mapper;
-        this.dataIndexClient = dataIndexClient;
+    public JBPMDevuiJsonRPCService(@ConfigProperty(name = "kogito.data-index.url") String dataIndexURL, Vertx vertx, FormsStorage formsStorage) {
+        this.dataIndexURL = dataIndexURL;
+        this.vertx = vertx;
         this.formsStorage = formsStorage;
     }
 
-    @NonBlocking
-    public String queryProcessInstancesCount() {
-        return doQuery(() -> this.dataIndexClient.queryProcessInstances(ALL_PROCESS_INSTANCES_IDS_QUERY).getData().getProcessInstances().size());
-    }
-
-    @NonBlocking
-    public String queryTasksCount() {
-        return doQuery(() -> this.dataIndexClient.queryTasks(ALL_TASKS_IDS_QUERY).getData().getTasks().size());
-    }
-
-    @NonBlocking
-    public String queryJobsCount() {
-        return doQuery(() -> this.dataIndexClient.queryJobs(ALL_JOBS_IDS_QUERY).getData().getJobs().size());
-    }
-
-    @NonBlocking
-    public String getFormsCount() {
-        return doQuery(this.formsStorage::getFormsCount);
-    }
-
-    private String doQuery(Supplier<Integer> countSupplier) {
+    @PostConstruct
+    public void init() {
         try {
-            return String.valueOf(countSupplier.get());
+            this.dataIndexWebClient = WebClient.create(vertx, buildWebClientOptions());
         } catch (Exception ex) {
-            return "-";
+            LOGGER.warn("Cannot configure dataIndexWebClient with 'kogito.data-index.url'='{}':", dataIndexURL, ex);
         }
+    }
+
+    protected WebClientOptions buildWebClientOptions() throws MalformedURLException {
+        URL dataIndexURL = new URL(this.dataIndexURL);
+        return new WebClientOptions()
+                .setDefaultHost(dataIndexURL.getHost())
+                .setDefaultPort((dataIndexURL.getPort() != -1 ? dataIndexURL.getPort() : dataIndexURL.getDefaultPort()))
+                .setSsl(dataIndexURL.getProtocol().compareToIgnoreCase("https") == 0);
+    }
+
+    public Uni<String> queryProcessInstancesCount() {
+        return doQuery(ALL_PROCESS_INSTANCES_IDS_QUERY, PROCESS_INSTANCES);
+    }
+
+    public Uni<String> queryTasksCount() {
+        return doQuery(ALL_TASKS_IDS_QUERY, USER_TASKS);
+    }
+
+    public Uni<String> queryJobsCount() {
+        return doQuery(ALL_JOBS_IDS_QUERY, JOBS);
+    }
+
+    private Uni<String> doQuery(String query, String graphModelName) {
+        if(dataIndexWebClient == null) {
+            return Uni.createFrom().item("-");
+        }
+        return Uni.createFrom().completionStage(this.dataIndexWebClient.post("/graphql")
+                .putHeader("content-type", "application/json")
+                .sendJson(new JsonObject(query))
+                .map(response -> {
+                    if(response.statusCode() == 200) {
+                        JsonObject responseData = response.bodyAsJsonObject().getJsonObject("data");
+                        return String.valueOf(responseData.getJsonArray(graphModelName).size());
+                    }
+                    return "-";
+                }).toCompletionStage());
+    }
+
+    public Uni<String> getFormsCount() {
+        return Uni.createFrom().item(String.valueOf(this.formsStorage.getFormsCount()));
     }
 }
--- packages/sonataflow-quarkus-devui/sonataflow-quarkus-devui/src/main/java/org/kie/sonataflow/swf/tools/runtime/rpc/SonataFlowQuarkusExtensionJsonRPCService.java
+++ packages/sonataflow-quarkus-devui/sonataflow-quarkus-devui/src/main/java/org/kie/sonataflow/swf/tools/runtime/rpc/SonataFlowQuarkusExtensionJsonRPCService.java
@@ -16,31 +16,76 @@
 
 package org.kie.sonataflow.swf.tools.runtime.rpc;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.function.Supplier;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.smallrye.common.annotation.NonBlocking;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.kie.sonataflow.swf.tools.runtime.dataindex.DataIndexClient;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class SonataFlowQuarkusExtensionJsonRPCService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SonataFlowQuarkusExtensionJsonRPCService.class);
+
+    public static final String PROCESS_INSTANCES = "ProcessInstances";
+
     public static final String ALL_WORKFLOWS_IDS_QUERY = "{ \"operationName\": \"getAllProcessesIds\", \"query\": \"query getAllProcessesIds{  ProcessInstances{ id } }\" }";
 
-    private final ObjectMapper mapper;
-    private final DataIndexClient dataIndexClient;
+    private WebClient dataIndexWebClient;
+
+    private final String dataIndexURL;
+    private final Vertx vertx;
 
     @Inject
-    public SonataFlowQuarkusExtensionJsonRPCService(ObjectMapper mapper, @RestClient DataIndexClient dataIndexClient) {
-        this.mapper = mapper;
-        this.dataIndexClient = dataIndexClient;
+    public SonataFlowQuarkusExtensionJsonRPCService(@ConfigProperty(name = "kogito.data-index.url") String dataIndexURL, Vertx vertx) {
+        this.dataIndexURL = dataIndexURL;
+        this.vertx = vertx;
     }
 
-    @NonBlocking
-    public String queryWorkflowsCount() {
-        return doQuery(() -> this.dataIndexClient.queryWorkflows(ALL_WORKFLOWS_IDS_QUERY).getData().getWorkflows().size());
+    @PostConstruct
+    public void init() {
+        try {
+            this.dataIndexWebClient = WebClient.create(vertx, buildWebClientOptions());
+        } catch (Exception ex) {
+            LOGGER.warn("Cannot configure dataIndexWebClient with 'kogito.data-index.url'='{}':", dataIndexURL, ex);
+        }
+    }
+
+    protected WebClientOptions buildWebClientOptions() throws MalformedURLException {
+        URL dataIndexURL = new URL(this.dataIndexURL);
+        return new WebClientOptions()
+                .setDefaultHost(dataIndexURL.getHost())
+                .setDefaultPort((dataIndexURL.getPort() != -1 ? dataIndexURL.getPort() : dataIndexURL.getDefaultPort()))
+                .setSsl(dataIndexURL.getProtocol().compareToIgnoreCase("https") == 0);
+    }
+
+    public Uni<String> queryWorkflowsCount() {
+        return doQuery(ALL_WORKFLOWS_IDS_QUERY, PROCESS_INSTANCES);
+    }
+
+    private Uni<String> doQuery(String query, String graphModelName) {
+        if(dataIndexWebClient == null) {
+            return Uni.createFrom().item("-");
+        }
+        return Uni.createFrom().completionStage(this.dataIndexWebClient.post("/graphql")
+                .putHeader("content-type", "application/json")
+                .sendJson(new JsonObject(query))
+                .map(response -> {
+                    if(response.statusCode() == 200) {
+                        JsonObject responseData = response.bodyAsJsonObject().getJsonObject("data");
+                        return String.valueOf(responseData.getJsonArray(graphModelName).size());
+                    }
+                    return "-";
+                }).toCompletionStage());
     }
 
     private String doQuery(Supplier<Integer> countSupplier) {
