911de1baf24f377d2598b3f52032d49d27a75b89
Jozef Marko <jozef.marko@ibm.com>
2024-08-05 17:11:49 +0200

    add skip and info

--- packages/dmn-editor/tests-e2e/drds/modelDecisionServicesInDrds.spec.ts
+++ packages/dmn-editor/tests-e2e/drds/modelDecisionServicesInDrds.spec.ts
@@ -17,6 +17,7 @@
  * under the License.
  */
 
+import { TestAnnotations } from "@kie-tools/playwright-base/annotations";
 import { test } from "../__fixtures__/base";
 
 test.beforeEach(async ({ editor }) => {
@@ -27,23 +28,43 @@ test.describe("Model Decision Services - DRD", () => {
   //TODO Extract scenarios from https://github.com/apache/incubator-kie-issues/issues/870
 
   test("888 add to DRD, all DRDs depiction same", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/888");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/888",
+    });
     // create non empty DS
     // add the ^ DS into non default DRD
     // all the DS content needs to be added into DRD also
   });
 
   test("888 add to DRD, automatically layout", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/888");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/888",
+    });
     // add decision service, non empty, that is currently in no DRD
     // its content should be automatically layout-ed
   });
 
   test("888 add to DRD, mix of collapsed and expanded", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/888");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/888",
+    });
     // However, if one of its contained Decisions it also contained by other Decision Services
     // present on that DRD, and one of them is in expanded form,
     // the Decision Service is added as collapsed, and no Decisions are added or moved.
   });
 
   test("888 add to DRD, collapsed", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/888");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/888",
+    });
     // prepare on empty DS
     // add some of its content to different DRD
     // collapse the DS
@@ -52,80 +73,170 @@ test.describe("Model Decision Services - DRD", () => {
   });
 
   test("894 do not allow add node that is already in DS and in DRD expanded", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/894");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/894",
+    });
     // On the DMN Editor, do not allow adding a Decision to a DRD
     // if this Decision is contained by one or more Decision Services already present
     // on that DRD, even if in collapsed state.
   });
 
   test("894 do not allow add node that is already in DS and in DRD collapsed", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/894");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/894",
+    });
     // On the DMN Editor, do not allow adding a Decision to a DRD
     // if this Decision is contained by one or more Decision Services already present
     // on that DRD, even if in collapsed state.
   });
 
   test("972 autolayout", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/972");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/972",
+    });
     // TODO, question, not understand
   });
 
-  test.skip("892 collapse - remove its content", async ({ drds }) => {
+  test("892 collapse - remove its content", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/892");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/892",
+    });
     // TODO
   });
 
-  test.skip("892 collapse - allow only if no other DS is expanded in the DRD", async ({ drds }) => {
+  test("892 collapse - allow only if no other DS is expanded in the DRD", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/892");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/892",
+    });
     // TODO
   });
 
-  test.skip("892 expand - same depiction as other DRDs", async ({ drds }) => {
+  test("892 expand - same depiction as other DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/892");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/892",
+    });
     // TODO
   });
 
-  test.skip("892 expand - allow only if no other DS is collpased in the DRD", async ({ drds }) => {
+  test("892 expand - allow only if no other DS is collpased in the DRD", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/892");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/892",
+    });
     // TODO
   });
 
-  test.skip("889 move DS content and replicate in DRDs", async ({ drds }) => {
+  test("889 move DS content and replicate in DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/889");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/889",
+    });
     // TODO
   });
 
-  test.skip("889 resize DS content and replicate in DRDs", async ({ drds }) => {
+  test("889 resize DS content and replicate in DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/889");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/889",
+    });
     // TODO
   });
 
-  test.skip("898 move DS divider and replicate in DRDs", async ({ drds }) => {
+  test("898 move DS divider and replicate in DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/898");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/898",
+    });
     // TODO
   });
 
-  test.skip("890 add node to DS, check its added to all DRDs where DS in expanded form", async ({ drds }) => {
+  test("890 add node to DS, check its added to all DRDs where DS in expanded form", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/890");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/890",
+    });
     // TODO
   });
 
-  test.skip("890 add node to DS, check its removed from DRDs where DS in collapsed form", async ({ drds }) => {
+  test("890 add node to DS, check its removed from DRDs where DS in collapsed form", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/890");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/890",
+    });
     // TODO
   });
 
-  test.skip("891 remove node from DS, check its removed form DS in all DRDs", async ({ drds }) => {
+  test("891 remove node from DS, check its removed form DS in all DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/891");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/891",
+    });
     // TODO, question, isn't this the same as 889 - syncing depiction of DS in all DRDs?
   });
 
-  test.skip("893 collapsed external DS, synced in all DRDs", async ({ drds }) => {
+  test("893 collapsed external DS, synced in all DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/893");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/893",
+    });
     // TODO
   });
 
-  test.skip("893 expanded external DS, synced in all DRDs", async ({ drds }) => {
+  test("893 expanded external DS, synced in all DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/893");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/893",
+    });
     // TODO
   });
 
-  test.skip("893 remove node from DS, check its removed form DS in all DRDs", async ({ drds }) => {
+  test("893 remove node from DS, check its removed form DS in all DRDs", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/893");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/893",
+    });
     // TODO
   });
 
-  test.skip("895 ", async ({ drds }) => {
+  test("895 ", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/895");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/895",
+    });
     // TODO , Question, isn't that in conflict with other DRD tickets?
     // is that even possible to be in state DS_One is collapsed
     // DS_Two is expanded and both of them contain the same decision?
   });
 
-  test.skip("1298 ", async ({ drds }) => {
+  test("1298 ", async ({ drds }) => {
+    test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/1298");
+    test.info().annotations.push({
+      type: TestAnnotations.REGRESSION,
+      description: "https://github.com/apache/incubator-kie-issues/issues/1298",
+    });
     // TODO
   });
 });
