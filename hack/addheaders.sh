42b886ccacf0e36b0e175416d58af3dde909c88b
Walter Medvedeo <wmedvedeo@gmail.com>
2024-10-10 18:31:18 +0200

    kie-tools#2650: SonataFlow operator apache-rat tool suggestions and license header fixes (#549)

--- hack/addheaders.sh
+++ hack/addheaders.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -7,7 +8,7 @@
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
@@ -15,10 +16,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+#
 
 if ! hash addlicense 2>/dev/null; then
   go install github.com/google/addlicense@latest
 fi
 
 # shellcheck disable=SC2035
-addlicense -c "Apache Software Foundation (ASF)" -l=apache -ignore=test/samples/*.yaml -ignore=container-builder/examples/**/*.yaml images hack api bddframework internal utils test testbdd container-builder workflowproj
+addlicense -f=hack/license-header.txt -ignore=test/samples/*.yaml -ignore=container-builder/examples/**/*.yaml images hack api bddframework internal utils test testbdd container-builder workflowproj bundle config ./operator.yaml
