6db45cd434b8081283b21974cebe46f12b266325
bncriju <bincy.riju@ibm.com>
2024-10-18 11:59:08 +0530

    Incorporated suggestion

--- packages/runtime-tools-process-dev-ui-webapp/src/components/containers/ProcessFormContainer/ProcessFormContainer.tsx
+++ packages/runtime-tools-process-dev-ui-webapp/src/components/containers/ProcessFormContainer/ProcessFormContainer.tsx
@@ -57,9 +57,7 @@ const ProcessFormContainer: React.FC<ProcessFormContainerProps & OUIAProps> = ({
               onSubmitSuccess(id);
             })
             .catch((error) => {
-              const message = error.response
-                ? ` ${error.message}   ' ${error.response.request.statusText} '`
-                : error.message;
+              const message = error.response ? `${error.response.statusText} : ${error.message}` : error.message;
               onSubmitError(message);
             });
         },
