b6826b53a87d8fa334ebc7c403f8081f66eaaf29
Tiago Bento <tiagobentofernandes@gmail.com>
2024-11-05 18:34:41 -0500

    Oops

--- examples/uniforms-patternfly/src/index.tsx
+++ examples/uniforms-patternfly/src/index.tsx
@@ -21,6 +21,6 @@ import "@patternfly/react-core/dist/styles/base.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./App";
-import "../static/resources/styles.css";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
