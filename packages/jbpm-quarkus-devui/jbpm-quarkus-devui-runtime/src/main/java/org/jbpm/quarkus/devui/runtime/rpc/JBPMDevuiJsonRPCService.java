0832d4efad0cba9bc8cae1b9993aace15b21000f
bncriju <bincy.riju@ibm.com>
2025-01-09 19:43:39 +0530

    kie-issues#1669: jBPM Quarkus DevUI seems to not update the # of items (#2814)
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>
    Signed-off-by: Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
    Co-authored-by: Jozef Marko <Jozef.Marko@ibm.com>
    Co-authored-by: Luiz João Motta <lmotta@ibm.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
    Co-authored-by: Deepak Joseph <159427631+josedee@users.noreply.github.com>
    Co-authored-by: Pere Fernández <pefernan@users.noreply.github.com>

--- packages/jbpm-quarkus-devui/jbpm-quarkus-devui-runtime/src/main/java/org/jbpm/quarkus/devui/runtime/rpc/JBPMDevuiJsonRPCService.java
+++ packages/jbpm-quarkus-devui/jbpm-quarkus-devui-runtime/src/main/java/org/jbpm/quarkus/devui/runtime/rpc/JBPMDevuiJsonRPCService.java
@@ -24,11 +24,14 @@ import java.net.URL;
 import java.util.Optional;
 
 import io.smallrye.mutiny.Uni;
+import io.quarkus.arc.profile.IfBuildProfile;
+import io.smallrye.mutiny.Multi;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jbpm.quarkus.devui.runtime.forms.FormsStorage;
 
@@ -38,6 +41,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
+@IfBuildProfile("dev")
 public class JBPMDevuiJsonRPCService {
     private static final String DATA_INDEX_URL = "kogito.data-index.url";
 
@@ -54,11 +58,16 @@ public class JBPMDevuiJsonRPCService {
     private WebClient dataIndexWebClient;
 
     private final Vertx vertx;
+    private final JBPMDevUIEventPublisher eventPublisher;
     private final FormsStorage formsStorage;
+    private DataIndexCounter processesCounter;
+    private DataIndexCounter tasksCounter;
+    private DataIndexCounter jobsCounter;
 
     @Inject
-    public JBPMDevuiJsonRPCService(Vertx vertx, FormsStorage formsStorage) {
+    public JBPMDevuiJsonRPCService(Vertx vertx, JBPMDevUIEventPublisher eventPublisher, FormsStorage formsStorage) {
         this.vertx = vertx;
+        this.eventPublisher = eventPublisher;
         this.formsStorage = formsStorage;
     }
 
@@ -70,50 +79,50 @@ public class JBPMDevuiJsonRPCService {
 
     private void initDataIndexWebClient(String dataIndexURL) {
         try {
-            this.dataIndexWebClient = WebClient.create(vertx, buildWebClientOptions(dataIndexURL));
+            URL url = new URL(dataIndexURL);
+            this.dataIndexWebClient = WebClient.create(vertx, buildWebClientOptions(url));
+
+            String contextPath = url.getPath();
+            this.processesCounter = new DataIndexCounter(ALL_PROCESS_INSTANCES_IDS_QUERY, PROCESS_INSTANCES,
+                    contextPath, vertx, dataIndexWebClient);
+            this.tasksCounter = new DataIndexCounter(ALL_TASKS_IDS_QUERY, USER_TASKS, contextPath, vertx, dataIndexWebClient);
+            this.jobsCounter = new DataIndexCounter(ALL_JOBS_IDS_QUERY, JOBS, contextPath, vertx, dataIndexWebClient);
+
+            this.eventPublisher.setOnProcessEventListener(processesCounter::refresh);
+            this.eventPublisher.setOnTaskEventListener(tasksCounter::refresh);
+            this.eventPublisher.setOnJobEventListener(jobsCounter::refresh);
         } catch (Exception ex) {
             LOGGER.warn("Cannot configure dataIndexWebClient with 'kogito.data-index.url'='{}':", dataIndexURL, ex);
         }
     }
 
-    protected WebClientOptions buildWebClientOptions(String dataIndexURL) throws MalformedURLException {
-        URL url = new URL(dataIndexURL);
+    protected WebClientOptions buildWebClientOptions(URL dataIndexURL) throws MalformedURLException {
         return new WebClientOptions()
-                .setDefaultHost(url.getHost())
-                .setDefaultPort((url.getPort() != -1 ? url.getPort() : url.getDefaultPort()))
-                .setSsl(url.getProtocol().compareToIgnoreCase("https") == 0);
+                .setDefaultHost(dataIndexURL.getHost())
+                .setDefaultPort((dataIndexURL.getPort() != -1 ? dataIndexURL.getPort() : dataIndexURL.getDefaultPort()))
+                .setSsl(dataIndexURL.getProtocol().compareToIgnoreCase("https") == 0);
     }
 
-    public Uni<String> queryProcessInstancesCount() {
-        return doQuery(ALL_PROCESS_INSTANCES_IDS_QUERY, PROCESS_INSTANCES);
+    public Multi<String> queryProcessInstancesCount() {
+        return processesCounter.getMulti();
     }
 
-    public Uni<String> queryTasksCount() {
-        return doQuery(ALL_TASKS_IDS_QUERY, USER_TASKS);
+    public Multi<String> queryTasksCount() {
+        return tasksCounter.getMulti();
     }
 
-    public Uni<String> queryJobsCount() {
-        return doQuery(ALL_JOBS_IDS_QUERY, JOBS);
-    }
-
-    private Uni<String> doQuery(String query, String graphModelName) {
-        if(dataIndexWebClient == null) {
-            LOGGER.warn("Cannot perform '{}' query, dataIndexWebClient couldn't be set. Is DataIndex correctly? Please verify '{}' value", graphModelName, DATA_INDEX_URL);
-            return Uni.createFrom().item("-");
-        }
-        return Uni.createFrom().completionStage(this.dataIndexWebClient.post("/graphql")
-                .putHeader("content-type", "application/json")
-                .sendJson(new JsonObject(query))
-                .map(response -> {
-                    if(response.statusCode() == 200) {
-                        JsonObject responseData = response.bodyAsJsonObject().getJsonObject("data");
-                        return String.valueOf(responseData.getJsonArray(graphModelName).size());
-                    }
-                    return "-";
-                }).toCompletionStage());
+    public Multi<String> queryJobsCount() {
+        return jobsCounter.getMulti();
     }
 
     public Uni<String> getFormsCount() {
         return Uni.createFrom().item(String.valueOf(this.formsStorage.getFormsCount()));
+    }
+
+    @PreDestroy
+    public void destroy() {
+        processesCounter.stop();
+        tasksCounter.stop();
+        jobsCounter.stop();
     }
 }
