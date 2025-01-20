51407ee45a28ef1637a344bde59fa3a395499c9d
Thiago Lugli <thiagoelg@users.noreply.github.com>
2025-01-20 12:40:47 -0300

    NO-ISSUE:  Fix for daily-dev Jenkins CI failing to build due to missing git config and hard-linked files (#2861)

--- packages/sonataflow-management-console-image/resources/modules/sonataflow/management-console/added/entrypoint.sh
+++ packages/sonataflow-management-console-image/resources/modules/sonataflow/management-console/added/entrypoint.sh
@@ -19,7 +19,7 @@
 #
 set -e
 
-# Copying the Task Console assets here is essential for when the container is running with the readOnlyRootFilesystem flag.
+# Copying the Management Console assets here is essential for when the container is running with the readOnlyRootFilesystem flag.
 # But, just like any other directory modified during runtime, the /var/www/html must be a mounted volume in the container in this case.
 cp -R "${KOGITO_HOME}/management-console/app/"* "${HTTPD_DATA_PATH}/html"
 
