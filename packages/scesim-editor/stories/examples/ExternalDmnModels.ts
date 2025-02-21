b0a5754e8b966db7334c2787d03ff4fa099a3201
Yeser Amer <yeser.amer@ibm.com>
2025-02-21 22:54:30 +0100

    kie-issues#970: Test Scenario Editor: Integrate the `@kie-tools/scesim-editor` component with the DMN Marshaller (#2887)
    Co-authored-by: Luiz João Motta <lmotta@ibm.com>
    Co-authored-by: Jozef Marko <Jozef.Marko@ibm.com>

--- packages/scesim-editor/stories/examples/ExternalDmnModels.ts
+++ packages/scesim-editor/stories/examples/ExternalDmnModels.ts
@@ -1000,3 +1000,39 @@ export const TRAFFIC_VIOLATION = `<?xml version='1.0' encoding='UTF-8'?>
     </dmndi:DMNDiagram>
   </dmndi:DMNDI>
 </dmn:definitions>`;
+
+export const SIMPLE = `<?xml version="1.0" encoding="UTF-8" ?>
+<definitions xmlns="https://www.omg.org/spec/DMN/20230324/MODEL/" xmlns:dmndi="https://www.omg.org/spec/DMN/20230324/DMNDI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:kie="https://kie.org/dmn/extensions/1.0" expressionLanguage="https://www.omg.org/spec/DMN/20230324/FEEL/" namespace="https://kie.org/dmn/_0457D4F2-02B6-4A3D-957F-8F971A8321BC" id="_DE888083-9CD8-44DC-BE0A-0EC85244DE3B" name="DMN_44D5E2A8-EC16-45A8-8797-14373283EF69">
+  <inputData name="Input" id="_926B9E31-3CEA-4A0E-8A66-97F875EFB428">
+    <variable name="Input" id="_9B4713BA-E468-447E-9649-91E108CB2F4F" typeRef="number" />
+  </inputData>
+  <decision name="Decision" id="_FBA6E903-26B7-40FA-99C1-7230A3E61D7D">
+    <variable name="Decision" id="_AC19E28B-2336-46E9-8D5B-62AC2D3BF182" typeRef="boolean" />
+    <informationRequirement id="_6F72FA6A-A6E8-403D-8AB7-192FB474878E">
+      <requiredInput href="#_926B9E31-3CEA-4A0E-8A66-97F875EFB428" />
+    </informationRequirement>
+  </decision>
+  <dmndi:DMNDI>
+    <dmndi:DMNDiagram id="_27FA44E1-3CD0-4B6F-A0A4-1D9862D52B87" name="Default DRD" useAlternativeInputDataShape="false">
+      <di:extension>
+        <kie:ComponentsWidthsExtension>
+          <kie:ComponentWidths />
+        </kie:ComponentsWidthsExtension>
+      </di:extension>
+      <dmndi:DMNShape id="_01BEE78F-F02F-4EC3-9CC8-6A5B69E963A2" dmnElementRef="_926B9E31-3CEA-4A0E-8A66-97F875EFB428" isCollapsed="false" isListedInputData="false">
+        <dc:Bounds x="440" y="240" width="160" height="80" />
+      </dmndi:DMNShape>
+      <dmndi:DMNShape id="_1F89C8D2-9957-497D-BAC3-6FDA237FD2BA" dmnElementRef="_FBA6E903-26B7-40FA-99C1-7230A3E61D7D" isCollapsed="false" isListedInputData="false">
+        <dc:Bounds x="440" y="40" width="160" height="80" />
+      </dmndi:DMNShape>
+      <dmndi:DMNEdge id="_FCC85D46-43CA-4F3A-A91A-8A5638E05213" dmnElementRef="_6F72FA6A-A6E8-403D-8AB7-192FB474878E" sourceElement="_01BEE78F-F02F-4EC3-9CC8-6A5B69E963A2" targetElement="_1F89C8D2-9957-497D-BAC3-6FDA237FD2BA">
+        <di:waypoint x="520" y="280" />
+        <di:waypoint x="520" y="120" />
+      </dmndi:DMNEdge>
+    </dmndi:DMNDiagram>
+  </dmndi:DMNDI>
+</definitions>`;
+
+export const EMPTY = `<?xml version="1.0" encoding="UTF-8" ?>
+<definitions xmlns="https://www.omg.org/spec/DMN/20230324/MODEL/" expressionLanguage="https://www.omg.org/spec/DMN/20230324/FEEL/" namespace="https://kie.org/dmn/_14487CEE-1B30-453E-976D-C11ED911548F" id="_6FEE4554-BE5D-4F30-B523-6DFDA563221A" name="Empty" xmlns:dmndi="https://www.omg.org/spec/DMN/20230324/DMNDI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:kie="https://kie.org/dmn/extensions/1.0" />
+`;
