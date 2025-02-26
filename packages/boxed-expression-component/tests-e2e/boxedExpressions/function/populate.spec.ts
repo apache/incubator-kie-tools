bd2b61715ea20a349f9a67ba5440cf1c513e7f1b
Daniel José dos Santos <djdossantos@live.com>
2025-02-26 10:15:20 -0300

    Tests

--- packages/boxed-expression-component/tests-e2e/boxedExpressions/function/populate.spec.ts
+++ packages/boxed-expression-component/tests-e2e/boxedExpressions/function/populate.spec.ts
@@ -25,11 +25,7 @@ test.describe("Populate Boxed Function", () => {
     await stories.openBoxedFunction();
 
     await page.getByRole("columnheader", { name: "Expression Name (<Undefined>)" }).click();
-    await monaco.fill({
-      monacoParentLocator: page.getByTestId("kie-tools--bee--expression-popover-menu"),
-      content: "Affordability calculation",
-      submit: false,
-    });
+    await page.getByPlaceholder("Expression Name").fill("Affordability calculation");
 
     await page.getByLabel("<Undefined>").click();
     await page.getByRole("option", { name: "boolean" }).click();
