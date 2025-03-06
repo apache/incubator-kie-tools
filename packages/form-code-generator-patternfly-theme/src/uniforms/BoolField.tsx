c243a16dbf5384d20013f6bf238ccf3132ff312d
Luiz João Motta <luizjoaomotta@gmail.com>
2025-03-06 19:20:55 -0300

    NO-ISSUE: `form-code-generator-patternfly-theme` doesn't support nested ListField (#2958)

--- packages/form-code-generator-patternfly-theme/src/uniforms/BoolField.tsx
+++ packages/form-code-generator-patternfly-theme/src/uniforms/BoolField.tsx
@@ -39,8 +39,6 @@ export type BoolFieldProps = HTMLFieldProps<
 const Bool: React.FC<BoolFieldProps> = (props: BoolFieldProps) => {
   const ref: InputReference = getInputReference(props.name, DEFAULT_DATA_TYPE_BOOLEAN);
 
-  const stateCode = getStateCodeFromRef(ref);
-
   const jsxCode = `<FormGroup fieldId='${props.id}'>
     <Checkbox
       isChecked={${props.itemProps?.isListItem ? getListItemValue({ itemProps: props.itemProps, name: props.name }) : ref.stateName}}
@@ -56,8 +54,9 @@ const Bool: React.FC<BoolFieldProps> = (props: BoolFieldProps) => {
     ref,
     pfImports: ["Checkbox", "FormGroup"],
     reactImports: ["useState"],
+    requiredCode: undefined,
     jsxCode,
-    stateCode,
+    stateCode: props.itemProps ? "" : getStateCodeFromRef(ref),
     isReadonly: props.disabled,
   };
 
