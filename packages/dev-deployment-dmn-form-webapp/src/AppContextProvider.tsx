9d0cdad31b44673b55b6f9e223fe39b3e97d55ea
Luiz Motta <luizjoaomotta@gmail.com>
2025-03-25 15:21:56 -0300

    dev-deployment-dmn-form-webapp: Update to React Router v6

--- packages/dev-deployment-dmn-form-webapp/src/AppContextProvider.tsx
+++ packages/dev-deployment-dmn-form-webapp/src/AppContextProvider.tsx
@@ -61,7 +61,7 @@ export function AppContextProvider(props: Props) {
             }
             setData(data);
           })
-          .catch((error: any) => console.error(error))
+          .catch((error: any) => console.error("aaaaaa", error))
           .finally(() => setFetchDone(true));
       },
       [quarkusAppOrigin, quarkusAppPath]
