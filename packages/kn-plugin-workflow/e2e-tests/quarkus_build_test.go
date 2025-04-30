cbb6fe1c000cba3cd0291155838a9540c66f19e4
Dmitrii Tikhomirov <treblereel@users.noreply.github.com>
2025-04-30 14:23:49 -0700

    [NO_ISSUE] restore kn-plugin quarkus tests (#3096)

---
+++ packages/kn-plugin-workflow/e2e-tests/quarkus_build_test.go
@@ -0,0 +1,156 @@
+//go:build e2e_tests
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package e2e_tests
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/apache/incubator-kie-tools/packages/kn-plugin-workflow/pkg/command/quarkus"
+)
+
+var cfgTestInputPrepareQuarkusCreateBuild = CfgTestInputQuarkusCreate{
+	input: quarkus.CreateQuarkusProjectConfig{
+		ProjectName: "new-project",
+	},
+}
+
+type CfgTestInputQuarkusBuild struct {
+	input quarkus.BuildCmdConfig
+}
+
+var cfgTestInputQuarkusBuild_Success = []CfgTestInputQuarkusBuild{
+	{input: quarkus.BuildCmdConfig{
+		Image: "dev.local/my-project",
+	}},
+	{input: quarkus.BuildCmdConfig{
+		Image:      "my-user/my-project:1.0.0",
+		Repository: "other-user",
+		Tag:        "1.0.1",
+	}},
+	{input: quarkus.BuildCmdConfig{
+		Image: "dev.local/my-project",
+		Jib:   true,
+	}},
+	//{input: quarkus.BuildCmdConfig{
+	//	Image:     "dev.local/my-project",
+	//	JibPodman: true,
+	//}},
+	// {input: quarkus.BuildCmdConfig{
+	// 	Image: "dev.local/my-project",
+	// 	Jib:   true,
+	// 	Push:  true,
+	// }},
+	// {input: quarkus.BuildCmdConfig{
+	// 	Image: "dev.local/my-project",
+	// 	Push:  true,
+	// }},
+}
+
+func transformQuarkusBuildCmdCfgToArgs(cfg quarkus.BuildCmdConfig) []string {
+	args := []string{"build"}
+	if cfg.Image != "" {
+		args = append(args, "--image", cfg.Image)
+	}
+	if cfg.ImageName != "" {
+		args = append(args, "--image-name", cfg.ImageName)
+	}
+	if cfg.Registry != "" {
+		args = append(args, "--image-registry", cfg.Registry)
+	}
+	if cfg.Repository != "" {
+		args = append(args, "--image-repository", cfg.Repository)
+	}
+	if cfg.Tag != "" {
+		args = append(args, "--image-tag", cfg.Tag)
+	}
+	if cfg.Jib == true {
+		args = append(args, "--jib")
+	}
+	if cfg.JibPodman == true {
+		args = append(args, "--jib-podman")
+	}
+	if cfg.Push == true {
+		args = append(args, "--push")
+	}
+	if cfg.Test == true {
+		args = append(args, "--test")
+	}
+	return args
+}
+
+func TestQuarkusBuildCommand(t *testing.T) {
+	for testIndex, test := range cfgTestInputQuarkusBuild_Success {
+		t.Run(fmt.Sprintf("Test build project success index: %d", testIndex), func(t *testing.T) {
+			defer CleanUpAndChdirTemp(t)
+			RunQuarkusBuildTest(t, cfgTestInputPrepareQuarkusCreateBuild, test, true)
+		})
+	}
+}
+
+func RunQuarkusBuildTest(t *testing.T, cfgTestInputQuarkusCreate CfgTestInputQuarkusCreate, test CfgTestInputQuarkusBuild, cleanUp bool) string {
+	var err error
+
+	// Create the project
+	projectName := RunQuarkusCreateTest(t, cfgTestInputQuarkusCreate)
+	projectDir := filepath.Join(TempTestsPath, projectName)
+
+	err = os.Chdir(projectDir)
+	require.NoErrorf(t, err, "Expected nil error, got %v", err)
+	WriteMavenConfigFileWithTailDirs(projectDir)
+
+	// Run `quarkus build` command
+	args := transformQuarkusBuildCmdCfgToArgs(test.input)
+	_, err = ExecuteKnWorkflowQuarkus(args...)
+	require.NoErrorf(t, err, "Expected nil error, got %v", err)
+
+	require.FileExists(t, filepath.Join("target", "kubernetes", "knative.yml"))
+
+	// Clean up images from docker and podman
+	if cleanUp {
+		CleanUpDockerPodman(t, test)
+	}
+
+	return projectName
+}
+
+func CleanUpDockerPodman(t *testing.T, test CfgTestInputQuarkusBuild) {
+	var err error
+	expectedImageName := ExpectedImageName(test.input)
+	var removeCmd *exec.Cmd
+	if test.input.JibPodman {
+		// Remove built image from podman
+		removeCmd = exec.Command("podman", "image", "rm", expectedImageName) // podman only takes `rm` for removing images
+	} else {
+		// Remove built image from docker
+		removeCmd = exec.Command("docker", "image", "rm", expectedImageName) // docker takes both `rm` and `remove` for removing images
+	}
+	t.Log("Removing image:", removeCmd.Args)
+	out, err := removeCmd.Output()
+	fmt.Print(string(out))
+	require.NoErrorf(t, err, "Error when removing image: %s. Expected nil error, got %v", expectedImageName, err)
+}
---
+++ packages/kn-plugin-workflow/e2e-tests/quarkus_create_test.go
@@ -0,0 +1,168 @@
+//go:build e2e_tests
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package e2e_tests
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/apache/incubator-kie-tools/packages/kn-plugin-workflow/pkg/command/quarkus"
+	"github.com/apache/incubator-kie-tools/packages/kn-plugin-workflow/pkg/common"
+	"github.com/apache/incubator-kie-tools/packages/kn-plugin-workflow/pkg/metadata"
+)
+
+type CfgTestInputQuarkusCreate struct {
+	input quarkus.CreateQuarkusProjectConfig
+}
+
+var cfgTestInputQuarkusCreate_Success = []CfgTestInputQuarkusCreate{
+	{input: quarkus.CreateQuarkusProjectConfig{}},
+	{input: quarkus.CreateQuarkusProjectConfig{
+		ProjectName: "my-project",
+	}},
+	{input: quarkus.CreateQuarkusProjectConfig{
+		Extensions: "quarkus-jsonp,quarkus-smallrye-openapi",
+	}},
+	{input: quarkus.CreateQuarkusProjectConfig{
+		ProjectName: "serverless-workflow-hello-world",
+		Extensions:  "quarkus-jsonp,quarkus-smallrye-openapi",
+		DependenciesVersion: metadata.DependenciesVersion{
+			QuarkusPlatformGroupId: quarkusDependencies.QuarkusPlatformGroupId,
+			QuarkusVersion:         quarkusDependencies.QuarkusVersion,
+		},
+	}},
+}
+
+var cfgTestInputQuarkusCreate_Fail = []CfgTestInputQuarkusCreate{
+	{input: quarkus.CreateQuarkusProjectConfig{
+		ProjectName: "wrong/project-name",
+	}},
+	{input: quarkus.CreateQuarkusProjectConfig{
+		Extensions: "nonexistent-extension",
+	}},
+}
+
+func transformQuarkusCreateCmdCfgToArgs(cfg quarkus.CreateQuarkusProjectConfig) []string {
+	args := []string{"create"}
+	if cfg.ProjectName != "" {
+		args = append(args, "--name", cfg.ProjectName)
+	}
+	if cfg.Extensions != "" {
+		args = append(args, "--extension", cfg.Extensions)
+	}
+	if cfg.DependenciesVersion.QuarkusPlatformGroupId != "" {
+		args = append(args, "--quarkus-platform-group-id", cfg.DependenciesVersion.QuarkusPlatformGroupId)
+	}
+	if cfg.DependenciesVersion.QuarkusVersion != "" {
+		args = append(args, "--quarkus-version", cfg.DependenciesVersion.QuarkusVersion)
+	}
+	return args
+}
+
+func GetQuarkusCreateProjectName(t *testing.T, config CfgTestInputQuarkusCreate) string {
+	if config.input.ProjectName != "" {
+		return config.input.ProjectName
+	} else {
+		projectDefaultName, err := LookupFlagDefaultValue("name", quarkus.NewCreateCommand())
+		require.NoErrorf(t, err, "Error: %v", err)
+		return projectDefaultName
+	}
+}
+
+func TestQuarkusCreateProjectSuccess(t *testing.T) {
+	for testIndex, test := range cfgTestInputQuarkusCreate_Success {
+		t.Run(fmt.Sprintf("Test quarkus create project success index: %d", testIndex), func(t *testing.T) {
+			defer CleanUpAndChdirTemp(t)
+			RunQuarkusCreateTest(t, test)
+		})
+	}
+}
+
+func RunQuarkusCreateTest(t *testing.T, test CfgTestInputQuarkusCreate) string {
+	var err error
+
+	projectName := GetQuarkusCreateProjectName(t, test)
+	projectDir := filepath.Join(TempTestsPath, projectName)
+
+	// Run `quarkus create` command
+	_, err = ExecuteKnWorkflowQuarkus(transformQuarkusCreateCmdCfgToArgs(test.input)...)
+
+	err = os.Chdir(projectDir)
+	require.NoErrorf(t, err, "Expected nil error, got: %v", err)
+	WriteMavenConfigFileWithTailDirs(projectDir)
+
+	// Check if the project directory was created
+	require.DirExistsf(t, projectDir, "Expected project directory '%s' to be created", projectDir)
+
+	// Check if the expected directories and files are present
+	expectedDirectories := []string{
+		"src/main/java",
+		"src/main/resources",
+		"src/main/docker",
+		"src/main",
+		"src",
+	}
+	VerifyDirectoriesExist(t, projectDir, expectedDirectories)
+	expectedFiles := []string{
+		"src/main/resources/application.properties",
+		"src/main/resources/workflow.sw.json",
+		"src/main/docker/Dockerfile.legacy-jar",
+		"src/main/docker/Dockerfile.jvm",
+		"src/main/docker/Dockerfile.native",
+		"src/main/docker/Dockerfile.native-micro",
+		".gitignore",
+		"pom.xml",
+		"README.md",
+		".dockerignore",
+	}
+	VerifyFilesExist(t, projectDir, expectedFiles)
+
+	// Verify the content of the file `workflow.sw.json`
+	workflowFilePath := filepath.Join(projectDir, "src/main/resources/workflow.sw.json")
+	workflowFileData, err := common.GetWorkflowTemplate(false)
+	require.NoErrorf(t, err, "Error reading workflow template: %v", err)
+	expectedFileContent := string(workflowFileData)
+	VerifyFileContent(t, workflowFilePath, expectedFileContent)
+	return projectName
+}
+
+func TestQuarkusCreateProjectFail(t *testing.T) {
+	for testIndex, test := range cfgTestInputQuarkusCreate_Fail {
+		t.Run(fmt.Sprintf("Test quarkus create project fail index: %d", testIndex), func(t *testing.T) {
+			defer CleanUpAndChdirTemp(t)
+
+			projectName := GetQuarkusCreateProjectName(t, test)
+			projectDir := filepath.Join(TempTestsPath, projectName)
+
+			// Run `quarkus create` command
+			_, err := ExecuteKnWorkflowQuarkus(transformQuarkusCreateCmdCfgToArgs(test.input)...)
+			require.Errorf(t, err, "Expected error, got nil")
+
+			// Check if the project directory was not created
+			require.NoDirExistsf(t, projectDir, "Expected project directory '%s' not to be created", projectDir)
+		})
+	}
+}
