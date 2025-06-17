560cc5bf8ea78982b08dd91968d0f09f792f234e
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2025-06-17 10:39:06 -0300

    [kogito-data-index-images] Fix kie-issues#1981 - Add Data Index UI to Images; Small install.js refactor (#3181)
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

--- packages/kogito-db-migrator-tool-image/install.js
+++ packages/kogito-db-migrator-tool-image/install.js
@@ -17,48 +17,15 @@
  * under the License.
  */
 
-const { execSync } = require("child_process");
-const fs = require("fs");
-
-const { env } = require("./env");
 const path = require("path");
-const pythonVenvDir = path.dirname(require.resolve("@kie-tools/python-venv/package.json"));
-const sonataflowImageCommonDir = path.dirname(require.resolve("@kie-tools/sonataflow-image-common/package.json"));
-const replaceInFile = require("replace-in-file");
+const { runKogitoImageInstall } = require("@kie/kogito-images-install-helper");
+const { env } = require("./env");
 
-const activateCmd =
-  process.platform === "win32"
-    ? `${pythonVenvDir}\\venv\\Scripts\\Activate.bat`
-    : `. ${pythonVenvDir}/venv/bin/activate`;
+const { buildTag, registry, account, name: imageName } = env.kogitoDbMigratorToolImage;
 
-execSync(
-  `${activateCmd} && \
-  python3 ${sonataflowImageCommonDir}/resources/scripts/versions_manager.py --bump-to ${env.kogitoDbMigratorToolImage.buildTag} --source-folder ./resources`,
-  { stdio: "inherit" }
-);
-
-// Find and read the -image.yaml file
-const resourcesPath = path.resolve(__dirname, "./resources");
-const files = fs.readdirSync(resourcesPath);
-const imageYamlFiles = files.filter((fileName) => fileName.endsWith("image.yaml"));
-if (imageYamlFiles.length !== 1) {
-  throw new Error("There should only be one image.yaml file on ./resources!");
-}
-const originalYamlPath = path.join(resourcesPath, imageYamlFiles[0]);
-let imageYaml = fs.readFileSync(originalYamlPath, "utf8");
-
-const imageUrl = `${env.kogitoDbMigratorToolImage.registry}/${env.kogitoDbMigratorToolImage.account}/${env.kogitoDbMigratorToolImage.name}`;
-
-// Replace the whole string between quotes ("") with the image name
-imageYaml = imageYaml.replace(/(?<=")(.*kie-kogito-db-migrator-tool.*)(?=")/gm, imageUrl);
-
-// Write file and then rename it to match the image name
-fs.writeFileSync(originalYamlPath, imageYaml);
-fs.renameSync(originalYamlPath, path.join(resourcesPath, `${env.kogitoDbMigratorToolImage.name}-image.yaml`));
-
-// Replace image URL in .feature files
-replaceInFile.sync({
-  files: ["**/*.feature"],
-  from: /@docker.io\/apache\/.*/g,
-  to: `@${imageUrl}`,
+runKogitoImageInstall({
+  finalImageName: imageName,
+  imageTag: { buildTag, registry, account, name: imageName },
+  resourceDir: path.resolve(__dirname, "./resources"),
+  imagePkgDir: __dirname,
 });
