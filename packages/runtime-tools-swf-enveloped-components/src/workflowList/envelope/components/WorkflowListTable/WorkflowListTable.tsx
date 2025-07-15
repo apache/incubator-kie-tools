adaf16a4ee07f68506f36ad9a4617a9f828f25d0
Kumar Aditya Raj <rajanish4444@gmail.com>
2025-07-15 16:12:33 +0530

    kie-tools-issues#3184- [sonataflow-management-console-webapp] Management console is unable to sort workflow instances by ID  (#3189)
    Co-authored-by: kumaradityaraj <sedulous.0007@gmail.com>
    Co-authored-by: Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>

--- packages/runtime-tools-swf-enveloped-components/src/workflowList/envelope/components/WorkflowListTable/WorkflowListTable.tsx
+++ packages/runtime-tools-swf-enveloped-components/src/workflowList/envelope/components/WorkflowListTable/WorkflowListTable.tsx
@@ -85,7 +85,7 @@ const WorkflowListTable: React.FC<WorkflowListTableProps & OUIAProps> = ({
   ouiaSafe,
 }) => {
   const [rowPairs, setRowPairs] = useState<any>([]);
-  const columns: string[] = ["__Toggle", "__Select", "Id", "Status", "Created", "Last update", "__Actions"];
+  const columns: string[] = ["__Toggle", "__Select", "Process name", "Status", "Created", "Last update", "__Actions"];
   const [modalTitle, setModalTitle] = useState<string>("");
   const [modalContent, setModalContent] = useState<string>("");
   const [titleType, setTitleType] = useState<string>("");
