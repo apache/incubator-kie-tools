b22830d6878e3893ad53184b4a4d83ffae7b80b6
kumaradityaraj <sedulous.0007@gmail.com>
2025-09-22 12:27:08 +0530

    Webapp for workflow endpoints

---
+++ packages/workflow-endpoints-webapp/webpack.config.js
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("@kie-tools-core/webpack-base/webpack.common.config");
+const { env } = require("./env");
+const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = async (webpackEnv) =>
+  merge(common(webpackEnv), {
+    entry: {},
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "./src/styles.css", to: "./styles.css" },
+          { from: "./static/favicon.svg", to: "./favicon.svg" },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+        filename: "index.html",
+        chunks: ["app"],
+      }),
+      new HtmlReplaceWebpackPlugin([
+        {
+          pattern: /\${WORKFLOW_ENDPOINTS_WEBAPP_TITLE}/g,
+          replacement: () => env.workflowEndpointsWebapp.title ?? "",
+        },
+        {
+          pattern: /\${WORKFLOW_ENDPOINTS_WEBAPP_LOGO}/g,
+          replacement: () => env.workflowEndpointsWebapp.logo ?? "",
+        },
+        {
+          pattern: /\${WORKFLOW_ENDPOINTS_WEBAPP_DOCLINK_HREF}/g,
+          replacement: () => env.workflowEndpointsWebapp.docLink.href ?? "",
+        },
+        {
+          pattern: /\${WORKFLOW_ENDPOINTS_WEBAPP_DOCLINK_TEXT}/g,
+          replacement: () => env.workflowEndpointsWebapp.docLink.text ?? "",
+        },
+      ]),
+    ],
+    ignoreWarnings: [/Failed to parse source map/],
+    devServer: {
+      static: {
+        directory: "./dist",
+      },
+      port: env.workflowEndpointsWebapp.dev.port,
+    },
+  });
