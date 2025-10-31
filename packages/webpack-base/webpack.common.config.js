6f54245cb2e3c5638a6788354d1cf932139350be
Kusuma <kusuma-latha@ibm.com>
2025-10-31 19:39:28 +0530

    tests

--- packages/webpack-base/webpack.common.config.js
+++ packages/webpack-base/webpack.common.config.js
@@ -86,6 +86,7 @@ module.exports = (webpackEnv) => {
                 compilerOptions: {
                   ...verbatimModuleSyntax,
                   sourceMap: sourceMaps,
+                  jsx: "react-jsx",
                 },
               },
             },
