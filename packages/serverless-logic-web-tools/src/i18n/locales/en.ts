8438ac3c6aac265ca9b5bfe2948683cbd4bc75db
Kusuma <kusuma-latha@ibm.com>
2025-10-31 14:12:35 +0530

    removed wrapped

--- packages/serverless-logic-web-tools/src/i18n/locales/en.ts
+++ packages/serverless-logic-web-tools/src/i18n/locales/en.ts
@@ -17,7 +17,6 @@
  * under the License.
  */
 
-import { wrapped } from "@kie-tools-core/i18n/dist/core";
 import { en as en_common } from "@kie-tools/i18n-common-dictionary";
 import { AppI18n } from "..";
 
