d944f06a94447386b91ba130322ac5a8d158dd68
Luiz Motta <luizjoaomotta@gmail.com>
2026-02-17 12:40:26 -0300

    Add e2e keyboard tests

--- packages/boxed-expression-component/tests-e2e/features/keyboard/keyboard.spec.ts
+++ packages/boxed-expression-component/tests-e2e/features/keyboard/keyboard.spec.ts
@@ -17,25 +17,120 @@
  * under the License.
  */
 
-import { test } from "../../__fixtures__/base";
+import { expect, test } from "../../__fixtures__/base";
+
+const ACTIVE_CLASS_REGEXP = /(^|\s)active(\s|$)/;
 
 test.describe("Keyboard", () => {
-  test.skip(true, "https://github.com/apache/incubator-kie-issues/issues/542");
   test.describe("Navigation", () => {
-    test("should correctly navigate", async () => {
-      // enter, shift+enter, tab, shift+tab, escape
-    });
+    test("should correctly navigate", async ({ bee, page, useCases }) => {
+      await useCases.openLoanOriginations("bureau-strategy-decision-service", "bureau-call-type");
+      const decisionTable = bee.expression.asDecisionTable();
+      await decisionTable.cellAt({ row: 1, column: 1 }).select();
 
-    test.describe("Arrow keys", () => {
-      test("should correctly navigate", async () => {
-        // arrow up/down/left/right
-      });
-    });
+      await page.keyboard.press("ArrowRight");
+      await expect(decisionTable.cellAt({ row: 1, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
 
-    test.describe("Nested stories", () => {
-      test("should correctly navigate", async () => {
-        // enter, shift+enter, tab, shift+tab, escape
-      });
+      await page.keyboard.press("ArrowDown");
+      await expect(decisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("ArrowUp");
+      await expect(decisionTable.cellAt({ row: 1, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("ArrowLeft");
+      await expect(decisionTable.cellAt({ row: 1, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.type(`"test"`);
+      await expect(decisionTable.cellAt({ row: 1, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Enter");
+      await expect(decisionTable.cellAt({ row: 1, column: 1 }).content).toContainText(`"test"`);
+      await expect(decisionTable.cellAt({ row: 2, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.type(`"test2"`);
+      await expect(decisionTable.cellAt({ row: 2, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Escape");
+      await expect(decisionTable.cellAt({ row: 1, column: 1 }).content).not.toContainText(`"test2"`);
+      await expect(decisionTable.cellAt({ row: 2, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Tab");
+      await expect(decisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Tab");
+      await expect(decisionTable.cellAt({ row: 2, column: 3 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Shift+Tab");
+      await expect(decisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Escape");
+      await expect(decisionTable.cellAt({ row: 2, column: 2 }).content).not.toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Enter");
+      await expect(decisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+    });
+  });
+
+  test.describe("Nested stories", () => {
+    test("should correctly navigate", async ({ bee, page, useCases }) => {
+      await useCases.openLoanOriginations("bureau-strategy-decision-service", "pre-bureau-risk-category");
+      const contextExpression = bee.expression.asContext();
+      const literalExpression = contextExpression.entry(0).expression.asLiteral();
+      const resultDecisionTable = contextExpression.result.expression.asDecisionTable();
+      await resultDecisionTable.cellAt({ row: 1, column: 1 }).select();
+
+      // Check nested decision table
+      await page.keyboard.press("ArrowRight");
+      await expect(resultDecisionTable.cellAt({ row: 1, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("ArrowDown");
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("ArrowUp");
+      await expect(resultDecisionTable.cellAt({ row: 1, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("ArrowLeft");
+      await expect(resultDecisionTable.cellAt({ row: 1, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.type(`"test"`);
+      await expect(resultDecisionTable.cellAt({ row: 1, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Enter");
+      await expect(resultDecisionTable.cellAt({ row: 1, column: 1 }).content).toContainText(`"test"`);
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.type(`"test2"`);
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Escape");
+      await expect(resultDecisionTable.cellAt({ row: 1, column: 1 }).content).not.toContainText(`"test2"`);
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 1 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Tab");
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Tab");
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 3 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Shift+Tab");
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Escape");
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 2 }).content).not.toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Enter");
+      await expect(resultDecisionTable.cellAt({ row: 2, column: 2 }).content).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      // Selecting context expression result cell (decision table container)
+      await page.keyboard.press("Escape");
+      await expect(contextExpression.resultExpressionContainer).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      // Selecting context expression entry 0 cell (literal expression container)
+      await page.keyboard.press("ArrowUp");
+      await expect(contextExpression.entry(0).contextExpressionContainer).toHaveClass(ACTIVE_CLASS_REGEXP);
+
+      await page.keyboard.press("Enter");
+      await expect(literalExpression.content).toHaveClass(ACTIVE_CLASS_REGEXP);
     });
   });
 });
