d9b456fb3363681e45e132908587343abe47691b
Kusuma <kusuma-latha@ibm.com>
2026-05-08 12:33:36 +0530

    updated example

--- examples/uniforms-patternfly/src/index.tsx
+++ examples/uniforms-patternfly/src/index.tsx
@@ -19,8 +19,8 @@
 
 import "@patternfly/react-core/dist/styles/base.css";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.createRoot(document.getElementById("app")!).render(<App />);
