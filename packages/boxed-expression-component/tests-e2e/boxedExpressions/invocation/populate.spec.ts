cbb15d22646cbb0b43c395da14c1bbae8d8a79af
Kusuma <kusuma-latha@ibm.com>
2026-05-27 19:43:51 +0530

    test failures

--- packages/boxed-expression-component/tests-e2e/boxedExpressions/invocation/populate.spec.ts
+++ packages/boxed-expression-component/tests-e2e/boxedExpressions/invocation/populate.spec.ts
@@ -69,7 +69,15 @@ test.describe("Populate Boxed Invocation", () => {
     await resizing.reset(page.getByRole("cell", { name: "Required monthly installment" }));
 
     await bee.expression.asInvocation().invokedFunctionNameCell.click();
-    await page.keyboard.type(`Affordability calculation`);
+
+    const textbox = bee.expression.asInvocation().invokedFunctionNameCell.getByRole("textbox");
+
+    await expect(textbox).toBeVisible();
+
+    await textbox.focus();
+
+    await page.keyboard.type("Affordability calculation");
+
     await page.keyboard.press("Enter");
 
     await monaco.fill({ monacoParentLocator: page, nth: 0, content: "Aplicant data.Monthly.Income" });
