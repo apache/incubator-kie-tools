26c63d064241d5dc998325484eda1652295240e7
Kbowers <kennedy.bowers@ibm.com>
2026-07-29 08:02:38 +0100

    update git ref

--- packages/drools-and-kogito/env/index.js
+++ packages/drools-and-kogito/env/index.js
@@ -28,7 +28,7 @@ module.exports = composeEnv([rootEnv], {
       description: "Git repository URL for Drools",
     },
     DROOLS_AND_KOGITO__droolsRepoGitRef: {
-      default: "9fa488684650e3fb163e34069b8ba9dc83ee89c1",
+      default: "8816430978be02c31a59d8e35a629c3fab6dd37e",
       description: "Git ref for the Drools repository (SHA, branch, or tag)",
     },
     DROOLS_AND_KOGITO__skip: {
